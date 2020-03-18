373dc7d2d82eea591e11d9a7917b72814799f539
Roman971 <romanlasnier@hotmail.com>
2020-03-18 21:21:45 +0100

    Fix ovl_Demo_Go non matchings (2/3 matched)
    `z_demo_go.c` is now functionally equivalent and almost fully matched.
    
    Note that I added a Vec3i struct in `z64actor.h` now used in `CsCmdActorAction`.
    
    Also fixed some missing VT codes in various actor strings.

--- include/z64cutscene.h
+++ include/z64cutscene.h
@@ -83,9 +83,9 @@ typedef struct
     /* 0x02 */ u16 startFrame;
     /* 0x04 */ u16 endFrame;
     /* 0x06 */ Vec3s rot;
-    /* 0x0C */ Vec3f startPos;
-    /* 0x18 */ Vec3f endPos;
-    /* 0x24 */ Vec3f normal;
+    /* 0x0C */ Vec3i startPos;
+    /* 0x18 */ Vec3i endPos;
+    /* 0x24 */ Vec3i normal;
 } CsCmdActorAction; // size = 0x30
 
 typedef enum
