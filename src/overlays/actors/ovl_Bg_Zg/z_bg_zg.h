8c99bdd00f2fd191d69e02f5091bc36d3eb2a45d
Lucas Shaw <lgs84@cornell.edu>
2020-03-22 22:55:05 -0700

    ovl_Bg_Zg OK

--- src/overlays/actors/ovl_Bg_Zg/z_bg_zg.h
+++ src/overlays/actors/ovl_Bg_Zg/z_bg_zg.h
@@ -3,11 +3,13 @@
 
 #include <ultra64.h>
 #include <global.h>
+#include <vt.h>
 
 typedef struct
 {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x20];
+    /* 0x0000 */ DynaPolyActor dyna;
+    /* 0x0164 */ s32 funcIndex;
+    /* 0x0168 */ s32 unk_168;
 } BgZg; // size = 0x016C
 
 extern const ActorInit Bg_Zg_InitVars;
