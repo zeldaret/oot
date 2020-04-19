eefbe9afe1891ea27eb39315a3c87e87abef0828
Synray <31429825+Synray@users.noreply.github.com>
2020-04-18 19:56:53 -0700

    Decompile code_800D2E30 and use struct in code_800A9F30 (#80)
    * Match func_800AA000
    
    * Match z_camera/func_800437F0
    
    * Decompile code_800D2E30, use struct in 800A9F30
    
    * Format
    
    * Remove 'U' from literals
    
    * Remove bss, cleanup z_camera
    
    * Review changes, rename temp
    
    * Move externs to variables.h

--- include/z64.h
+++ include/z64.h
@@ -1572,4 +1572,20 @@ typedef struct {
     /* 0x0C */ char unk_0C[0x0C];
 } VisMonoStruct; // size = 0x18
 
+typedef struct {
+    /* 0x000 */ u8 rumbleEnable[4];
+    /* 0x004 */ u8 unk_04[0x40];
+    /* 0x044 */ u8 unk_44[0x40];
+    /* 0x084 */ u8 unk_84[0x40];
+    /* 0x0C4 */ u8 unk_C4[0x40];
+    /* 0x104 */ u8 unk_104;
+    /* 0x105 */ u8 unk_105;
+    /* 0x106 */ u16 unk_106;
+    /* 0x108 */ u16 unk_108;
+    /* 0x10A */ u8 unk_10A;
+    /* 0x10B */ u8 unk_10B;
+    /* 0x10C */ u8 unk_10C;
+    /* 0x10D */ u8 unk_10D;
+} UnkRumbleStruct; // size = 0x10E
+
 #endif
