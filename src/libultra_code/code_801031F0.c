b59904d1b9a7d7794acaca8066b18ce6d0e89d60
shawlucas <lgs84@cornell.edu>
2020-04-21 08:05:28 -0700

    Removed line breaks from comments

--- src/libultra_code/code_801031F0.c
+++ src/libultra_code/code_801031F0.c
@@ -10,7 +10,6 @@ s32 osAfterPreNMI(void) {
  * Starts to read the values for SI device status and type which are connected to the controller port and joyport
  * connector.
  **/
-
 s32 osContStartQuery(OSMesgQueue* mq) {
     s32 ret;
     ret = 0;
@@ -31,7 +30,6 @@ s32 osContStartQuery(OSMesgQueue* mq) {
  * osContGetQuery:
  * Returns the values from osContStartQuery to status. Both functions must be paired for use.
  **/
-
 void osContGetQuery(OSContStatus* data) {
     u8 pattern;
     __osContGetInitData(&pattern, data);
--- src/libultra_code/guLookAtHilite.c
+++ src/libultra_code/guLookAtHilite.c
@@ -28,7 +28,6 @@
  * hiliteWidth: width of texture for specular highlight
  * hiliteHeight: height of texture for specular highlight
  **/
-
 void guLookAtHiliteF(f32 mf[4][4], LookAt* l, Hilite* h, f32 xEye, f32 yEye, f32 zEye, f32 xAt, f32 yAt, f32 zAt,
                      f32 xUp, f32 yUp, f32 zUp, f32 xl1, f32 yl1, f32 zl1, /* light 1 direction */
                      f32 xl2, f32 yl2, f32 zl2,                            /* light 2 direction */
@@ -163,7 +162,6 @@ void guLookAtHiliteF(f32 mf[4][4], LookAt* l, Hilite* h, f32 xEye, f32 yEye, f32
  * This function creates the viewing matrix (fixed point) and sets the LookAt/Hilite structures
  * Same args as previous function
  **/
-
 void guLookAtHilite(Mtx* m, LookAt* l, Hilite* h, f32 xEye, f32 yEye, f32 zEye, f32 xAt, f32 yAt, f32 zAt, f32 xUp,
                     f32 yUp, f32 zUp, f32 xl1, f32 yl1, f32 zl1, f32 xl2, f32 yl2, f32 zl2, s32 hiliteWidth,
                     s32 hiliteHeight) {
