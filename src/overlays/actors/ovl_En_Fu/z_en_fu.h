22d84b8193d9957f9885000f28a84280bfa74177
fig02 <fig02srl@gmail.com>
2020-05-01 09:50:24 -0400

    en_fu OK

--- src/overlays/actors/ovl_En_Fu/z_en_fu.h
+++ src/overlays/actors/ovl_En_Fu/z_en_fu.h
@@ -10,9 +10,9 @@ typedef struct {
     /* 0x0198 */ SkelAnime skelanime;
     /* 0x01DC */ Vec3s limbDrawTable[16];
     /* 0x023C */ Vec3s transitionDrawTable[16];
-    /* 0x029C */ Vec3s unk_29C;
-    /* 0x02A2 */ Vec3s unk_2A2;
-    /* 0x02A8 */ u16 unk_2A8; //0 = stop, 2 = moving, 1 = ?
+    /* 0x029C */ Vec3s unk_29C; // xz offset applied to neck rotation to look at link when he is close
+    /* 0x02A2 */ Vec3s unk_2A2; // seems unused
+    /* 0x02A8 */ u16 unk_2A8; // 0 = moving, 1 = reset look angle to center, 2 = stop
     /* 0x02AA */ u16 facialExpression;
     /* 0x02AC */ ActorFunc actionFunc;
 } EnFu; // size = 0x02B0
