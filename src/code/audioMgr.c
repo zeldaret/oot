a3ca7390ff4679d294004d97246e00a3ccda02c6
Tharo <17233964+Thar0@users.noreply.github.com>
2020-05-10 19:30:38 +0100

    Remove prototypes from audioMgr.c
    Co-authored-by: Roman971 <32455037+Roman971@users.noreply.github.com>

--- src/code/audioMgr.c
+++ src/code/audioMgr.c
@@ -1,13 +1,6 @@
 #include <ultra64.h>
 #include <global.h>
 
-void func_800C3C80(AudioMgr* audioMgr);
-void AudioMgr_HandleRetrace(AudioMgr* audioMgr);
-void AudioMgr_HandlePRENMI(AudioMgr* audioMgr);
-void AudioMgr_ThreadEntry(void* arg0);
-void AudioMgr_Unlock(AudioMgr* audioMgr);
-void AudioMgr_Start(AudioMgr* audioMgr, void* stack, OSPri pri, OSId id, SchedContext* sched, IrqMgr* irqMgr);
-
 void func_800C3C80(AudioMgr* audioMgr) {
     Sub_AudioMgr_18* sub;
 
