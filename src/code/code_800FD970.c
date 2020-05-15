e9832e48c22ccdeb60849eb811a9ea8dbb19ac08
Thar0 <17233964+Thar0@users.noreply.github.com>
2020-05-15 18:47:43 +0100

    Comment cleanups, ensure formatter won't cause a matching issue later

--- src/code/code_800FD970.c
+++ src/code/code_800FD970.c
@@ -1,21 +1,21 @@
 #include <ultra64.h>
 #include <global.h>
 
-// The latest generated random number.
+// The latest generated random number, used to generate the next number in the sequence.
 u32 randomNumber = 1;
 
-// Space to store a value to be re-interpreted as a float
+// Space to store a value to be re-interpreted as a float.
 u32 floatStore;
 
 /**
- * Gets the next integer in the sequence of pseudo-random numbers
+ * Gets the next integer in the sequence of pseudo-random numbers.
  */
 u32 Math_Rand_Next() {
     return randomNumber = (randomNumber * 1664525) + 1013904223;
 }
 
 /**
- * Seeds the pseudo-random number generator by providing a starting value
+ * Seeds the pseudo-random number generator by providing a starting value.
  */
 void Math_Rand_Seed(u32 seed) {
     randomNumber = seed;
@@ -50,7 +50,7 @@ void Math_Rand_Seed_Variable(u32* rndNum, u32 seed) {
 }
 
 /**
- * Generates the next pseudo-random integer from the provided rndNum
+ * Generates the next pseudo-random integer from the provided rndNum.
  */
 u32 Math_Rand_Next_Variable(u32* rndNum) {
     return *rndNum = (*rndNum * 1664525) + 1013904223;
@@ -58,24 +58,28 @@ u32 Math_Rand_Next_Variable(u32* rndNum) {
 
 /**
  * Generates the next pseudo-random floating-point number between 0.0f and
- * 1.0f from the provided rndNum
+ * 1.0f from the provided rndNum.
  */
 f32 Math_Rand_ZeroOne_Variable(u32* rndNum) {
     u32 next;
 
     next = (*rndNum * 1664525) + 1013904223;
+    // clang-format off
     *rndNum = next; floatStore = (next >> 9) | 0x3F800000;
+    // clang-format on
     return *((f32*)&floatStore) - 1.0f;
 }
 
 /**
  * Generates the next pseudo-random floating-point number between -0.5f and
- * 0.5f from the provided rndNum
+ * 0.5f from the provided rndNum.
  */
 f32 Math_Rand_Centered_Variable(u32* rndNum) {
     u32 next;
 
     next = (*rndNum * 1664525) + 1013904223;
+    // clang-format off
     *rndNum = next; floatStore = (next >> 9) | 0x3F800000;
+    // clang-format on
     return *((f32*)&floatStore) - 1.5f;
 }
