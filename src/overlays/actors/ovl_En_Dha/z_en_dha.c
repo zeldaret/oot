9752d666101a44107a1f1a3187cc352a37973809
Zelllll <56516451+Zelllll@users.noreply.github.com>
2020-05-16 23:29:56 -0500

    Fixed all issues with `En_Dha`

--- src/overlays/actors/ovl_En_Dha/z_en_dha.c
+++ src/overlays/actors/ovl_En_Dha/z_en_dha.c
@@ -16,11 +16,11 @@ void EnDha_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnDha_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void EnDha_SetupAction(EnDha* this, EnDhaActionFunc actionFunc);
-void EnDha_SetAnimation(EnDha* this);
-void EnDha_GrabLink(EnDha* this, GlobalContext* globalCtx);
-void EnDha_SetAnimation2(EnDha* this);
-void EnDha_FreeLink(EnDha* this, GlobalContext* globalCtx);
-void EnDha_SetupDeath(EnDha* this);
+void func_809EC9C8(EnDha* this);
+void func_809ECA50(EnDha* this, GlobalContext* globalCtx);
+void func_809ECF60(EnDha* this);
+void func_809ECF8C(EnDha* this, GlobalContext* globalCtx);
+void EnDha_SetDeadAction(EnDha* this);
 void EnDha_Die(EnDha* this, GlobalContext* globalCtx);
 void EnDha_UpdateHealth(EnDha* this, GlobalContext* globalCtx);
 s32 EnDha_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx);
@@ -72,7 +72,7 @@ static ColliderJntSphItemInit colliderItemsInit[] = {
 static ColliderJntSphInit colliderInit = {
     { COLTYPE_UNK6, 0x00, 0x09, 0x19, 0x10, COLSHAPE_JNTSPH },
     5,
-    colliderItemsInit,
+    &colliderItemsInit,
 };
 
 static InitChainEntry initChain[] = {
@@ -89,8 +89,8 @@ void EnDha_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnDha* this = THIS;
 
     Actor_ProcessInitChain(&this->actor, initChain);
-    this->actor.colChkInfo.damageTable = damageTable;
-    SkelAnime_InitSV(globalCtx, &this->skelAnime, D_06000BD8, D_060015B0, this->limbDrawTable,
+    this->actor.colChkInfo.damageTable = &damageTable;
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_06000BD8, &D_060015B0, this->limbDrawTable,
                      this->transitionDrawTable, 4);
     ActorShape_Init(&this->actor.shape, 0, ActorShadow_DrawFunc_Teardrop, 90.0f);
     this->actor.posRot2.pos = this->actor.posRot.pos;
@@ -99,9 +99,9 @@ void EnDha_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->actor.colChkInfo.health = 8;
     this->unk_1CE = -0x4000;
     Collider_InitJntSph(globalCtx, &this->collider);
-    Collider_SetJntSph(globalCtx, &this->collider, &this->actor, colliderInit, &this->colliderItem);
+    Collider_SetJntSph(globalCtx, &this->collider, &this->actor, &colliderInit, &this->colliderItem);
     this->actor.flags &= ~1;
-    EnDha_SetAnimation(this);
+    func_809EC9C8(this);
 }
 
 void EnDha_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -110,17 +110,17 @@ void EnDha_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
-void EnDha_SetAnimation(EnDha* this) {
-    SkelAnime_ChangeAnimDefaultRepeat(&this->skelAnime, D_060015B0);
+void func_809EC9C8(EnDha* this) {
+    SkelAnime_ChangeAnimDefaultRepeat(&this->skelAnime, &D_060015B0);
     this->unk_1C0 = 0;
     this->unk_1C8 = ((Math_Rand_ZeroOne() * 10.0f) + 5.0f);
     this->actor.speedXZ = 0.0f;
     this->actor.posRot.rot.y = this->actor.shape.rot.y;
     this->actor.initPosRot.rot.z = 1;
-    EnDha_SetupAction(this, EnDha_GrabLink);
+    EnDha_SetupAction(this, func_809ECA50);
 }
 
-void EnDha_GrabLink(EnDha* this, GlobalContext* globalCtx) {
+void func_809ECA50(EnDha* this, GlobalContext* globalCtx) {
     Vec3f D_809ED740 = { 0.0f, 0.0f, 0.0f };
     Vec3f D_809ED74C = { 0.0f, 0.0f, 55.0f };
     Vec3f D_809ED758 = { 0.0f, 0.0f, -54.0f };
@@ -147,7 +147,7 @@ void EnDha_GrabLink(EnDha* this, GlobalContext* globalCtx) {
         this->unk_1D0.y = 0;
         this->unk_1D0.z = this->unk_1D0.y;
         this->unk_1D6.x = this->unk_1D0.y;
-        if (Math_Vec3f_DistXYZ(playerPos, &this->unk_1DC) <= 12.0f) {
+        if (Math_Vec3f_DistXYZ(&playerPos, &this->unk_1DC) <= 12.0f) {
             if (this->unk_1CC == 0) {
                 if (globalCtx->unk_11D4C(globalCtx, player) != 0) {
                     this->unk_1CA = 0;
@@ -162,14 +162,14 @@ void EnDha_GrabLink(EnDha* this, GlobalContext* globalCtx) {
                 this->unk_1D0.y = (Math_Sins(this->unk_1CA) * 1820.0f);
                 if ((player->stateFlags2 & 0x80) == 0) {
                     this->unk_1CC = 0;
-                    EnDha_SetAnimation2(this);
+                    func_809ECF60(this);
                     return;
                 }
                 if (this->unk_1CA < -0x6E6B) {
                     Audio_PlayActorSound2(&this->actor, NA_SE_EN_DEADHAND_GRIP);
                 }
             }
-            func_80035844(&this->unk_1E8, playerPos, &this->unk_1D0.z, 0);
+            func_80035844(&this->unk_1E8, &playerPos, &this->unk_1D0.z, 0);
             this->unk_1D6.x = ((this->unk_1D6.x - this->actor.shape.rot.y) - this->unk_1D0.y);
             this->unk_1D0.z = (((this->unk_1D0.z - this->actor.shape.rot.x) - this->unk_1CE) - this->unk_1D0.x);
         } else {
@@ -185,16 +185,16 @@ void EnDha_GrabLink(EnDha* this, GlobalContext* globalCtx) {
                 this->actor.initPosRot.rot.z = 0;
             }
         }
-        this->actor.shape.rot.y = Math_Vec3f_Yaw(&this->actor.posRot, playerPos);
+        this->actor.shape.rot.y = Math_Vec3f_Yaw(&this->actor.posRot, &playerPos);
         Math_SmoothScaleMaxMinF(&this->unk_1DC, playerPos.x, 1.0f, 16.0f, 0.0f);
         Math_SmoothScaleMaxMinF(&this->unk_1DC.y, playerPos.y, 1.0f, 16.0f, 0.0f);
         Math_SmoothScaleMaxMinF(&this->unk_1DC.z, playerPos.z, 1.0f, 16.0f, 0.0f);
-        func_80035844(&this->unk_1F4, &this->unk_1DC, unkVar3, 0);
+        func_80035844(&this->unk_1F4, &this->unk_1DC, &unkVar3, 0);
         Matrix_Translate(this->unk_1DC.x, this->unk_1DC.y, this->unk_1DC.z, 0);
         Matrix_RotateRPY(unkVar3, unkVar2, 0, 1);
-        Matrix_MultVec3f(D_809ED758, &this->unk_1F4);
+        Matrix_MultVec3f(&D_809ED758, &this->unk_1F4);
         Matrix_Translate(this->actor.posRot.pos.x, this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0);
-        func_80035844(&this->actor.posRot, &this->unk_1F4, unkVar3, 0);
+        func_80035844(&this->actor.posRot, &this->unk_1F4, &unkVar3, 0);
         Matrix_RotateRPY(unkVar3, unkVar2, 0, 1);
         Matrix_MultVec3f(&D_809ED74C, &this->unk_1F4);
         this->unk_1CE = Math_Vec3f_Pitch(&this->actor.posRot, &this->unk_1F4);
@@ -225,12 +225,12 @@ void EnDha_GrabLink(EnDha* this, GlobalContext* globalCtx) {
     }
 }
 
-void EnDha_SetAnimation2(EnDha* this) {
+void func_809ECF60(EnDha* this) {
     this->unk_1C8 = 15;
-    EnDha_SetupAction(this, EnDha_FreeLink);
+    EnDha_SetupAction(this, func_809ECF8C);
 }
 
-void EnDha_FreeLink(EnDha* this, GlobalContext* globalCtx) {
+void func_809ECF8C(EnDha* this, GlobalContext* globalCtx) {
     Player* player = PLAYER;
 
     if ((player->stateFlags2 & 0x80) != 0) {
@@ -246,13 +246,14 @@ void EnDha_FreeLink(EnDha* this, GlobalContext* globalCtx) {
     SkelAnime_FrameUpdateMatrix(&this->skelAnime);
     this->unk_1C8--;
     if (this->unk_1C8 == 0) {
-        EnDha_SetAnimation(this);
+        func_809EC9C8(this);
     }
 }
 
-void EnDha_SetupDeath(EnDha* this) {
+void EnDha_SetDeadAction(EnDha* this) {
     this->unk_1C0 = 8;
     this->unk_1C8 = 300;
+
     if (this->actor.attachedA != NULL) {
         if (this->actor.attachedA->params != 0xA) {
             Audio_PlayActorSound2(&this->actor, NA_SE_EN_DEADHAND_HAND_DEAD);
@@ -285,7 +286,7 @@ void EnDha_Die(EnDha* this, GlobalContext* globalCtx) {
         if (this->unk_1C8 != 0) {
             if (-12000.0f < this->actor.shape.unk_08) {
                 this->actor.shape.unk_08 -= 1000.0f;
-                func_80033480(globalCtx, vector, 7.0f, 1, 0x5A, 0x14, 1);
+                func_80033480(globalCtx, &vector, 7.0f, 1, 0x5A, 0x14, 1);
                 return;
             }
             this->unk_1C8--;
@@ -297,9 +298,9 @@ void EnDha_Die(EnDha* this, GlobalContext* globalCtx) {
             }
         } else {
             this->actor.shape.unk_08 += 500.0f;
-            func_80033480(globalCtx, vector, 7.0f, 1, 0x5A, 0x14, 1);
+            func_80033480(globalCtx, &vector, 7.0f, 1, 0x5A, 0x14, 1);
             if (0.0f == this->actor.shape.unk_08) {
-                EnDha_SetAnimation(this);
+                func_809EC9C8(this);
             }
         }
     }
@@ -313,19 +314,19 @@ void EnDha_UpdateHealth(EnDha* this, GlobalContext* globalCtx) {
         } else {
             func_8003426C(&this->actor, 0x4000, 0xFF, 0, 8);
             if (Actor_ApplyDamage(&this->actor) == 0) {
-                EnDha_SetupDeath(this);
+                EnDha_SetDeadAction(this);
                 this->actor.colChkInfo.health = 8;
                 Item_DropCollectibleRandom(globalCtx, &this->actor, &this->actor.posRot.pos, 0xE0);
             } else {
                 Audio_PlayActorSound2(this, NA_SE_EN_DEADHAND_DAMAGE);
                 this->unk_1C0 = 9;
-                EnDha_SetAnimation2(this);
+                func_809ECF60(this);
             }
         }
     }
     if (this->actor.attachedA != NULL) {
         if (this->actor.attachedA->params == 0xA) {
-            EnDha_SetupDeath(this);
+            EnDha_SetDeadAction(this);
         }
     }
 }
@@ -333,6 +334,7 @@ void EnDha_UpdateHealth(EnDha* this, GlobalContext* globalCtx) {
 void EnDha_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnDha* this = THIS;
     s32* colChkCtxTemp;
+
     colChkCtxTemp = &globalCtx->colChkCtx;
 
     if (this->actor.attachedA == NULL) {
@@ -369,6 +371,7 @@ s32 EnDha_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList,
 void EnDha_OverridePostDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
     Vec3f D_809ED764 = { 1100.0f, 0.0f, 0.0f };
     Vec3f D_809ED770 = { 0.0f, 0.0f, 0.0f };
+
     EnDha* this = THIS;
 
     switch (limbIndex) {
@@ -385,8 +388,8 @@ void EnDha_OverridePostDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList
 
         case 3:
             func_800628A4(1, &this->collider);
-            Matrix_MultVec3f(D_809ED764, &this->unk_1DC);
-            Matrix_MultVec3f(D_809ED770, &this->unk_1E8);
+            Matrix_MultVec3f(&D_809ED764, &this->unk_1DC);
+            Matrix_MultVec3f(&D_809ED770, &this->unk_1E8);
             break;
     }
 }
