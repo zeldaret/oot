3175f5c988030056bf53f7f46cb57b03ec5c359a
Half-asian <37233118+Half-asian@users.noreply.github.com>
2020-05-27 02:53:27 +1200

    En_Skjneedle OK (#162)
    * En_Skjneedle OK
    
    * fix
    
    * file deletions
    
    * file deletions 2
    
    * fix extern
    
    * fix
    
    * cleanup

--- spec
+++ spec
@@ -3214,8 +3214,7 @@ endseg
 beginseg
     name "ovl_En_Skjneedle"
     include "build/src/overlays/actors/ovl_En_Skjneedle/z_en_skjneedle.o"
-    include "build/data/overlays/actors/z_en_skjneedle.data.o"
-    include "build/data/overlays/actors/z_en_skjneedle.reloc.o"
+    include "build/src/overlays/actors/ovl_En_Skjneedle/z_en_skjneedle_reloc.o"
 endseg
 
 beginseg
--- src/overlays/actors/ovl_En_Skjneedle/z_en_skjneedle.h
+++ src/overlays/actors/ovl_En_Skjneedle/z_en_skjneedle.h
@@ -8,7 +8,11 @@ struct EnSkjneedle;
 
 typedef struct EnSkjneedle {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x9C];
+    /* 0x014C */ char unk_14C[0x48];
+    /* 0x0194 */ ColliderCylinder collider;
+    /* 0x01E0 */ s16 unk_1E0;
+    /* 0x01E2 */ s16 unk_1E2;
+    /* 0x01E4 */ char unk_1E4[4];
 } EnSkjneedle; // size = 0x01E8
 
 extern const ActorInit En_Skjneedle_InitVars;
