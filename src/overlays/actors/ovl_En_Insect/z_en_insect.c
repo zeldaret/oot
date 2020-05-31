c1975d707861398cf0858aae393fa0497a77a3e5
Blackop778 <nefalt2@gmail.com>
2020-05-30 23:43:07 -0500

    Start decompiling bugs

--- src/overlays/actors/ovl_En_Insect/z_en_insect.c
+++ src/overlays/actors/ovl_En_Insect/z_en_insect.c
@@ -9,7 +9,10 @@ void EnInsect_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnInsect_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnInsect_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+f32 D_80A7DEB0 = 0.0f;
+s32 D_80A7DEB4 = 0;
+s16 D_80A7DEB8 = 0;
+
 const ActorInit En_Insect_InitVars = {
     ACTOR_EN_INSECT,
     ACTORTYPE_ITEMACTION,
@@ -21,22 +24,129 @@ const ActorInit En_Insect_InitVars = {
     (ActorFunc)EnInsect_Update,
     (ActorFunc)EnInsect_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7BE20.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7BE40.s")
+extern SkeletonHeader D_04035590;
+extern AnimationHeader D_040341FC;
+
+ColliderJntSphItemInit sColliderItemInit[1] = {
+    {
+        { 0x00, { 0x00000000, 0x00, 0x00 }, { 0xFFCFFFFF, 0x00, 0x00 }, 0x00, 0x00, 0x01 },
+        { 0, { { 0, 0, 0 }, 5 }, 100 },
+    },
+};
+
+static ColliderJntSphInit sColliderInit = 
+{
+    { COLTYPE_UNK10, 0x00, 0x00, 0x19, 0x10, COLSHAPE_JNTSPH },
+    1, sColliderItemInit,
+};
+
+u16 D_80A7DF10[] = {
+    0, 5, 7, 7,
+};
+
+static InitChainEntry sInitChain[] = {
+    ICHAIN_VEC3F_DIV1000(scale, 10, ICHAIN_CONTINUE),
+    ICHAIN_F32(unk_F4, 700, ICHAIN_CONTINUE),
+    ICHAIN_F32(unk_F8, 20, ICHAIN_CONTINUE),
+    ICHAIN_F32(unk_FC, 600, ICHAIN_STOP),
+};
+
+Vec3f D_80A7DF28[2] = {
+    { 0.0f, 0.0f, 0.0f },
+    { 0.0f, 0.0f, 0.0f }
+};
+
+void func_80A7BE20(EnInsect* this)
+{
+    this->unk_314 = D_80A7DF10[this->actor.params & 3];
+}
+
+f32 func_80A7BE40(Vec3f* v1, Vec3f* v2)
+{
+    return (v1->x - v2->x) * (v1->x - v2->x) + (v1->z - v2->z) * (v1->z - v2->z);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7BE6C.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7BF58.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7BFA0.s")
+// s32 func_80A7BFA0(EnInsect* this, GlobalContext* globalCtx) {
+
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7C058.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/EnInsect_Init.s")
+void EnInsect_Init(Actor* thisx, GlobalContext* globalCtx)
+{
+    EnInsect* this = THIS;
+    f32 randomResult;
+    s16 temp_s2;
+    s32 count;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/EnInsect_Destroy.s")
+    Actor_ProcessInitChain(&this->actor, sInitChain);
+    func_80A7BE20(this);
+
+    temp_s2 = this->actor.params & 3;
+
+    SkelAnime_Init(globalCtx, &this->skelAnime, &D_04035590, &D_040341FC, this->limbDrawTable, this->transitionDrawTable, 24);
+    Collider_InitJntSph(globalCtx, &this->collider);
+    Collider_SetJntSph(globalCtx, &this->collider, &this->actor, &sColliderInit, &this->colliderItem);
+
+    this->actor.colChkInfo.mass = 30;
+
+    if (this->unk_314 & 1) {
+        this->actor.gravity = -0.2f;
+        this->actor.minVelocityY = -2.0f;
+    }
+    if (this->unk_314 & 4) {
+        this->unk_31C = Math_Rand_S16Offset(200, 40);
+        this->actor.flags |= 0x10;
+    }
+
+    if (temp_s2 == 2 || temp_s2 == 3) {
+        if (func_80A7BFA0(this, globalCtx) != 0) {
+            this->unk_314 |= 0x10;
+            D_80A7DEB0 = 0.0f;
+        }
+        if (temp_s2 == 2) {
+            this->actor.posRot.rot.z = 0;
+            this->actor.shape.rot.z = this->actor.posRot.rot.z;
+
+            for (count = 0; count < 2; count++)
+            {
+                Actor_Spawn(&globalCtx->actorCtx, globalCtx, 32, this->actor.posRot.pos.x, this->actor.posRot.pos.y, this->actor.posRot.pos.z, this->actor.shape.rot.x, this->actor.shape.rot.y, this->actor.shape.rot.z, 3);
+                if (globalCtx) {}
+            }
+        }
+        func_80A7D39C(this);
+        if (1) {}
+        D_80A7DEB8++;
+    } else {
+        randomResult = Math_Rand_ZeroOne();
+        if (randomResult < 0.3f) {
+            func_80A7C3A0(this);
+        } else if (randomResult < 0.4f) {
+            func_80A7C598(this);
+        } else {
+            func_80A7C818(this);
+        }
+    }
+}
+
+void EnInsect_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+    s16 temp_v0;
+    EnInsect* this = thisx;
+
+    temp_v0 = thisx->params & 3;
+    Collider_DestroyJntSph(globalCtx, &this->collider);
+    if (temp_v0 == 2 || temp_v0 == 3) {
+        if (D_80A7DEB8 > 0) {
+            D_80A7DEB8--;
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7C3A0.s")
 
@@ -57,6 +167,47 @@ const ActorInit En_Insect_InitVars = {
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7CBC8.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7CC3C.s")
+// void func_80A7CC3C(EnInsect* this, GlobalContext* globalCtx) {
+//     f32 sp3C;
+//     f32 sp38;
+//     f32 sp34;
+//     f32 sp2C;
+//     f32 temp_f0;
+//     f32 phi_f2;
+
+//     Math_SmoothScaleMaxMinF(&this->actor.speedXZ, 0.0f, 0.1f, 0.5f, 0.0f);
+//     Math_ApproxS(&this->actor.shape.rot.x, (u16)0x2AAA, (u16)0x160);
+//     temp_f0 = this->actor.scale.x - 0.0002f;
+//     if (temp_f0 < 0.001f) {
+//         phi_f2 = 0.001f;
+//     } else {
+//         phi_f2 = temp_f0;
+//     }
+//     Actor_SetScale((Actor *) this, phi_f2);
+//     this->actor.shape.unk_08 = (f32) (this->actor.shape.unk_08 - 0.8f);
+//     this->actor.posRot.pos.x = (f32) ((Math_Rand_ZeroOne() + this->actor.initPosRot.pos.x) - 0.5f);
+//     this->actor.posRot.pos.z = (f32) ((Math_Rand_ZeroOne() + this->actor.initPosRot.pos.z) - 0.5f);
+//     SkelAnime_FrameUpdateMatrix(this + 0x1AC);
+//     if (this->unk_31A >= 0x15) {
+//         if (Math_Rand_ZeroOne() < 0.1f) {
+//             sp34 = Math_Sins(this->actor.shape.rot.y) * -0.6f;
+//             sp38 = Math_Sins(this->actor.shape.rot.x) * 0.6f;
+//             sp3C = Math_Coss(this->actor.shape.rot.y) * -0.6f;
+//             sp2C = Math_Rand_ZeroOne();
+//             func_800286CC(globalCtx, (Vec3f *) &this->actor.posRot, (Vec3f *) &sp34, &D_80A7DF28, (s32) ((sp2C * 5.0f) + 8.0f), (s32) ((Math_Rand_ZeroOne() * 5.0f) + 8.0f));
+//         }
+//     }
+//     if (this->unk_31A <= 0) {
+//         if (this->unk_314 & 0x10) {
+//             if (this->unk_320 != NULL) {
+//                 if (func_800CB650(this->unk320 + 0x24, (Vec3f *) &this->actor.posRot) < 64.0f) {
+//                     this->unk320->unk152 = (u16)1;
+//                 }
+//             }
+//         }
+//         Actor_Kill((Actor *) this);
+//     }
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Insect/func_80A7CE60.s")
 
--- src/overlays/actors/ovl_En_Insect/z_en_insect.h
+++ src/overlays/actors/ovl_En_Insect/z_en_insect.h
@@ -8,7 +8,19 @@ struct EnInsect;
 
 typedef struct EnInsect {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x1E0];
+    /* 0x014C */ ColliderJntSph collider;
+    /* 0x016C */ ColliderJntSphItem colliderItem;
+    /* 0x01AC */ SkelAnime skelAnime;
+    /* 0x01F0 */ Vec3s limbDrawTable[24];
+    /* 0x0280 */ Vec3s transitionDrawTable[24];
+    /* 0x0310 */ char unk_310[0x004];
+    /* 0x0314 */ u16 unk_314;
+    /* 0x0316 */ char unk_316[4];
+    /* 0x031A */ s16 unk_31A;
+    /* 0x031C */ s16 unk_31C;
+    /* 0x031E */ char unk_31E[0x002];
+    /* 0x0320 */ UNK_PTR unk_320;
+    /* 0x031E */ char unk_324[0x008];
 } EnInsect; // size = 0x032C
 
 extern const ActorInit En_Insect_InitVars;
