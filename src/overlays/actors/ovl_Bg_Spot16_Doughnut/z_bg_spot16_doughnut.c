11eaf951ed15d19e04defd406b47d1b125d8b397
Thar0 <17233964+Thar0@users.noreply.github.com>
2020-06-04 23:04:53 +0100

    Ran formatter

--- src/overlays/actors/ovl_Bg_Spot16_Doughnut/z_bg_spot16_doughnut.c
+++ src/overlays/actors/ovl_Bg_Spot16_Doughnut/z_bg_spot16_doughnut.c
@@ -84,7 +84,6 @@ void BgSpot16Doughnut_Init(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void BgSpot16Doughnut_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-
 }
 
 void BgSpot16Doughnut_Update(Actor* thisx, GlobalContext* globalCtx) {
@@ -97,7 +96,8 @@ void BgSpot16Doughnut_Update(Actor* thisx, GlobalContext* globalCtx) {
         } else {
             this->envColorAlpha = 0xFF;
         }
-    } else if (globalCtx->csCtx.state != 0 && globalCtx->csCtx.npcActions[2] != NULL && (u16)globalCtx->csCtx.npcActions[2]->action == 2) {
+    } else if (globalCtx->csCtx.state != 0 && globalCtx->csCtx.npcActions[2] != NULL &&
+               (u16)globalCtx->csCtx.npcActions[2]->action == 2) {
         if (this->envColorAlpha >= 6) {
             this->envColorAlpha -= 5;
         } else {
@@ -131,9 +131,12 @@ void BgSpot16Doughnut_Draw(Actor* thisx, GlobalContext* globalCtx) {
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_bg_spot16_doughnut.c", 210);
     func_80093D84(globalCtx->state.gfxCtx);
     if (1) {}
-    gSPMatrix(gfxCtx->polyXlu.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot16_doughnut.c", 213), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPMatrix(gfxCtx->polyXlu.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot16_doughnut.c", 213),
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     if (this->fireFlag & 1) {
-        gSPSegment(gfxCtx->polyXlu.p++, 0x08, Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, scroll*(-1), 0, 16, 32, 1, scroll, scroll*(-2), 16, 32));
+        gSPSegment(
+            gfxCtx->polyXlu.p++, 0x08,
+            Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, scroll * (-1), 0, 16, 32, 1, scroll, scroll * (-2), 16, 32));
         gDPSetEnvColor(gfxCtx->polyXlu.p++, 0xFF, 0x00, 0x00, this->envColorAlpha);
         gSPDisplayList(gfxCtx->polyXlu.p++, D_06000660);
     } else {
@@ -153,7 +156,8 @@ void BgSpot16Doughnut_Draw2(Actor* thisx, GlobalContext* globalCtx) {
     gfxCtx = globalCtx->state.gfxCtx;
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_bg_spot16_doughnut.c", 245);
     func_80093D84(globalCtx->state.gfxCtx);
-    gSPMatrix(gfxCtx->polyXlu.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot16_doughnut.c", 248), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPMatrix(gfxCtx->polyXlu.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot16_doughnut.c", 248),
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     gDPSetEnvColor(gfxCtx->polyXlu.p++, 0xFF, 0xFF, 0xFF, this->envColorAlpha);
     gDPSetPrimColor(gfxCtx->polyXlu.p++, 0, 0, 0xFF, 0xFF, 0xFF, 0xFF);
     gSPDisplayList(gfxCtx->polyXlu.p++, D_06000FC0);
