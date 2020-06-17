d96dcd6a023a67efdcef4aee819dfa424efa4217
Lucas Shaw <lgs84@cornell.edu>
2020-06-16 19:24:51 -0700

    __osPfsSelectBank.c OK

--- src/libultra_code/__osPfsSelectBank.c
+++ src/libultra_code/__osPfsSelectBank.c
@@ -1,19 +1,19 @@
 #include <ultra64/pfs.h>
 #include <global.h>
 
-s32 __osPfsSelectBank(OSPfs* pfs, u8 bank)            /* by YASU */
+s32 __osPfsSelectBank(OSPfs* pfs, u8 bank)
 {
-	  u8    temp[BLOCKSIZE];
-	  int   i;
-	  s32   ret = 0;
+    u8 temp[BLOCKSIZE];
+    s32 i;
+    s32 ret = 0;
 
-	  for (i = 0; i < BLOCKSIZE; i ++){
-	    	temp[i] = bank;
-	  }
+    for (i = 0; i < BLOCKSIZE; i++) {
+        temp[i] = bank;
+    }
 
-	  ret = __osContRamWrite(pfs->queue,
-				 pfs->channel, 0x8000/BLOCKSIZE, temp, 0);
-	  if (ret == 0) pfs->activebank = bank;
-	  return(ret);
+    ret = __osContRamWrite(pfs->queue, pfs->channel, 0x8000 / BLOCKSIZE, temp, 0);
+    if (ret == 0) {
+        pfs->activebank = bank;
+    }
+    return ret;
 }
-
