49c42f324fcb397782b4f8b210f76376134a725f
fig02 <fig02srl@gmail.com>
2020-07-08 13:19:50 -0400

    add prototype

--- include/functions.h
+++ include/functions.h
@@ -264,6 +264,8 @@ void func_80029CA4(GlobalContext* globalCtx, s32 a, Vec3f* pos);
 void func_80029E8C(GlobalContext* globalCtx, Vec3f* burstDepthY, Vec3f* burstDepthX, Vec3f* burstOrigin,
                    s16 gravityInfluence, s16 u0, s16 rotSpeed, s16 burstVel, u8 u1, s16 scale, u8 u2, s16 jitter,
                    s32 duration, s16 u3, s16 objectId, u32 dList);
+void EffectSsSolderSrchBall_Spawn(GlobalContext* globalCtx, Vec3f* pos, Vec3f* velocity, Vec3f* accel, s16 arg4,
+                                  s16* linkDetected)
 // ? func_80029F44(?);
 // ? func_80029FAC(?);
 // ? func_8002A140(?);
