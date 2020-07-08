2de33894126f2740b8e25678f0e64fc7cf792647
Léo Peltier <contact@leo-peltier.fr>
2020-07-08 17:45:33 +0200

    Decomp EnFhgFire_Init and EnFhgFire_SetActorFunc

--- src/overlays/actors/ovl_En_Fhg_Fire/z_en_fhg_fire.c
+++ src/overlays/actors/ovl_En_Fhg_Fire/z_en_fhg_fire.c
@@ -1,3 +1,9 @@
+/*
+ * File: z_en_fhg_fire.c
+ * Overlay: ovl_En_Fhg_Fire
+ * Description: Phantom Ganon's Lighting Attack
+ */
+
 #include "z_en_fhg_fire.h"
 
 #define FLAGS 0x00000030
@@ -8,6 +14,17 @@ void EnFhgFire_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnFhgFire_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnFhgFire_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnFhgFire_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+void EnFhgFire_SetActionFunc(EnFhgFire *this, EnFhgFireActionFunc func);
+void func_80A0F6F8(EnFhgFire*);
+void func_80A0FA90(EnFhgFire*);
+void func_80A0FC48(EnFhgFire*);
+void func_80A0FD8C(EnFhgFire*);
+void func_80A10008(EnFhgFire*);
+void func_80A10220(EnFhgFire*);
+void func_80A10F18(EnFhgFire*);
+
+extern ColliderCylinderInit D_80A11790;
 
 /*
 const ActorInit En_Fhg_Fire_InitVars = {
@@ -22,9 +39,108 @@ const ActorInit En_Fhg_Fire_InitVars = {
     (ActorFunc)EnFhgFire_Draw,
 };
 */
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Fhg_Fire/func_80A0F260.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Fhg_Fire/EnFhgFire_Init.s")
+void EnFhgFire_SetActionFunc(EnFhgFire *this, EnFhgFireActionFunc func) {
+    this->actionFunc = func;
+}
+
+void EnFhgFire_Init(Actor *thisx, GlobalContext *globalCtx) {
+    f32 tempf0;
+    EnFhgFire* this = THIS;
+    Player* player = PLAYER;
+
+    f32 tempf1;
+    f32 tempf2;
+    f32 tempf3;
+
+    ActorShape_Init(&thisx->shape, 0.0f, NULL, 0.0f);
+    if (((thisx->params == 0x23) || (thisx->params == 0x24)) || (thisx->params == 0x32)) {
+        Collider_InitCylinder(globalCtx, &this->collider);
+        Collider_SetCylinder(globalCtx, &this->collider, thisx, &D_80A11790);
+    }
+    this->unk_160 = 200.0f;
+    Actor_SetScale(thisx, 0.0f);
+
+    if (thisx->params == 0x01) {
+        EnFhgFire_SetActionFunc(this, func_80A0F6F8);
+        Audio_PlayActorSound2(thisx, NA_SE_EN_FANTOM_THUNDER);
+    } else if (thisx->params >= 0x64) {
+        EnFhgFire_SetActionFunc(this, func_80A0FA90);
+        thisx->shape.rot = thisx->posRot.rot;
+    }
+
+    if (thisx->params == 0x23) {
+        thisx->draw = NULL;
+        EnFhgFire_SetActionFunc(this, func_80A0FC48);
+        thisx->speedXZ = 30.0f;
+        Audio_PlayActorSound2(thisx, NA_SE_EN_FANTOM_SPARK);
+        return;
+    }
+
+    if (thisx->params == 0x24) {
+        EnFhgFire_SetActionFunc(this, func_80A0FD8C);
+        this->unk_160 = 255.0f;
+        this->unk_150.x = 0x20;
+        this->unk_150.y = 0x32;
+        this->unk_1FE = 0x0A;
+
+        tempf2 = thisx->posRot.rot.x;
+        this->unk_18C = tempf2 / 100.0f;
+        tempf1 = tempf2 * 0.13f;
+
+        this->collider.dim.radius = tempf1;
+        this->collider.dim.height = tempf1;
+        this->collider.dim.yShift = 0;
+        return;
+    }
+
+    if (thisx->params == 0x26) {
+        osSyncPrintf("yari hikari ct 1\n"); // "light spear"
+        EnFhgFire_SetActionFunc(this, func_80A10008);
+        osSyncPrintf("yari hikari ct 2\n");
+        this->unk_150.x = thisx->posRot.rot.x;
+        this->unk_158 = thisx->posRot.rot.y;
+        return;
+    }
+
+
+    switch (thisx->params) {
+    case 0x27:
+    case 0x28:
+    case 0x29:
+        Actor_SetScale(thisx, 7.0f);
+        EnFhgFire_SetActionFunc(this, func_80A10F18);
+        if (thisx->params == 0x29) {
+            this->unk_150.x = 0x1B8;
+            thisx->scale.z = 1.0f;
+        } else {
+            this->unk_150.x = 0x4C;
+            Audio_PlayActorSound2(thisx, NA_SE_EV_FANTOM_WARP_S);
+            Audio_PlayActorSound2(thisx, NA_SE_EV_FANTOM_WARP_S2);
+        }
+        return;
+    }
+
+    if (thisx->params == 0x32) {
+        thisx->speedXZ = (thisx->posRot.rot.x == 0) ? 8.0f : 3.0f;
+        EnFhgFire_SetActionFunc(this, func_80A10220);
+
+        this->unk_150.x = 0x46;
+        this->unk_150.y = 0x02;
+
+        tempf1 = player->actor.posRot.pos.x - thisx->posRot.pos.x;
+        tempf2 = player->actor.posRot.pos.y + 30.0f - thisx->posRot.pos.y;
+        tempf3 = player->actor.posRot.pos.z - thisx->posRot.pos.z;
+        thisx->posRot.rot.y = Math_atan2f(tempf1, tempf3) * 10430.378f; // 65536/(2*M_PI)
+        tempf0 = sqrtf((tempf1 * tempf1) + (tempf3 * tempf3));
+        thisx->posRot.rot.x = Math_atan2f(tempf2, tempf0) * 10430.378f; // 65536/(2*M_PI)
+        this->collider.dim.radius = 40;
+        this->collider.dim.height = 50;
+        this->collider.dim.yShift = -25;
+        this->unk_19C = Lights_Insert(globalCtx, &globalCtx->lightCtx, (void *) (&this->unk_1A0));
+        Lights_InitType0PositionalLight(&this->unk_1A0, thisx->posRot.pos.x, thisx->posRot.pos.y, thisx->posRot.pos.z, 0xFF, 0xFF, 0xFF, 0xFF);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Fhg_Fire/EnFhgFire_Destroy.s")
 
--- src/overlays/actors/ovl_En_Fhg_Fire/z_en_fhg_fire.h
+++ src/overlays/actors/ovl_En_Fhg_Fire/z_en_fhg_fire.h
@@ -5,10 +5,26 @@
 #include <global.h>
 
 struct EnFhgFire;
+typedef void (*EnFhgFireActionFunc)(struct EnFhgFire*);
 
 typedef struct EnFhgFire {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0xB8];
+    /* 0x014C */ EnFhgFireActionFunc actionFunc;
+    /* 0x0150 */ Vec3s unk_150; // rotation
+    /* 0x0156 */ char unk_156[0x02]; // either padding or the start of another Vec3s
+    /* 0x0158 */ s16 unk_158; // used to store a rot.y
+    /* 0x015A */ char unk_15A[0x06];
+    /* 0x0160 */ f32 unk_160;
+    /* 0x0164 */ char unk_164[0x28];
+    /* 0x018C */ f32 unk_18C;
+    /* 0x0190 */ char unk_190[0x0C];
+    /* 0x019C */ z_Light* unk_19C;
+    /* 0x01A0 */ LightInfoPositional unk_1A0;
+    /* 0x01AE */ char padding_1AE[2];
+    /* 0x01B0 */ ColliderCylinder collider;
+    /* 0x01FC */ char padding_1FC[2];
+    /* 0x01FE */ u16 unk_1FE; // probable Vec3s
+    /* 0x0200 */ char unk_200[0x4];
 } EnFhgFire; // size = 0x0204
 
 extern const ActorInit En_Fhg_Fire_InitVars;
