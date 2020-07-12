7304cb84406601f6d11395edcd38580ab9933404
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2020-07-12 12:01:16 -0700

    Update src/overlays/actors/ovl_Shot_Sun/z_shot_sun.c
    Co-authored-by: Roman971 <32455037+Roman971@users.noreply.github.com>

--- src/overlays/actors/ovl_Shot_Sun/z_shot_sun.c
+++ src/overlays/actors/ovl_Shot_Sun/z_shot_sun.c
@@ -155,7 +155,7 @@ void ShotSun_UpdateHyliaSun(ShotSun* this, GlobalContext* globalCtx) {
         osSyncPrintf(VT_FGCOL(CYAN) "SHOT_SUN HIT!!!!!!!\n" VT_RST);
         if (INV_CONTENT(SLOT_ARROW_FIRE) == ITEM_NONE) {
             Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_ITEM_ETCETERA, 700.0f, -800.0f, 7261.0f, 0, 0, 0, 7);
-            globalCtx->csCtx.segment = SEGMENTED_TO_VIRTUAL(&D_02007020);
+            globalCtx->csCtx.segment = SEGMENTED_TO_VIRTUAL(D_02007020);
             if (1) {}
             gSaveContext.cutsceneTrigger = 1;
         } else {
