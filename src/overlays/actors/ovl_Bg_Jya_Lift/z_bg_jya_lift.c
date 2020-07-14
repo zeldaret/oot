625dd08ca7e19a2d6a0d2900af893c9c292ed531
louist103 <35883445+louist103@users.noreply.github.com>
2020-07-14 19:27:09 -0400

    Fix errors in Lift

--- src/overlays/actors/ovl_Bg_Jya_Lift/z_bg_jya_lift.c
+++ src/overlays/actors/ovl_Bg_Jya_Lift/z_bg_jya_lift.c
@@ -38,9 +38,9 @@ const ActorInit Bg_Jya_Lift_InitVars = {
 
 static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_CONTINUE),
-    ICHAIN_F32(unk_F4, 1400, ICHAIN_CONTINUE),
-    ICHAIN_F32(unk_F8, 1800, ICHAIN_CONTINUE),
-    ICHAIN_F32(unk_FC, 2500, ICHAIN_STOP),
+    ICHAIN_F32(uncullZoneForward, 1400, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneScale, 1800, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneDownward, 2500, ICHAIN_STOP),
 };
 
 extern UNK_TYPE D_0600D7E8;
