2e324291388e86b4b6241043333911ab15900080
louist103 <35883445+louist103@users.noreply.github.com>
2020-07-16 19:42:36 -0400

    Decomp BgYdanHasi

--- src/overlays/actors/ovl_Bg_Ydan_Hasi/z_bg_ydan_hasi.h
+++ src/overlays/actors/ovl_Bg_Ydan_Hasi/z_bg_ydan_hasi.h
@@ -6,9 +6,14 @@
 
 struct BgYdanHasi;
 
+typedef void (*BgYdanHasiActionFunc)(struct BgYdanHasi*, struct GlobalCtx*);
+
 typedef struct BgYdanHasi {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x20];
+    /* 0x0000 */ DynaPolyActor dyna;
+    /* 0x0164 */ BgYdanHasiActionFunc actionFunc;
+    /* 0x0168 */ u8 unk_168;
+    /* 0x0169*/ s8 unk_169;
+    /* 0x016A */ s16 timer;
 } BgYdanHasi; // size = 0x016C
 
 extern const ActorInit Bg_Ydan_Hasi_InitVars;
