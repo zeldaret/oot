5dd62932b910469d05ffd51d67d3f7aaaf543f30
Kelebek1 <eeeedddccc@hotmail.co.uk>
2020-07-17 04:07:32 +0100

    Memes

--- src/overlays/actors/ovl_En_Bx/z_en_bx.c
+++ src/overlays/actors/ovl_En_Bx/z_en_bx.c
@@ -23,54 +23,38 @@ const ActorInit En_Bx_InitVars = {
     (ActorFunc)EnBx_Draw,
 };
 
-static ColliderCylinderInit sCylinderInit =
-{
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK6, 0x11, 0x09, 0x00, 0x00, COLSHAPE_CYLINDER },
     { 0x01, { 0xFFCFFFFF, 0x03, 0x04 }, { 0xFFCFFFFF, 0x01, 0x00 }, 0x01, 0x01, 0x00 },
     { 60, 100, 100, { 0, 0, 0 } },
 };
 
-static ColliderQuadInit sQuadInit =
-{
+static ColliderQuadInit sQuadInit = {
     { COLTYPE_UNK10, 0x11, 0x00, 0x00, 0x00, COLSHAPE_QUAD },
     { 0x00, { 0xFFCFFFFF, 0x03, 0x04 }, { 0x00000000, 0x00, 0x00 }, 0x01, 0x00, 0x00 },
     { { { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } } },
 };
 
-Vec3f D_809D250C = {
-    0.0149999996647f, 0.0149999996647f, 0.0149999996647f
- };
+Vec3f D_809D250C = { 0.0149999996647f, 0.0149999996647f, 0.0149999996647f };
 
- Vec3f D_809D2518 = {
-    0.0f, 0.0f, 0.0f
-};
+Vec3f D_809D2518 = { 0.0f, 0.0f, 0.0f };
 
 static InitChainEntry sInitChain[] = {
     ICHAIN_F32(unk_4C, 5300, ICHAIN_STOP),
 };
 
-Vec3f D_809D2528 = {
-    8000.0f, 15000.0f, 2500.0f
-};
+Vec3f D_809D2528 = { 8000.0f, 15000.0f, 2500.0f };
 
-Vec3f D_809D2534 = {
-    8000.0f, 10000.0f, 2500.0f
-};
+Vec3f D_809D2534 = { 8000.0f, 10000.0f, 2500.0f };
 
-Vec3f D_809D2540 = {
-    -8000.0f, 15000.0f, 2500.0f
-};
+Vec3f D_809D2540 = { -8000.0f, 15000.0f, 2500.0f };
 
-Vec3f D_809D254C = {
-    -8000.0f, 10000.0f, 2500.0f
-};
+Vec3f D_809D254C = { -8000.0f, 10000.0f, 2500.0f };
 
 u32 D_809D2558 = 0xFFFFFFFF;
 u32 D_809D255C = 0xC8FFFFFF;
 
-u32 D_809D2560[] = {
-    0x060024F0, 0x060027F0, 0x060029F0, 0x00000000
-};
+u32 D_809D2560[] = { 0x060024F0, 0x060027F0, 0x060029F0, 0x00000000 };
 
 void EnBx_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnBx* this = THIS;
@@ -82,7 +66,7 @@ void EnBx_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_ProcessInitChain(&this->actor, sInitChain);
     thisx->scale.x = thisx->scale.z = 0.01f;
     thisx->scale.y = 0.03f;
-    
+
     thisx->posRot.pos.y = (f32)thisx->posRot.pos.y - 100.0f;
     for (i = 0; i < 4; i++) {
         this->unk_184[i] = sp48;
@@ -90,7 +74,7 @@ void EnBx_Init(Actor* thisx, GlobalContext* globalCtx) {
             this->unk_1B4[i].x = thisx->shape.rot.x - 0x4000;
         }
         this->unk_154[i] = thisx->posRot.pos;
-        this->unk_154[i].y = thisx->posRot.pos.y + ((i+1) * 140.0f);
+        this->unk_154[i].y = thisx->posRot.pos.y + ((i + 1) * 140.0f);
     }
 
     ActorShape_Init(&thisx->shape, 0.0f, ActorShadow_DrawFunc_Circle, 48.0f);
@@ -113,14 +97,12 @@ void EnBx_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void func_809D1D0C(Actor* thisx, GlobalContext* globalCtx) {
-    Vec3f sp5C;
-    Vec3f sp50;
+    Vec3f sp5C = D_809D2528;
+    Vec3f sp50 = D_809D2534;
     Vec3f sp44;
     Vec3f sp38;
     EnBx* this = THIS;
 
-    sp5C = D_809D2528;
-    sp50 = D_809D2534;
     Matrix_MultVec3f(&D_809D2540, &sp44);
     Matrix_MultVec3f(&D_809D254C, &sp38);
     Matrix_MultVec3f(&sp5C, &this->colliderQuad.dim.quad[1]);
@@ -138,15 +120,17 @@ void EnBx_Update(Actor* thisx, GlobalContext* globalCtx) {
     s16 tmp16;
     u32 test;
 
-    if ((thisx->xzDistFromLink <= 70.0f) || (this->collider.base.atFlags & 2) || (this->collider.base.acFlags & 2) || (this->colliderQuad.base.atFlags & 2)) {
-        if ((thisx->xzDistFromLink <= 70.0f) || (&player->actor == this->collider.base.at) || (&player->actor == this->collider.base.ac) || (&player->actor == this->colliderQuad.base.at)) {
+    if ((thisx->xzDistFromLink <= 70.0f) || (this->collider.base.atFlags & 2) || (this->collider.base.acFlags & 2) ||
+        (this->colliderQuad.base.atFlags & 2)) {
+        if ((thisx->xzDistFromLink <= 70.0f) || (&player->actor == this->collider.base.at) ||
+            (&player->actor == this->collider.base.ac) || (&player->actor == this->colliderQuad.base.at)) {
             tmp33 = player->invincibilityTimer & 0xFF;
             tmp32 = thisx->posRot.rot.y;
             if (!(thisx->params & 0x80)) {
                 tmp32 = thisx->yawTowardsLink;
             }
-            if ((&player->actor != this->collider.base.at) && (&player->actor != this->collider.base.ac) && 
-                (&player->actor != this->colliderQuad.base.at) && (player->invincibilityTimer <= 0)) { 
+            if ((&player->actor != this->collider.base.at) && (&player->actor != this->collider.base.ac) &&
+                (&player->actor != this->colliderQuad.base.at) && (player->invincibilityTimer <= 0)) {
                 if (player->invincibilityTimer < -0x27) {
                     player->invincibilityTimer = 0;
                 } else {
@@ -173,11 +157,10 @@ void EnBx_Update(Actor* thisx, GlobalContext* globalCtx) {
             if (!((this->unk_14C + (i << 1)) % 4)) {
                 tmp16 = (s32)Math_Rand_CenteredFloat(12288.0f);
                 tmp16 = (tmp16 + (i * 16384)) + 0x2000;
-                sp58.x = Math_Rand_CenteredFloat(5.0f)  + thisx->posRot.pos.x;
+                sp58.x = Math_Rand_CenteredFloat(5.0f) + thisx->posRot.pos.x;
                 sp58.y = Math_Rand_CenteredFloat(30.0f) + thisx->posRot.pos.y + 170.0f;
-                sp58.z = Math_Rand_CenteredFloat(5.0f)  + thisx->posRot.pos.z;
-                func_800295A0(globalCtx, &sp58, &D_809D2558, &D_809D255C, 0xE6, 
-                    tmp16, 6, 0);
+                sp58.z = Math_Rand_CenteredFloat(5.0f) + thisx->posRot.pos.z;
+                func_800295A0(globalCtx, &sp58, &D_809D2558, &D_809D255C, 0xE6, tmp16, 6, 0);
             }
         }
 
@@ -199,7 +182,7 @@ void EnBx_Draw(Actor* thisx, GlobalContext* globalCtx) {
     void* memory = Graph_Alloc(globalCtx->state.gfxCtx, 0x100U);
     f32 tmpf1;
     f32 tmpf2;
-    
+
     {
         GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
         Gfx* dispRefs[4];
@@ -210,13 +193,12 @@ void EnBx_Draw(Actor* thisx, GlobalContext* globalCtx) {
         func_80093D18(globalCtx->state.gfxCtx);
 
         gSPSegment(gfxCtx->polyOpa.p++, 0x0C, matrix);
-        gSPSegment(gfxCtx->polyOpa.p++, 0x08, 
-            SEGMENTED_TO_VIRTUAL(D_809D2560[this->actor.params & 0x7F]));
-        gSPSegment(gfxCtx->polyOpa.p++, 0x09, 
-            Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, 0, 0, 0x10, 0x10, 1, 0, 
-                (-globalCtx->gameplayFrames * 10) & 0x7F, 0x20, 0x20));
-        gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_bx.c", 0x1DE), 
-            G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(D_809D2560[this->actor.params & 0x7F]));
+        gSPSegment(gfxCtx->polyOpa.p++, 0x09,
+                   Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, 0, 0, 0x10, 0x10, 1, 0,
+                                    (-globalCtx->gameplayFrames * 10) & 0x7F, 0x20, 0x20));
+        gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_bx.c", 0x1DE),
+                  G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
 
         if (this->actor.params & 0x80) {
             func_809D1D0C(&this->actor, globalCtx);
