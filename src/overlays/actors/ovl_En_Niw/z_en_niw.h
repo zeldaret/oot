2dcad25fbaa63da77b51123ee2a02e06e3bcb57b
Kelebek1 <eeeedddccc@hotmail.co.uk>
2020-07-19 07:16:55 +0100

    Attack!

--- src/overlays/actors/ovl_En_Niw/z_en_niw.h
+++ src/overlays/actors/ovl_En_Niw/z_en_niw.h
@@ -8,7 +8,9 @@ struct EnNiw;
 
 typedef struct EnNiw {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x66C];
+    /* 0x014C */ char unk_14C[0x11E];
+    /* 0x026A */ s16 unk_26A;
+    /* 0x026C */ char unk_26C[0x54C];
 } EnNiw; // size = 0x07B8
 
 extern const ActorInit En_Niw_InitVars;
