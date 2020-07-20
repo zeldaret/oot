e649e9147af0205db25996fa871775735fda39f8
Lucas Shaw <lgs84@cornell.edu>
2020-07-19 23:32:31 -0700

    bg_mori_hineri OK

--- src/overlays/actors/ovl_Bg_Mori_Hineri/z_bg_mori_hineri.h
+++ src/overlays/actors/ovl_Bg_Mori_Hineri/z_bg_mori_hineri.h
@@ -6,9 +6,15 @@
 
 struct BgMoriHineri;
 
+typedef void (*BgMoriHineriActionFunc)(struct BgMoriHineri*, GlobalContext*);
+
 typedef struct BgMoriHineri {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x20];
+    /* 0x0000 */ DynaPolyActor dyna;
+    /* 0x0164 */ BgMoriHineriActionFunc actionFunc;
+    /* 0x0168 */ s8 objBankIndex;
+    /* 0x0169 */ s8 textureIdx;
+    /* 0x016A */ s8 objBoxIndex;
+    /* 0x016B */ s8 switchFlag;
 } BgMoriHineri; // size = 0x016C
 
 extern const ActorInit Bg_Mori_Hineri_InitVars;
