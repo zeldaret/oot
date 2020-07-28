326332499ac767a6282073da4aa932620b27669e
louist103 <35883445+louist103@users.noreply.github.com>
2020-07-28 19:33:11 -0400

    PR fixes

--- include/z64math.h
+++ include/z64math.h
@@ -7,69 +7,69 @@
 
 typedef struct {
     f32 x, y;
-} Vec2f; //size = 0x08
+} Vec2f; // size = 0x08
 
 typedef struct {
     f32 x, y, z;
-} Vec3f; //size = 0x0C
+} Vec3f; // size = 0x0C
 
 typedef struct {
     u16 x, y, z;
-} Vec3us; //size = 0x06
+} Vec3us; // size = 0x06
 
 typedef struct {
     s16 x, y, z;
-} Vec3s; //size = 0x06
+} Vec3s; // size = 0x06
 
 typedef struct {
     s32 x, y, z;
-} Vec3i; //size = 0x0C
+} Vec3i; // size = 0x0C
 
 typedef struct {
     Vec3s center;
     s16 radius;
-} Sphere16; //size = 0x08
+} Sphere16; // size = 0x08
 
 typedef struct {
     Vec3f center;
     f32 radius;
-} Spheref; //size = 0x10
+} Spheref; // size = 0x10
 
 
 typedef struct {
     Vec3f normal;
     f32 originDist;
-} Plane; //size = 0x10
+} Plane; // size = 0x10
 
 typedef struct {
     Vec3f vtx[3];
     Plane plane;
-} TriNorm; //size = 0x34
+} TriNorm; // size = 0x34
 
 typedef struct {
     s16 radius;
     s16 height;
     s16 yShift;
     Vec3s pos;
-} Cylinder16; //size = 0x0C
+} Cylinder16; // size = 0x0C
 
 typedef struct {
     f32 radius;
     f32 height;
     f32 yShift;
     Vec3f pos;
-} Cylinderf; //size = 0x10
+} Cylinderf; // size = 0x18
 
 typedef struct {
     Vec3f a;
     Vec3f b;
-} Linef; //size = 0x18
+} Linef; // size = 0x18
 
 // Defines a point in the spherical coordinate system
 typedef struct {
     f32 r;      // radius
     s16 phi;    // polar (zenith) angle
     s16 theta;  // azimuthal angle
-} VecSph; //size = 0x08
+} VecSph; // size = 0x08
 
 #endif
