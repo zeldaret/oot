332357f58749a2e419fbecc48702e88d71e42203
louist103 <35883445+louist103@users.noreply.github.com>
2020-07-30 01:53:27 -0400

    Continue Decomp EnNiwGirl

--- src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.c
+++ src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.c
@@ -11,7 +11,9 @@ void EnNiwGirl_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnNiwGirl_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void func_80AB93C0(EnNiwGirl* this, GlobalContext* globalCtx);
-
+void func_80AB93C0(EnNiwGirl* this, GlobalContext* globalCtx);
+void func_80AB94D0(EnNiwGirl* this, GlobalContext* globalCtx);
+;
 const ActorInit En_Niw_Girl_InitVars = {
     ACTOR_EN_NIW_GIRL,
     ACTORTYPE_NPC,
@@ -32,18 +34,18 @@ static ColliderCylinderInit sCylinderInit = {
 };
                     //0.2f          //0.2     //0.2
 s32 D_80AB99CC[] = {0x3E4CCCCD, 0x3E4CCCCD, 0x3E4CCCCD };
+
 s32 D_80AB99D8[] = {0x06004178, 0x06004978, 0x06005178, 0x00000000, 0x00000000, 0x00000000 };
 
 extern SkeletonHeader D_06009948;
 extern AnimationHeader D_06000378;
-
+extern AnimationHeader D_06009C78;
     //#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/EnNiwGirl_Init.s")
 void EnNiwGirl_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnNiwGirl* this = THIS;
     s32 pad;
     Vec3f tempSrc;
     Vec3f tempDst;
-    //f32 localConst;
     s32 pad2;
     SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_06009948, &D_06000378, &this->limbDrawTable,
                      &this->transitionDrawTable, 17);
@@ -57,23 +59,19 @@ void EnNiwGirl_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->unkFlag = ((thisx->params >> 8) & 0xFF);
     thisx->gravity = -3.0f;
     Matrix_RotateY((thisx->shape.rot.y / 32768.0f) * M_PI, 0);
-    tempDst.x = 50.0f;
-    tempDst.z = 0.0f;
-    tempSrc.z = 0.0f;
-    tempSrc.y = 0.0f;
-    tempSrc.x = 0.0f;
-    tempDst.y = 0.0f;
+    tempDst.x = tempDst.y = tempDst.z = 0.0f;
+    tempSrc.x = tempSrc.y = 0.0f;
+    tempSrc.z = 50.0;
     Matrix_MultVec3f(&tempSrc, &tempDst);
     this->attachedActor = Actor_SpawnAttached(&globalCtx->actorCtx, thisx, globalCtx, ACTOR_EN_NIW,
-                                              thisx->posRot.pos.x + tempSrc.x, thisx->posRot.pos.y + tempSrc.y,
-                                              thisx->posRot.pos.z + tempSrc.z, 0, thisx->posRot.rot.y, 0, 0xA);
+                                              thisx->posRot.pos.x + tempDst.x, thisx->posRot.pos.y + tempDst.y,
+                                              thisx->posRot.pos.z + tempDst.z, 0, thisx->posRot.rot.y, 0, 0xA);
     if (this->attachedActor != 0) {
-
         osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ シツレイしちゃうわね！プンプン ☆☆☆☆☆ %d\n" VT_RST, thisx->params);
         osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ きゃははははは、まてー ☆☆☆☆☆ %d\n" VT_RST, this->unkFlag);
         osSyncPrintf("\n\n");
         thisx->colChkInfo.mass = 0xFF;
-        this->actionFunc = &func_80AB93C0;
+        this->actionFunc = func_80AB93C0;
     } else {
         osSyncPrintf("\n\n");
         osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ なぜか、セットできむぅあせん ☆☆☆☆☆ %d\n" VT_RST, thisx->params);
@@ -89,7 +87,42 @@ void EnNiwGirl_Init(Actor* thisx, GlobalContext* globalCtx) {
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/func_80AB9210.s")
 
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/func_80AB93C0.s")
+#ifdef NON_MATCHING
+void func_80AB93C0(EnNiwGirl* this, GlobalContext* globalCtx) {
+    u32 currentMask;
+
+    SkelAnime_ChangeAnim(&this->skelAnime, &D_06009C78, 1.0f, 0.0f, SkelAnime_GetFrameCount(&D_06009C78), 0, -10.0f);
+    this->actor.flags = (this->actor.flags | 1);
+    this->actor.textId = 0x7000; // 	I can't catch that Cucco! 
+    if (((gSaveContext.eventChkInf[8]) & 1) != 0) {
+        if (this->unk_27A == 0) {
+            this->actor.textId = 0x70EA; //That scary-looking man took off out of here really fast ! (Ganon?)
+        }
+    }
+    currentMask = func_8008F080(globalCtx) - 1;
+        switch (currentMask) {
+            case 0:
+                this->actor.textId = 0x7118; //Oh wow! It's Keaton! Hi, Keaton ! 
+                break;
+            case 2:
+                this->actor.textId = 0x7119; // 	How spooky! There's a boy in Kakariko Village who was looking for a mask like that! 
+                break;
+            case 3:
+            case 5:
+            case 6:
+                this->actor.textId = 0x711A;//   Hmm... well... don't you have any other masks ?
+                break;
+            case 7:
+                this->actor.textId = 0x711B;//What a strange mask!  Hee hah haa ! 
+                break;
+        }
+        this->unk_270 = 6;
+        this->actionFunc = func_80AB94D0;
+ }
+#else
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/func_80AB93C0.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/func_80AB94D0.s")
 
--- src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.h
+++ src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.h
@@ -14,9 +14,13 @@ typedef struct EnNiwGirl {
     /* 0x0190 */ Vec3s limbDrawTable[17];
     /* 0x01F6 */ Vec3s transitionDrawTable[17];
     /* 0x025C */ EnNiwGirlActionFunc actionFunc;
-    /* 0x0260 */ char unk_260[0x16];
+    /* 0x0260 */ char unk_260[0x10];
+    /* 0x0270 */ s16 unk_270;
+    /* 0x0272 */ char unk_272[4];
     /* 0x0276 */ s16 unkFlag;
-    /* 0x027A*/ char unk_278[0xC];
+    /* 0x0278 */ char unk_278[0x2];
+    /* 0x027A */ s16 unk_27A;
+    /* 0x027C */ char unk_27C[8];
     /* 0x0284 */ Actor* attachedActor;
     /* 0x0288 */ ColliderCylinder collider;
     /* 0x02D4 */ char unk_2D4[0x28];
