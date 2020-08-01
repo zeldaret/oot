e3354d74ef321376eee4ff9101e74363907afb70
louist103 <35883445+louist103@users.noreply.github.com>
2020-08-01 02:28:33 -0400

    Contimue Decomp

--- src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.c
+++ src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.c
@@ -15,7 +15,7 @@ void func_80AB93C0(EnNiwGirl* this, GlobalContext* globalCtx);
 void func_80AB94D0(EnNiwGirl* this, GlobalContext* globalCtx);
 void func_80AB918C(EnNiwGirl* this, GlobalContext* globalCtx);
 void func_80AB9210(EnNiwGirl* this, GlobalContext* globalCtx);
-
+PostLimbDraw func_80AB97E4(EnNiwGirl*);
 const ActorInit En_Niw_Girl_InitVars = {
     ACTOR_EN_NIW_GIRL,
     ACTORTYPE_NPC,
@@ -35,9 +35,9 @@ static ColliderCylinderInit sCylinderInit = {
     { 10, 30, 0, { 0, 0, 0 } },
 };
                     //0.2f          //0.2     //0.2
-s32 D_80AB99CC[] = {0x3E4CCCCD, 0x3E4CCCCD, 0x3E4CCCCD };
+f32 D_80AB99CC[] = {0x3E4CCCCD, 0x3E4CCCCD, 0x3E4CCCCD };
 
-s32 D_80AB99D8[] = {0x06004178, 0x06004978, 0x06005178, 0x00000000, 0x00000000, 0x00000000 };
+Gfx* D_80AB99D8[] = {0x06004178, 0x06004978, 0x06005178, 0x00000000, 0x00000000, 0x00000000 };
 
 extern SkeletonHeader D_06009948;
 extern AnimationHeader D_06000378;
@@ -102,7 +102,7 @@ void func_80AB9210(EnNiwGirl* this, GlobalContext* globalCtx) {
     Path* path;
     f32 xDistBetween;
     f32 zDistBetween;
-    f32 distBetween;
+    //f32 distBetween;
     path = &globalCtx->setupPathList[this->unkFlag];
     SkelAnime_FrameUpdateMatrix(&this->skelAnime);
     Math_SmoothScaleMaxF(&this->actor.speedXZ, 3.0f, 0.2f, 0.4f);
@@ -111,15 +111,11 @@ void func_80AB9210(EnNiwGirl* this, GlobalContext* globalCtx) {
     if (func_8010BDBC(&globalCtx->msgCtx) != 0) {
         this->attachedActor->unk_2E8 = 0;
     }
-    
-    distBetween = sqrtf((xDistBetween * xDistBetween) + (zDistBetween * zDistBetween));
-    if (distBetween < 70.0f) {
+    if (sqrtf(SQ(xDistBetween) + SQ(zDistBetween)) < 70.0f) {
         this->attachedActor->unk_2E8 = (this->unkFlag + 1);
         this->attachedActor->unk_2EC = path->count;
-    } else {
-        if (150.0f < distBetween) {
-            this->attachedActor->unk_2E8 = 0;
-        }
+    } else if (sqrtf(SQ(xDistBetween) + SQ(zDistBetween)) > 150.0f) {
+        this->attachedActor->unk_2E8 = 0;
     }
     Math_SmoothScaleMaxMinS(&this->actor.shape.rot.y,((Math_atan2f(xDistBetween, zDistBetween) * 10430.378f)), 3, this->unk_27C , 0);
     Math_SmoothScaleMaxF(&this->unk_27C, 5000.0f, 30.0f, 150.0f);
@@ -194,8 +190,81 @@ void func_80AB93C0(EnNiwGirl* this, GlobalContext* globalCtx) {
  }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/EnNiwGirl_Update.s")
+ /*void EnNiwGirl_Update(Actor* thisx, GlobalContext* globalCtx) {
+    s32 pad;
+    EnNiwGirl* this = THIS;
+    Player* player = PLAYER;
+
+     
+     Actor_SetScale(&this->actor, 0.013f);
+     this->unk_26E = (this->unk_26E + 1);
+     if (this->unk_274 == 0) {
+         this->unk_272 = (this->unk_272 + 1);
+         if (this->unk_272 >= 3) {
+             this->unk_274 = ((s32)Math_Rand_ZeroFloat(60.0f) + 0x14);
+             this->unk_272 = 0;
+         }
+     }
+     this->unk_280 = 30.0f;
+     Actor_SetHeight(&this->actor, 30.0f);
+     if (func_80AB94D0 == this->actionFunc) {
+         this->unk_2D4.unk_18.x = player->actor.posRot.pos.x; // unk24;
+         this->unk_2D4.unk_18.y = player->actor.posRot.pos.y; // unk28;
+         this->unk_2D4.unk_18.z = player->actor.posRot.pos.z;
+         if (LINK_IS_CHILD) {
+             this->unk_2D4.unk_18.y = (player->actor.posRot.pos.y - 10.0f);
+         }
+         func_80034A14(&this->actor, &this->unk_2D4, 2, 4);
+         this->unk_260 = this->unk_2D4.unk_08.x;
+         this->unk_266 = this->unk_2D4.unk_0E.x;
+         this->unk_264 = this->unk_2D4.unk_08.z;
+         this->unk_26A = this->unk_2D4.unk_0E.z;
+     } else {
+         Math_SmoothScaleMaxMinS(&this->unk_268, 0, 5, 3000, 0);
+         Math_SmoothScaleMaxMinS(&this->unk_262, 0, 5, 3000, 0);
+         Math_SmoothScaleMaxMinS(&this->unk_264, 0, 5, 3000, 0);
+     }
+     if (this->unk_274 != 0) {
+         this->unk_274 = (this->unk_274 - 1);
+     }
+     if (this->unk_26C != 0) {
+         this->unk_26C = (this->unk_26C - 1);
+     }
+     this->actionFunc(&this->actor, globalCtx);
+     Actor_MoveForward(&this->actor);
+     func_8002E4B4(globalCtx, &this->actor, 100.0f, 100.0f, 200.0f, 0x1C);
+
+
+     Collider_CylinderUpdate(&this->actor, &this->collider);
+     CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider);
+ }*/
+
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/func_80AB97E4.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/EnNiwGirl_Draw.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Girl/EnNiwGirl_Draw.s")
+ void EnNiwGirl_Draw(Actor* thisx, GlobalContext* globalCtx) {
+     Vec3f sp4C;
+    GraphicsContext* gfxCtx;
+     Gfx* dispRefs[4];
+    // Gfx* temp_v1;
+     EnNiwGirl* this = THIS;
+     sp4C.z = D_80AB99CC[0];
+     sp4C.y = D_80AB99CC[1];
+     sp4C.x = D_80AB99CC[2];
+     gfxCtx = globalCtx->state.gfxCtx;
+     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_niw_girl.c", 0x23D);
+     func_80093D18(globalCtx->state.gfxCtx);
+    // temp_v1 = globalCtx->state.gfxCtx->polyOpa.p;
+    // globalCtx->state.gfxCtx->polyOpa.p = (Gfx*)(temp_v1 + 8);
+    // temp_v1->words.w0 = 0xDB060020U;
+    // temp_v1->words.w1 =
+    //     (u32)(((D_80AB99D8[thisx->unk272] & 0xFFFFFF) + gSegments[((u32)(D_80AB99D8[thisx->unk272] * 0x10) >> 0x1C)]) +
+    //           0x80000000);
+     gSPSegment(gfxCtx->polyOpa.p++, 0x08,SEGMENTED_TO_VIRTUAL(D_80AB99D8[this->unk_272])); 
+     SkelAnime_DrawSV(globalCtx, this->skelAnime.skeleton, this->skelAnime.limbDrawTbl, this->skelAnime.dListCount, func_80AB97E4, 0);
+     func_80033C30(&thisx->posRot, &sp4C, (u8)0xFFU, globalCtx);
+     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_niw_girl.c", 0x250);
+ }
+
  
--- src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.h
+++ src/overlays/actors/ovl_En_Niw_Girl/z_en_niw_girl.h
@@ -7,7 +7,7 @@
 
 struct EnNiwGirl;
 
-typedef void (*EnNiwGirlActionFunc)(struct EnNuGIrl*, GlobalContext*);
+typedef void (*EnNiwGirlActionFunc)(struct EnNiwGirl*, GlobalContext*);
 
 typedef struct EnNiwGirl {
     /* 0x0000 */ Actor actor;
@@ -15,19 +15,26 @@ typedef struct EnNiwGirl {
     /* 0x0190 */ Vec3s limbDrawTable[17];
     /* 0x01F6 */ Vec3s transitionDrawTable[17];
     /* 0x025C */ EnNiwGirlActionFunc actionFunc;
-    /* 0x0260 */ char unk_260[0xC];
+    /* 0x0260 */ s16 unk_260;
+    /* 0x0262 */ s16 unk_262;
+    /* 0x0264 */ s16 unk_264;
+    /* 0x0266 */ s16 unk_266;
+    /* 0x0268 */ s16 unk_268;
+    /* 0x026A */ s16 unk_26A;
     /* 0x026C*/ s16 unk_26C;
-    /* 0x026E*/ char unk_26E[2];
+    /* 0x026E*/ s16 unk_26E;
     /* 0x0270 */ s16 unk_270;
-    /* 0x0272 */ char unk_272[4];
+    /* 0x0272 */ s16 unk_272;
+    /* 0x0274 */ s16 unk_274;
     /* 0x0276 */ s16 unkFlag;
     /* 0x0278 */ char unk_278[0x2];
     /* 0x027A */ s16 unk_27A;
     /* 0x027C */ f32 unk_27C;
-    /* 0x0280 */ char unk_280[4];
+    /* 0x0280 */ f32 unk_280;
     /* 0x0284 */ EnNiw* attachedActor;
     /* 0x0288 */ ColliderCylinder collider;
-    /* 0x02D4 */ char unk_2D4[0x28];
+    /* 0x02D4 */ struct_80034A14_arg1 unk_2D4;
+   // /* 0x02D4 */ char unk_2D4[0x28];
 } EnNiwGirl; // size = 0x02FC
 
 extern const ActorInit En_Niw_Girl_InitVars;
