8ffac8edff803146bf4f908e0f6f759de81f7bd6
Mario <mariottf2@gmail.com>
2020-08-01 19:04:07 +0200

    Decompiles z_bg_ingate
    	* BgIngate_SetupAction
    	* BgIngate_Init
    	* BgIngate_Destroy
    	* BgIngate_Update
    	* func_80892990
    	* func_80892890
    	* BgIngate_Draw
    Removes unused asm's
    Formats z_bg_ingate.c

--- src/overlays/actors/ovl_Bg_Ingate/z_bg_ingate.c
+++ src/overlays/actors/ovl_Bg_Ingate/z_bg_ingate.c
@@ -5,6 +5,8 @@
  */
 
 #include "z_bg_ingate.h"
+#include <ultra64.h>
+#include <global.h>
 
 #define FLAGS 0x00000000
 
@@ -15,10 +17,9 @@ void BgIngate_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgIngate_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgIngate_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-void func_80892890(BgIngate* this, GlobalContext* globalCtx);
+void func_80892890(BgIngate* thisx, GlobalContext* globalCtx);
 void func_80892990(BgIngate* this, GlobalContext* globalCtx);
 
-/*
 const ActorInit Bg_Ingate_InitVars = {
     ACTOR_BG_INGATE,
     ACTORTYPE_PROP,
@@ -30,17 +31,93 @@ const ActorInit Bg_Ingate_InitVars = {
     (ActorFunc)BgIngate_Update,
     (ActorFunc)BgIngate_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/BgIngate_SetupAction.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/BgIngate_Init.s")
+extern UNK_TYPE D_060011B8;
+extern Gfx D_06001040[];
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/BgIngate_Destroy.s")
+void BgIngate_SetupAction(BgIngate* this, BgIngateActionFunc func) {
+    this->actionFunc = func;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/func_80892890.s")
+void BgIngate_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgIngate* this = THIS;
+    s32 pad;
+    s32 sp32 = 0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/func_80892990.s")
+    DynaPolyInfo_SetActorMove(&this->dyna, 0);
+    DynaPolyInfo_Alloc(&D_060011B8, &sp32);
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/BgIngate_Update.s")
+    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp32);
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ingate/BgIngate_Draw.s")
+    if ((globalCtx->sceneNum != 0x63 || gSaveContext.linkAge != 0) ||
+        (((gSaveContext.eventChkInf[1] & 0x100) != 0) && (gSaveContext.cutsceneIndex != 0xFFF0))) {
+        Actor_Kill(&this->dyna.actor);
+        return;
+    }
+
+    Actor_SetScale(&this->dyna.actor, 0.1f);
+    if (((this->dyna.actor.params & 1) != 0) && ((gSaveContext.eventInf[0] & 0xF) == 6)) {
+        globalCtx->csCtx.frames = (u16)0U;
+        BgIngate_SetupAction(this, &func_80892890);
+        return;
+    }
+    BgIngate_SetupAction(this, &func_80892990);
+}
+
+void BgIngate_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgIngate* this = THIS;
+
+    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, (s32)this->dyna.dynaPolyId);
+}
+
+void func_80892890(BgIngate* this, GlobalContext* globalCtx) {
+    s32 phi0;
+    s16 phi1;
+    s16 ctxFrames;
+
+    if (globalCtx->csCtx.frames >= 0x32) {
+        phi0 = 0x4000;
+        if ((this->dyna.actor.params & 2) == 0) {
+            phi0 = -0x4000;
+        }
+        this->dyna.actor.shape.rot.y = this->dyna.actor.posRot.rot.y + phi0;
+        BgIngate_SetupAction(this, &func_80892990);
+    } else if (globalCtx->csCtx.frames >= 0xA) {
+        ctxFrames = globalCtx->csCtx.frames - 0xA;
+        ctxFrames *= 400;
+        phi1 = ctxFrames;
+        if (ctxFrames > 0x4000) {
+            ctxFrames = 0x4000;
+        }
+        ctxFrames = (Math_Sins(ctxFrames) * 16384.0f);
+        phi1 = ctxFrames;
+        if ((this->dyna.actor.params & 2) == 0) {
+            phi1 = -phi1;
+        }
+        this->dyna.actor.shape.rot.y = this->dyna.actor.posRot.rot.y + phi1;
+    }
+}
+
+void func_80892990(BgIngate* this, GlobalContext* globalCtx) {
+    // does nothing
+}
+
+void BgIngate_Update(Actor* thisx, GlobalContext* globalCtx) {
+    BgIngate* this = THIS;
+    this->actionFunc(this, globalCtx);
+}
+
+void BgIngate_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
+    Gfx* dispRefs[4];
+
+    Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_bg_ingate.c", 0xF0);
+
+    func_80093D18(globalCtx->state.gfxCtx);
+
+    gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_ingate.c", 0xF5),
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+
+    gSPDisplayList(gfxCtx->polyOpa.p++, D_06001040);
+    Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_bg_ingate.c", 0xFA);
+}
