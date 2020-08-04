b80c7635c28a3e212a929dd193e2530453028f66
Thar0 <17233964+Thar0@users.noreply.github.com>
2020-08-04 21:29:40 +0100

    Refactor text id declarations

--- src/code/z_message_PAL.c
+++ src/code/z_message_PAL.c
@@ -1,12 +1,42 @@
 #include <ultra64.h>
 #include <global.h>
 
+#include <message_data_static.h>
+
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_message_PAL/const.rodata.s")
-#include "z_message_tables.c"
-extern const MessageTableEntry D_8014B320[];
-extern const char* D_8014F548[];
-extern const char* D_80151658[];
-extern const MessageTableEntry D_80153768[];
+
+#define MESSAGE_ENTRY(textId, type, yPos) DECLARE_MESSAGE_ENTRY(textId, type, yPos, nes),
+#define MESSAGE_TABLE_MAIN
+
+static const MessageTableEntry D_8014B320[] = {
+    #include "text/message_entries.h"
+};
+
+#undef MESSAGE_TABLE_MAIN
+#undef MESSAGE_ENTRY
+#define MESSAGE_ENTRY(textId, type, yPos) MESSAGE_SEGMENT(textId, ger),
+
+static const char* D_8014F548[] = {
+    #include "text/message_entries.h"
+    NULL,
+};
+
+#undef MESSAGE_ENTRY
+#define MESSAGE_ENTRY(textId, type, yPos) MESSAGE_SEGMENT(textId, fra),
+
+static const char* D_80151658[] = {
+    #include "text/message_entries.h"
+    NULL,
+};
+
+#undef MESSAGE_ENTRY
+#define MESSAGE_ENTRY(textId, type, yPos) DECLARE_MESSAGE_ENTRY(textId, type, yPos, staff),
+
+static const MessageTableEntry D_80153768[] = {
+    #include "text/staff_entries.h"
+};
+
+#undef MESSAGE_ENTRY
 
 static const MessageTableEntry* D_801538F0 = D_8014B320;
 static const char* D_801538F4 = D_8014F548;
@@ -14,6 +44,8 @@ static const char* D_801538F8 = D_80151658;
 static const MessageTableEntry* D_801538FC = D_80153768;
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_message_PAL/const2.rodata.s")
+
+
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_message_PAL/func_801069B0.s")
 
