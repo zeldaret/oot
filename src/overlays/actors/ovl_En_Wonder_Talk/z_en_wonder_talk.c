7f62321b8e73742f64fcd7401b7d44ccccb278a8
i82orbom <i82orbom i82orbom@github>
2020-08-09 20:53:42 +0200

    Addresses PR comments
    * Uses VT macros in z_en_wonder_talk
    
    * Adds translations to stdout messages in comments

--- src/overlays/actors/ovl_En_Wonder_Talk/z_en_wonder_talk.c
+++ src/overlays/actors/ovl_En_Wonder_Talk/z_en_wonder_talk.c
@@ -5,6 +5,7 @@
  */
 
 #include "z_en_wonder_talk.h"
+#include <vt.h>
 
 #define FLAGS 0x08000009
 
@@ -36,18 +37,19 @@ void EnWonderTalk_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnWonderTalk* this = THIS;
 
     osSyncPrintf("\n\n");
-    osSyncPrintf("\x1b[32m☆☆☆☆☆ 特殊会話くん ☆☆☆☆☆ %x\n\x1b[m", this->actor.params);
+    // Special conversation
+    osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ 特殊会話くん ☆☆☆☆☆ %x\n" VT_RST, this->actor.params);
 
     this->unk_150 = (this->actor.params >> 0xB) & 0x1F;
     this->unk_152 = (this->actor.params >> 6) & 0x1F;
-    this->unk_154 = this->actor.params & 0x3F;
-    if (this->unk_154 == 0x3F) {
-        this->unk_154 = -1;
+    this->switchFlag = this->actor.params & 0x3F;
+    if (this->switchFlag == 0x3F) {
+        this->switchFlag = -1;
     }
     this->actor.unk_1F = 1;
-    if (this->unk_154 >= 0) {
-        if (Flags_GetSwitch(globalCtx, this->unk_154)) {
-            osSyncPrintf("\x1b[32m☆☆☆☆☆ Ｙｏｕ ａｒｅ Ｓｈｏｃｋ！  ☆☆☆☆☆ %d\n\x1b[m", this->unk_154);
+    if (this->switchFlag >= 0) {
+        if (Flags_GetSwitch(globalCtx, this->switchFlag)) {
+            osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ Ｙｏｕ ａｒｅ Ｓｈｏｃｋ！  ☆☆☆☆☆ %d\n" VT_RST, this->switchFlag);
             Actor_Kill(&this->actor);
             return;
         }
@@ -57,44 +59,52 @@ void EnWonderTalk_Init(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void func_80B391CC(EnWonderTalk* this, GlobalContext* globalCtx) {
-    if (this->unk_154 < 0 || Flags_GetSwitch(globalCtx, this->unk_154) == 0) {
+    if (this->switchFlag < 0 || Flags_GetSwitch(globalCtx, this->switchFlag) == 0) {
         switch (this->unk_150) {
             case 1:
-                osSyncPrintf("\x1b[32m ☆☆☆☆☆ 石板ＧＯ！ ☆☆☆☆☆ \n\x1b[m");
+                // Slate GO!
+                osSyncPrintf(VT_FGCOL(GREEN) " ☆☆☆☆☆ 石板ＧＯ！ ☆☆☆☆☆ \n" VT_RST);
                 this->fHeight = 0.0f;
                 this->unk_15C = 80.0f;
-                osSyncPrintf("\x1b[35m☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n\x1b[m", 0.0f);
+                // Attention coordinates
+                osSyncPrintf(VT_FGCOL(PURPLE) "☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n" VT_RST, 0.0f);
                 if (LINK_IS_CHILD) {
                     this->actor.textId = 0x7040;
-                    osSyncPrintf("\x1b[32m ☆☆☆☆☆ こども ☆☆☆☆☆ \n\x1b[m");
+                    // Children
+                    osSyncPrintf(VT_FGCOL(GREEN) " ☆☆☆☆☆ こども ☆☆☆☆☆ \n" VT_RST);
                 } else {
-                    osSyncPrintf("\x1b[36m ☆☆☆☆☆ おとな ☆☆☆☆☆ \n\x1b[m");
+                    // Adult
+                    osSyncPrintf(VT_FGCOL(CYAN) " ☆☆☆☆☆ おとな ☆☆☆☆☆ \n" VT_RST);
                     this->actor.textId = 0x7088;
                 }
 
                 this->unk_156 = 5;
-                osSyncPrintf("\x1b[36m☆☆☆☆☆ this->actor.talk_message    ☆☆☆☆☆ %x\n\x1b[m", this->actor.textId);
+                osSyncPrintf(VT_FGCOL(CYAN) "☆☆☆☆☆ this->actor.talk_message    ☆☆☆☆☆ %x\n" VT_RST, this->actor.textId);
                 break;
             case 2:
-                osSyncPrintf("\x1b[32m ☆☆☆☆☆ 日記帳スタート！ ☆☆☆☆☆ \n\x1b[m");
+                // Diary start!
+                osSyncPrintf(VT_FGCOL(GREEN) " ☆☆☆☆☆ 日記帳スタート！ ☆☆☆☆☆ \n" VT_RST);
                 this->actor.textId = 0x5002;
                 this->unk_156 = 4;
                 this->fHeight = 30.0f;
                 this->unk_15C = 40.0f;
-                osSyncPrintf("\x1b[35m☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n\x1b[m", 30.0f);
+                // Attention coordinates
+                osSyncPrintf(VT_FGCOL(PURPLE) "☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n" VT_RST, 30.0f);
                 break;
             case 3:
                 this->actor.textId = 0x501E;
                 this->unk_156 = 5;
                 this->fHeight = 0.0f;
                 this->unk_15C = 110.0f;
-                osSyncPrintf("\x1b[35m☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n\x1b[m", 0.0f);
+                // Attention coordinates
+                osSyncPrintf(VT_FGCOL(PURPLE) "☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n" VT_RST, 0.0f);
                 break;
             case 4:
                 this->actor.textId = 0x5020;
                 this->unk_156 = 6;
                 this->fHeight = 0.0f;
-                osSyncPrintf("\x1b[35m☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n\x1b[m", 0.0f);
+                // Attention coordinates
+                osSyncPrintf(VT_FGCOL(PURPLE) "☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n" VT_RST, 0.0f);
                 this->unk_15C = 120.0f;
                 if ((gSaveContext.eventChkInf[1] & 0x2000) != 0) {
                     Actor_Kill(&this->actor);
@@ -105,7 +115,8 @@ void func_80B391CC(EnWonderTalk* this, GlobalContext* globalCtx) {
                 this->unk_156 = 5;
                 this->fHeight = 0.0f;
                 this->unk_15C = 110.0f;
-                osSyncPrintf("\x1b[35m☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n\x1b[m", 0.0f);
+                // Attention coordinates
+                osSyncPrintf(VT_FGCOL(PURPLE) "☆☆☆☆☆ 注目座標\t       \t☆☆☆☆☆ %f\n" VT_RST, 0.0f);
                 break;
             default:
                 this->actor.textId = 0x7072;
@@ -113,43 +124,44 @@ void func_80B391CC(EnWonderTalk* this, GlobalContext* globalCtx) {
                 break;
         }
 
-        osSyncPrintf("\x1b[36m☆☆☆☆☆ this->actor.talk_message    ☆☆☆☆☆ %x\n\x1b[m", this->actor.textId);
+        osSyncPrintf(VT_FGCOL(CYAN) "☆☆☆☆☆ this->actor.talk_message    ☆☆☆☆☆ %x\n" VT_RST, this->actor.textId);
         this->actionFunc = func_80B3943C;
     }
 }
 
 void func_80B3943C(EnWonderTalk* this, GlobalContext* globalCtx) {
     s16 yawDiff;
-    s16 phi_v1;
+    s16 yawDiffTemp;
 
     this->unk_15A++;
     if (this->unk_150 == 4 && (gSaveContext.eventChkInf[1] & 0x2000)) {
         Actor_Kill(&this->actor);
-    } else if ((this->unk_154 < 0) || (Flags_GetSwitch(globalCtx, this->unk_154) == 0)) {
+    } else if ((this->switchFlag < 0) || (Flags_GetSwitch(globalCtx, this->switchFlag) == 0)) {
         if ((func_8002F194(&this->actor, globalCtx))) {
             if (this->unk_156 != 6) {
                 this->actionFunc = func_80B395F0;
             } else {
-                if (this->unk_154 >= 0) {
-                    this->actor.flags &= -2;
-                    Flags_SetSwitch(globalCtx, this->unk_154);
+                if (this->switchFlag >= 0) {
+                    this->actor.flags &= ~1;
+                    Flags_SetSwitch(globalCtx, this->switchFlag);
                 }
                 this->actionFunc = func_80B391CC;
             }
         } else if (!(this->unk_15C < this->actor.xzDistFromLink)) {
-            yawDiff = (this->actor.yawTowardsLink - this->actor.posRot.rot.y);
-            if (yawDiff >= 0) {
-                phi_v1 = yawDiff;
-            } else {
-                phi_v1 = -yawDiff;
-            }
-            if (phi_v1 < 0x4000) {
+            yawDiffTemp = (this->actor.yawTowardsLink - this->actor.posRot.rot.y);
+            yawDiff = ABS(yawDiffTemp);
+        
+            if (yawDiff < 0x4000) {
                 if (this->unk_15A >= 2) {
                     osSyncPrintf("\n\n");
-                    osSyncPrintf("\x1b[33m☆☆☆☆☆ セーブ情報\t\t☆☆☆☆☆ %d\n\x1b[m", this->unk_154);
-                    osSyncPrintf("\x1b[35m☆☆☆☆☆ 種類インデックス\t☆☆☆☆☆ %d\n\x1b[m", this->unk_150);
-                    osSyncPrintf("\x1b[36m☆☆☆☆☆ 実質メッセージ種類     %x\n\x1b[m", this->actor.textId);
-                    osSyncPrintf("\x1b[32m☆☆☆☆☆ 指定範囲               %d\n\x1b[m", this->actor.posRot.rot.z);
+                    // Save information
+                    osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ セーブ情報\t\t☆☆☆☆☆ %d\n" VT_RST, this->switchFlag);
+                    // Type index
+                    osSyncPrintf(VT_FGCOL(PURPLE) "☆☆☆☆☆ 種類インデックス\t☆☆☆☆☆ %d\n" VT_RST, this->unk_150);
+                    // Actual message type
+                    osSyncPrintf(VT_FGCOL(CYAN) "☆☆☆☆☆ 実質メッセージ種類     %x\n" VT_RST, this->actor.textId);
+                    // Specified range
+                    osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ 指定範囲               %d\n" VT_RST, this->actor.posRot.rot.z);
                     osSyncPrintf("\n\n");
                 }
                 this->unk_15A = 0;
@@ -162,9 +174,9 @@ void func_80B3943C(EnWonderTalk* this, GlobalContext* globalCtx) {
 void func_80B395F0(EnWonderTalk* this, GlobalContext* globalCtx) {
     if (this->unk_156 == func_8010BDBC(&globalCtx->msgCtx)) {
         if (func_80106BC8(globalCtx)) {
-            if (this->unk_154 >= 0) {
+            if (this->switchFlag >= 0) {
                 this->actor.flags &= -2;
-                Flags_SetSwitch(globalCtx, this->unk_154);
+                Flags_SetSwitch(globalCtx, this->switchFlag);
             }
             switch (this->unk_150) {
                 case 1:
@@ -176,17 +188,17 @@ void func_80B395F0(EnWonderTalk* this, GlobalContext* globalCtx) {
                         case 0:
                             if (LINK_IS_CHILD) {
                                 // I'm still a child!
-                                osSyncPrintf("\x1b[32m ☆☆☆☆☆ まだコドモなの！ ☆☆☆☆☆ \n\x1b[m");
+                                osSyncPrintf(VT_FGCOL(GREEN) " ☆☆☆☆☆ まだコドモなの！ ☆☆☆☆☆ \n" VT_RST);
                                 this->actor.textId = 0x5001;
                             } else {
                                 // I'm an adult. .. ..
-                                osSyncPrintf("\x1b[33m ☆☆☆☆☆ アダルトなの。。。 ☆☆☆☆☆ \n\x1b[m");
+                                osSyncPrintf(VT_FGCOL(YELLOW) " ☆☆☆☆☆ アダルトなの。。。 ☆☆☆☆☆ \n" VT_RST);
                                 this->actor.textId = 0x5003;
                             }
                             break;
                         case 1:
                             // Out!
-                            osSyncPrintf("\x1b[35m ☆☆☆☆☆ はずれ！ ☆☆☆☆☆ \n\x1b[m");
+                            osSyncPrintf(VT_FGCOL(PURPLE) " ☆☆☆☆☆ はずれ！ ☆☆☆☆☆ \n" VT_RST);
                             this->actor.textId = 0x5004U;
                             break;
                     }
@@ -198,7 +210,7 @@ void func_80B395F0(EnWonderTalk* this, GlobalContext* globalCtx) {
                 case 3:
                     func_80106CCC(globalCtx);
                     if (this->unk_164 == 0) {
-                        Actor_Spawn(&globalCtx->actorCtx, globalCtx, 0xD, this->actor.posRot.pos.x,
+                        Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_POH, this->actor.posRot.pos.x,
                                     this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0, 0, 0, 2);
                         this->unk_164 = 1;
                     }
@@ -208,7 +220,7 @@ void func_80B395F0(EnWonderTalk* this, GlobalContext* globalCtx) {
                 case 5:
                     func_80106CCC(globalCtx);
                     if (this->unk_164 == 0) {
-                        Actor_Spawn(&globalCtx->actorCtx, globalCtx, 0xD, this->actor.posRot.pos.x,
+                        Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_POH, this->actor.posRot.pos.x,
                                     this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0, 0, 0, 3);
                         this->unk_164 = 1;
                     }
@@ -233,12 +245,12 @@ void EnWonderTalk_Update(Actor* thisx, GlobalContext* globalCtx) {
             if ((this->unk_15A & 1) == 0) {
                 DebugDisplay_AddObject(this->actor.posRot.pos.x, this->actor.posRot.pos.y, this->actor.posRot.pos.z,
                                        this->actor.posRot.rot.x, this->actor.posRot.rot.y, this->actor.posRot.rot.z,
-                                       1.0f, 1.0f, 1.0f, 0xA, 0xA, 0xA, 0xFF, 4, globalCtx->state.gfxCtx);
+                                       1.0f, 1.0f, 1.0f, 10, 10, 10, 255, 4, globalCtx->state.gfxCtx);
             }
         } else {
             DebugDisplay_AddObject(this->actor.posRot.pos.x, this->actor.posRot.pos.y, this->actor.posRot.pos.z,
                                    this->actor.posRot.rot.x, this->actor.posRot.rot.y, this->actor.posRot.rot.z, 1.0f,
-                                   1.0f, 1.0f, 0, 0xFF, 0, 0xFF, 4, globalCtx->state.gfxCtx);
+                                   1.0f, 1.0f, 0, 255, 0, 255, 4, globalCtx->state.gfxCtx);
         }
     }
 }
