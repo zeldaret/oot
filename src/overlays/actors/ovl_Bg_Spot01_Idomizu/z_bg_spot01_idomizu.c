3d0101035ece32dc34a2be79cba1b42e94fbe15e
tom-overton <tom.overton@outlook.com>
2020-08-09 19:23:16 -0700

    Address review feedback

--- src/overlays/actors/ovl_Bg_Spot01_Idomizu/z_bg_spot01_idomizu.c
+++ src/overlays/actors/ovl_Bg_Spot01_Idomizu/z_bg_spot01_idomizu.c
@@ -37,8 +37,9 @@ extern Gfx D_060007D0[];
 
 void BgSpot01Idomizu_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgSpot01Idomizu* this = THIS;
+
     Actor_ProcessInitChain(&this->actor, sInitChain);
-    if ((gSaveContext.eventChkInf[6] & 0x80) != 0 || LINK_AGE_IN_YEARS == YEARS_ADULT) {
+    if (gSaveContext.eventChkInf[6] & 0x80 || LINK_AGE_IN_YEARS == YEARS_ADULT) {
         this->waterHeight = -550.0f;
     } else {
         this->waterHeight = 52.0f;
@@ -51,12 +52,12 @@ void BgSpot01Idomizu_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void func_808ABB84(BgSpot01Idomizu* this, GlobalContext* globalCtx) {
-    if ((gSaveContext.eventChkInf[6] & 0x80) != 0) {
+    if (gSaveContext.eventChkInf[6] & 0x80) {
         this->waterHeight = -550.0f;
     }
     globalCtx->colCtx.stat.colHeader->waterBoxes[0].unk_02 = this->actor.posRot.pos.y;
     if (this->waterHeight < this->actor.posRot.pos.y) {
-        Audio_PlaySoundGeneral(NA_SE_EV_WATER_LEVEL_DOWN - SFX_FLAG, &D_801333D4, 4U, &D_801333E0, &D_801333E0,
+        Audio_PlaySoundGeneral(NA_SE_EV_WATER_LEVEL_DOWN - SFX_FLAG, &D_801333D4, 4, &D_801333E0, &D_801333E0,
                                &D_801333E8);
     }
     Math_SmoothScaleMaxF(&this->actor.posRot.pos.y, this->waterHeight, 1.0f, 2.0f);
@@ -64,6 +65,7 @@ void func_808ABB84(BgSpot01Idomizu* this, GlobalContext* globalCtx) {
 
 void BgSpot01Idomizu_Update(Actor* thisx, GlobalContext* globalCtx) {
     BgSpot01Idomizu* this = THIS;
+
     this->actionFunc(this, globalCtx);
 }
 
