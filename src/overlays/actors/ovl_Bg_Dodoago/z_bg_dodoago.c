287ac1fbf507fa327d77d33e570e0e5f993818a7
i82orbom <i82orbom i82orbom@github>
2020-08-11 13:49:17 +0200

    * func_80871A08

--- src/overlays/actors/ovl_Bg_Dodoago/z_bg_dodoago.c
+++ src/overlays/actors/ovl_Bg_Dodoago/z_bg_dodoago.c
@@ -16,7 +16,7 @@ void BgDodoago_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgDodoago_Draw(Actor* thisx, GlobalContext* globalCtx);
 void BgDodoago_SetupAction(BgDodoago* this, BgDodoagoActionFunc actionFunc);
 
-void func_80871A08(Vec3f* vec, GlobalContext* globalCtx); // Not 100% sure
+void func_80871A08(Vec3f* vec, GlobalContext* globalCtx);
 void func_80871CF4(BgDodoago* this, GlobalContext* globalCtx);
 void func_80871FB8(BgDodoago* this, GlobalContext* globalCtx);
 void func_8087227C(BgDodoago* this, GlobalContext* globalCtx);
@@ -39,10 +39,16 @@ s32 D_80872540[] = { 0x0A003900, 0x00010000, 0x02000000, 0x00000000, 0x00000000,
 s32 D_8087256C[] = { 0x0A00003D, 0x20010000, 0x02000000, 0x00000000, 0x00000000, 0x00000000,
                      0x00000000, 0x00000100, 0x0032003C, 0x01180000, 0x00000000 };
 s32 D_80872598 = 0x00000000;
-Color_RGB8 D_8087259C = { 100, 100, 100 };
-Color_RGB8 D_808725A0 = { 40, 40, 40 };
-s32 D_808725A4[] = { 0x00000000, 0xBFC00000, 0x00000000 };
-s32 D_808725B0[] = { 0x00000000, 0xBE4CCCCD, 0x00000000 };
+
+Color_RGBA8_n D_8087259C = { 100, 100, 100 };
+Color_RGBA8_n D_808725A0 = { 40, 40, 40 };
+
+Vec3f VELOCITY = { 0.0f, -1.5f, 0.0f};
+Vec3f ACCELERATION = {0.0f, -0.20000000298023223876953125f, 0.0f};
+
+// s32 velocity[] = { 0x00000000, 0xBFC00000, 0x00000000 };
+// s32 acceleration[] = { 0x00000000, 0xBE4CCCCD, 0x00000000 };
+
 s32 D_808725BC[] = { 0xC8500064, 0xB0F41388, 0xB0F803E8, 0x30FC0320 };
 s32 D_808725CC[] = { 0x00000000, 0xC3480000, 0x43D70000, 0x41A00000, 0xC3480000, 0x43D20000, 0xC1A00000,
                      0xC3480000, 0x43D20000, 0x42200000, 0xC3480000, 0x43BE0000, 0xC2200000, 0xC3480000,
@@ -61,20 +67,19 @@ void BgDodoago_SetupAction(BgDodoago* this, BgDodoagoActionFunc actionFunc) {
     this->actionFunc = actionFunc;
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Dodoago/func_80871A08.s")
-// void func_80871A08(Vec3f* vec, GlobalContext* param_2) {
-//     Vec3f tempVec;
-//     s32 counter;
-//     Color_RGB8 color1 = D_8087259C;
-//     Color_RGB8 color2 = D_808725A0;
+void func_80871A08(Vec3f* vec, GlobalContext* globalCtx) {
+    Vec3f currentPosition;
+    Color_RGBA8_n primColor = D_8087259C;
+    Color_RGBA8_n envColor = D_808725A0;
+    s32 i;
 
-//     for (counter = 4; counter != 0; counter--) {
-//         tempVec.x = Math_Rand_CenteredFloat(20.0f) + vec->x;
-//         tempVec.y = Math_Rand_CenteredFloat(10.0f) + vec->y;
-//         tempVec.z = Math_Rand_CenteredFloat(20.0f) + vec->z;
-//         func_80028B74(param_2, &tempVec, &D_808725A4, &D_808725B0, &color1, &color2);
-//     }
-// }
+    for (i = 4; i > 0; i--) {
+        currentPosition.x = Math_Rand_CenteredFloat(20.0f) + vec->x;
+        currentPosition.y = Math_Rand_CenteredFloat(10.0f) + vec->y;
+        currentPosition.z = Math_Rand_CenteredFloat(20.0f) + vec->z;
+        func_80028B74(globalCtx, &currentPosition, &VELOCITY, &ACCELERATION, &primColor, &envColor);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Dodoago/BgDodoago_Init.s")
 // void BgDodoago_Init(BgDodoago *this, GlobalContext *globalCtx) {
@@ -114,7 +119,6 @@ void BgDodoago_SetupAction(BgDodoago* this, BgDodoagoActionFunc actionFunc) {
 //     D_808727C0 = (u8)0;
 // }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Dodoago/BgDodoago_Destroy.s")
 void BgDodoago_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgDodoago* this = THIS;
 
