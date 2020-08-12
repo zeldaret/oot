0ac78f03c1cc442041c595474d4068c891ab513a
gns <adamkiddle23@gmail.com>
2020-08-12 01:43:59 +0100

    Formatting changes, not currently building

--- src/overlays/actors/ovl_Elf_Msg2/z_elf_msg2.c
+++ src/overlays/actors/ovl_Elf_Msg2/z_elf_msg2.c
@@ -1,3 +1,9 @@
+/*
+ * File: z_elf_msg2.c
+ * Overlay: ovl_Elf_Msg2
+ * Description: Targetable navi check spot
+ */
+
 #include "z_elf_msg2.h"
 #include "vt.h"
 
@@ -9,9 +15,9 @@ void ElfMsg2_Init(Actor* thisx, GlobalContext* globalCtx);
 void ElfMsg2_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ElfMsg2_Update(Actor* thisx, GlobalContext* globalCtx);
 void ElfMsg2_Draw(Actor* thisx, GlobalContext* globalCtx);
-s32 func_809AD968(ElfMsg2 *this);
-void func_809ADA28(ElfMsg2* this, GlobalContext* globalCtx);
-void func_809AD9F4(ElfMsg2* this, GlobalContext* globalCtx);
+s32 ElfMsg2_GetMessageId(ElfMsg2 *this);
+void ElfMsg2_TextInit(ElfMsg2* this, GlobalContext* globalCtx);
+void ElfMsg2_CheckForRead(ElfMsg2* this, GlobalContext* globalCtx);
 
 
 const ActorInit Elf_Msg2_InitVars = {
@@ -26,118 +32,10 @@ const ActorInit Elf_Msg2_InitVars = {
     (ActorFunc)ElfMsg2_Draw,
 };
 
-// ElfMsg2_SetupAction
-void func_809AD700(ElfMsg2 *this, ElfMsg2ActionFunc actionFunc) {
-    this->actionFunc = actionFunc;
-}
-
-s32 func_809AD708(ElfMsg2* this, GlobalContext* globalCtx) {
-
-    if ((this->actor.posRot.rot.y > 0) && (this->actor.posRot.rot.y < 0x41) &&
-        Flags_GetSwitch(globalCtx, this->actor.posRot.rot.y - 1)) {
-        LOG_STRING("共倒れ","../z_elf_msg2.c",171);
-        if (((this->actor.params >> 8) & 0x3F) != 0x3F) {
-            Flags_SetSwitch(globalCtx, (this->actor.params >> 8) & 0x3F);
-        }
-
-        Actor_Kill(&this->actor);
-        return 1;
-    }
-
-    if ((this->actor.posRot.rot.y == -1) && Flags_GetClear(globalCtx, this->actor.room)) {
-        LOG_STRING("共倒れ２","../z_elf_msg2.c",182);
-        if (((this->actor.params >> 8) & 0x3F) != 0x3F) {
-            Flags_SetSwitch(globalCtx, (this->actor.params >> 8) & 0x3F);
-        }
-
-        Actor_Kill(&this->actor);
-        return 1;
-    }
-
-    if (((this->actor.params >> 8) & 0x3F) == 0x3F) {
-        return 0;
-    } else if (Flags_GetSwitch(globalCtx, (this->actor.params >> 8) & 0x3F)) {
-        LOG_STRING("共倒れ","../z_elf_msg2.c",192);
-        Actor_Kill(&this->actor);
-        return 1;
-    }             
-    return 0;
-}
-
 static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale.x, 200, ICHAIN_CONTINUE),
     ICHAIN_F32(uncullZoneForward, 1000, ICHAIN_STOP),
 };
-
-void ElfMsg2_Init(Actor* thisx, GlobalContext* globalCtx) {
-    ElfMsg2* this = THIS;
-    s32 flags;
-
-    osSyncPrintf(VT_FGCOL(CYAN) " Elf_Msg2_Actor_ct %04x\n\n" VT_RST, this->actor.params);
-    if (!func_809AD708(this, globalCtx)) {
-        if (0 < this->actor.posRot.rot.x && this->actor.posRot.rot.x < 8) {
-            this->actor.unk_1F = (this->actor.posRot.rot.x - 1);
-        }
-        Actor_ProcessInitChain(thisx, sInitChain);
-        if (this->actor.posRot.rot.y >= 0x41) {
-            func_809AD700(this, &func_809ADA28);
-        } else {
-            func_809AD700(this, &func_809AD9F4);
-            this->actor.flags = (this->actor.flags | 0x40001);
-            this->actor.textId = func_809AD968(this);
-        }
-        this->actor.shape.rot.z = 0;
-        this->actor.shape.rot.y = this->actor.shape.rot.z;
-        this->actor.shape.rot.x = this->actor.shape.rot.z;
-    }
-}
-
-void ElfMsg2_Destroy(Actor* thisx, GlobalContext* globalCtx){
-
-}
-
-s32 func_809AD968(ElfMsg2 *this) {
-    return (this->actor.params & 0xFF) + 0x100;
-}
-
-void func_809AD978(ElfMsg2 *this, GlobalContext *globalCtx) {
-    s32 temp_a1;
-    if (func_8002F334(&this->actor, globalCtx) != 0) {
-        if (this->actor.posRot.rot.z != 1) {
-            Actor_Kill(&this->actor);
-            temp_a1 = (this->actor.params >> 8) & 0x3F;
-            if (temp_a1 != 0x3F) {
-                Flags_SetSwitch(globalCtx, temp_a1);
-            }
-            return;
-        }
-        func_809AD700(this, &func_809AD9F4);
-    }
-}
-
-void func_809AD9F4(ElfMsg2 *this, GlobalContext *globalCtx) {
-    if (func_8002F194(&this->actor, globalCtx)) {
-        func_809AD700(this, &func_809AD978);
-    }
-}
-
-void func_809ADA28(ElfMsg2* this, GlobalContext* globalCtx) {
-    s32 roty = this->actor.posRot.rot.y;
-    if (0x41 <= roty && roty < 0x81) {
-        if (Flags_GetSwitch(globalCtx, roty - 0x41) != 0) {
-            func_809AD700(this, &func_809AD9F4);
-            this->actor.flags = this->actor.flags | 0x40001;
-            this->actor.textId = func_809AD968(this);
-        }
-    }
-}
-
-void ElfMsg2_Update(Actor* thisx, GlobalContext* globalCtx) {
-    ElfMsg2* this = THIS;
-    if (!func_809AD708(this, globalCtx)) {
-        this->actionFunc(this, globalCtx);
-    }
-}
 
 Gfx D_809ADC38[] = {
     gsDPPipeSync(),
@@ -171,6 +69,116 @@ Gfx D_809ADCF8[] = {
     gsSP2Triangles(3, 0, 7, 0, 0, 7, 4, 0),
     gsSPEndDisplayList(),
 };
+
+void ElfMsg2_SetupAction(ElfMsg2 *this, ElfMsg2ActionFunc actionFunc) {
+    this->actionFunc = actionFunc;
+}
+
+/**
+ * Checks a scene flag and kills the actor if set. Can also set a switch flag from rot.y or params.
+ */
+s32 ElfMsg2_KillCheck(ElfMsg2* this, GlobalContext* globalCtx) {
+    // Checking a switch or temp switch flag (from rot.y):
+    if (0x00 < this->actor.posRot.rot.y && this->actor.posRot.rot.y < 0x41 && Flags_GetSwitch(globalCtx, this->actor.posRot.rot.y - 1)) {
+        LOG_STRING("共倒れ","../z_elf_msg2.c",171);
+        if ((this->actor.params >> 8 & 0x3F) != 0x3F) {
+            Flags_SetSwitch(globalCtx, (this->actor.params >> 8 & 0x3F));
+        }
+        Actor_Kill(&this->actor);
+        return 1;
+    }
+    // Checking a room clear flag:
+    else if ((this->actor.posRot.rot.y == -1) && Flags_GetClear(globalCtx, this->actor.room)) {
+        LOG_STRING("共倒れ２","../z_elf_msg2.c",182);
+        if ((this->actor.params >> 8 & 0x3F) != 0x3F) {
+            Flags_SetSwitch(globalCtx, (this->actor.params >> 8 & 0x3F));
+        }
+        Actor_Kill(&this->actor);
+        return 1;
+    }
+    else if ((this->actor.params >> 8 & 0x3F) == 0x3F) {
+        return 0;
+    }
+    // Checking a switch or temp switch flag (from params):
+    else if (Flags_GetSwitch(globalCtx, (this->actor.params >> 8 & 0x3F))) {
+        LOG_STRING("共倒れ","../z_elf_msg2.c",192);
+        Actor_Kill(&this->actor);
+        return 1;
+    }             
+    return 0;
+}
+
+void ElfMsg2_Init(Actor* thisx, GlobalContext* globalCtx) {
+    ElfMsg2* this = THIS;
+
+    osSyncPrintf(VT_FGCOL(CYAN) " Elf_Msg2_Actor_ct %04x\n\n" VT_RST, this->actor.params);
+    if (!ElfMsg2_KillCheck(this, globalCtx)) {
+        if (0 < this->actor.posRot.rot.x && this->actor.posRot.rot.x < 8) {
+            this->actor.unk_1F = (this->actor.posRot.rot.x - 1);
+        }
+        Actor_ProcessInitChain(thisx, sInitChain);
+        if (this->actor.posRot.rot.y >= 0x41) {
+            ElfMsg2_SetupAction(this, ElfMsg2_TextInit);
+        } else {
+            ElfMsg2_SetupAction(this, ElfMsg2_CheckForRead);
+            this->actor.flags = (this->actor.flags | 0x00040001);
+            this->actor.textId = ElfMsg2_GetMessageId(this);
+        }
+        this->actor.shape.rot.x = this->actor.shape.rot.y = this->actor.shape.rot.z = 0x0000;
+    }
+}
+
+void ElfMsg2_Destroy(Actor* thisx, GlobalContext* globalCtx){}
+
+s32 ElfMsg2_GetMessageId(ElfMsg2 *this) {
+    return (this->actor.params & 0xFF) + 0x100;
+}
+
+/**
+ * Runs while navi text is up. Handles the killing of the actor upon closing the text box, can also set a switch flag from params.
+ */
+void ElfMsg2_Read(ElfMsg2 *this, GlobalContext *globalCtx) {
+    s32 switchFlag;
+    
+    // If the text box is closing this frame:
+    if (func_8002F334(&this->actor, globalCtx)) {
+        if (this->actor.posRot.rot.z != 1) {
+            Actor_Kill(&this->actor);
+            switchFlag = (this->actor.params >> 8) & 0x3F;
+            if (switchFlag != 0x3F) {
+                Flags_SetSwitch(globalCtx, switchFlag);
+            }
+            return;
+        }
+        ElfMsg2_SetupAction(this, ElfMsg2_CheckForRead);
+    }
+}
+
+/**
+ * Runs while navi text is not up. If text box flag is set, unset it and proceed to ElfMsg2_Read 
+ */
+void ElfMsg2_CheckForRead(ElfMsg2 *this, GlobalContext *globalCtx) {
+    if (func_8002F194(&this->actor, globalCtx)) {
+        ElfMsg2_SetupAction(this, ElfMsg2_Read);
+    }
+}
+
+void ElfMsg2_TextInit(ElfMsg2* this, GlobalContext* globalCtx) {
+    if (0x41 <= this->actor.posRot.rot.y && this->actor.posRot.rot.y < 0x81) {
+        if (Flags_GetSwitch(globalCtx, this->actor.posRot.rot.y - 0x41)) {
+            ElfMsg2_SetupAction(this, ElfMsg2_CheckForRead);
+            this->actor.flags = this->actor.flags | 0x00040001;
+            this->actor.textId = ElfMsg2_GetMessageId(this);
+        }
+    }
+}
+
+void ElfMsg2_Update(Actor* thisx, GlobalContext* globalCtx) {
+    ElfMsg2* this = THIS;
+    if (!ElfMsg2_KillCheck(this, globalCtx)) {
+        this->actionFunc(this, globalCtx);
+    }
+}
 
 void ElfMsg2_Draw(Actor* thisx, GlobalContext* globalCtx) {
     GraphicsContext *gfxCtx;
