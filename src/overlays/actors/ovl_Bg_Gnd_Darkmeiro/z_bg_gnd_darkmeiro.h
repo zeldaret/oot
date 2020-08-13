b708801fae225f0fa5f4349f4484db8c24926bc9
petrie911 <petrie911@yahoo.com>
2020-08-13 18:51:52 -0500

    Now with better formatting

--- src/overlays/actors/ovl_Bg_Gnd_Darkmeiro/z_bg_gnd_darkmeiro.h
+++ src/overlays/actors/ovl_Bg_Gnd_Darkmeiro/z_bg_gnd_darkmeiro.h
@@ -10,10 +10,12 @@ typedef void (*BgGndDarkmeiroActionFunc)(struct BgGndDarkmeiro*, GlobalContext*)
 
 typedef struct BgGndDarkmeiro {
     /* 0x0000 */ DynaPolyActor dyna;
-    /* 0x0164 */ u16 actorState; // Bitflags for whether a platform or timer is on. 0x8 for timer 2, 0x4 for timer 1, 0x2 for whether a platform is on.
-	/* 0x0166 */ s16 timer1; //Timer for switch flag N+1. Platforms use it to count down to vanishing after the switch flag is unset.
-	/* 0x0168 */ s16 timer2; //Timer for switch flag N+2. Platforms use it to control their transparency (does not work).
-	/* 0x016A */ s16 unk16A; //unused
+    /* 0x0164 */ u16 actorState;     /* Bitflags for whether a platform or timer is on.
+                                        0x8 for timer 2, 0x4 for timer 1, 0x2 for platforms.*/
+    /* 0x0166 */ s16 timer1;         /* Timer for switch flag N+1. Platforms use it to count 
+                                        down to vanishing after the switch flag is unset.   */
+    /* 0x0168 */ s16 timer2;         /* Timer for switch flag N+2. Platforms use it to
+                                        control their transparency (does not work).         */
     /* 0x016C */ BgGndDarkmeiroActionFunc actionFunc;
 } BgGndDarkmeiro; // size = 0x0170
 
