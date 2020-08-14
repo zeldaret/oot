5527a956a5ab7f5debd2fe95fdae48e52c6be663
fullgrowngaming <fullgrowngaming@gmail.com>
2020-08-14 14:22:22 -0700

    Changed unk_152 to unsigned to get rid of cast

--- src/overlays/actors/ovl_En_Yukabyun/z_en_yukabyun.c
+++ src/overlays/actors/ovl_En_Yukabyun/z_en_yukabyun.c
@@ -140,7 +140,7 @@ void EnYukabyun_Draw(Actor* thisx, GlobalContext* globalCtx) {
 
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_yukabyun.c", 366);
     func_80093D18(globalCtx->state.gfxCtx);
-    gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(D_80B43F64[(u8)this->unk_152]));
+    gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(D_80B43F64[this->unk_152]));
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_yukabyun.c", 373),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     gSPDisplayList(gfxCtx->polyOpa.p++, D_06000970);
--- src/overlays/actors/ovl_En_Yukabyun/z_en_yukabyun.h
+++ src/overlays/actors/ovl_En_Yukabyun/z_en_yukabyun.h
@@ -12,7 +12,7 @@ typedef struct EnYukabyun {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ EnYukabyunActionFunc actionfunc;
     /* 0x0150 */ s16 unk_150;
-    /* 0x0152 */ s8 unk_152;
+    /* 0x0152 */ u8 unk_152;
     /* 0x0154 */ ColliderCylinder collider;
 } EnYukabyun; // size = 0x01A0
 
