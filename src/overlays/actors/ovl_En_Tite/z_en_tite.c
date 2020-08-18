5ef331c43b993af43e2879aff282f7cc9ff20f2d
gns <adamkiddle23@gmail.com>
2020-08-18 23:53:18 +0100

    EnTite_Update matching

--- src/overlays/actors/ovl_En_Tite/z_en_tite.c
+++ src/overlays/actors/ovl_En_Tite/z_en_tite.c
@@ -15,7 +15,7 @@ void EnTite_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnTite_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnTite_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+
 const ActorInit En_Tite_InitVars = {
     ACTOR_EN_TITE,
     ACTORTYPE_ENEMY,
@@ -27,8 +27,11 @@ const ActorInit En_Tite_InitVars = {
     (ActorFunc)EnTite_Update,
     (ActorFunc)EnTite_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Tite/func_80B18A80.s")
+
+//EnTite_SetupAction
+void func_80B18A80(EnTite* this, EnTiteActionFunc actionFunc) {
+    this->actionFunc = actionFunc;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Tite/EnTite_Init.s")
 
@@ -74,7 +77,83 @@ const ActorInit En_Tite_InitVars = {
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Tite/func_80B1ABBC.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Tite/EnTite_Update.s")
+u8 func_80B1ABBC(Actor *thisx, GlobalContext *globalCtx);
+s32 func_80042244(GlobalContext* globalCtx, CollisionContext* colCtx, f32 x, f32 z, f32* ySurface,
+                  WaterBox** outWaterBox);
+// waterbox:
+//   /* 0x00 */ s16 xMin;
+//   /* 0x02 */ s16 ySurface;
+//   /* 0x04 */ s16 zMin;
+//   /* 0x06 */ s16 xLength;
+//   /* 0x08 */ s16 zLength;
+//   /* 0x0C */ u32 properties;
+
+//func_80029444(GlobalContext *globalCtxt, Vec3f *vec, s32 arg2, s32 arg3, s32 arg4);
+
+void EnTite_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnTite* this = THIS;
+    char pad[0x4];
+    CollisionPoly *sp3C;
+    WaterBox *sp38;    
+    f32 sp34;
+    
+
+    func_80B1ABBC(thisx, globalCtx);
+    if (thisx->colChkInfo.damageEffect != 0xE) {
+        this->actionFunc(this, globalCtx);
+        Actor_MoveForward(thisx);
+        func_8002E4B4(globalCtx, thisx, 25.0f, 40.0f, 20.0f, this->unk2DC);
+        if ((thisx->params == -2) && ((thisx->bgCheckFlags & 0x20) != 0)) {
+            sp3C = thisx->floorPoly;
+            if ((((globalCtx->gameplayFrames & 7) == 0) || (thisx->velocity.y < 0.0f)) && 
+                (func_80042244(globalCtx, &globalCtx->colCtx, this->unk360.x, this->unk360.z, &sp34, &sp38)) &&
+                (this->unk360.y <= sp34)) {
+                this->unk360.y = sp34;
+                func_80029444(globalCtx, &this->unk360, 0, 0xDC, 0);
+            }
+            if (((((globalCtx->gameplayFrames + 2) & 7) == 0) || (thisx->velocity.y < 0.0f)) &&
+                (func_80042244(globalCtx, &globalCtx->colCtx, this->unk36C.x, this->unk36C.z, &sp34, &sp38)) &&
+                (this->unk36C.y <= sp34)) {
+                this->unk36C.y = sp34;
+                func_80029444(globalCtx, &this->unk36C, 0, 0xDC, 0);
+            }
+            if (((((globalCtx->gameplayFrames + 4) & 7) == 0) || (thisx->velocity.y < 0.0f)) &&
+                (func_80042244(globalCtx, &globalCtx->colCtx, this->unk348.x, this->unk348.z, &sp34, &sp38)) &&
+                (this->unk348.y <= sp34)) {
+                this->unk348.y = sp34;
+                func_80029444(globalCtx, &this->unk348, 0, 0xDC, 0);
+            }
+            if (((((globalCtx->gameplayFrames + 1) & 7) == 0) || (thisx->velocity.y < 0.0f)) &&
+                (func_80042244(globalCtx, &globalCtx->colCtx, this->unk354.x, this->unk354.z, &sp34, &sp38)) &&
+                (this->unk354.y <= sp34)) {
+                this->unk354.y = sp34;
+                func_80029444(globalCtx, &this->unk354, 0, 0xDC, 0);
+            }
+            thisx->floorPoly = sp3C;
+        
+        }
+        if (thisx->bgCheckFlags & 3) {
+            func_800359B8(thisx, thisx->shape.rot.y, &thisx->shape.rot);
+            if ((u8)this->unk2BD >= 2) {
+                thisx->shape.rot.z = (thisx->shape.rot.z + 0x7FFF);
+            }
+        } else {
+            Math_SmoothScaleMaxMinS(&thisx->shape.rot.x, 0, 1, 0x3E8, 0);
+            if ((u8)this->unk2BD < 2) {
+                Math_SmoothScaleMaxMinS(&thisx->shape.rot.z, 0, 1, 0x3E8, 0);
+                if (0.0f < thisx->shape.unk_08) {
+                    thisx->shape.unk_08 -= 400.0f;
+                }
+            }
+        }
+    }
+    thisx->posRot2.pos = thisx->posRot.pos;
+    thisx->posRot2.pos.y += 20.0f;
+
+    CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &this->unk2E8);
+    CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->unk2E8);
+}
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Tite/EnTite_Update.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Tite/func_80B1B178.s")
 
--- src/overlays/actors/ovl_En_Tite/z_en_tite.h
+++ src/overlays/actors/ovl_En_Tite/z_en_tite.h
@@ -6,9 +6,23 @@
 
 struct EnTite;
 
+typedef void (*EnTiteActionFunc)(struct EnTite*, GlobalContext*);
+
 typedef struct EnTite {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x22C];
+    /* 0x014C */ char unk_14C[0x171];
+    /* 0x02BD */ u8 unk2BD;
+    /* 0x02BE */ char unk_2BE[0x2];
+    /* 0x02C0 */ EnTiteActionFunc actionFunc;
+    /* 0x02C4 */ char unk_0x24C[0x18];
+    /* 0x02DC */ s32 unk2DC;
+    /* 0x02E0 */ char unk_2C4[0x8];
+    /* 0x02E8 */ Collider* unk2E8;
+    /* 0x02EC */ char unk_2EC[0x5C];
+    /* 0x0348 */ Vec3f unk348;
+    /* 0x0354 */ Vec3f unk354;
+    /* 0x0360 */ Vec3f unk360;
+    /* 0x036C */ Vec3f unk36C;
 } EnTite; // size = 0x0378
 
 extern const ActorInit En_Tite_InitVars;
