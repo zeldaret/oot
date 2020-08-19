276ae48e7ef708aa39b3c7f0b9a09097fb65ebe1
fullgrowngaming <fullgrowngaming@gmail.com>
2020-08-19 15:14:20 -0700

    Made all requested changes to z_en_weather_tag

--- src/overlays/actors/ovl_En_Weather_Tag/z_en_weather_tag.c
+++ src/overlays/actors/ovl_En_Weather_Tag/z_en_weather_tag.c
@@ -43,7 +43,7 @@ const ActorInit En_Weather_Tag_InitVars = {
     NULL,
 };
 
-void func_80B31020(EnWeatherTag* this, EnWeatherTagActionFunc actionfunc) {
+void EnWeatherTag_SetupAction(EnWeatherTag* this, EnWeatherTagActionFunc actionfunc) {
     this->actionfunc = actionfunc;
 }
 
@@ -61,7 +61,7 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
             osSyncPrintf("\n\n");
             //"☆☆☆☆☆ (;o;) About ☆☆☆☆☆☆"
             osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ （;o;) くらいよー ☆☆☆☆☆ \n" VT_RST);
-            func_80B31020(this, func_80B31658);
+            EnWeatherTag_SetupAction(this, func_80B31658);
             break;
         case 1:
             osSyncPrintf("\n\n");
@@ -70,7 +70,7 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
             if (Flags_GetEventChkInf(0x18)) {
                 Actor_Kill(&this->actor);
             }
-            func_80B31020(this, func_80B31700);
+            EnWeatherTag_SetupAction(this, func_80B31700);
             break;
         case 2:
             osSyncPrintf("\n\n");
@@ -80,7 +80,7 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
             if (gSaveContext.eventChkInf[4] & 0x400) {
                 Actor_Kill(&this->actor);
             }
-            func_80B31020(this, func_80B31850);
+            EnWeatherTag_SetupAction(this, func_80B31850);
             break;
         case 3:
             osSyncPrintf("\n\n");
@@ -90,7 +90,7 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
             if (gSaveContext.eventChkInf[4] & 0x400) {
                 Actor_Kill(&this->actor);
             }
-            func_80B31020(this, func_80B31924);
+            EnWeatherTag_SetupAction(this, func_80B31924);
             break;
         case 4:
             osSyncPrintf("\n\n");
@@ -99,7 +99,7 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
             if (gSaveContext.eventChkInf[4] & 0x200) {
                 Actor_Kill(&this->actor);
             }
-            func_80B31020(this, func_80B317A8);
+            EnWeatherTag_SetupAction(this, func_80B317A8);
             break;
         case 5:
             osSyncPrintf("\n\n");
@@ -107,24 +107,24 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
             osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ くもり雨雷 (;O;) うおお 憂鬱 ☆☆☆☆☆ \n" VT_RST);
 
             temp_v0 = gSaveContext.eventChkInf[4];
-            if (((((temp_v0 & 0x100) == 0) || ((temp_v0 & 0x200) == 0)) || ((temp_v0 & 0x400) == 0)) ||
+            if ((((temp_v0 & 0x100) == 0) || ((temp_v0 & 0x200) == 0) || ((temp_v0 & 0x400) == 0)) ||
                 CHECK_QUEST_ITEM(QUEST_MEDALLION_SHADOW)) {
                 Actor_Kill(&this->actor);
             }
-            func_80B31020(this, func_80B31A10);
+            EnWeatherTag_SetupAction(this, func_80B31A10);
             break;
         case 6:
             osSyncPrintf("\n\n");
             //"☆☆☆☆☆ The desert becomes thicker ☆☆☆☆☆"
             osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ 砂漠が濃くなります ☆☆☆☆☆ \n" VT_RST);
-            func_80B31020(this, func_80B31B24);
+            EnWeatherTag_SetupAction(this, func_80B31B24);
             break;
         case 7:
             osSyncPrintf("\n\n");
             //"☆☆☆☆☆ Wow wa wa na wa saa ki ha (^o^) ☆☆☆☆☆"
             osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ わわわわー なーがーさーきーはー (^o^) ☆☆☆☆☆ \n" VT_RST);
 
-            func_80B31020(this, func_80B31C20);
+            EnWeatherTag_SetupAction(this, func_80B31C20);
             break;
     }
 }
@@ -132,7 +132,6 @@ void EnWeatherTag_Init(Actor* thisx, GlobalContext* globalCtx) {
 u8 func_80B312C8(EnWeatherTag* this, GlobalContext* globalCtx, u8 arg2, u8 arg3, u8 arg4, u8 arg5, u16 arg6, u8 arg7) {
     s32 pad;
     u8 ret = 0;
-
     Player* player = PLAYER;
 
     if (func_8002DB8C(&player->actor, &this->actor) < (this->actor.params >> 8) * 100.0f) {
@@ -176,10 +175,11 @@ u8 func_80B3149C(EnWeatherTag* this, GlobalContext* globalCtx, u8 arg2, u8 arg3,
     u8 ret = 0;
     Player* player = PLAYER;
 
-    if (((((this->actor.params >> 8) * 100.0f) + 100.0f) < func_8002DB8C(&player->actor, &this->actor))) {
+    if ((((this->actor.params >> 8) * 100.0f) + 100.0f) < func_8002DB8C(&player->actor, &this->actor)) {
         if ((globalCtx->envCtx.unk_1E != 0) ||
             (!D_8011FB3C || !(globalCtx->skyboxId == 1 || globalCtx->envCtx.unk_1F != globalCtx->envCtx.unk_20))) {
-            if ((D_8011FB38 = 1, globalCtx->envCtx.gloomySkyEvent == 0) &&
+            D_8011FB38 = 1;
+            if ((globalCtx->envCtx.gloomySkyEvent == 0) &&
                 ((globalCtx->envCtx.unk_1E != 0) ||
                  (!(globalCtx->envCtx.unk_1F == 1 || globalCtx->envCtx.unk_21 != 0)))) {
                 D_8011FB38 = 0;
@@ -208,51 +208,51 @@ u8 func_80B3149C(EnWeatherTag* this, GlobalContext* globalCtx, u8 arg2, u8 arg3,
 
 void func_80B31658(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B312C8(this, globalCtx, 0, 1, 0, 3, 0x3C, 1) != 0) {
-        func_80B31020(this, func_80B316B0);
+        EnWeatherTag_SetupAction(this, func_80B316B0);
     }
 }
 
 void func_80B316B0(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B3149C(this, globalCtx, 1, 0, 3, 0, 0x3C) != 0) {
-        func_80B31020(this, func_80B31658);
+        EnWeatherTag_SetupAction(this, func_80B31658);
     }
 }
 
 void func_80B31700(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B312C8(this, globalCtx, 0, 1, 0, 2, 0x64, 2) != 0) {
-        func_80B31020(this, func_80B31758);
+        EnWeatherTag_SetupAction(this, func_80B31758);
     }
 }
 
 void func_80B31758(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B3149C(this, globalCtx, 1, 0, 2, 0, 0x64) != 0) {
-        func_80B31020(this, func_80B31700);
+        EnWeatherTag_SetupAction(this, func_80B31700);
     }
 }
 
 void func_80B317A8(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B312C8(this, globalCtx, 0, 1, 0, 2, 0x3C, 2) != 0) {
-        func_80B31020(this, func_80B31800);
+        EnWeatherTag_SetupAction(this, func_80B31800);
     }
 }
 
 void func_80B31800(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B3149C(this, globalCtx, 1, 0, 2, 0, 0x3C) != 0) {
-        func_80B31020(this, func_80B31700);
+        EnWeatherTag_SetupAction(this, func_80B31700);
     }
 }
 
 void func_80B31850(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B312C8(this, globalCtx, 0, 1, 0, 2, 0x3C, 3) != 0) {
         globalCtx->envCtx.unk_EE[3] = 0x40;
-        func_80B31020(this, func_80B318C0);
+        EnWeatherTag_SetupAction(this, func_80B318C0);
     }
 }
 
 void func_80B318C0(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B3149C(this, globalCtx, 1, 0, 2, 0, 0x3C) != 0) {
         globalCtx->envCtx.unk_EE[3] = 0;
-        func_80B31020(this, func_80B31850);
+        EnWeatherTag_SetupAction(this, func_80B31850);
     }
 }
 
@@ -260,7 +260,7 @@ void func_80B31924(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B312C8(this, globalCtx, 0, 1, 0, 2, 0x64, 4) != 0) {
         func_80077624(globalCtx);
         globalCtx->envCtx.unk_EE[0] = 0x19;
-        func_80B31020(this, func_80B319A0);
+        EnWeatherTag_SetupAction(this, func_80B319A0);
     }
 }
 
@@ -268,7 +268,7 @@ void func_80B319A0(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_80B3149C(this, globalCtx, 1, 0, 2, 0, 0x64) != 0) {
         func_80077684(globalCtx);
         globalCtx->envCtx.unk_EE[0] = 0;
-        func_80B31020(this, func_80B31924);
+        EnWeatherTag_SetupAction(this, func_80B31924);
     }
 }
 
@@ -277,7 +277,7 @@ void func_80B31A10(EnWeatherTag* this, GlobalContext* globalCtx) {
         func_80077624(globalCtx);
         globalCtx->envCtx.lightning = 1;
         globalCtx->envCtx.unk_EE[0] = 0x1E;
-        func_80B31020(this, func_80B31AA0);
+        EnWeatherTag_SetupAction(this, func_80B31AA0);
     }
 }
 
@@ -286,7 +286,7 @@ void func_80B31AA0(EnWeatherTag* this, GlobalContext* globalCtx) {
         func_80077684(globalCtx);
         globalCtx->envCtx.lightning = 2;
         globalCtx->envCtx.unk_EE[0] = 0;
-        func_80B31020(this, func_80B31A10);
+        EnWeatherTag_SetupAction(this, func_80B31A10);
     }
 }
 
@@ -296,10 +296,10 @@ void func_80B31B24(EnWeatherTag* this, GlobalContext* globalCtx) {
     if (func_8002DB8C(&player->actor, &this->actor) < ((this->actor.params >> 8) * 100.0f)) {
         Math_SmoothScaleMaxMinS(&globalCtx->envCtx.unk_9E, -0x50, 1, 2, 1);
         Math_SmoothScaleMaxMinS(&globalCtx->envCtx.unk_A0, -0x7D0, 1, 0x32, 1);
-        return;
+    } else {
+        Math_SmoothScaleMaxMinS(&globalCtx->envCtx.unk_9E, 0, 1, 1, 1);
+        Math_SmoothScaleMaxMinS(&globalCtx->envCtx.unk_A0, 0, 1, 0x19, 1);
     }
-    Math_SmoothScaleMaxMinS(&globalCtx->envCtx.unk_9E, 0, 1, 1, 1);
-    Math_SmoothScaleMaxMinS(&globalCtx->envCtx.unk_A0, 0, 1, 0x19, 1);
 }
 
 void func_80B31C20(EnWeatherTag* this, GlobalContext* globalCtx) {
@@ -309,7 +309,7 @@ void func_80B31C20(EnWeatherTag* this, GlobalContext* globalCtx) {
         func_80077624(globalCtx);
         globalCtx->envCtx.lightning = 1;
         globalCtx->envCtx.unk_EE[0] = 0x19;
-        func_80B31020(this, func_80B31CC0);
+        EnWeatherTag_SetupAction(this, func_80B31CC0);
     }
 }
 
@@ -321,7 +321,7 @@ void func_80B31CC0(EnWeatherTag* this, GlobalContext* globalCtx) {
         globalCtx->envCtx.lightning = 2;
         globalCtx->envCtx.unk_EE[0] = 0;
         globalCtx->envCtx.unk_EE[1] = 0xA;
-        func_80B31020(this, func_80B31C20);
+        EnWeatherTag_SetupAction(this, func_80B31C20);
     }
 }
 
