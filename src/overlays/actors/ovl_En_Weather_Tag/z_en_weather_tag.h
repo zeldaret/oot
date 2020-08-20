0e6b36759436f37b25fc08b30d942d7a556e7f4e
fullgrowngaming <fullgrowngaming@gmail.com>
2020-08-19 18:49:54 -0700

    Renamed what I thought was struct padding to more accurately represent the fact that that field is an unused variable.

--- src/overlays/actors/ovl_En_Weather_Tag/z_en_weather_tag.h
+++ src/overlays/actors/ovl_En_Weather_Tag/z_en_weather_tag.h
@@ -11,7 +11,7 @@ typedef void (*EnWeatherTagActionFunc)(struct EnWeatherTag*, GlobalContext*);
 typedef struct EnWeatherTag {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ EnWeatherTagActionFunc actionFunc;
-    /* 0x0150 */ char unk_150[4];
+    /* 0x0150 */ s16 unk_150; //unused
 } EnWeatherTag; // size = 0x0154
 
 extern const ActorInit En_Weather_Tag_InitVars;
