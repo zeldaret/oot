752255c7f1fc9bc0680de7bb88d00daeb5f81cfa
Random <28494085+Random06457@users.noreply.github.com>
2020-08-20 15:14:11 +0200

    progress

--- src/code/db_camera.c
+++ src/code/db_camera.c
@@ -11,7 +11,12 @@ typedef struct {
     /* 0x000C */ char unk_0C[0x24-0xC];
     /* 0x0024 */ CutsceneCameraPoint position[129];
     /* 0x0834 */ CutsceneCameraPoint lookAt[129];
-    /* 0x1044 */ char unk_1044[0x1050-0x1044];
+    /* 0x1044 */ s16 unk_1044;
+    /* 0x1046 */ s16 unk_1046;
+    /* 0x1048 */ s16 unk_1048;
+    /* 0x104A */ s16 unk_104A;
+    /* 0x104C */ s16 unk_104C;
+    /* 0x104E */ s16 unk_104E;
 } DbCameraSub; // size = 0x1050
 
 typedef struct {
@@ -19,12 +24,21 @@ typedef struct {
     /* 0x0004 */ Vec3f at;
     /* 0x0010 */ Vec3f eye;
     /* 0x001C */ Vec3f unk_1C;
-    /* 0x0028 */ char unk_28[0x48-0x28];
+    /* 0x0028 */ char unk_28[0xC];
+    /* 0x0034 */ s32 unk_34;
+    /* 0x0038 */ s32 unk_38;
+    /* 0x003C */ s32 unk_3C;
+    /* 0x0040 */ s32 unk_40;
+    /* 0x0044 */ s32 unk_44;
     /* 0x0048 */ f32 unk_48;
     /* 0x004C */ s16 unk_4C;
     /* 0x004E */ char unk_4E[0x2];
     /* 0x0050 */ f32 unk_50;
-    /* 0x0054 */ char unk_54[0x7C-0x54];
+    /* 0x0054 */ char unk_54[0x60-0x54];
+    /* 0x0060 */ Vec3f unk_60;
+    /* 0x006C */ Vec3f unk_6C;
+    /* 0x0078 */ s16 unk_78;
+    /* 0x007A */ s16 unk_7A;
     /* 0x007C */ DbCameraSub sub;
 } DbCamera; // size = 0x10CC
 
@@ -118,12 +132,13 @@ u8 D_8012D158[] = {
 
 u32 D_8012D170 = 0;
 extern DbCamera* D_80161108;
+extern GlobalContext* D_80161100;
 
 Vec3f* func_800B3B50(Vec3f* outVec, Vec3f* inVec, VecSph* sph) {
     Vec3f ret;
     Vec3f vec;
 
-    OLib_VecSphRot90ToVec3f(&vec, sph);
+    OLib_VecSphGeoToVec3f(&vec, sph);
 
     ret.x = inVec->x + vec.x;
     ret.y = inVec->y + vec.y;
@@ -132,60 +147,160 @@ Vec3f* func_800B3B50(Vec3f* outVec, Vec3f* inVec, VecSph* sph) {
     return outVec;
 }
 
+#ifdef NON_MATCHING
+Vec3f* func_800B3BD4(Vec3f* arg0, s16 pitch, s16 yaw, s16 roll) {
+
+    f32 sinPitch;
+    f32 cosPitch;
+    f32 sinYaw;
+    f32 cosYaw;
+    f32 sinNegRoll;
+    f32 cosNegRoll;
+    Vec3f sp9C;
+
+    sinPitch = Math_Sins(pitch);
+    cosPitch = Math_Coss(pitch);
+    sinYaw = Math_Sins(yaw);
+    cosYaw = Math_Coss(yaw);
+    sinNegRoll = Math_Sins(-roll);
+    cosNegRoll = Math_Coss(-roll);
+
+    sp9C.x = 
+        (-sinPitch * sinYaw * (((1.0f - SQ(cosPitch * sinYaw)) * cosNegRoll) + SQ(cosPitch * sinYaw))) +
+        (cosPitch * ((cosPitch * sinYaw * sinPitch * (1.0f - cosNegRoll)) - cosPitch * cosYaw * sinNegRoll)) +
+        (-sinPitch * cosYaw * ((cosPitch * cosYaw * cosPitch * sinYaw * (1.0f - cosNegRoll)) + (sinPitch * sinNegRoll)));
+	
+    sp9C.y = 
+        (-sinPitch * sinYaw * (cosPitch * sinYaw * sinPitch * (1.0f - cosNegRoll) + cosPitch * cosYaw * sinNegRoll)) +
+        (cosPitch * (((1.0f - SQ(cosPitch * sinYaw)) * cosNegRoll) + SQ(cosPitch * sinYaw))) +
+        (-sinPitch * cosYaw * ((sinPitch * cosPitch * cosYaw * (1.0f - cosNegRoll)) - cosPitch * sinYaw * sinNegRoll));
+
+    sp9C.z =
+        (-sinPitch * sinYaw * ((cosPitch * cosYaw * cosPitch * sinYaw * (1.0f - cosNegRoll)) - sinPitch * sinNegRoll)) + 
+        (cosPitch * ((sinPitch * cosPitch * cosYaw * (1.0f - cosNegRoll)) + (cosPitch * sinYaw * sinNegRoll))) +
+        (-sinPitch * cosYaw * (((1.0f - SQ(cosPitch * cosYaw)) * cosNegRoll) + SQ(cosPitch * cosYaw)));
+    
+    
+    *arg0 = sp9C;
+    return arg0;
+    /*
+    f32 sinYaw;
+    f32 cosYaw;
+    f32 sinNegRoll;
+    Vec3f sp9C;
+    f32 sp54;
+    //f32 sp50;
+    f32 sp48;
+    f32 cosPitchCosYawSinRoll;
+    f32 sp3C;
+    f32 sp38;
+    Vec3f* temp_t9;
+    f32 cosNegRoll;
+    f32 temp_f10;
+    f32 cosPitchCosYaw;
+    f32 negSinPitchSinYaw;
+    f32 negSinPitchCosYaw;
+    f32 temp_f18;
+    f32 cosPitch;
+    f32 sinPitch;
+    f32 cosPitchSinYaw;
+    f32 negSinPitch;
+    f32 tmp0;
+    //f32 tmp1;
+
+    sinPitch = Math_Sins(pitch);
+    cosPitch = Math_Coss(pitch);
+    sinYaw = Math_Sins(yaw);
+    cosYaw = Math_Coss(yaw);
+    //negSinPitch = -sinPitch;
+    sinNegRoll = Math_Sins(-roll);
+    cosNegRoll = Math_Coss(-roll);
+
+    negSinPitchSinYaw = (-sinPitch * sinYaw);
+    temp_f18 = 1.0f - cosNegRoll;
+    cosPitchSinYaw = cosPitch * sinYaw;
+    //sp50 = SQ(cosPitchSinYaw);
+    sp48 = (cosPitchSinYaw * sinPitch) * temp_f18;
+    cosPitchCosYaw = cosPitch * cosYaw;
+    tmp0 = ((1.0f - SQ(cosPitchSinYaw)) * cosNegRoll) + SQ(cosPitchSinYaw);
+    //tmp1 = ((1.0f - SQ(cosPitchCosYaw)) * cosNegRoll) + SQ(cosPitchCosYaw);
+    cosPitchCosYawSinRoll = cosPitchCosYaw * sinNegRoll;
+    negSinPitchCosYaw = -sinPitch * cosYaw;
+    sp3C = (cosPitchCosYaw * cosPitchSinYaw) * temp_f18;
+    sp38 = sinPitch * sinNegRoll;
+
+    sp9C.x = 
+        (negSinPitchSinYaw * (tmp0)) +
+        (cosPitch * (sp48 - cosPitchCosYawSinRoll)) +
+        (negSinPitchCosYaw * (sp3C + sp38));
+    
+    temp_f10 = (sinPitch * cosPitchCosYaw) * temp_f18;
+    sp54 = cosPitchSinYaw * sinNegRoll;
+
+    sp9C.y = 
+        (negSinPitchSinYaw * (sp48 + cosPitchCosYawSinRoll)) +
+        (cosPitch * ((tmp0))) +
+        (negSinPitchCosYaw * (temp_f10 - sp54));
+
+    sp9C.z =
+        (negSinPitchSinYaw * (sp3C - sp38)) + 
+        (cosPitch * (temp_f10 + sp54)) +
+        (negSinPitchCosYaw * (((1.0f - SQ(cosPitchCosYaw)) * cosNegRoll) + SQ(cosPitchCosYaw)));
+
+    *arg0 = sp9C;
+    return arg0;
+    */
+}
+
+#else
 #pragma GLOBAL_ASM("asm/non_matchings/code/db_camera/func_800B3BD4.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/db_camera/func_800B3DF8.s")
 
-void func_800B3EBC(Vec3s* in, Vec3f* out)
-{
+void func_800B3EBC(Vec3s* in, Vec3f* out) {
     out->x = in->x;
     out->y = in->y;
     out->z = in->z;
 }
 
-void func_800B3EFC(Vec3f* in, Vec3s* out)
-{
+void func_800B3EFC(Vec3f* in, Vec3s* out) {
     out->x = in->x;
     out->y = in->y;
     out->z = in->z;
 }
 
-void func_800B3F38(Vec3f* in, Vec3f* out)
-{
+void func_800B3F38(Vec3f* in, Vec3f* out) {
     out->x = in->x;
     out->y = in->y;
     out->z = in->z;
 }
 
-void func_800B3F54(Vec3s* in, Vec3f* out)
-{
+void func_800B3F54(Vec3s* in, Vec3f* out) {
     out->x = in->x;
     out->y = in->y;
     out->z = in->z;
 }
 
-void func_800B3F94(PosRot* posRot, Vec3f* vec, Vec3s* outVec)
-{
+void func_800B3F94(PosRot* posRot, Vec3f* vec, Vec3s* outVec) {
     VecSph sph;
     Vec3f tempVec;
-    OLib_Vec3fDiffToVecSphRot90(&sph, &posRot->pos, vec);
-    sph.theta -= posRot->rot.y;
-    OLib_VecSphRot90ToVec3f(&tempVec, &sph);
+    OLib_Vec3fDiffToVecSphGeo(&sph, &posRot->pos, vec);
+    sph.yaw -= posRot->rot.y;
+    OLib_VecSphGeoToVec3f(&tempVec, &sph);
     func_800B3EFC(&tempVec, outVec);
 }
 
-void func_800B3FF4(PosRot* posRot, Vec3f* vec, Vec3f* outVec)
-{
+void func_800B3FF4(PosRot* posRot, Vec3f* vec, Vec3f* outVec) {
     VecSph sph;
     Vec3f tempVec;
     func_800B3F38(vec, &tempVec);
-    OLib_Vec3fToVecSphRot90(&sph, &tempVec);
-    sph.theta += posRot->rot.y;
+    OLib_Vec3fToVecSphGeo(&sph, &tempVec);
+    sph.yaw += posRot->rot.y;
     func_800B3B50(outVec, &posRot->pos, &sph);
 }
 
-void func_800B404C(PosRot* posRot, Vec3s* vec, Vec3f* outVec)
-{
+void func_800B404C(PosRot* posRot, Vec3s* vec, Vec3f* outVec) {
     Vec3f tempVec;
     func_800B3EBC(vec, &tempVec);
     func_800B3FF4(posRot, &tempVec, outVec);
@@ -278,12 +393,12 @@ void func_800B4AE4(s16 value)
     osSyncPrintf("\n@@@%d,%d,", a[0], a[1]);
 }
 
-#ifdef NON_MACTHING
 void func_800B4B20(DbCameraCut* cut) {
     CutsceneCameraPoint* point;
+    CutsceneCameraPoint* points;
     s32 i;
 
-    point = cut->lookAt;
+    points = cut->lookAt;
     osSyncPrintf("\n@@@ 0,0,0,2,\t/* Look Camera\t*/");
     osSyncPrintf("\n@@@ 0,1,\t/* dousa\t*/");
 
@@ -293,6 +408,7 @@ void func_800B4B20(DbCameraCut* cut) {
 
     osSyncPrintf("\n@@@0,0,\t/*  Dammy\t*/\n@@@ ");
     for (i = 0; i < cut->nPoints; i++) {
+        point = points + i;
         osSyncPrintf("\n@@@    %d, /*     code     */", point->continueFlag);
         osSyncPrintf("\n@@@    %d,  /*     z        */", point->cameraRoll);
         func_800B4AA8(point->nextPointFrame);
@@ -304,12 +420,11 @@ void func_800B4B20(DbCameraCut* cut) {
         func_800B4AE4(point->pos.y);
         osSyncPrintf("\t/*  y pos\t*/");
         func_800B4AE4(point->pos.z);
-        osSyncPrintf("\t/*  z pos\t*/");
+        osSyncPrintf("\t/*  z pos\t*/\n");
         osSyncPrintf("\n@@@0,0,\t/*  Dammy\t*/\n@@@ ");
-        point++;
     }
 
-    point = cut->position;
+    points = cut->position;
     osSyncPrintf("\n@@@ 0,0,0,1,\t/* Position Camera */");
     osSyncPrintf("\n@@@ 0,1,\t/* dousa\t*/");
 
@@ -319,8 +434,9 @@ void func_800B4B20(DbCameraCut* cut) {
 
     osSyncPrintf("\n@@@0,0,\t/*  Dammy\t*/\n@@@ ");
     for (i = 0; i < cut->nPoints; i++) {
+        point = points + i;
         osSyncPrintf("\n@@@    %d, /*     code     */", point->continueFlag);
-        osSyncPrintf("\n@@@    %d,  /*     z        */", point->cameraRoll);
+        osSyncPrintf("\n@@@    %d, /*     z        */", point->cameraRoll);
         func_800B4AA8(point->nextPointFrame);
         osSyncPrintf("\t/*  sokudo\t*/");
         func_800B4A68(point->viewAngle);
@@ -332,20 +448,38 @@ void func_800B4B20(DbCameraCut* cut) {
         func_800B4AE4(point->pos.z);
         osSyncPrintf("\t/*  z pos\t*/");
         osSyncPrintf("\n@@@0,0,\t/*  Dammy\t*/\n@@@ ");
-        point++;
     }
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/code/db_camera/func_800B4B20.s")
-#endif
 
-#ifdef NON_MATCHING
 void func_800B4D58(DbCamera* dbCamera, Camera* cameraPtr) {
-
+    dbCamera->sub.unk_104E = 0;
+    dbCamera->unk_44 = 0;
+    dbCamera->unk_00 = 0;
+    dbCamera->unk_34 = 0;
+    dbCamera->unk_3C = 0;
+    dbCamera->unk_38 = -1;
+    dbCamera->unk_40 = -1;
+    dbCamera->unk_4C = 0;
+    dbCamera->sub.unk_104C = dbCamera->sub.unk_104E;
+    dbCamera->sub.unk_104A = dbCamera->sub.unk_104E;
+    dbCamera->unk_48 = 0.0f;
+    dbCamera->unk_50 = 0.0f;
+    D_80161100 = cameraPtr->globalCtx;
+    dbCamera->sub.mode = 0;
+    dbCamera->sub.nFrames = -1;
+    dbCamera->sub.nPoints = 1;
+    dbCamera->sub.unkIdx = 0;
+    dbCamera->sub.unk_08 = 0;
+    dbCamera->sub.unk_0A = 0;
+    dbCamera->unk_78 = 0;
+    dbCamera->unk_7A = 0;
+    dbCamera->sub.unk_1044 = 0;
+    dbCamera->sub.unk_1046 = 0;
+    dbCamera->sub.unk_1048 = 0;
+    dbCamera->unk_6C.x = 0;
+    dbCamera->unk_6C.y = 0;
+    dbCamera->unk_6C.z = 0;
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/code/db_camera/func_800B4D58.s")
-#endif
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/db_camera/func_800B4DE4.s")
 
