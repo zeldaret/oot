56bf61988cf2a28ac866772e2c5549778077bde8
Louis <35883445+louist103@users.noreply.github.com>
2020-08-22 00:19:19 -0400

    Accidently left a git string in the file

--- src/overlays/actors/ovl_En_Ex_Ruppy/z_en_ex_ruppy.c
+++ src/overlays/actors/ovl_En_Ex_Ruppy/z_en_ex_ruppy.c
@@ -305,12 +305,6 @@ void EnExRuppy_BlowUp(EnExRuppy* this, GlobalContext* globalCtx) {
     Vec3f zeroVector = { 0.0f, 0.0f, 0.0f };
     s16 explosionScale;
     s16 explosionScaleStep;
-<<<<<<< HEAD
-=======
-    
-    point1Vec = sPoint1Vec;
-    zeroVector = sZeroVector;
->>>>>>> 450fb631e879b5493dea4eb135639b6234e99823
     distToBlowUp = 50.0f;
     if (this->unk_152 == 2) {
         distToBlowUp = 30.0f;
