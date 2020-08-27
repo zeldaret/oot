f6bb51424427cd12fa30deb560f670ea261b4326
i82orbom <i82orbom i82orbom@github>
2020-08-27 10:30:56 +0200

    BgSpot11Bakudankabe_Init

--- src/overlays/actors/ovl_Bg_Spot11_Bakudankabe/z_bg_spot11_bakudankabe.c
+++ src/overlays/actors/ovl_Bg_Spot11_Bakudankabe/z_bg_spot11_bakudankabe.c
@@ -15,6 +15,7 @@ void BgSpot11Bakudankabe_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot11Bakudankabe_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot11Bakudankabe_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_808B2180(BgSpot11Bakudankabe* this, GlobalContext* globalCtx);
 
 const ActorInit Bg_Spot11_Bakudankabe_InitVars = {
     ACTOR_BG_SPOT11_BAKUDANKABE,
@@ -32,12 +33,30 @@ s32 D_808B2700[] = {0x0A000900, 0x20010000, 0x00000000, 0x00000000, 0x00000000, 
 s32 D_808B272C[] = {0x450D3000, 0x42D80000, 0xC4C1C000};
 s32 D_808B2738[] = {0x450D3000, 0x42D80000, 0xC4C1C000, 0x00000000, 0x00000000, 0x00000000};
 
+extern UNK_TYPE D_06001A58;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/func_808B2180.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/func_808B2218.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/BgSpot11Bakudankabe_Init.s")
+void BgSpot11Bakudankabe_Init(Actor *thisx, GlobalContext *globalCtx) {
+    BgSpot11Bakudankabe *this = THIS;
+    s32 pad;
+    s32 sp24;
+
+    sp24 = 0;
+    DynaPolyInfo_SetActorMove(&this->dyna, 0);
+    if (Flags_GetSwitch(globalCtx, (this->dyna.actor.params & 0x3F))) {
+        Actor_Kill(&this->dyna.actor);
+        return;
+    }
+    func_808B2180(this, globalCtx);
+    DynaPolyInfo_Alloc(&D_06001A58, &sp24);
+    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, (Actor *) this, sp24);
+    Actor_SetScale(&this->dyna.actor, 1.0f);
+    osSyncPrintf("(spot11 爆弾壁)(arg_data 0x%04x)\n", this->dyna.actor.params);
+}
+
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/BgSpot11Bakudankabe_Destroy.s")
 
