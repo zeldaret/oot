e6f73a78c156c24e41e8e3c15846704410ee8916
i82orbom <i82orbom i82orbom@github>
2020-08-27 10:41:12 +0200

    BgSpot11Bakudankabe_Update

--- src/overlays/actors/ovl_Bg_Spot11_Bakudankabe/z_bg_spot11_bakudankabe.c
+++ src/overlays/actors/ovl_Bg_Spot11_Bakudankabe/z_bg_spot11_bakudankabe.c
@@ -16,6 +16,7 @@ void BgSpot11Bakudankabe_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot11Bakudankabe_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void func_808B2180(BgSpot11Bakudankabe* this, GlobalContext* globalCtx);
+void func_808B2218(BgSpot11Bakudankabe* this, GlobalContext* globalCtx);
 
 const ActorInit Bg_Spot11_Bakudankabe_InitVars = {
     ACTOR_BG_SPOT11_BAKUDANKABE,
@@ -29,9 +30,11 @@ const ActorInit Bg_Spot11_Bakudankabe_InitVars = {
     (ActorFunc)BgSpot11Bakudankabe_Draw,
 };
 
-s32 D_808B2700[] = {0x0A000900, 0x20010000, 0x00000000, 0x00000000, 0x00000000, 0x00000008, 0x00000000, 0x00010000, 0x00280050, 0x000008D3, 0x006CF9D4};
-s32 D_808B272C[] = {0x450D3000, 0x42D80000, 0xC4C1C000};
-s32 D_808B2738[] = {0x450D3000, 0x42D80000, 0xC4C1C000, 0x00000000, 0x00000000, 0x00000000};
+s32 D_808B2700[] = { 0x0A000900, 0x20010000, 0x00000000, 0x00000000, 0x00000000, 0x00000008,
+                     0x00000000, 0x00010000, 0x00280050, 0x000008D3, 0x006CF9D4 };
+s32 D_808B272C[] = { 0x450D3000, 0x42D80000, 0xC4C1C000 };
+
+Vec3f D_808B2738 = { 2259.0f, 108.0f, -1550.0f };
 
 extern UNK_TYPE D_06001A58;
 
@@ -39,8 +42,8 @@ extern UNK_TYPE D_06001A58;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/func_808B2218.s")
 
-void BgSpot11Bakudankabe_Init(Actor *thisx, GlobalContext *globalCtx) {
-    BgSpot11Bakudankabe *this = THIS;
+void BgSpot11Bakudankabe_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgSpot11Bakudankabe* this = THIS;
     s32 pad;
     s32 sp24;
 
@@ -52,19 +55,30 @@ void BgSpot11Bakudankabe_Init(Actor *thisx, GlobalContext *globalCtx) {
     }
     func_808B2180(this, globalCtx);
     DynaPolyInfo_Alloc(&D_06001A58, &sp24);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, (Actor *) this, sp24);
+    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, (Actor*)this, sp24);
     Actor_SetScale(&this->dyna.actor, 1.0f);
     osSyncPrintf("(spot11 爆弾壁)(arg_data 0x%04x)\n", this->dyna.actor.params);
 }
 
-void BgSpot11Bakudankabe_Destroy(Actor *thisx, GlobalContext *globalCtx) {
-    BgSpot11Bakudankabe *this = THIS;
+void BgSpot11Bakudankabe_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgSpot11Bakudankabe* this = THIS;
 
     DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
     Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
+void BgSpot11Bakudankabe_Update(Actor* thisx, GlobalContext* globalCtx) {
+    BgSpot11Bakudankabe* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/BgSpot11Bakudankabe_Update.s")
+    if (this->collider.base.acFlags & 2) {
+        func_808B2218(this, globalCtx);
+        Flags_SetSwitch(globalCtx, (this->dyna.actor.params & 0x3F));
+        Audio_PlaySoundAtPosition(globalCtx, &D_808B2738, 40, NA_SE_EV_WALL_BROKEN);
+        func_80078884(NA_SE_SY_CORRECT_CHIME);
+        Actor_Kill(&this->dyna.actor);
+        return;
+    }
+    CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot11_Bakudankabe/BgSpot11Bakudankabe_Draw.s")
