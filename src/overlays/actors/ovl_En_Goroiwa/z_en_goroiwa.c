983130f8cb8ebf603017bdf8b4f9e94989faf1f7
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2020-09-05 20:22:06 -0700

    func_80A4C814

--- src/overlays/actors/ovl_En_Goroiwa/z_en_goroiwa.c
+++ src/overlays/actors/ovl_En_Goroiwa/z_en_goroiwa.c
@@ -272,21 +272,21 @@ bool func_80A4C6C8(EnGoroiwa* this, GlobalContext* globalCtx) {
     return temp_v0;
 }
 
-#ifdef NON_MATCHING
 bool func_80A4C814(EnGoroiwa* this, GlobalContext* globalCtx) {
     Path* path = &globalCtx->setupPathList[this->actor.params & 0xFF];
+    s32 pad;
     Vec3s* temp_v0 = (Vec3s*)SEGMENTED_TO_VIRTUAL(path->points) + this->waypoint2;
     Vec3s* temp_t1 = (Vec3s*)SEGMENTED_TO_VIRTUAL(path->points) + this->waypoint1;
+    bool result;
     Vec3f sp38;
     Vec3f sp2C;
-    bool result;
 
     sp2C.x = temp_v0->x;
     sp2C.y = temp_v0->y;
     sp2C.z = temp_v0->z;
     Math_ApproxF(&this->actor.speedXZ, mREG(12) * 0.01f, 0.3f);
-    if (Math3D_Vec3fDistSq(&sp2C, &this->actor.posRot.pos.x) < 25.0f) {
-        Math_Vec3f_Diff(&sp2C, &this->actor.posRot.pos.x, &sp38);
+    if (Math3D_Vec3fDistSq(&sp2C, &this->actor.posRot.pos) < 25.0f) {
+        Math_Vec3f_Diff(&sp2C, &this->actor.posRot.pos, &sp38);
     } else {
         sp38.x = sp2C.x - temp_t1->x;
         sp38.y = sp2C.y - temp_t1->y;
@@ -301,9 +301,6 @@ bool func_80A4C814(EnGoroiwa* this, GlobalContext* globalCtx) {
     result &= Math_ApproxF(&this->actor.posRot.pos.z, sp2C.z, fabsf(this->actor.velocity.z));
     return result;
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Goroiwa/func_80A4C814.s")
-#endif
 
 bool func_80A4CA50(EnGoroiwa* this, GlobalContext* globalCtx) {
     s32 pad;
