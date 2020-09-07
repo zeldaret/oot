a659f1f3802856b2860eadd44d776ba27e3c0306
fig02 <fig02srl@gmail.com>
2020-09-07 12:27:34 -0400

    progress

--- src/overlays/effects/ovl_Effect_Ss_Fire_Tail/z_eff_ss_fire_tail.c
+++ src/overlays/effects/ovl_Effect_Ss_Fire_Tail/z_eff_ss_fire_tail.c
@@ -69,8 +69,8 @@ u32 EffectSsFireTail_Init(GlobalContext* globalCtx, u32 index, EffectSs* this, v
     return 1;
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/effects/ovl_Effect_Ss_Fire_Tail/func_809A5858.s")
-/*
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/effects/ovl_Effect_Ss_Fire_Tail/func_809A5858.s")
+
 void func_809A5858(GlobalContext* globalCtx, u32 index, EffectSs* this) {
     GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
     s32 pad;
@@ -80,28 +80,26 @@ void func_809A5858(GlobalContext* globalCtx, u32 index, EffectSs* this) {
     f32 sin;
     f32 dist;
 
-    f32 scale;
-    //f32 temp_f2;
-    f32 scale2;
-
+    // f32 scale;
+    // f32 temp_f2;
+    // f32 scale2;
 
     OPEN_DISPS(gfxCtx, "../z_eff_fire_tail.c", 182);
 
-    spA0.x = spA0.y = spA0.z = 0.0f;
-
+    spA0.x = 0.0f;
+    spA0.y = 0.0f;
+    spA0.z = 0.0f;
 
     if (this->actor != NULL) { // 16C
-        Player* player;
-        s16 bodypart;
 
         this->vec = this->actor->velocity;
-        bodypart = this->regs[SS_FIRE_TAIL_B];
-        if (bodypart < 0) { // 194
+
+        if (this->regs[SS_FIRE_TAIL_B] < 0) { // 194
             Matrix_Translate(this->pos.x + this->actor->posRot.pos.x, this->pos.y + this->actor->posRot.pos.y,
                              this->pos.z + this->actor->posRot.pos.z, MTXMODE_NEW);
         } else {
-            player = PLAYER;
-
+            Player* player = PLAYER; // stacks a lil messy. this might need to go after null check
+            s16 bodypart = this->regs[SS_FIRE_TAIL_B];
             this->pos.x = player->unk_908[bodypart].x - (Math_Sins(func_8005A9F4(ACTIVE_CAM)) * 5.0f);
             this->pos.y = player->unk_908[bodypart].y;
             this->pos.z = player->unk_908[bodypart].z - (Math_Coss(func_8005A9F4(ACTIVE_CAM)) * 5.0f);
@@ -115,20 +113,24 @@ void func_809A5858(GlobalContext* globalCtx, u32 index, EffectSs* this) {
     yaw = Math_Vec3f_Yaw(&spA0, &this->vec) - func_8005A9F4(ACTIVE_CAM);
     cos = fabsf(Math_Coss(yaw));
     sin = Math_Sins(yaw);
-    dist = Math_Vec3f_DistXZ(&spA0, &this->vec) / (this->regs[SS_FIRE_TAIL_A] * 0.1f); // div being weird
+    dist = Math_Vec3f_DistXZ(&spA0, &this->vec) / (this->regs[SS_FIRE_TAIL_A] * 0.1f);
 
+    
     Matrix_RotateY(((((s16)(func_8005A9F4(ACTIVE_CAM) + 0x8000)))) * 0.0000958738f, MTXMODE_APPLY);
-    Matrix_RotateZ(((this->regs[SS_FIRE_TAIL_2] * sin) * dist) * 0.017453292f, MTXMODE_APPLY);
+    sin *= this->regs[SS_FIRE_TAIL_2];
+    sin *= dist;
+    Matrix_RotateZ(sin * 0.017453292f, MTXMODE_APPLY);
 
-    scale = 1.0f - ((this->life + 1) / this->regs[SS_FIRE_TAIL_1]);
+    // scale = 1.0f - ((f32)(this->life + 1) / this->regs[SS_FIRE_TAIL_1]);
+    
+    spA0.x = spA0.y = spA0.z = (1.0f - SQ(1.0f - ((f32)(this->life + 1) / this->regs[SS_FIRE_TAIL_1]))) *
+                               (this->regs[SS_FIRE_TAIL_SCALE] * 0.000010000001f);
+    // spA0.x = spA0.y = spA0.z = scale;
+    Matrix_Scale(spA0.x, spA0.y, spA0.z, MTXMODE_APPLY);
 
-    scale = (this->regs[SS_FIRE_TAIL_SCALE] * 0.000010000001f) * (1.0f - SQ(scale));
-    spA0.x = spA0.y = spA0.z = scale;
-    Matrix_Scale(scale, scale, scale, MTXMODE_APPLY);
-
-    scale2 = (((this->regs[SS_FIRE_TAIL_3] * 0.01f) * cos) * dist) + 1.0f;
-    scale2 = CLAMP_MIN(scale2, 0.1f);
-    Matrix_Scale(1.0f, scale2, 1.0f / scale2, 1);
+    spA0.y = ((((this->regs[SS_FIRE_TAIL_3] * 0.01f) * cos) * dist) + 1.0f);
+    spA0.y = CLAMP_MIN(spA0.y, 0.1f);
+    Matrix_Scale(1.0f, spA0.y, 1.0f / spA0.y, 1);
 
     gSPMatrix(oGfxCtx->polyXlu.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_eff_fire_tail.c", 238),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
@@ -149,7 +151,6 @@ void func_809A5858(GlobalContext* globalCtx, u32 index, EffectSs* this) {
 
     CLOSE_DISPS(gfxCtx, "../z_eff_fire_tail.c", 273);
 }
-*/
 
 void func_809A5D98(GlobalContext* globalCtx, u32 index, EffectSs* this) {
     this->regs[SS_FIRE_TAIL_SCALE] *= 0.9f;
