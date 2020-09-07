b6eff2caa1e8f1ae9e4c998c30d14649321ff8f1
kroemker <lk1994@gmx.net>
2020-09-08 00:11:44 +0200

    PR fixes

--- src/overlays/actors/ovl_En_Ice_Hono/z_en_ice_hono.c
+++ src/overlays/actors/ovl_En_Ice_Hono/z_en_ice_hono.c
@@ -68,7 +68,7 @@ static InitChainEntry sInitChainSmallFlame[] = {
 f32 EnIceHono_SquareDist(Vec3f* pos1, Vec3f* pos2) {
     f32 dx = pos1->x - pos2->x;
     f32 dz = pos1->z - pos2->z;
-    
+
     return SQ(dx) + SQ(dz);
 }
 
@@ -105,7 +105,7 @@ void EnIceHono_InitDroppedFlame(Actor* thisx, GlobalContext* globalCtx) {
 
     this->collider.dim.radius = this->actor.scale.x * 4000.4f;
     this->collider.dim.height = this->actor.scale.y * 8000.2f;
-    this->actor.colChkInfo.mass = 0xFD;
+    this->actor.colChkInfo.mass = 253;
     EnIceHono_SetupActionDroppedFlame(this);
 }
 
@@ -139,12 +139,11 @@ void EnIceHono_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     if ((this->actor.params == -1) || (this->actor.params == 0)) {
-        Lights_PointNoGlowSetInfo(&this->lightInfo, this->actor.posRot.pos.x,
-                                        (s16)this->actor.posRot.pos.y + 0x0A, this->actor.posRot.pos.z, 0x9B, 0xD2,
-                                        0xFF, 0);
+        Lights_PointNoGlowSetInfo(&this->lightInfo, this->actor.posRot.pos.x, (s16)this->actor.posRot.pos.y + 0x0A,
+                                  this->actor.posRot.pos.z, 155, 210, 255, 0);
         this->lightNode = LightContext_InsertLight(globalCtx, &globalCtx->lightCtx, &this->lightInfo);
-        this->unk154 = Math_Rand_ZeroOne() * 65535.5f;
-        this->unk156 = Math_Rand_ZeroOne() * 65535.5f;
+        this->unk_154 = Math_Rand_ZeroOne() * (0x1FFFF / 2.0f);
+        this->unk_156 = Math_Rand_ZeroOne() * (0x1FFFF / 2.0f);
         // Translates to: "(ice flame)"
         osSyncPrintf("(ice 炎)(arg_data 0x%04x)\n", this->actor.params);
     }
@@ -167,7 +166,7 @@ u32 EnIceHono_LinkCloseAndFacing(EnIceHono* this, GlobalContext* globalCtx) {
         tempPos.x = Math_Sins(this->actor.yawTowardsLink + 0x8000) * 40.0f + player->actor.posRot.pos.x;
         tempPos.y = player->actor.posRot.pos.y;
         tempPos.z = Math_Coss(this->actor.yawTowardsLink + 0x8000) * 40.0f + player->actor.posRot.pos.z;
-        if (EnIceHono_SquareDist(&tempPos, &this->actor.posRot.pos) <= 1600.0f) {
+        if (EnIceHono_SquareDist(&tempPos, &this->actor.posRot.pos) <= SQ(40.0f)) {
             return 1;
         }
     }
@@ -176,7 +175,7 @@ u32 EnIceHono_LinkCloseAndFacing(EnIceHono* this, GlobalContext* globalCtx) {
 
 void EnIceHono_SetupActionCapturableFlame(EnIceHono* this) {
     this->actionFunc = EnIceHono_CapturableFlame;
-    this->alpha = 0xFF;
+    this->alpha = 255;
     this->actor.shape.unk_08 = -1000.0f;
 }
 
@@ -189,13 +188,13 @@ void EnIceHono_CapturableFlame(EnIceHono* this, GlobalContext* globalCtx) {
     if (this->actor.xzDistFromLink < 200.0f) {
         CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
     }
-    func_8002F8F0(&this->actor, 0x20A3);
+    func_8002F8F0(&this->actor, NA_SE_EV_FIRE_PILLAR_S - SFX_FLAG);
 }
 
 void EnIceHono_SetupActionDroppedFlame(EnIceHono* this) {
     this->actionFunc = EnIceHono_DropFlame;
-    this->timer = 0xC8;
-    this->alpha = 0xFF;
+    this->timer = 200;
+    this->alpha = 255;
 }
 
 void EnIceHono_DropFlame(EnIceHono* this, GlobalContext* globalCtx) {
@@ -207,10 +206,10 @@ void EnIceHono_DropFlame(EnIceHono* this, GlobalContext* globalCtx) {
 
     if (bgFlag != 0) {
         s32 i;
-        for (i = 0; i != 0x10000; i += 0x2000) {
+        for (i = 0; i < 8; i++) {
             Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_ICE_HONO, this->actor.posRot.pos.x,
                         this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0,
-                        ((s32)(Math_Rand_ZeroOne() * 1000.0f) + i) - 0x1F4, 0, 1);
+                        ((s32)(Math_Rand_ZeroOne() * 1000.0f) + i * 0x2000) - 0x1F4, 0, 1);
         }
         EnIceHono_SetupActionSpreadFlames(this);
     }
@@ -229,12 +228,12 @@ void EnIceHono_DropFlame(EnIceHono* this, GlobalContext* globalCtx) {
 
 void EnIceHono_SetupActionSpreadFlames(EnIceHono* this) {
     this->actionFunc = EnIceHono_SpreadFlames;
-    this->timer = 0x3C;
-    this->alpha = 0xFF;
+    this->timer = 60;
+    this->alpha = 255;
 }
 
 void EnIceHono_SpreadFlames(EnIceHono* this, GlobalContext* globalCtx) {
-    if (this->timer >= 0x15) {
+    if (this->timer > 20) {
         Math_ApproxF(&this->actor.scale.x, 0.011f, 0.00014f);
         Math_ApproxF(&this->actor.scale.y, 0.006f, 0.00012f);
     } else {
@@ -244,25 +243,26 @@ void EnIceHono_SpreadFlames(EnIceHono* this, GlobalContext* globalCtx) {
     this->actor.scale.z = this->actor.scale.x;
     Actor_MoveForward(&this->actor);
     func_8002E4B4(globalCtx, &this->actor, 10.0f, this->actor.scale.x * 3500.0f, 0.0f, 4);
-    if (this->timer < 0x19) {
-        this->alpha -= 0xA;
-        this->alpha = CLAMP(this->alpha, 0, 0xFF);
+    if (this->timer < 25) {
+        this->alpha -= 10;
+        this->alpha = CLAMP(this->alpha, 0, 255);
     }
 
-    if (this->alpha >= 0x65) {
-        if (this->timer < 0x28) {
+    if (this->alpha > 100) {
+        if (this->timer < 40) {
             Collider_CylinderUpdate(&this->actor, &this->collider);
             this->collider.dim.radius = this->actor.scale.x * 6000.0f;
             this->collider.dim.height = this->actor.scale.y * 8000.0f;
             CollisionCheck_SetAT(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
         }
     }
-    if (this->timer == 0x2E) {
+    if (this->timer == 46) {
         s32 i;
-        for (i = 0; i != 0xFFFA; i += 0x1999) {
+        for (i = 0; i < 10; i++) {
+            s32 rot = i * 0x1999;
             Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_ICE_HONO, this->actor.posRot.pos.x,
                         this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0,
-                        ((s32)(Math_Rand_ZeroOne() * 1000.0f) + i) - 0x1F4, 0, 2);
+                        ((s32)(Math_Rand_ZeroOne() * 1000.0f) + rot) - 0x1F4, 0, 2);
         }
     }
 
@@ -275,14 +275,12 @@ void EnIceHono_SetupActionSmallFlame(EnIceHono* this) {
     f32 temp_f0;
 
     this->actionFunc = EnIceHono_SmallFlameMove;
-    this->timer = 0x2C;
-    this->alpha = 0xFF;
+    this->timer = 44;
+    this->alpha = 255;
     if (this->actor.params == 1) {
         this->smallFlameTargetYScale = (Math_Rand_ZeroOne() * 0.005f) + 0.004f;
         this->actor.speedXZ = (Math_Rand_ZeroOne() * 1.6f) + 0.5f;
-    }
-    else
-    {
+    } else {
         this->smallFlameTargetYScale = (Math_Rand_ZeroOne() * 0.005f) + 0.003f;
         temp_f0 = Math_Rand_ZeroOne();
         this->actor.speedXZ = (temp_f0 + temp_f0) + 0.5f;
@@ -292,7 +290,7 @@ void EnIceHono_SetupActionSmallFlame(EnIceHono* this) {
 void EnIceHono_SmallFlameMove(EnIceHono* this, GlobalContext* globalCtx) {
     f32 unk = 10.0f;
 
-    if (this->timer >= 0x15) {
+    if (this->timer > 20) {
         Math_ApproxF(&this->actor.scale.x, 0.006f, 0.00016f);
         Math_ApproxF(&this->actor.scale.y, this->smallFlameTargetYScale * 0.667f, 0.00014f);
     } else {
@@ -304,9 +302,9 @@ void EnIceHono_SmallFlameMove(EnIceHono* this, GlobalContext* globalCtx) {
     Actor_MoveForward(&this->actor);
     func_8002E4B4(globalCtx, &this->actor, unk, unk, 0.0f, 5);
 
-    if (this->timer < 0x19) {
-        this->alpha -= 0xA;
-        this->alpha = CLAMP(this->alpha, 0, 0xFF);
+    if (this->timer < 25) {
+        this->alpha -= 10;
+        this->alpha = CLAMP(this->alpha, 0, 255);
     }
     if (this->timer <= 0) {
         Actor_Kill(&this->actor);
@@ -315,35 +313,34 @@ void EnIceHono_SmallFlameMove(EnIceHono* this, GlobalContext* globalCtx) {
 
 void EnIceHono_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnIceHono* this = THIS;
-    f32 temp_f16;
-    f32 temp_f2;
-    f32 pad;
+    s32 pad1;
+    f32 intensity;
+    s32 pad2;
     f32 sin154;
     f32 sin156;
 
     if (this->timer > 0) {
-        this->timer -= 1;
+        this->timer--;
     }
     if (this->actor.params == 0) {
-        func_8002F8F0(&this->actor, 0x1048);
+        func_8002F8F0(&this->actor, NA_SE_IT_FLAME - SFX_FLAG);
     }
     if ((this->actor.params == -1) || (this->actor.params == 0)) {
-        this->unk154 += 0x1111;
-        this->unk156 += 0x4000;
-        sin156 = Math_Sins(this->unk156);
-        sin154 = Math_Sins(this->unk154);
-        temp_f16 = (Math_Rand_ZeroOne() * 0.05f) + ((sin154 * 0.125f) + (sin156 * 0.1f));
-        temp_f2 = temp_f16 + 0.425f;
-        if ((0.7f < temp_f2) || (temp_f2 < 0.2f)) {
+        this->unk_154 += 0x1111;
+        this->unk_156 += 0x4000;
+        sin156 = Math_Sins(this->unk_156);
+        sin154 = Math_Sins(this->unk_154);
+        intensity = (Math_Rand_ZeroOne() * 0.05f) + ((sin154 * 0.125f) + (sin156 * 0.1f)) + 0.425f;
+        if ((0.7f < intensity) || (intensity < 0.2f)) {
             // Translates to: "impossible value(ratio = %f)"
-            osSyncPrintf("ありえない値(ratio = %f)\n", (f64)temp_f2);
+            osSyncPrintf("ありえない値(ratio = %f)\n", (f64)intensity);
         }
-        Lights_PointNoGlowSetInfo(
-            &this->lightInfo, this->actor.posRot.pos.x, (s16)this->actor.posRot.pos.y + 0x0A, this->actor.posRot.pos.z,
-            (s32)(155.0f * temp_f2), (s32)(210.0f * temp_f2), (s32)(255.0f * temp_f2), 0x578);
+        Lights_PointNoGlowSetInfo(&this->lightInfo, this->actor.posRot.pos.x, (s16)this->actor.posRot.pos.y + 0x0A,
+                                  this->actor.posRot.pos.z, (s32)(155.0f * intensity), (s32)(210.0f * intensity),
+                                  (s32)(255.0f * intensity), 1400);
     }
 
-    if (this->actionFunc != 0) {
+    if (this->actionFunc != NULL) {
         this->actionFunc(this, globalCtx);
     }
 }
--- src/overlays/actors/ovl_En_Ice_Hono/z_en_ice_hono.h
+++ src/overlays/actors/ovl_En_Ice_Hono/z_en_ice_hono.h
@@ -13,8 +13,8 @@ typedef struct EnIceHono {
     /* 0x014C */ EnIceHonoActionFunc actionFunc;
     /* 0x0150 */ s16 alpha;
     /* 0x0152 */ s16 timer;
-    /* 0x0154 */ s16 unk154;
-    /* 0x0156 */ s16 unk156;
+    /* 0x0154 */ s16 unk_154;
+    /* 0x0156 */ s16 unk_156;
     /* 0x0158 */ f32 smallFlameTargetYScale;
     /* 0x015C */ ColliderCylinder collider;
     /* 0x01A8 */ LightNode* lightNode;
