407d9a5204c60a4f126216024c0ff0edebcb2733
andrewzwicky <andrew.zwicky@gmail.com>
2020-09-09 13:13:55 -0500

    Another round of review comments.

--- src/overlays/actors/ovl_Bg_Ice_Shutter/z_bg_ice_shutter.c
+++ src/overlays/actors/ovl_Bg_Ice_Shutter/z_bg_ice_shutter.c
@@ -5,7 +5,6 @@
  */
 
 #include "z_bg_ice_shutter.h"
-#include "sfx.h"
 
 #define FLAGS 0x00000010
 
@@ -39,16 +38,16 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
 };
 
-void func_80891AC0(BgIceShutter* thisx) {
+void func_80891AC0(BgIceShutter* this) {
     f32 sp24;
 
-    sp24 = Math_Sins(thisx->dyna.actor.shape.rot.x) * thisx->dyna.actor.velocity.y;
-    thisx->dyna.actor.posRot.pos.y =
-        (Math_Coss(thisx->dyna.actor.shape.rot.x) * thisx->dyna.actor.velocity.y) + thisx->dyna.actor.initPosRot.pos.y;
-    thisx->dyna.actor.posRot.pos.x =
-        (Math_Sins(thisx->dyna.actor.shape.rot.y) * sp24) + thisx->dyna.actor.initPosRot.pos.x;
-    thisx->dyna.actor.posRot.pos.z =
-        (Math_Coss(thisx->dyna.actor.shape.rot.y) * sp24) + thisx->dyna.actor.initPosRot.pos.z;
+    sp24 = Math_Sins(this->dyna.actor.shape.rot.x) * this->dyna.actor.velocity.y;
+    this->dyna.actor.posRot.pos.y =
+        (Math_Coss(this->dyna.actor.shape.rot.x) * this->dyna.actor.velocity.y) + this->dyna.actor.initPosRot.pos.y;
+    this->dyna.actor.posRot.pos.x =
+        (Math_Sins(this->dyna.actor.shape.rot.y) * sp24) + this->dyna.actor.initPosRot.pos.x;
+    this->dyna.actor.posRot.pos.z =
+        (Math_Coss(this->dyna.actor.shape.rot.y) * sp24) + this->dyna.actor.initPosRot.pos.z;
 }
 
 void BgIceShutter_Init(Actor* thisx, GlobalContext* globalCtx) {
@@ -59,50 +58,53 @@ void BgIceShutter_Init(Actor* thisx, GlobalContext* globalCtx) {
     f32 temp_f6;
 
     localC = 0;
-    Actor_ProcessInitChain(thisx, sInitChain);
+    Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     DynaPolyInfo_SetActorMove(&this->dyna, 0);
-    sp28 = thisx->params & 0xFF;
-    thisx->params = (thisx->params >> 8) & 0xFF;
+    sp28 = this->dyna.actor.params & 0xFF;
+    this->dyna.actor.params = (this->dyna.actor.params >> 8) & 0xFF;
     DynaPolyInfo_Alloc(&D_06002854, &localC);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, thisx, localC);
+    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, localC);
     if (sp28 == 2) {
-        thisx->shape.rot.x = -0x4000;
+        this->dyna.actor.shape.rot.x = -0x4000;
     }
 
     if (sp28 != 1) {
-        if (Flags_GetClear(globalCtx, thisx->room)) {
-            Actor_Kill(thisx);
+        if (Flags_GetClear(globalCtx, this->dyna.actor.room)) {
+            Actor_Kill(&this->dyna.actor);
         } else {
             this->actionFunc = func_80891CF4;
         }
 
     } else {
-        if (Flags_GetSwitch(globalCtx, thisx->params)) {
-            Actor_Kill(thisx);
+        if (Flags_GetSwitch(globalCtx, this->dyna.actor.params)) {
+            Actor_Kill(&this->dyna.actor);
         } else {
             this->actionFunc = func_80891D6C;
         }
     }
 
     if (sp28 == 2) {
-        temp_f6 = Math_Sins(thisx->shape.rot.x) * 50.0f;
-        thisx->posRot2.pos.x = (Math_Sins(thisx->shape.rot.y) * temp_f6) + thisx->initPosRot.pos.x;
-        thisx->posRot2.pos.y = thisx->initPosRot.pos.y;
-        thisx->posRot2.pos.z = thisx->initPosRot.pos.z + (Math_Coss(thisx->shape.rot.y) * temp_f6);
+        temp_f6 = Math_Sins(this->dyna.actor.shape.rot.x) * 50.0f;
+        this->dyna.actor.posRot2.pos.x =
+            (Math_Sins(this->dyna.actor.shape.rot.y) * temp_f6) + this->dyna.actor.initPosRot.pos.x;
+        this->dyna.actor.posRot2.pos.y = this->dyna.actor.initPosRot.pos.y;
+        this->dyna.actor.posRot2.pos.z =
+            this->dyna.actor.initPosRot.pos.z + (Math_Coss(this->dyna.actor.shape.rot.y) * temp_f6);
     } else {
-        Actor_SetHeight(thisx, 50.0f);
+        Actor_SetHeight(&this->dyna.actor, 50.0f);
     }
 }
 
 void BgIceShutter_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, THIS->dyna.dynaPolyId);
+    BgIceShutter* this = THIS;
+    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
 }
 
 void func_80891CF4(BgIceShutter* this, GlobalContext* globalCtx) {
-    if (Flags_GetTempClear(globalCtx, this->dyna.actor.room) != 0) {
+    if (Flags_GetTempClear(globalCtx, this->dyna.actor.room)) {
         Flags_SetClear(globalCtx, this->dyna.actor.room);
-        Audio_PlaySoundAtPosition(globalCtx, &(this->dyna.actor.posRot), 0x1E, 0x2814);
-        this->actionFunc = &func_80891DD4;
+        Audio_PlaySoundAtPosition(globalCtx, &this->dyna.actor.posRot.pos, 30, NA_SE_EV_SLIDE_DOOR_OPEN);
+        this->actionFunc = func_80891DD4;
         if (this->dyna.actor.shape.rot.x == 0) {
             func_80080480(globalCtx, this);
         }
@@ -110,9 +112,9 @@ void func_80891CF4(BgIceShutter* this, GlobalContext* globalCtx) {
 }
 
 void func_80891D6C(BgIceShutter* this, GlobalContext* globalCtx) {
-    if (Flags_GetSwitch(globalCtx, this->dyna.actor.params) != 0) {
-        Audio_PlaySoundAtPosition(globalCtx, &(this->dyna.actor.posRot), 0x1E, NA_SE_EV_SLIDE_DOOR_OPEN);
-        this->actionFunc = &func_80891DD4;
+    if (Flags_GetSwitch(globalCtx, this->dyna.actor.params)) {
+        Audio_PlaySoundAtPosition(globalCtx, &this->dyna.actor.posRot.pos, 30, NA_SE_EV_SLIDE_DOOR_OPEN);
+        this->actionFunc = func_80891DD4;
         func_80080480(globalCtx, this);
     }
 }
@@ -120,7 +122,7 @@ void func_80891D6C(BgIceShutter* this, GlobalContext* globalCtx) {
 void func_80891DD4(BgIceShutter* this, GlobalContext* globalCtx) {
     Math_ApproxF(&this->dyna.actor.speedXZ, 30.0f, 2.0f);
     if (Math_ApproxF(&this->dyna.actor.velocity.y, 210.0f, this->dyna.actor.speedXZ)) {
-        Actor_Kill(this);
+        Actor_Kill(&this->dyna.actor);
         return;
     }
 
@@ -128,7 +130,8 @@ void func_80891DD4(BgIceShutter* this, GlobalContext* globalCtx) {
 }
 
 void BgIceShutter_Update(Actor* thisx, GlobalContext* globalCtx) {
-    THIS->actionFunc(THIS, globalCtx);
+    BgIceShutter* this = THIS;
+    this->actionFunc(this, globalCtx);
 }
 void BgIceShutter_Draw(Actor* thisx, GlobalContext* globalCtx) {
     Gfx_DrawDListOpa(globalCtx, D_06002740);
