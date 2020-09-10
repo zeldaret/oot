f7d632d580e2b030c73519c9faf72ac57f7f1cdc
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2020-09-10 15:31:21 -0700

    Match EnHorseNormal_Init

--- src/overlays/actors/ovl_En_Horse_Normal/z_en_horse_normal.c
+++ src/overlays/actors/ovl_En_Horse_Normal/z_en_horse_normal.c
@@ -9,6 +9,12 @@ void EnHorseNormal_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnHorseNormal_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnHorseNormal_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+// TODO: Remove later if unnecessary
+void func_80A6C6B0(EnHorseNormal* this);
+void func_80A6C4CC(EnHorseNormal* this);
+void func_80A6B91C(EnHorseNormal* this, GlobalContext* globalCtx);
+void func_80A6BC48(EnHorseNormal* this);
+
 /*
 const ActorInit En_Horse_Normal_InitVars = {
     ACTOR_EN_HORSE_NORMAL,
@@ -22,11 +28,110 @@ const ActorInit En_Horse_Normal_InitVars = {
     (ActorFunc)EnHorseNormal_Draw,
 };
 */
+
+extern AnimationHeader* D_80A6D370[];
+extern ColliderCylinderInit D_80A6D394;
+extern ColliderCylinderInit D_80A6D3C0;
+extern ColliderJntSphInit D_80A6D410;
+extern CollisionCheckInfoInit D_80A6D420;
+extern InitChainEntry D_80A6D4EC[];
+
+extern AnimationHeader D_06004580;
+extern SkeletonHeader D_06009FAC;
+
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Normal/func_80A6B250.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Normal/func_80A6B30C.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Normal/EnHorseNormal_Init.s")
+void EnHorseNormal_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnHorseNormal* this = THIS;
+    s32 pad;
+
+    Actor_ProcessInitChain(&this->actor, D_80A6D4EC);
+    Actor_SetScale(&this->actor, 0.01f);
+    this->actor.gravity = -3.5f;
+    ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawFunc_Squiggly, 20.0f);
+    this->actor.speedXZ = 0.0f;
+    this->actor.posRot2.pos = this->actor.posRot.pos;
+    this->actor.posRot2.pos.y += 70.0f;
+    this->unk_14C = 0;
+    this->unk_150 = 0;
+    Collider_InitCylinder(globalCtx, &this->unk_228);
+    Collider_SetCylinder(globalCtx, &this->unk_228, &this->actor, &D_80A6D394);
+    Collider_InitJntSph(globalCtx, &this->unk_274);
+    Collider_SetJntSph(globalCtx, &this->unk_274, &this->actor, &D_80A6D410, &this->unk_294);
+    Collider_InitCylinder(globalCtx, &this->unk_2D4);
+    Collider_SetCylinder(globalCtx, &this->unk_2D4, &this->actor, &D_80A6D3C0);
+    func_80061ED4(&this->actor.colChkInfo, NULL, &D_80A6D420);
+    if (globalCtx->sceneNum == SCENE_SPOT20) {
+        if (this->actor.posRot.rot.z == 0 || gSaveContext.nightFlag) {
+            Actor_Kill(&this->actor);
+            return;
+        }
+        if (LINK_IS_CHILD) {
+            if (Flags_GetEventChkInf(0x14)) {
+                if (this->actor.posRot.rot.z != 3) {
+                    Actor_Kill(&this->actor);
+                    return;
+                }
+            } else if (this->actor.posRot.rot.z != 1) {
+                Actor_Kill(&this->actor);
+                return;
+            }
+        } else {
+            if (Flags_GetEventChkInf(0x18) || (DREG(1) != 0)) {
+                if (this->actor.posRot.rot.z != 7) {
+                    Actor_Kill(&this->actor);
+                    return;
+                }
+            } else if (this->actor.posRot.rot.z != 5) {
+                Actor_Kill(&this->actor);
+                return;
+            }
+        }
+        this->actor.shape.rot.z = 0;
+        this->actor.initPosRot.rot.z = this->actor.posRot.rot.z = this->actor.shape.rot.z;
+        func_800A663C(globalCtx, &this->unk_154, &D_06009FAC, &D_06004580);
+        SkelAnime_ChangeAnimDefaultStop(&this->unk_154.skelAnime, D_80A6D370[this->unk_150]);
+        if ((this->actor.posRot.pos.x == -730.0f && this->actor.posRot.pos.y == 0.0f &&
+             this->actor.posRot.pos.z == -1100.0f) ||
+            (this->actor.posRot.pos.x == 880.0f && this->actor.posRot.pos.y == 0.0f &&
+             this->actor.posRot.pos.z == -1170.0f)) {
+            func_80A6C6B0(this);
+            return;
+        }
+    } else if (globalCtx->sceneNum == SCENE_MALON_STABLE) {
+        if (!gSaveContext.nightFlag) {
+            Actor_Kill(&this->actor);
+            return;
+        } else {
+            func_800A663C(globalCtx, &this->unk_154, &D_06009FAC, &D_06004580);
+            SkelAnime_ChangeAnimDefaultStop(&this->unk_154.skelAnime, D_80A6D370[this->unk_150]);
+            func_80A6C6B0(this);
+            return;
+        }
+    } else if (globalCtx->sceneNum == SCENE_SPOT12) {
+        if (this->actor.posRot.pos.x == 3707.0f && this->actor.posRot.pos.y == 1413.0f &&
+            this->actor.posRot.pos.z == -665.0f) {
+            func_800A663C(globalCtx, &this->unk_154, &D_06009FAC, &D_06004580);
+            SkelAnime_ChangeAnimDefaultStop(&this->unk_154.skelAnime, D_80A6D370[this->unk_150]);
+            func_80A6C4CC(this);
+            return;
+        }
+        func_800A663C(globalCtx, &this->unk_154, &D_06009FAC, &D_06004580);
+        SkelAnime_ChangeAnimDefaultStop(&this->unk_154.skelAnime, D_80A6D370[this->unk_150]);
+    } else {
+        func_800A663C(globalCtx, &this->unk_154, &D_06009FAC, &D_06004580);
+        SkelAnime_ChangeAnimDefaultStop(&this->unk_154.skelAnime, D_80A6D370[this->unk_150]);
+    }
+    if ((this->actor.params & 0xF0) == 0x10 && (this->actor.params & 0xF) != 0xF) {
+        func_80A6B91C(this, globalCtx);
+    } else {
+        func_80A6BC48(this);
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Normal/EnHorseNormal_Init.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Normal/EnHorseNormal_Destroy.s")
 
--- src/overlays/actors/ovl_En_Horse_Normal/z_en_horse_normal.h
+++ src/overlays/actors/ovl_En_Horse_Normal/z_en_horse_normal.h
@@ -8,7 +8,15 @@ struct EnHorseNormal;
 
 typedef struct EnHorseNormal {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x1DC];
+    /* 0x014C */ s32 unk_14C;
+    /* 0x0150 */ s32 unk_150;
+    /* 0x0154 */ PSkinAwb unk_154;
+    /* 0x01E4 */ char unk_1E4[0x44];
+    /* 0x0228 */ ColliderCylinder unk_228;
+    /* 0x0274 */ ColliderJntSph unk_274;
+    /* 0x0294 */ ColliderJntSphItem unk_294;
+    /* 0x02D4 */ ColliderCylinder unk_2D4;
+    /* 0x0320 */ char unk_320[0x08];
 } EnHorseNormal; // size = 0x0328
 
 extern const ActorInit En_Horse_Normal_InitVars;
