36cfd879d66466f5408c8339128a228c26e8c02d
FryJay <darin.j.fry@gmail.com>
2020-09-11 22:40:49 -0700

    Added ability to provide ichaindis a symbol and gave it ability to look up actor var names

--- tools/ichaindis.py
+++ tools/ichaindis.py
@@ -4,6 +4,7 @@ import os
 import sys
 import struct
 import argparse
+import re
 
 ICHAIN_MACROS = [
     'ICHAIN_U8',
@@ -19,11 +20,54 @@ ICHAIN_MACROS = [
     'ICHAIN_VEC3S',
 ]
 
+Z64_ACTOR_PATH = "../include/z64actor.h"
+
+actor_variable_names = {}
+
+def get_rom_address(offset):
+    # Run the sym_info script and parse out the ROM address
+    stream = os.popen('cd .. && python3 sym_info.py ' + offset)
+    output = stream.read()
+    rom_address = re.match(r'.*ROM:\s+(.*),.*', output)
+
+    if rom_address:
+        return rom_address[1][2:]
+    return None
+
+def get_actor_var_names():
+    in_actor = False
+    actor_vars = {}
+    with open(Z64_ACTOR_PATH) as actor_h:
+        for line in actor_h:
+            if in_actor:
+                # Check if we've reached the end of the Actor struct
+                if "}" in line:
+                    break
+                
+                # Parse out the memory address from the comment and the variable name
+                regex = r'.*\/\* (.*) \*\/\s+(struct)?\s*.+\s+(.+);.*'
+                actor_var_info = re.match(regex, line)
+
+                if actor_var_info:
+                    # Strip off the 0x0* part and store it
+                    new_var_index = re.sub('0x0*', '', actor_var_info[1])
+                    actor_vars[new_var_index] = actor_var_info[3]
+
+            elif "typedef struct Actor {" in line:
+                # Found the Actor struct
+                in_actor = True
+    return actor_vars
+
 def main():
     parser = argparse.ArgumentParser(description='Decompiles an InitChain')
     parser.add_argument('filename', help='ROM file path')
-    parser.add_argument('offset', help='ROM offset to an InitChain')
+    parser.add_argument('offset', help='ROM offset or symbol of an InitChain')
     args = parser.parse_args()
+    
+    # Check if it was a symbol provided and convert it to a ROM address if so
+    if 'D_' in args.offset:
+        args.offset = get_rom_address(args.offset)
+
     romOff = int(args.offset, 16)
 
     try:
@@ -34,6 +78,9 @@ def main():
         sys.exit(1)
 
     print ('static InitChainEntry sInitChain[] = {')
+
+    actor_variable_names = get_actor_var_names()
+
     while True:
         entry = struct.unpack('>I', romData[romOff:romOff+4])[0]
         romOff += 4
@@ -43,7 +90,14 @@ def main():
         offset = ((entry) >> 16) & 0x7FF
         value = (entry) & 0xFFFF
 
-        print('    {0}(unk_{1:X}, {2}, {3}),'.format(ICHAIN_MACROS[t], offset, value, ('ICHAIN_CONTINUE' if cont == 1 else 'ICHAIN_STOP')))
+        var_name = '{0:X}'.format(offset)
+
+        if var_name in actor_variable_names:
+            var_name = actor_variable_names[var_name]
+        else:
+            var_name = "unk_" + var_name
+        
+        print('    {0}({1}, {2}, {3}),'.format(var_name, offset, value, ('ICHAIN_CONTINUE' if cont == 1 else 'ICHAIN_STOP')))
         if cont == 0:
             break
     print ('};')
