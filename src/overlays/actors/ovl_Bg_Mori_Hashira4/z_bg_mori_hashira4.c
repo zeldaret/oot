901bab545399f6cd0fddc493d25ec6730269cb02
petrie911 <petrie911@yahoo.com>
2020-09-22 15:38:36 -0500

    merge and cleanup

--- src/overlays/actors/ovl_Bg_Mori_Hashira4/z_bg_mori_hashira4.c
+++ src/overlays/actors/ovl_Bg_Mori_Hashira4/z_bg_mori_hashira4.c
@@ -9,12 +9,15 @@ void BgMoriHashira4_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgMoriHashira4_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgMoriHashira4_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-void BgMoriHashira4_SetupObjectCheck(BgMoriHashira4* this);
-void BgMoriHashira4_SetupPillars(BgMoriHashira4* this);
+void BgMoriHashira4_SetupCheckForMoriTex(BgMoriHashira4* this);
+void BgMoriHashira4_CheckForMoriTex(BgMoriHashira4* this, GlobalContext* globalCtx);
+void BgMoriHashira4_SetupPillarsRotate(BgMoriHashira4* this);
 void BgMoriHashira4_PillarsRotate(BgMoriHashira4* this, GlobalContext* globalCtx);
-void BgMoriHashira4_GateOpen(BgMoriHashira4* this, GlobalContext* globalCtx);
-void BgMoriHashira4_ObjectCheck(BgMoriHashira4* this, GlobalContext* globalCtx);
 void BgMoriHashira4_GateWait(BgMoriHashira4* this, GlobalContext* globalCtx);
+void BgMoriHashira4_GateOpen(BgMoriHashira4* this, GlobalContext* globalCtx);
+
+extern ColHeader D_06001AF8;
+extern ColHeader D_060089E0;
 
 const ActorInit Bg_Mori_Hashira4_InitVars = {
     ACTOR_BG_MORI_HASHIRA4,
@@ -35,12 +38,9 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 1000, ICHAIN_STOP),
 };
 
-extern ColHeader D_06001AF8;
-extern ColHeader D_060089E0;
+static Gfx* sDispLists[] = { 0x06001300, 0x06008840 };
 
-static Gfx* displayLists[] = { 0x06001300, 0x06008840 };
-
-static s16 unkTimer; //seems to be unused
+static s16 sUnkTimer; // seems to be unused
 
 void BgMoriHashira4_SetupAction(BgMoriHashira4* this, BgMoriHashira4ActionFunc actionFunc) {
     this->actionFunc = actionFunc;
@@ -65,49 +65,53 @@ void BgMoriHashira4_InitDynaPoly(BgMoriHashira4* this, GlobalContext* globalCtx,
 }
 
 void BgMoriHashira4_Init(Actor* thisx, GlobalContext* globalCtx) {
-    GlobalContext* globalCtx2 = globalCtx;
+    s32 pad;
     BgMoriHashira4* this = THIS;
 
     this->switchFlag = (this->dyna.actor.params >> 8) & 0x3F;
     this->dyna.actor.params &= 0xFF;
 
     if (this->dyna.actor.params == 0) {
-        BgMoriHashira4_InitDynaPoly(this, globalCtx2, &D_06001AF8, DPM_UNK3);
+        BgMoriHashira4_InitDynaPoly(this, globalCtx, &D_06001AF8, DPM_UNK3);
     } else {
-        BgMoriHashira4_InitDynaPoly(this, globalCtx2, &D_060089E0, DPM_UNK);
+        BgMoriHashira4_InitDynaPoly(this, globalCtx, &D_060089E0, DPM_UNK);
     }
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
-    this->objBankIndex = Object_GetIndex(&globalCtx2->objectCtx, OBJECT_MORI_TEX);
-    if (this->objBankIndex < 0) {
+    this->moriTexObjIndex = Object_GetIndex(&globalCtx->objectCtx, OBJECT_MORI_TEX);
+    if (this->moriTexObjIndex < 0) {
         Actor_Kill(&this->dyna.actor);
+        // Bank danger!
         osSyncPrintf("Error : バンク危険！(arg_data 0x%04x)(%s %d)\n", this->dyna.actor.params,
                      "../z_bg_mori_hashira4.c", 196);
-    } else if ((this->dyna.actor.params != 0) && Flags_GetSwitch(globalCtx2, this->switchFlag)) {
-        Actor_Kill(&this->dyna.actor);
-    } else {
-        Actor_SetHeight(&this->dyna.actor, 50.0f);
-        BgMoriHashira4_SetupObjectCheck(this);
-        osSyncPrintf("(森の神殿 ４本柱)(arg_data 0x%04x)\n", this->dyna.actor.params);
-        unkTimer = 0;
+        return;
     }
+    if ((this->dyna.actor.params != 0) && Flags_GetSwitch(globalCtx, this->switchFlag)) {
+        Actor_Kill(&this->dyna.actor);
+        return;
+    }
+    Actor_SetHeight(&this->dyna.actor, 50.0f);
+    BgMoriHashira4_SetupCheckForMoriTex(this);
+    // (4 pillars of the Forest Temple) Bank danger
+    osSyncPrintf("(森の神殿 ４本柱)(arg_data 0x%04x)\n", this->dyna.actor.params);
+    sUnkTimer = 0;
 }
 
 void BgMoriHashira4_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    GlobalContext* globalCtx2 = globalCtx;
+    s32 pad;
     BgMoriHashira4* this = THIS;
 
-    DynaPolyInfo_Free(globalCtx2, &globalCtx2->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
 }
 
-void BgMoriHashira4_SetupObjectCheck(BgMoriHashira4* this) {
-    BgMoriHashira4_SetupAction(this, BgMoriHashira4_ObjectCheck);
+void BgMoriHashira4_SetupCheckForMoriTex(BgMoriHashira4* this) {
+    BgMoriHashira4_SetupAction(this, BgMoriHashira4_CheckForMoriTex);
 }
 
-void BgMoriHashira4_ObjectCheck(BgMoriHashira4* this, GlobalContext* globalCtx) {
-    if (Object_IsLoaded(&globalCtx->objectCtx, this->objBankIndex)) {
+void BgMoriHashira4_CheckForMoriTex(BgMoriHashira4* this, GlobalContext* globalCtx) {
+    if (Object_IsLoaded(&globalCtx->objectCtx, this->moriTexObjIndex)) {
         this->gateTimer = 0;
         if (this->dyna.actor.params == 0) {
-            BgMoriHashira4_SetupPillars(this);
+            BgMoriHashira4_SetupPillarsRotate(this);
         } else {
             BgMoriHashira4_SetupAction(this, BgMoriHashira4_GateWait);
         }
@@ -115,7 +119,7 @@ void BgMoriHashira4_ObjectCheck(BgMoriHashira4* this, GlobalContext* globalCtx) 
     }
 }
 
-void BgMoriHashira4_SetupPillars(BgMoriHashira4* this) {
+void BgMoriHashira4_SetupPillarsRotate(BgMoriHashira4* this) {
     BgMoriHashira4_SetupAction(this, BgMoriHashira4_PillarsRotate);
 }
 
@@ -131,7 +135,7 @@ void BgMoriHashira4_GateWait(BgMoriHashira4* this, GlobalContext* globalCtx) {
             Audio_PlayActorSound2(&this->dyna.actor, NA_SE_EV_METALDOOR_OPEN);
             BgMoriHashira4_SetupAction(this, BgMoriHashira4_GateOpen);
             func_800800F8(globalCtx, 0x177A, 0x14, &this->dyna.actor, 0);
-            unkTimer++;
+            sUnkTimer++;
         }
     }
 }
@@ -143,26 +147,26 @@ void BgMoriHashira4_GateOpen(BgMoriHashira4* this, GlobalContext* globalCtx) {
 }
 
 void BgMoriHashira4_Update(Actor* thisx, GlobalContext* globalCtx) {
-    GlobalContext* globalCtx2 = globalCtx;
+    s32 pad;
     BgMoriHashira4* this = THIS;
 
     if (this->actionFunc != NULL) {
-        this->actionFunc(this, globalCtx2);
+        this->actionFunc(this, globalCtx);
     }
 }
 
 void BgMoriHashira4_Draw(Actor* thisx, GlobalContext* globalCtx) {
-    GlobalContext* globalCtx2 = globalCtx;
+    s32 pad;
     BgMoriHashira4* this = THIS;
 
-    OPEN_DISPS(globalCtx2->state.gfxCtx, "../z_bg_mori_hashira4.c", 339);
-    func_80093D18(globalCtx2->state.gfxCtx);
+    OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_mori_hashira4.c", 339);
+    func_80093D18(globalCtx->state.gfxCtx);
 
-    gSPSegment(oGfxCtx->polyOpa.p++, 0x08, globalCtx2->objectCtx.status[this->objBankIndex].segment);
+    gSPSegment(oGfxCtx->polyOpa.p++, 0x08, globalCtx->objectCtx.status[this->moriTexObjIndex].segment);
 
-    gSPMatrix(oGfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx2->state.gfxCtx, "../z_bg_mori_hashira4.c", 344),
+    gSPMatrix(oGfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_mori_hashira4.c", 344),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
 
-    gSPDisplayList(oGfxCtx->polyOpa.p++, displayLists[this->dyna.actor.params]);
-    CLOSE_DISPS(globalCtx2->state.gfxCtx, "../z_bg_mori_hashira4.c", 348);
+    gSPDisplayList(oGfxCtx->polyOpa.p++, sDispLists[this->dyna.actor.params]);
+    CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_mori_hashira4.c", 348);
 }
