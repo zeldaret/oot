7285660578fb50c7400eefbbe041856e0b585081
Louis <35883445+louist103@users.noreply.github.com>
2020-10-17 01:11:39 -0400

    Start functions

--- src/overlays/actors/ovl_En_Niw_Lady/z_en_niw_lady.c
+++ src/overlays/actors/ovl_En_Niw_Lady/z_en_niw_lady.c
@@ -1,4 +1,5 @@
 #include "z_en_niw_lady.h"
+#include "vt.h"
 
 #define FLAGS 0x00000019
 
@@ -7,6 +8,12 @@
 void EnNiwLady_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnNiwLady_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnNiwLady_Update(Actor* thisx, GlobalContext* globalCtx);
+
+void func_80AB9F24(EnNiwLady* this, GlobalContext* globalCtx);
+void func_80ABB228(EnNiwLady* this, GlobalContext* globalCtx);
+void func_80ABA21C(EnNiwLady* this, GlobalContext* globalCtx);
+void func_80ABA778(EnNiwLady* this, GlobalContext* globalCtx);
+void func_80ABAD38(EnNiwLady* this, GlobalContext* globalCtx);
 
 const ActorInit En_Niw_Lady_InitVars = {
     ACTOR_EN_NIW_LADY,
@@ -31,26 +38,14 @@ s32 D_80ABB3B4[] = {
     0x80000000,
 };
 
-s32 D_80ABB3C4[] = {
-    0x0A000039, 0x20010000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00010100, 0x000A000A, 0x00000000, 0x00000000,
+static ColliderCylinderInit sCylinderInit = {
+    { COLTYPE_UNK10, 0x00, 0x00, 0x39, 0x20, COLSHAPE_CYLINDER },
+    { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000000, 0x00, 0x00 }, 0x00, 0x01, 0x01 },
+    { 10, 10, 0, { 0, 0, 0 } },
 };
 
 s16 D_80ABB3F0[] = {
-    0x503E, 0x503F, 0x5047,
-//};
-
-//s16 D_80ABB3F6[] = {
-    0x5040,
-    0x5042,
-//};
-//s16 D_80ABB3FA[] = {
-    0x5043,
-    0x5044,
-//}
-
-//s16 D_80ABB3FE[] = {
-    0x00CF,
+    0x503E, 0x503F, 0x5047, 0x5040, 0x5042, 0x5043, 0x5044, 0x00CF,
 };
 
 s32 D_80ABB400[] = {
@@ -58,17 +53,82 @@ s32 D_80ABB400[] = {
     0x50270000,
 };
 
-s32 D_80ABB408[] ={
+s32 D_80ABB408[] = {
     0x060008C8, 0x060010C8, 0x060018C8, 0x00000000, 0x00000000, 0x00000000,
 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Lady/EnNiwLady_Init.s")
+extern AnimationHeader D_060000F0;
+extern SkeletonHeader D_0600A630;
+extern SkeletonHeader D_060007D0;
+
+void EnNiwLady_Init(Actor* thisx, GlobalContext* globalCtx) {
+    // s16 this->skelAnime;
+    s32 pad;
+    EnNiwLady* this = THIS;
+    this->ObjectAneIndex = Object_GetIndex(&globalCtx->objectCtx, OBJECT_ANE);
+    this->ObjectOsAnimeIndex = Object_GetIndex(&globalCtx->objectCtx, OBJECT_OS_ANIME);
+    if ((this->ObjectOsAnimeIndex < 0) || (this->ObjectAneIndex < 0)) {
+        Actor_Kill(thisx);
+        return;
+    }
+
+    this->unk_278 = 0;
+    if (globalCtx->sceneNum == SCENE_LABO) {
+        this->unk_278 = 1;
+    }
+    // this->skelAnime = this->unk_278;
+    if ((this->unk_278 != 0) && (gSaveContext.nightFlag == 0)) {
+        Actor_Kill(thisx);
+        return;
+    }
+    osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ ねぇちゃんうっふん ☆☆☆☆☆ %d\n" VT_RST, this->unk_278);
+    osSyncPrintf("\n\n");
+    this->actionFunc = func_80AB9F24;
+    thisx->uncullZoneForward = 600.0f;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Lady/EnNiwLady_Destroy.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Lady/func_80AB9D60.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Lady/func_80AB9F24.s")
+/*void func_80AB9F24(EnNiwLady* this, GlobalContext* globalCtx) {
+    if ((Object_IsLoaded(&globalCtx->objectCtx, this->ObjectAneIndex) != 0) &&
+        (Object_IsLoaded(&globalCtx->objectCtx, this->ObjectOsAnimeIndex) != 0)) {
+
+        gSegments[6] = PHYSICAL_TO_VIRTUAL(globalCtx->objectCtx.status[this->ObjectAneIndex].segment);
+        SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_060000F0, NULL, &this->limbDrawTable,
+                         &this->transitionDrawTable, 16);
+        gSegments[6] = PHYSICAL_TO_VIRTUAL(globalCtx->objectCtx.status[this->ObjectOsAnimeIndex].segment);
+        this->unk_27E = (u16)1;
+        this->actor.gravity = -3.0f;
+        Actor_SetScale(&this->actor, 0.01f);
+        ActorShape_Init(&this->actor.shape, 0.0f, &ActorShadow_DrawFunc_Circle, 20.0f);
+        Collider_InitCylinder(globalCtx, &this->collider);
+        Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+        this->unk_272 = 0;
+        this->actor.unk_1F = 6;
+        this->actor.draw = func_80ABB228;
+        if (this->unk_278 != 0) {
+            if (!(gSaveContext.itemGetInf[0] & 0x1000) && LINK_IS_CHILD) {
+                SkelAnime_ChangeAnim(&this->skelAnime, &D_0600A630, 1.0f, 0.0f,
+                                     (f32)(s16)(s32)(f32)SkelAnime_GetFrameCount(&D_0600A630), 0, 0.0f);
+            } else {
+                SkelAnime_ChangeAnim(&this->skelAnime, &D_060007D0, 1.0f, 0.0f,
+                                     (f32)(s16)(s32)(f32)SkelAnime_GetFrameCount(&D_060007D0), 0, 0.0f);
+            }
+            if (LINK_IS_ADULT) {
+                this->actionFunc = func_80ABA778;
+            } else {
+                this->actionFunc = func_80ABA21C;
+            }
+        } else if (this->unk_278 != 1) {
+            SkelAnime_ChangeAnim(&this->skelAnime, &D_060007D0, 1.0f, 0.0f,
+                                 (f32)(s16)(s32)(f32)SkelAnime_GetFrameCount(&D_060007D0), 0, 0.0f);
+            this->actionFunc = func_80ABAD38;
+        }
+    }
+}*/
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Niw_Lady/func_80ABA21C.s")
 
--- src/overlays/actors/ovl_En_Niw_Lady/z_en_niw_lady.h
+++ src/overlays/actors/ovl_En_Niw_Lady/z_en_niw_lady.h
@@ -6,9 +6,24 @@
 
 struct EnNiwLady;
 
+typedef void (*EnNiwLadyActionFunc)(struct EnNiwLadyt*, GlobalContext*);
+
 typedef struct EnNiwLady {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x1B0];
+    /* 0x014C */ SkelAnime skelAnime;
+    /* 0x0190 */ Vec3s limbDrawTable[16];//char unk_190[0xC0];
+    /* 0x01F0 */ Vec3s transitionDrawTable[16];
+    /* 0x0250 */ EnNiwLadyActionFunc actionFunc;
+    /* 0x0254 */ char unk_254[0x1E];
+    /* 0x0272 */ u8 unk_272;
+    /* 0x0273 */ char unk_273[5];
+    /* 0x0278 */ s16 unk_278;
+    /* 0x027A */ char unk_27A[4];
+    /* 0x027E */ u16 unk_27E;
+    /* 0x0280 */ s8 ObjectAneIndex;
+    /* 0x0281 */ s8 ObjectOsAnimeIndex;
+    /* 0x0282 */ char unk_282[0x2E];
+    /* 0x02B0 */ ColliderCylinder collider;
 } EnNiwLady; // size = 0x02FC
 
 extern const ActorInit En_Niw_Lady_InitVars;
