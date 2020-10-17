8577f616dec5f5bbbe9269919560339695fd0cba
maekclena <maggot_fff@hotmail.fr>
2020-10-17 16:51:01 +0200

    ovl_En_Siofuki

--- src/overlays/actors/ovl_En_Siofuki/z_en_siofuki.h
+++ src/overlays/actors/ovl_En_Siofuki/z_en_siofuki.h
@@ -6,9 +6,26 @@
 
 struct EnSiofuki;
 
+typedef void (*EnSiofukiActionFunc)(struct EnSiofuki*, GlobalContext*);
+
 typedef struct EnSiofuki {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x54];
+    /* 0x0000 */ DynaPolyActor dyna;
+    /* 0x0164 */ EnSiofukiActionFunc actionFunc;
+    /* 0x0168 */ s32 timer;
+    /* 0x016C */ f32 initPosY;
+    /* 0x0170 */ f32 unk_170;
+    /* 0x0174 */ f32 unk_174;
+    /* 0x0178 */ f32 unk_178;
+    /* 0x017C */ f32 unk_17C;
+    /* 0x0180 */ f32 unk_180;
+    /* 0x0184 */ s32 splashTimer;
+    /* 0x0188 */ s32 applySpeed;
+    /* 0x018C */ f32 appliedSpeed;
+    /* 0x0190 */ f32 targetAppliedSpeed;
+    /* 0x0194 */ s16 appliedYaw;
+    /* 0x0196 */ s16 initRotY;
+    /* 0x0198 */ f32 unk_198;
+    /* 0x019C */ u8 sfxFlags;
 } EnSiofuki; // size = 0x01A0
 
 extern const ActorInit En_Siofuki_InitVars;
