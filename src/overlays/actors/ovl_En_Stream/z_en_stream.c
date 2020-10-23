709a909c90ec72e59351591f7b21691df207fc01
tom-overton <tom.overton@outlook.com>
2020-10-23 10:39:06 -0700

    func_80B0B934 OK

--- src/overlays/actors/ovl_En_Stream/z_en_stream.c
+++ src/overlays/actors/ovl_En_Stream/z_en_stream.c
@@ -88,38 +88,37 @@ void EnStream_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 //     return phi_v1;
 // }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Stream/func_80B0B934.s")
-// void func_80B0B934(EnStream* this, GlobalContext* globalCtx) {
-//     Player* player = PLAYER;
-//     f32 sp34;
-//     Vec3f sp3C;
-//     s32 pad;
-//     f32 temp_f2;
-//     f32 temp_f2_2;
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Stream/func_80B0B934.s")
+void func_80B0B934(EnStream* this, GlobalContext* globalCtx) {
+    Player* player = PLAYER;
+    s32 pad48;
+    Vec3f sp3C;
+    f32 sp38;
+    f32 sp34;
+    s32 pad30;
+    s32 pad2C;
 
-//     if (func_80B0B81C(&this->actor.posRot, &player->actor.posRot, &sp3C, this->actor.scale.y) != 0) {
-//         temp_f2 = sqrtf(SQ(sp3C.x) + SQ(sp3C.z));
-//         sp34 = player->actor.posRot.pos.y - (this->actor.posRot.pos.y - 90.0f);
-//         temp_f2_2 = temp_f2;
-//         player->windDirection = (s16) (s32) (Math_atan2f(-sp3C.x, -sp3C.z) * 10430.378f);
-//         if (3.0f < temp_f2_2) {
-//             Math_SmoothScaleMaxMinF(&player->windSpeed, 3.0f, 0.5f, temp_f2_2, 0.0f);
-//         } else {
-//             player->windSpeed = 0.0f;
-//             Math_SmoothScaleMaxMinF(&player->actor.posRot.pos.x, this->actor.posRot.pos.x, 0.5f, 3.0f, 0.0f);
-//             Math_SmoothScaleMaxMinF(&player->actor.posRot.pos.z, this->actor.posRot.pos.z, 0.5f, 3.0f, 0.0f);
-//         }
-//         if (0.0f < sp34) {
-//             Math_SmoothScaleMaxMinF(&player->actor.velocity.y, -3.0f, 0.7f, sp34, 0.0f);
-//             if (sp3C.y < -70.0f) {
-//                 player->stateFlags2 |= 0x80000000;
-//                 return;
-//             }
-//         }
-//     } else {
-//         func_80B0B7A0(this, &func_80B0BAC8);
-//     }
-// }
+    if (func_80B0B81C(&this->actor.posRot, &player->actor.posRot, &sp3C, this->actor.scale.y) != 0) {
+        sp38 = sqrtf(SQ(sp3C.x) + SQ(sp3C.z));
+        sp34 = player->actor.posRot.pos.y - (this->actor.posRot.pos.y - 90.0f);
+        player->windDirection = Math_atan2f(-sp3C.x, -sp3C.z) * 10430.378f;
+        if (3.0f < sp38) {
+            Math_SmoothScaleMaxMinF(&player->windSpeed, 3.0f, 0.5f, sp38, 0.0f);
+        } else {
+            player->windSpeed = 0.0f;
+            Math_SmoothScaleMaxMinF(&player->actor.posRot.pos.x, this->actor.posRot.pos.x, 0.5f, 3.0f, 0.0f);
+            Math_SmoothScaleMaxMinF(&player->actor.posRot.pos.z, this->actor.posRot.pos.z, 0.5f, 3.0f, 0.0f);
+        }
+        if (0.0f < sp34) {
+            Math_SmoothScaleMaxMinF(&player->actor.velocity.y, -3.0f, 0.7f, sp34, 0.0f);
+            if (sp3C.y < -70.0f) {
+                player->stateFlags2 |= 0x80000000;
+            }
+        }
+    } else {
+        func_80B0B7A0(this, &func_80B0BAC8);
+    }
+}
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Stream/func_80B0BAC8.s")
 void func_80B0BAC8(EnStream* this, GlobalContext* globalCtx) {
