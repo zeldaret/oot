52a99a58b1906649db0622db17d470f698ee43aa
Parker Burnett <burnettparker@gmail.com>
2020-10-27 23:55:28 -0400

    more progress. almost matches init

--- src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
+++ src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -44,13 +44,15 @@ static ColliderCylinderInit D_80A9C208 = {
 };
 
 // Collision Check data
+// sColChkInfoInit
 // s32 D_80A9C234[] = { 0x0000000C, 0x001EFF00 };
-CollisionCheckInfoInit sColChkInfoInit = { 0, 0xC, 0x1E, 0xFF };
+CollisionCheckInfoInit D_80A9C234 = { 0, 0xC, 0x1E, 0xFF };
 
 s32 D_80A9C23C[] = { 0x00000000, 0x3F350481, 0x3F350481, 0x3F350481, 0x3F350481, 0x00000000,
                      0x00000000, 0x3F350481, 0xBF350481, 0xBF350481, 0x3F350481, 0x00000000 };
 
 s32 D_80A9C26C[] = { 0x006C0066, 0x00600054, 0x00420037, 0x002A0026 };
+
 
 // sInitChain
 InitChainEntry D_80A9C27C[] = {
@@ -64,7 +66,7 @@ s32 D_80A9C294[] = { 0x0500B9D0, 0x06000140, 0x06000140 };
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9AFA0.s")
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9AFAC.s")
-//Snap to floor?
+// Snap to floor?
 s32 func_80A9AFAC(EnKusa* this, GlobalContext* globalCtx, f32 arg2) {
     CollisionPoly* sp38;
     Vec3f sp28;
@@ -89,6 +91,16 @@ s32 func_80A9AFAC(EnKusa* this, GlobalContext* globalCtx, f32 arg2) {
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B07C.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B140.s")
+// f32 func_80A9B140(EnKusa *this) {
+//     f32 temp_f0;
+
+//     temp_f0 = this->unk70;
+//     this->unk60 = (f32) (this->unk60 + this->unk6C);
+//     if (this->unk60 < temp_f0) {
+//         this->unk60 = temp_f0;
+//     }
+//     return temp_f0;
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B174.s")
 
@@ -100,8 +112,7 @@ s32 func_80A9AFAC(EnKusa* this, GlobalContext* globalCtx, f32 arg2) {
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B630.s")
 // ColliderCylinder Init function from the looks of it
-void func_80A9B630(Actor* thisx, GlobalContext* globalCtx) {
-    EnKusa* this = THIS;
+void func_80A9B630(EnKusa* this, GlobalContext* globalCtx) {
 
     // Cylinder is going to be at actor(arg0) + 0x150
     Collider_InitCylinder(globalCtx, &this->collider);
@@ -114,35 +125,35 @@ void EnKusa_Init(EnKusa* this, GlobalContext* globalCtx) {
 
     Actor_ProcessInitChain(&this->actor, D_80A9C27C);
     if (globalCtx->csCtx.state != 0) {
-        this->actor.uncullZoneForward = this->actor.uncullZoneForward + 1000.0f;
+        this->actor.uncullZoneForward += 1000.0f;
     }
     // Init for the ColliderCylinder
-    func_80A9B630(&this->actor, globalCtx);
+    func_80A9B630(this, globalCtx);
 
-    func_80061ED4(&this->actor.colChkInfo, NULL, &sColChkInfoInit);
+    func_80061ED4(&this->actor.colChkInfo, NULL, &D_80A9C234);
     if (this->actor.shape.rot.y == 0) {
         // Random value I think?
-        s32 rand = (s32)Math_Rand_ZeroFloat(65536.0f);
+        s32 rand = (s32) Math_Rand_ZeroFloat(65536.0f);
         this->actor.posRot.rot.y = (s16)rand;
         this->actor.initPosRot.rot.y = (s16)rand;
         this->actor.shape.rot.y = (s16)rand;
     }
 
-    if (func_80A9AFAC(this, globalCtx, 0) == 0) {
+    if (func_80A9AFAC(&this->actor, globalCtx, 0) == 0) {
         Actor_Kill(&this->actor);
         return;
     }
     // Original Line
-    // this->unk19E = Object_GetIndex(&globalCtx->objectCtx, *(D_80A9C200 + ((this->actor.params & 3) * 2)));
-    this->kusaTexObjIndex = Object_GetIndex(&globalCtx->objectCtx, OBJECT_KUSA);
+    // TODO: Something is wrong with this line...
+    this->collider.dim.pos.z = Object_GetIndex(&globalCtx->objectCtx, *(D_80A9C200 + ((this->actor.params & 3) * 2)));
 
-    if (this->kusaTexObjIndex < 0) {
+    if ((s8)this->collider.dim.pos.z < 0) {
         // Prints -> Bank danger!
-        osSyncPrintf("Error : バンク危険！(arg_data 0x%04x)(%s %d)\n", this->actor.params, "../z_en_kusa.c", 561);
+        osSyncPrintf((const char *)"Error : バンク危険！(arg_data 0x%04x)(%s %d)\n", this->actor.params, "../z_en_kusa.c", 0x231);
         Actor_Kill(&this->actor);
         return;
     }
-    func_80A9B7EC(this, "../z_en_kusa.c");
+    func_80A9B7EC(this);
 }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/EnKusa_Destroy.s")
@@ -153,36 +164,119 @@ void EnKusa_Init(EnKusa* this, GlobalContext* globalCtx) {
 //     Collider_DestroyCylinder(globalCtx, thisx + 0x150);
 // }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B7EC.s")
-void func_80A9B7EC(void) {
-    func_80A9AFA0(&func_80A9B810);
-}
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B7EC.s")
+// void func_80A9B7EC(EnKusa *this, char* pathToFile) {
+//     func_80A9AFA0(&func_80A9B810);
+// }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B810.s")
-void func_80A9B810(EnKusa *this, GlobalContext* globalCtx) {
-    if (Object_IsLoaded(&globalCtx->objectCtx + 0x117A4, (s32) this->unk19E) != 0) {
-        if ((arg0->unk4 & 0x800) != 0) {
-            func_80A9BEAC(arg0);
-        } else {
-            func_80A9B89C(arg0);
-        }
-        this->unk134 = &func_80A9C164;
-        this->unk4 = (s32) (arg0->unk4 & -0x11);
-        this->unk1E = (s8) arg0->unk19E;
-    }
-}
-
-
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B810.s")
+// void func_80A9B810(EnKusa *this, GlobalContext* globalCtx) {
+//     if (Object_IsLoaded(&globalCtx->objectCtx + 0x117A4, this->kusaTexObjIndex) != 0) {
+//         if ((this->unk4 & 0x800) != 0) {
+//             func_80A9BEAC(this);
+//         } else {
+//             func_80A9B89C(this);
+//         }
+//         this->unk134 = &func_80A9C164;
+//         this->unk4 = (s32) (this->unk4 & -0x11);
+//         this->unk1E = (s8) this->unk19E;
+//     }
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B89C.s")
+// void func_80A9B89C(EnKusa *this) {
+//     func_80A9AFA0(&func_80A9B8D8);
+//     this->unk4 = (s32) (this->unk4 & -0x11);
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9B8D8.s")
+// void func_80A9B8D8(EnKusa *this, GlobalContext *globalCtx) {
+//     ColliderCylinder *sp28;
+//     CollisionCheckContext *sp24;
+//     ColliderCylinder *temp_a1;
+//     CollisionCheckContext *temp_a1_2;
+//     u8 temp_v0;
+//     u8 temp_v0_2;
+
+//     if (Actor_HasParent(this, globalCtx) != 0) {
+//         func_80A9BA98(this);
+//         Audio_PlaySoundAtPosition(globalCtx, &this->actor.posRot.pos, 20, NA_SE_PL_PULL_UP_PLANT);
+//         return;
+//     }
+//     temp_v0 = this->unk161;
+//     if ((temp_v0 & 2) != 0) {
+//         this->unk161 = (u8) (temp_v0 & 0xFFFD);
+//         func_80A9B21C(this, globalCtx);
+//         func_80A9B07C(this, globalCtx);
+//         Audio_PlaySoundAtPosition(globalCtx, &this->actor.posRot.pos, 20, NA_SE_EV_PLANT_BROKEN);
+//         if ((((s32) this->params >> 4) & 1) != 0) {
+//             func_80A9B574(this, globalCtx);
+//         }
+//         if ((this->params & 3) != 0) {
+//             func_80A9BEAC(this);
+//             this->flags = this->flags | 0x800;
+//             return;
+//         }
+//         Actor_Kill(this);
+//         return;
+//     }
+//     temp_v0_2 = this->unk162;
+//     temp_a1 = this + 0x150;
+//     if (((temp_v0_2 & 8) == 0) && (12.0f < this->xzDistFromLink)) {
+//         this->unk162 = (u8) (temp_v0_2 | 8);
+//     }
+//     if (this->xzDistFromLink < 600.0f) {
+//         sp28 = temp_a1;
+//         Collider_CylinderUpdate(this, temp_a1);
+//         temp_a1_2 = &globalCtx->colChkCtx;
+//         sp24 = temp_a1_2;
+//         CollisionCheck_SetAC(globalCtx, temp_a1_2, (Collider *) sp28);
+//         if (this->xzDistFromLink < 400.0f) {
+//             CollisionCheck_SetOC(globalCtx, temp_a1_2, (Collider *) sp28);
+//             if (this->xzDistFromLink < 100.0f) {
+//                 func_8002F580(this, globalCtx);
+//             }
+//         }
+//     }
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9BA98.s")
+// Possibly SetupLiftedUp? -> modeled after EnIshi_SetupLiftedUp
+// void func_80A9BA98(EnKusa *this) {
+//     func_80A9AFA0(&func_80A9BAD8);
+//     this->actor.room = -1;
+//     this->actor.flags |= 0x10;
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9BAD8.s")
+// Possibly LiftedUp?...
+// void func_80A9BAD8(EnKusa *this, GlobalContext *globalCtx) {
+//     f32 temp_f0;
+
+//     if (Actor_HasNoParent(this, globalCtx) != 0) {
+//         this->actor.room = globalCtx->roomCtx.curRoom.num;
+//         func_80A9BBB0(this);
+//         this->velocity.x = Math_Sins(this->posRot.rot.y) * this->speedXZ;
+//         temp_f0 = Math_Coss(this->posRot.rot.y);
+//         this->colChkInfo.mass = 0xF0;
+//         this->velocity.z = temp_f0 * this->speedXZ;
+//         this->gravity = -0.1f;
+//         func_80A9B140(this);
+//         func_80A9B174(&this->actor.velocity, 0x3BA3D70A);
+//         func_8002D7EC(&this->actor);
+//         func_8002E4B4(globalCtx, &this->actor, 7.5f, 35.0f, 0.0f, 0xC5);
+//         this->gravity = -3.2f;
+//     }
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9BBB0.s")
+// void func_80A9BBB0(EnKusa* this) {
+//     func_80A9AFA0(&func_80A9BC1C);
+//     *D_80A9C1D0 = (u16)-0xBB8;
+//     *D_80A9C1D8 = (s16) (s32) ((Math_Rand_ZeroOne() - 0.5f) * 1600.0f);
+//     *D_80A9C1D4 = (u16)0;
+//     *D_80A9C1DC = (u16)0;
+// }
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Kusa/func_80A9BC1C.s")
 
