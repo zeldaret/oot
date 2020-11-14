1ee91be458114fc60719f7adec508a7902603d01
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2020-11-14 15:22:29 -0800

    Cool data stuff

--- src/overlays/actors/ovl_Bg_Ganon_Otyuka/z_bg_ganon_otyuka.c
+++ src/overlays/actors/ovl_Bg_Ganon_Otyuka/z_bg_ganon_otyuka.c
@@ -36,40 +36,90 @@ InitChainEntry D_80876A60[] = {
     ICHAIN_VEC3F_DIV1000(scale, 1000, ICHAIN_STOP),
 };
 
-UNK_TYPE D_80876A64[] = { 0x01020408 };
-UNK_TYPE D_80876A68[] = { 0x42F00000, 0x00000000, 0x00000000, 0xC2F00000, 0x00000000, 0x00000000,
-                          0x00000000, 0x00000000, 0x42F00000, 0x00000000, 0x00000000, 0xC2F00000 };
-UNK_TYPE D_80876A98[] = { 0x3C3C0000 };
-UNK_TYPE D_80876A9C[] = { 0x32140000 };
-UNK_TYPE D_80876AA0[] = { 0x42700000, 0x00000000, 0x00000000, 0xC2700000, 0x00000000, 0x00000000,
-                          0x00000000, 0x00000000, 0x42700000, 0x00000000, 0x00000000, 0xC2700000 };
-UNK_TYPE D_80876AD0[] = { 0x3FC90FDB, 0xBFC90FDB, 0x00000000, 0x40490FDB };
+UNK_TYPE D_80876A64[] = {
+    0x01020408,
+};
+
+UNK_TYPE D_80876A68[] = {
+    0x42F00000, 0x00000000, 0x00000000, 0xC2F00000, 0x00000000, 0x00000000,
+    0x00000000, 0x00000000, 0x42F00000, 0x00000000, 0x00000000, 0xC2F00000,
+};
+
+UNK_TYPE D_80876A98[] = {
+    0x3C3C0000,
+};
+
+UNK_TYPE D_80876A9C[] = {
+    0x32140000,
+};
+
+UNK_TYPE D_80876AA0[] = {
+    0x42700000, 0x00000000, 0x00000000, 0xC2700000, 0x00000000, 0x00000000,
+    0x00000000, 0x00000000, 0x42700000, 0x00000000, 0x00000000, 0xC2700000,
+};
+
+UNK_TYPE D_80876AD0[] = {
+    0x3FC90FDB,
+    0xBFC90FDB,
+    0x00000000,
+    0x40490FDB,
+};
 
 #include "z_bg_ganon_otyuka_gfx.c"
 
-UNK_TYPE D_80877DA8[] = { 0x00000000, 0x00000000 };
-UNK_TYPE D_80877DB0[] = { 0x00000000, 0x000007C0, 0x00000000, 0x000007C2 };
-UNK_TYPE D_80877DC0[] = { 0x00000000, 0x00010002, 0x7FFF0000, 0x0000FFC4, 0x00000000, 0x00020003, 0x7FFF0000,
-                          0x0000FFC4, 0x00000003, 0x00020004, 0x00000000, 0x8001FFC4, 0x00000003, 0x00040005,
-                          0x00000000, 0x8001FFC4, 0x00000005, 0x00040006, 0x80010000, 0x0000FFC4, 0x00000005,
-                          0x00060007, 0x80010000, 0x0000FFC4, 0x00000007, 0x00060001, 0x00000000, 0x7FFFFFC4,
-                          0x00000007, 0x00010000, 0x00000000, 0x7FFFFFC4, 0x00010000, 0x00030005, 0x00007FFF,
-                          0x00000000, 0x00010000, 0x00050007, 0x00007FFF, 0x00000000 };
-UNK_TYPE D_80877E60[] = { 0x003C0000, 0x003C003C, 0xFFC4003C, 0x003CFFC4, 0xFFC4003C, 0x0000FFC4,
-                          0xFFC4FFC4, 0xFFC4FFC4, 0x0000FFC4, 0xFFC4FFC4, 0x003CFFC4, 0x0000003C };
+// cameraDataList
+CamData D_80877DA8[] = { { 0, 0, 0 } };
 
-UNK_TYPE D_80877E90[] = { 0xFFC4FFC4,  0xFFC4003C,  0x0000003C,  0x00080000, &D_80877E60, 0x000A0000,
-                          &D_80877DC0, &D_80877DB0, &D_80877DA8, 0x00000000, 0x00000000,  0x00000000 };
+// surfaceTypeList
+UNK_TYPE D_80877DB0[] = {
+    0x00000000,
+    0x000007C0,
+    0x00000000,
+    0x000007C2,
+};
+
+// polyList
+CollisionPoly D_80877DC0[] = {
+    { { 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02 }, { 32767, 0, 0 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x03 }, { 32767, 0, 0 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x03, 0x00, 0x02, 0x00, 0x04 }, { 0, 0, -32767 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x03, 0x00, 0x04, 0x00, 0x05 }, { 0, 0, -32767 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x05, 0x00, 0x04, 0x00, 0x06 }, { -32767, 0, 0 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x05, 0x00, 0x06, 0x00, 0x07 }, { -32767, 0, 0 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x07, 0x00, 0x06, 0x00, 0x01 }, { 0, 0, 32767 }, -60 },
+    { { 0x00, 0x00, 0x00, 0x07, 0x00, 0x01, 0x00, 0x00 }, { 0, 0, 32767 }, -60 },
+    { { 0x00, 0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x05 }, { 0, 32767, 0 }, 0 },
+    { { 0x00, 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x07 }, { 0, 32767, 0 }, 0 },
+};
+
+// vtxList
+Vec3s D_80877E60[] = {
+    { 60, 0, 60 },     { 60, -60, 60 }, { 60, -60, -60 }, { 60, 0, -60 },
+    { -60, -60, -60 }, { -60, 0, -60 }, { -60, -60, 60 }, { -60, 0, 60 },
+};
+
+CollisionHeader D_80877E90 = {
+    { -60, -60, -60 },
+    { 60, 0, 60 },
+    ARRAY_COUNT(D_80877E60),
+    D_80877E60,
+    ARRAY_COUNT(D_80877DC0),
+    D_80877DC0,
+    D_80877DB0,
+    D_80877DA8,
+    0,
+    NULL,
+};
 
 void BgGanonOtyuka_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgGanonOtyuka* this = THIS;
     s32 pad;
-    UNK_TYPE sp24 = 0;
+    CollisionHeader* colHeader = NULL;
 
     Actor_ProcessInitChain(thisx, &D_80876A60);
     DynaPolyInfo_SetActorMove(&this->dyna, 0);
-    DynaPolyInfo_Alloc(D_80877E90, &sp24);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, thisx, sp24);
+    DynaPolyInfo_Alloc(&D_80877E90, &colHeader);
+    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, thisx, colHeader);
     if (thisx->params != 0x23) {
         thisx->draw = NULL;
         this->actionFunc = func_80875A0C;
