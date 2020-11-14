44a9571083bbf859c2f384f249049847ab9b15e2
Louis <35883445+louist103@users.noreply.github.com>
2020-11-14 00:50:08 -0500

    match init

--- src/overlays/actors/ovl_Boss_Dodongo/z_boss_dodongo.h
+++ src/overlays/actors/ovl_Boss_Dodongo/z_boss_dodongo.h
@@ -8,7 +8,22 @@ struct BossDodongo;
 
 typedef struct BossDodongo {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x16D4];
+    /* 0x014C */ SkelAnime skelAnime;
+    /* 0x0190 */ char unk_190[4];
+    /* 0x0194 */ u16 unk_194;
+    /* 0x0196 */ char unk_196[0x62];
+    /* 0x01F8 */ f32 unk_1F8;
+    /* 0x01FC */ char unk_1FC[0x20];
+    /* 0x021C */ f32 unk_21C;
+    /* 0x0220 */ f32 unk_220;
+    /* 0x0224 */ f32 unk_224;
+    /* 0x0228 */ f32 unk_228;
+    /* 0x022C */ char unk_22C[0x214];
+    /* 0x0440 */ ColliderJntSph collider;
+    /* 0x0460 */ ColliderJntSphItem items[19];
+    /* 0x0920 */ s32 unk_920;
+    /* 0x0924 */ char unk_924[0xEFC];
+    /* 0x0460 */ //unk_48C[0x1394];
 } BossDodongo; // size = 0x1820
 
 extern const ActorInit Boss_Dodongo_InitVars;
