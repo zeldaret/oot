829f689b144a58c753069829fd7e149fc808d11e
Emily Brooks <emilybrooksemilybrooks@gmail.com>
2020-11-17 12:10:10 -0700

    Converted coordinates to decimal, replaced actor struct padding with char arrays

--- src/overlays/actors/ovl_En_Horse_Zelda/z_en_horse_zelda.h
+++ src/overlays/actors/ovl_En_Horse_Zelda/z_en_horse_zelda.h
@@ -13,12 +13,11 @@ typedef struct EnHorseZelda {
     /* 0x014C */ s32 actionIndex;
     /* 0x0150 */ s32 animationIndex;
     /* 0x0154 */ PSkinAwb skin;
-    /* 0x01E4 */ s32 pad;
-    /* 0x01E8 */ s32 pad2;
+    /* 0x01E4 */ char unk_1E4[0x8];
     /* 0x01EC */ s32 unk_1EC;
-    /* 0x01F0 */ s32 pad3;
+    /* 0x01F0 */ char unk_1F0[0x4];
     /* 0x01F4 */ f32 unk_1F4;
-    /* 0x01F8 */ s32 pad4;
+    /* 0x01F8 */ char unk_1F8[0x4];
     /* 0x01FC */ ColliderCylinder colliderCylinder;
     /* 0x0248 */ ColliderJntSph colliderSphere;
     /* 0x0268 */ ColliderJntSphItem colliderSphereItem;
