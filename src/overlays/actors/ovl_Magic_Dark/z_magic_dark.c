74c044329fb5e4bf038c6d1c402c285ea00939b5
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2020-11-20 18:15:56 -0800

    Use cast + 0xFF

--- src/overlays/actors/ovl_Magic_Dark/z_magic_dark.c
+++ src/overlays/actors/ovl_Magic_Dark/z_magic_dark.c
@@ -228,7 +228,7 @@ void func_80B87A18(Actor* thisx, GlobalContext* globalCtx) {
         Matrix_RotateY(this->actor.shape.rot.y * (M_PI / 0x8000), MTXMODE_APPLY);
         gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_magic_dark.c", 553),
                   G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 170, 255, 255, (u8)(s32)(this->unk_14E * 0.6f));
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 170, 255, 255, (s32)(this->unk_14E * 0.6f) & 0xFF);
         gDPSetEnvColor(POLY_XLU_DISP++, 0, 100, 255, 128);
         gSPDisplayList(POLY_XLU_DISP++, D_80B88A20);
         gSPDisplayList(POLY_XLU_DISP++,
