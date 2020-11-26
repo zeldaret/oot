3a0c36f0b8a471f55bcac2104ea9a380e3497e31
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2020-11-26 01:16:19 +0000

    just regalloc left in Init(?)

--- src/overlays/actors/ovl_En_Changer/z_en_changer.c
+++ src/overlays/actors/ovl_En_Changer/z_en_changer.c
@@ -42,7 +42,8 @@ s32 D_809D3148[] = { 0x00000000, 0x00000008, 0x00000008, 0x00000009, 0x00000009,
 
 s32 D_809D3160[] = { 0x00000000, 0x00000002, 0x00000004, 0x00000006, 0x00000008 };
 
-s32 D_809D3174[] = { 0x0000000A, 0x00000000, 0x00000000 };
+// s32 D_809D3174[] = { 0x0000000A, 0x00000000, 0x00000000 };
+s32 D_809D3174 = 0x0000000A;
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Changer/EnChanger_Destroy.s")
 void EnChanger_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -53,14 +54,15 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx2) {
     EnChanger* this = THIS;
 
     GlobalContext* globalCtx = globalCtx2;
-
     s16 sp6E;
     s16 sp6C;
     s16 sp6A;
-    s16 sp66;
-    s16 sp64;
-    s16 temp_v1_3;
     s16 phi_v1;
+    s16 temp_v1_3;
+    s16 sp64;
+    s16 new_var;
+    s16 sp66;
+    s32 new_var2;
 
     phi_v1 = globalCtx->roomCtx.curRoom.num - 1;
     if (phi_v1 < 0) {
@@ -69,63 +71,75 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx2) {
     if (Flags_GetTreasure(globalCtx, D_809D3160[phi_v1]) != 0) {
         this->unk_168 = 1;
     }
-    temp_v1_3 = phi_v1 * 2;
     osSyncPrintf((const char*)"\n\n");
     osSyncPrintf((const char*)"\x1b[32m☆☆☆☆☆ 宝発生(部屋はどれ？) %d\n\x1b[m", globalCtx->roomCtx.curRoom.num);
     osSyncPrintf((const char*)"\x1b[32m☆☆☆☆☆ ビットは？ \t     %x\n\x1b[m", globalCtx->actorCtx.flags.chest);
     osSyncPrintf((const char*)"\x1b[32m☆☆☆☆☆ セーブＢＩＴは？     %x\n\x1b[m", D_809D3160[phi_v1]);
     osSyncPrintf((const char*)"\x1b[32m☆☆☆☆☆ もう、ゾンビ？\t     %d\n\x1b[m", (s16)this->unk_168);
     osSyncPrintf((const char*)"\n\n");
+    phi_v1 *= 2;
     if (globalCtx->roomCtx.curRoom.num >= 6) {
-        sp6A = *D_809D3174 | ((gSaveContext.itemGetInf[1] & 0x800) ? 0x4EA0 : 0x4EC0);
+        // if (gSaveContext.itemGetInf[1] & 0x800) {
+        //     phi_v0 = 0x4EA0;
+        // } else {
+        //     sp6A = 0x4EC0;
+        //     phi_v0 = 0x4EC0;
+        // }
+        // temp_v0_2 = D_809D3174 | phi_v0;
+        // sp6A = temp_v0_2;
+        sp6A = ((gSaveContext.itemGetInf[1] & 0x800) ? (0x4EA0) : (0x4EC0));
+        new_var = D_809D3174;
+        sp6A = new_var | sp6A;
         this->unk_158 = (EnBox*)Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_BOX, 20.0f,
                                                    20.0f, -2500.0f, 0, 0x7FFF, 0, sp6A);
-        if (this->unk_158 != 0) {
-            if ((s16)this->unk_168 != 0) {
-
+        if (this->unk_158 != NULL) {
+            if (((s16)this->unk_168) != 0) {
                 Flags_SetTreasure(globalCtx, sp6A & 0x1F);
                 Actor_Kill(&this->actor);
                 return;
             } else {
+                new_var2 = ((gSaveContext.itemGetInf[1] & 0x800 ) ? (0xB) : (0xC)) & 0xFF;
                 Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_ITEM_ETCETERA, 20.0f, 20.0f, -2500.0f, 0, 0, 0,
-                            ((*D_809D3174 & 0x1F) << 8) + (((gSaveContext.itemGetInf[1] & 0x800) ? 0xB : 0xC) & 0xFF));
+                            ((D_809D3174 & 0x1F) << 8) + new_var2);
                 osSyncPrintf((const char*)"\x1b[33m☆☆☆☆☆ 中央宝発生(ＧＲＥＡＴ) ☆☆☆☆☆ %x\n\x1b[m", sp6A);
                 this->actionFunc = func_809D2F74;
                 return;
             }
         }
     }
-    this->unk_15C = temp_v1_3;
+    temp_v1_3 = phi_v1;
+    sp6E = (s16)((D_809D3130[globalCtx->roomCtx.curRoom.num] << 5) | 0x4000);
+    new_var = temp_v1_3;
+    this->unk_15C = new_var;
     this->unk_160 = (s16)D_809D3130[globalCtx->roomCtx.curRoom.num];
-    sp6C = temp_v1_3 | 0x4E21;
     sp64 = (s16)D_809D3148[globalCtx->roomCtx.curRoom.num];
-    this->unk_15E = temp_v1_3 | 1;
+    sp6E |= new_var;
+    sp6C = new_var | 0x4E21;
+    this->unk_15E = new_var | 1;
     this->unk_162 = GI_DOOR_KEY;
     sp66 = 0xD;
-    sp6E = (s16)((D_809D3130[globalCtx->roomCtx.curRoom.num] << 5) | 0x4000);
-    sp6E |= temp_v1_3;
     if (Math_Rand_ZeroFloat(1.99f) < 1.0f) {
-        this->unk_15E = temp_v1_3;
+        sp6C = (s16)((D_809D3130[globalCtx->roomCtx.curRoom.num] << 5) | 0x4000);
+        this->unk_15E = new_var;
         this->unk_162 = (s16)D_809D3130[globalCtx->roomCtx.curRoom.num];
-        sp6E = temp_v1_3 | 0x4E21;
         sp66 = (s16)D_809D3148[globalCtx->roomCtx.curRoom.num];
+        sp6E = new_var | 0x4E21;
+        sp6C |= new_var;
         this->unk_15C = temp_v1_3 | 1;
         this->unk_160 = GI_DOOR_KEY;
         sp64 = 0xD;
-        sp6C = (s16)((D_809D3130[globalCtx->roomCtx.curRoom.num] << 5) | 0x4000);
-        sp6C |= temp_v1_3;
     }
     this->unk_150 = (EnBox*)Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_BOX,
                                                D_809D30A0[globalCtx->roomCtx.curRoom.num].x,
                                                D_809D30A0[globalCtx->roomCtx.curRoom.num].y,
                                                D_809D30A0[globalCtx->roomCtx.curRoom.num].z, 0, -0x3FFF, 0, sp6E);
-    if (this->unk_150 != 0) {
+    if (this->unk_150 != NULL) {
         osSyncPrintf((const char*)"\x1b[35m☆☆☆☆☆ 左宝発生(ナニがはいってるの？) ☆☆☆☆☆ %x\n\x1b[m", sp6E);
         osSyncPrintf((const char*)"\x1b[35m☆☆☆☆☆ 部屋番号は？  %x\n\x1b[m", globalCtx->roomCtx.curRoom.num);
         osSyncPrintf((const char*)"\x1b[35m☆☆☆☆☆ ビットはなぁに？  %x\n\x1b[m", this->unk_15E);
         osSyncPrintf((const char*)"\x1b[35m☆☆☆☆☆ すけすけ君？ %x\n\x1b[m", sp66);
         osSyncPrintf((const char*)"\n\n");
-        if ((s16)this->unk_168 != 0) {
+        if (((s16)this->unk_168) != 0) {
             Flags_SetTreasure(globalCtx, this->unk_15C & 0x1F);
         } else {
             Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_ITEM_ETCETERA,
@@ -137,13 +151,13 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx2) {
     this->unk_154 = (EnBox*)Actor_SpawnAsChild(
         &globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_BOX, D_809D30E8[globalCtx->roomCtx.curRoom.num].x,
         D_809D30E8[globalCtx->roomCtx.curRoom.num].y, D_809D30E8[globalCtx->roomCtx.curRoom.num].z, 0, 0x3FFF, 0, sp6C);
-    if (this->unk_154 != 0) {
+    if (this->unk_154 != NULL) {
         osSyncPrintf((const char*)"\x1b[36m☆☆☆☆☆ 右宝発生(ナニがはいってるの？) ☆☆☆☆☆ %x\n\x1b[m", sp6C);
         osSyncPrintf((const char*)"\x1b[36m☆☆☆☆☆ 部屋番号は？  %d\n\x1b[m", globalCtx->roomCtx.curRoom.num);
         osSyncPrintf((const char*)"\x1b[36m☆☆☆☆☆ ビットはなぁに？  %x\n\x1b[m", this->unk_15C);
         osSyncPrintf((const char*)"\x1b[36m☆☆☆☆☆ すけすけ君？ %x\n\x1b[m", sp64);
         osSyncPrintf((const char*)"\n\n");
-        if ((s16)this->unk_168 != 0) {
+        if (((s16)this->unk_168) != 0) {
             Flags_SetTreasure(globalCtx, this->unk_15E & 0x1F);
             Actor_Kill(&this->actor);
             return;
