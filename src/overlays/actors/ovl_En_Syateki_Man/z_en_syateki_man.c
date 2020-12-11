19da203d18e4b5d0cd59576ba7c1b6dd00376a74
petrie911 <69443847+petrie911@users.noreply.github.com>
2020-12-11 11:29:10 -0600

    Update src/overlays/actors/ovl_En_Syateki_Man/z_en_syateki_man.c

--- src/overlays/actors/ovl_En_Syateki_Man/z_en_syateki_man.c
+++ src/overlays/actors/ovl_En_Syateki_Man/z_en_syateki_man.c
@@ -257,8 +257,8 @@ void EnSyatekiMan_EndGame(EnSyatekiMan* this, GlobalContext* globalCtx) {
                     this->actor.parent = NULL;
                     if (LINK_IS_CHILD) {
                         if (!(gSaveContext.itemGetInf[0] & 0x2000)) {
-                            osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ Equip_Pachinko ☆☆☆☆☆ %d\n" VT_RST, CUR_UPG_VALUE(5));
-                            if (CUR_UPG_VALUE(5) == 1) {
+                            osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ Equip_Pachinko ☆☆☆☆☆ %d\n" VT_RST, CUR_UPG_VALUE(UPG_BULLET_BAG));
+                            if (CUR_UPG_VALUE(UPG_BULLET_BAG) == 1) {
                                 this->getItemId = GI_BULLET_BAG_40;
                             } else {
                                 this->getItemId = GI_BULLET_BAG_50;
@@ -268,8 +268,8 @@ void EnSyatekiMan_EndGame(EnSyatekiMan* this, GlobalContext* globalCtx) {
                         }
                     } else {
                         if (!(gSaveContext.itemGetInf[0] & 0x4000)) {
-                            osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ Equip_Bow ☆☆☆☆☆ %d\n" VT_RST, CUR_UPG_VALUE(0));
-                            switch (CUR_UPG_VALUE(0)) {
+                            osSyncPrintf(VT_FGCOL(GREEN) "☆☆☆☆☆ Equip_Bow ☆☆☆☆☆ %d\n" VT_RST, CUR_UPG_VALUE(UPG_QUIVER));
+                            switch (CUR_UPG_VALUE(UPG_QUIVER)) {
                                 case 0:
                                     this->getItemId = GI_RUPEE_PURPLE;
                                     break;
