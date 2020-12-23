c0e138b1f31e59634826b239c07347110d09c924
engineer124 <engineer124engineer124@gmail.com>
2020-12-23 12:00:26 +1100

    init commit

--- src/overlays/actors/ovl_En_Viewer/z_en_viewer.c
+++ src/overlays/actors/ovl_En_Viewer/z_en_viewer.c
@@ -15,6 +15,14 @@ void EnViewer_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnViewer_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnViewer_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80B2A300(EnViewer* this, GlobalContext* globalCtx);
+void func_80B2A4D8(EnViewer* this, GlobalContext* globalCtx);
+void func_80B2A448(EnViewer* this, GlobalContext* globalCtx);
+void func_80B2B4A8(EnViewer* this, GlobalContext* globalCtx);
+void func_80B2B8FC(EnViewer* this, GlobalContext* globalCtx);
+void func_80B2BA38(EnViewer* this, GlobalContext* globalCtx);
+void func_80B2C130(EnViewer* this, GlobalContext* globalCtx);
+
 extern UNK_TYPE D_01000000;
 extern UNK_TYPE D_0404D4E0;
 extern UNK_TYPE D_06000450;
@@ -59,7 +67,8 @@ extern UNK_TYPE D_0600F378;
 extern UNK_TYPE D_0600F578;
 extern UNK_TYPE D_0600F778;
 
-/*
+s32 D_80B2CEC0[] = { 0x00000000 };
+
 const ActorInit En_Viewer_InitVars = {
     ACTOR_EN_VIEWER,
     ACTORTYPE_ITEMACTION,
@@ -71,10 +80,37 @@ const ActorInit En_Viewer_InitVars = {
     (ActorFunc)EnViewer_Update,
     (ActorFunc)EnViewer_Draw,
 };
-*/
+
+
+
+s32 D_80B2CEE4[] = { 0x30F8012C };
+s32 D_80B2CEE8[] = { 0x00460046, 0x01000214, 0x01000000, 0x06006B2C, 0x06007148, 0x00870047, 0x0100000A, 0x03000000, 0x0600F788, 0x060029CC, 0x01910047, 0x0100000A, 0x02000000, 0x0600E038, 0x06000450, 0x009B009B, 0x01FA000A, 0x00000000, 0x060119E8, 0x06002928, 0x002D002D, 0x01000214, 0x01000000, 0x06008668, 0x06003858, 0x009B009B, 0x01FA000A, 0x00000000, 0x060119E8, 0x060005B4, 0x002D002D, 0x01000214, 0x01000000, 0x06008668, 0x06002650, 0x009B009B, 0x01FA000A, 0x00000000, 0x060119E8, 0x06004260, 0x009B009B, 0x01FA000A, 0x00000000, 0x060119E8, 0x060050A8, 0x00E100E1, 0x01FA000A, 0x00000000, 0x060114E8, 0x06011348 };
+void* D_80B2CFB0[] = { func_80B2A300, func_80B2A4D8, func_80B2A300, func_80B2A448 };
+void* D_80B2CFC0[] = { NULL, ActorShadow_DrawFunc_Circle, ActorShadow_DrawFunc_Squiggly };
+s32 D_80B2CFCC[] = { 0x00000000 };
+s32 D_80B2CFD0[] = { 0x00000000, 0x00000000, 0x00000000 };
+void* D_80B2CFDC[] = { func_80B2B4A8, func_80B2B8FC, func_80B2BA38, func_80B2C130 };
+s32 D_80B2CFEC[] = { 0x00000000 };
+
+
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Viewer/func_80B2A200.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Viewer/EnViewer_Init.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Viewer/EnViewer_Init.s")
+void EnViewer_Init(Actor* thisx, GlobalContext *globalCtx) {
+    EnViewer* this = THIS;
+
+    s32 temp_v1;
+    Actor_ProcessInitChain(&this->actor, (InitChainEntry *) D_80B2CEE4);
+    func_80B2A200(this, func_80B2A570);
+    *D_80B2CEC0 = (u8)0;
+    this->unk1E4 = (u8)0;
+    temp_v1 = ((s32) this->actor.params >> 8) & 0xFF;
+    this->unk1E5 = (u8)0;
+    this->unk1E6 = (u8)0;
+    if ((temp_v1 == 3) || (temp_v1 == 5) || (temp_v1 == 7) || (temp_v1 == 8) || (temp_v1 == 9)) {
+        D_80B2D440 = Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_GANON_MANT, 0.0f, 0.0f, 0.0f, 0, 0, 0, 35);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Viewer/EnViewer_Destroy.s")
 
