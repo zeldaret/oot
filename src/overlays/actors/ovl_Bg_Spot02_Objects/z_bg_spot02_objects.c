2d7f3a478f931330378d826cdeb02677cfc5b457
engineer124 <engineer124engineer124@gmail.com>
2020-12-31 01:22:01 +1100

    Bg_Spot02 3 non-matching but all down to regalloc

--- src/overlays/actors/ovl_Bg_Spot02_Objects/z_bg_spot02_objects.c
+++ src/overlays/actors/ovl_Bg_Spot02_Objects/z_bg_spot02_objects.c
@@ -47,20 +47,23 @@ InitChainEntry D_808AD8A0[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
 };
 
+// velocity and accel
 Vec3f D_808AD8A4 = { 0x00000000, 0x00000000, 0x00000000 };
+
 Gfx* D_808AD8B0[] = { 0x06012A50, 0x060127C0, 0x060130B0 };
 
 #ifdef NON_MATCHING
 void BgSpot02Objects_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgSpot02Objects* this = THIS;
 
-    s32 localC;
+    u16 params;
+    s32 localC = 0;
 
-    localC = 0;
     DynaPolyInfo_SetActorMove(&this->dyna, 0);
-    this->unk_16B = this->dyna.actor.params >> 8;
-
-    switch (this->dyna.actor.params & 0xFF) {
+    this->unk_16B = (u16)((((BgSpot02Objects *) thisx)->dyna.actor.params) >> 8);
+    params = (this->dyna.actor.params & 0xFF);
+    this->dyna.actor.params = (u16)(this->dyna.actor.params & 0xFF) & 0xFFFF;
+    switch (params & 0xFFFF) {
         case 0:
             Actor_ProcessInitChain(&this->dyna.actor, D_808AD8A0);
             if (this->dyna.actor.params == 0) {
@@ -85,7 +88,7 @@ void BgSpot02Objects_Init(Actor* thisx, GlobalContext* globalCtx) {
             }
             this->dyna.dynaPolyId =
                 DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, localC);
-            if (((gSaveContext.eventChkInf[1] & 0x2000 != 0) && (globalCtx->sceneNum == SCENE_SPOT02) &&
+            if (((gSaveContext.eventChkInf[1] & 0x2000) && (globalCtx->sceneNum == SCENE_SPOT02) &&
                  (this->dyna.actor.params == 2)) ||
                 (LINK_IS_ADULT && (this->dyna.actor.params == 1))) {
                 Actor_Kill(&this->dyna.actor);
@@ -96,11 +99,10 @@ void BgSpot02Objects_Init(Actor* thisx, GlobalContext* globalCtx) {
             Actor_ChangeType(globalCtx, &globalCtx->actorCtx, &this->dyna.actor, ACTORTYPE_ITEMACTION);
             this->actionFunc = func_808ACC34;
             this->dyna.actor.draw = func_808ACCB8;
-            if (gSaveContext.eventChkInf[1] & 0x2000 != 0) {
+            if (gSaveContext.eventChkInf[1] & 0x2000) {
                 Actor_Kill(&this->dyna.actor);
             }
             return;
-
         case 4:
             this->timer = -0xC;
             this->unk_170 = 0xFFFF;
@@ -178,9 +180,9 @@ void func_808ACB58(BgSpot02Objects* this, GlobalContext* globalCtx) {
     if (Math_ApproxF(&this->dyna.actor.posRot.pos.y, this->dyna.actor.initPosRot.pos.y + 255.0f, 1.0f) != 0) {
         Audio_PlayActorSound2(&this->dyna.actor, NA_SE_EV_STONEDOOR_STOP);
         this->actionFunc = func_808AC8FC;
-        return;
+    } else {
+        func_8002F974(&this->dyna.actor, NA_SE_EV_WALL_MOVE_SP - SFX_FLAG);
     }
-    func_8002F974(&this->dyna.actor, NA_SE_EV_WALL_MOVE_SP - SFX_FLAG);
 }
 
 void BgSpot02Objects_Update(Actor* thisx, GlobalContext* globalCtx) {
@@ -205,48 +207,53 @@ void func_808ACC34(BgSpot02Objects* this, GlobalContext* globalCtx) {
             Actor_Kill(&this->dyna.actor);
         }
     }
+    
     if (globalCtx->csCtx.frames == 245 || globalCtx->csCtx.frames == 351) {
         func_800788CC(NA_SE_EV_LIGHTNING);
     }
 }
 
-#ifdef NON_MATCHING
 void func_808ACCB8(Actor* thisx, GlobalContext* globalCtx) {
     BgSpot02Objects* this = THIS;
-    Color_RGBA8 primColor;
-    Color_RGBA8 envColor;
-    s32 pad;
+    f32 pad;
+    f32 pad2;
+    u8 redPrim; // does not match with Color_RGB8/Color_RGBA8
+    u8 greenPrim;
+    u8 bluePrim;
+    u8 redEnv;
+    u8 greenEnv;
+    u8 blueEnv;
 
     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 600);
     if (globalCtx->csCtx.state != 0 && globalCtx->csCtx.npcActions[0] != NULL &&
         globalCtx->csCtx.npcActions[0]->action == 2) {
         if (this->unk_16A < 5) {
-            primColor.r = 255;
-            primColor.g = 255;
-            primColor.b = 255;
-            envColor.r  = 100.0f + 155.0f  * (this->unk_16A / 5.0f);
-            envColor.g  = 255;
-            envColor.b  = 255.0f - 155.0f  * (this->unk_16A / 5.0f);
+            pad = (this->unk_16A / 5.0f);
+            redPrim = greenPrim = bluePrim = 255;
+            redEnv = 100.0f + 155.0f * pad;
+            greenEnv = 255;
+            blueEnv = 255.0f - 155.0f * pad;
         } else {
-            primColor.r = 255.0f - (255.0f * ((this->unk_16A - 5) / 7.0f));
-            primColor.g = 255.0f - (55.0f  * ((this->unk_16A - 5) / 7.0f));
-            primColor.b = 255.0f - (255.0f * ((this->unk_16A - 5) / 7.0f));
-            envColor.r  = 255.0f - (105.0f * ((this->unk_16A - 5) / 7.0f));
-            envColor.g  = 255.0f - (255.0f * ((this->unk_16A - 5) / 7.0f));
-            envColor.b  = 100.0f + (100.0f * ((this->unk_16A - 5) / 7.0f));
+            pad = ((this->unk_16A - 5) / 7.0f);
+            redPrim = 255.0f - (255.0f * pad);
+            greenPrim = 255.0f - (55.0f * pad);
+            bluePrim = 255.0f - (255.0f * pad);
+            redEnv = 255.0f - (105.0f * pad);
+            greenEnv = 255.0f - (255.0f * pad);
+            blueEnv = 100.0f + (100.0f * pad);
         }
 
-        Matrix_Translate(globalCtx->csCtx.npcActions[0]->rot.x, globalCtx->csCtx.npcActions[0]->rot.y,
-                         globalCtx->csCtx.npcActions[0]->rot.z, MTXMODE_NEW);
-        Matrix_RotateX(globalCtx->csCtx.npcActions[0]->rot.x * (M_PI * SHT_MINV), MTXMODE_APPLY);
-        Matrix_RotateY(globalCtx->csCtx.npcActions[0]->rot.y * (M_PI * SHT_MINV), MTXMODE_APPLY);
-        Matrix_RotateZ(globalCtx->csCtx.npcActions[0]->rot.z * (M_PI * SHT_MINV), MTXMODE_APPLY);
+        Matrix_Translate(globalCtx->csCtx.npcActions[0]->startPos.x, globalCtx->csCtx.npcActions[0]->startPos.y,
+                         globalCtx->csCtx.npcActions[0]->startPos.z, MTXMODE_NEW);
+        Matrix_RotateX(globalCtx->csCtx.npcActions[0]->urot.x * (M_PI / (f32)0x8000), MTXMODE_APPLY);
+        Matrix_RotateY(globalCtx->csCtx.npcActions[0]->urot.y * (M_PI / (f32)0x8000), MTXMODE_APPLY);
+        Matrix_RotateZ(globalCtx->csCtx.npcActions[0]->urot.z * (M_PI / (f32)0x8000), MTXMODE_APPLY);
         Matrix_Scale(1.0f, 1.0f, 1.0f, MTXMODE_APPLY);
         func_80093D84(globalCtx->state.gfxCtx);
 
         gDPPipeSync(POLY_XLU_DISP++);
-        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, primColor.r, primColor.g, primColor.b, 255);
-        gDPSetEnvColor(POLY_XLU_DISP++, envColor.r, envColor.g, envColor.b, 255);
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, redPrim, greenPrim, bluePrim, 255);
+        gDPSetEnvColor(POLY_XLU_DISP++, redEnv, greenEnv, blueEnv, 255);
         gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 679),
                   G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gSPSegment(POLY_XLU_DISP++, 0x08, SEGMENTED_TO_VIRTUAL(D_808AD850[this->unk_16A]));
@@ -256,9 +263,6 @@ void func_808ACCB8(Actor* thisx, GlobalContext* globalCtx) {
     }
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 692);
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot02_Objects/func_808ACCB8.s")
-#endif
 
 void func_808AD3D4(BgSpot02Objects* this, GlobalContext* globalCtx) {
     if (globalCtx->csCtx.state != 0 && globalCtx->csCtx.npcActions[2] != NULL &&
@@ -275,18 +279,19 @@ void func_808AD3D4(BgSpot02Objects* this, GlobalContext* globalCtx) {
 }
 
 #ifdef NON_MATCHING
-void func_808AD450(Actor* thisx, GlobalContext *globalCtx) {
+void func_808AD450(Actor* thisx, GlobalContext* globalCtx) {
+    // u16 action;
     BgSpot02Objects* this = THIS;
-    f32 sp8C;
-    s16 temp_v1_2;
-    s32 temp_at;
     u16 temp_v1;
+    s8 pad;
+    s8 pad2;
+    f32 interpolated;
 
     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 736);
     if (globalCtx->csCtx.state != 0 && globalCtx->csCtx.npcActions[2] != NULL) {
-        temp_v1 = globalCtx->csCtx.npcActions[2]->rot.z * 0.00549325f;
+        temp_v1 = globalCtx->csCtx.npcActions[2]->urot.z * 0.00549325f;
 
-        if (temp_v1 != this->unk_170) {
+        if (this->unk_170 != temp_v1) {
             if (this->unk_170 == 0xFFFF) {
                 this->unk_170 = temp_v1;
                 this->unk_172 = temp_v1;
@@ -296,27 +301,29 @@ void func_808AD450(Actor* thisx, GlobalContext *globalCtx) {
             }
         }
 
-
-        sp8C = func_8006F93C(globalCtx->csCtx.npcActions[2]->endFrame, globalCtx->csCtx.npcActions[2]->startFrame, globalCtx->csCtx.frames);
-        if (globalCtx->csCtx.npcActions[2]->action == 2) {
-            Matrix_Translate(globalCtx->csCtx.npcActions[2]->startPos.x, globalCtx->csCtx.npcActions[2]->startPos.y, globalCtx->csCtx.npcActions[2]->startPos.z, MTXMODE_NEW);
-            Matrix_RotateX(globalCtx->csCtx.npcActions[2]->rot.x * (M_PI * SHT_MINV), MTXMODE_APPLY);
-            Matrix_RotateY(globalCtx->csCtx.npcActions[2]->rot.y * (M_PI * SHT_MINV), MTXMODE_APPLY);
-            Matrix_Scale(0.9f, 0.9f, (((f32) (this->unk_170 - this->unk_172) * sp8C) + this->unk_172) * 0.1f, MTXMODE_APPLY);
+        interpolated = func_8006F93C(globalCtx->csCtx.npcActions[2]->endFrame,
+                                     globalCtx->csCtx.npcActions[2]->startFrame, globalCtx->csCtx.frames);
+        if ((globalCtx->csCtx.npcActions[2]->action & 0xFFFF) == 2) {
+            Matrix_Translate(globalCtx->csCtx.npcActions[2]->startPos.x, globalCtx->csCtx.npcActions[2]->startPos.y,
+                             globalCtx->csCtx.npcActions[2]->startPos.z, MTXMODE_NEW);
+            Matrix_RotateX(globalCtx->csCtx.npcActions[2]->urot.x * (M_PI / (f32)0x8000), MTXMODE_APPLY);
+            Matrix_RotateY(globalCtx->csCtx.npcActions[2]->urot.y * (M_PI / (f32)0x8000), MTXMODE_APPLY);
+            Matrix_Scale(0.9f, 0.9f, (((this->unk_170 - this->unk_172) * interpolated) + this->unk_172) * 0.1f,
+                         MTXMODE_APPLY); // interpolation between unk_170 and unk_172
             func_80093D84(globalCtx->state.gfxCtx);
 
             gDPPipeSync(POLY_XLU_DISP++);
             gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 255, 255, 170, 128);
             gDPSetEnvColor(POLY_XLU_DISP++, 150, 120, 0, 128);
-            gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 795), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-            temp_v1_2 = this->timer;
-            temp_at = 0 - temp_v1_2;
-            gSPSegment(POLY_XLU_DISP++, 0x08, Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, temp_v1_2 * 2, temp_at * 3, 0x20, 0x40, 1, temp_v1_2 * 4, temp_at * 6, 0x20, 0x40));
+            gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 795),
+                      G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+            gSPSegment(POLY_XLU_DISP++, 0x08,
+                       Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, 2 * this->timer, (-3) * this->timer, 32, 64, 1,
+                                        4 * this->timer, (-6) * this->timer, 32, 64));
             gDPPipeSync(POLY_XLU_DISP++);
             gSPDisplayList(POLY_XLU_DISP++, &D_060013F0);
             gDPPipeSync(POLY_XLU_DISP++);
         }
-    
     }
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot02_objects.c", 818);
 }
