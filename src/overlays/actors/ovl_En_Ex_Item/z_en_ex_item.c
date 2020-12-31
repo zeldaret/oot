c39b4def84a9309cbbcf9d7c63a9b5987c600d8f
petrie911 <pmontag@DESKTOP-LG8A167.localdomain>
2020-12-31 14:16:03 -0600

    press FFFF

--- src/overlays/actors/ovl_En_Ex_Item/z_en_ex_item.c
+++ src/overlays/actors/ovl_En_Ex_Item/z_en_ex_item.c
@@ -503,7 +503,7 @@ void EnExItem_DrawKey(EnExItem* this, GlobalContext* globalCtx, s32 index) {
     func_8009460C(globalCtx->state.gfxCtx);
     gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_ex_item.c", 887),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPSegment(POLY_OPA_DISP++, 0x08, SEGMENTED_TO_VIRTUAL(keySegment[index]));
+    gSPSegment(POLY_OPA_DISP++, 0x08, SEGMENTED_TO_VIRTUAL(keySegments[index]));
     gSPDisplayList(POLY_OPA_DISP++, D_0403F070);
 
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_en_ex_item.c", 893);
