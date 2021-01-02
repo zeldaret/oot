64eb3b16650fb21e13ea2f18edea271335b514ee
Louis <35883445+louist103@users.noreply.github.com>
2021-01-02 01:14:21 -0500

    Match Init and one other

--- src/overlays/actors/ovl_En_Ny/z_en_ny.c
+++ src/overlays/actors/ovl_En_Ny/z_en_ny.c
@@ -9,6 +9,11 @@ void EnNy_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnNy_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnNy_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80ABDBF8(Actor* thisx, GlobalContext* globalCtx);
+void func_80ABCF4C(EnNy* this, GlobalContext* globalCtx);
+void func_80ABCD40(EnNy* this);
+void func_80ABCDBC(EnNy *this);
+
 extern UNK_TYPE D_06001DD0;
 extern UNK_TYPE D_06001EA8;
 extern UNK_TYPE D_06001FC8;
@@ -25,27 +30,25 @@ const ActorInit En_Ny_InitVars = {
     (ActorFunc)EnNy_Draw,
 };
 
-//s32 D_80ABE330[] = { 0x00000000, 0xFFCFFFFF, 0x04080000, 0xFFCFFFFF, 0x00000000,
- //                    0x01010100, 0x00000000, 0x00000000, 0x000F0064 };
-
 static ColliderJntSphItemInit sJntSphItemsInit[1] = {
     {
         { 0x00, { 0xFFCFFFFF, 0x04, 0x08 }, { 0xFFCFFFFF, 0x00, 0x00 }, 0x01, 0x01, 0x01 },
         { 0, { { 0, 0, 0 }, 15 }, 100 },
     },
 };
-//s32 D_80ABE354[] = { 0x0A110939, 0x10000000, 0x00000001, D_80ABE330 };
-static ColliderJntSphInit D_80ABE354 =
-{
+
+static ColliderJntSphInit sColliderInit = {
     { COLTYPE_UNK10, 0x11, 0x09, 0x39, 0x10, COLSHAPE_JNTSPH },
-    1, sJntSphItemsInit,
+    1,
+    sJntSphItemsInit,
 };
 
-//s32 D_80ABE364[] = { 0x000000F2, 0x00F2F212, 0x00F2F424, 0xF2F2E400, 0x00240000, 0x000000F4, 0xF200F8F4, 0x00000000 };
-static DamageTable D_80ABE364 = { { 0x00, 0x00, 0x00, 0xF2, 0x00, 0xF2, 0xF2, 0x12, 0x00, 0xF2, 0xF4, 0x24, 0xF2, 0xF2, 0xE4, 0x00, 0x00, 0x24, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF4, 0xF2, 0x00, 0xF8, 0xF4, 0x00, 0x00, 0x00, 0x00, } };
+static DamageTable sDamageTable = { {
+    0x00, 0x00, 0x00, 0xF2, 0x00, 0xF2, 0xF2, 0x12, 0x00, 0xF2, 0xF4, 0x24, 0xF2, 0xF2, 0xE4, 0x00,
+    0x00, 0x24, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF4, 0xF2, 0x00, 0xF8, 0xF4, 0x00, 0x00, 0x00, 0x00,
+} };
 
-//s32 D_80ABE384[] = { 0x89170028, 0x801F0002, 0x304C001E };
-static InitChainEntry D_80ABE384[] = {
+static InitChainEntry sInitChain[] = {
     ICHAIN_S8(naviEnemyId, 40, ICHAIN_CONTINUE),
     ICHAIN_U8(unk_1F, 2, ICHAIN_CONTINUE),
     ICHAIN_F32(unk_4C, 30, ICHAIN_STOP),
@@ -58,54 +61,46 @@ s32 D_80ABE39C[] = { 0x00000000, 0x3DCCCCCD, 0x00000000 };
 s32 D_80ABE3A8[] = { 0x40A00000, 0x00000000, 0x00000000, 0xC0A00000, 0x00000000, 0x00000000, 0x00000000,
                      0x00000000, 0x40A00000, 0x00000000, 0x00000000, 0xC0A00000, 0x00000000, 0x00000000 };
 
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ny/EnNy_Init.s")
-void EnNy_Init(Actor *thisx, GlobalContext *globalCtx) {
-    ColliderJntSph *sp28;
-    ColliderJntSph *temp_a1;
-    s16 temp_a1_2;
+void EnNy_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnNy* this = THIS;
 
-    Actor_ProcessInitChain(thisx, D_80ABE384);
-    thisx->colChkInfo.damageTable = &D_80ABE364;
+    Actor_ProcessInitChain(thisx, sInitChain);
+    thisx->colChkInfo.damageTable = &sDamageTable;
     thisx->colChkInfo.health = 2;
-    temp_a1 = thisx + 0x150;
-    sp28 = temp_a1;
-    Collider_InitJntSph(globalCtx, temp_a1);
-    Collider_SetJntSph(globalCtx, temp_a1, thisx, &D_80ABE354, thisx + 0x170);
+    Collider_InitJntSph(globalCtx, &this->collider);
+    Collider_SetJntSph(globalCtx, &this->collider, thisx, &sColliderInit, &this->items);
     ActorShape_Init(&thisx->shape, 0.0f, ActorShadow_DrawFunc_Circle, 20.0f);
-    thisx->unk1CA = (u16)0;
-    thisx->unk1D0 = (u8)0;
+    this->unk_1CA = 0;
+    this->unk_1D0 = 0;
     Actor_SetScale(thisx, 0.01f);
-    thisx->shape.rot.y = 0;
     thisx->speedXZ = 0.0f;
-    temp_a1_2 = thisx->params;
-    thisx->unk1CC = (u16)0;
-    thisx->unk1CE = (u16)2;
-    thisx->unk1D4 = 0xFF;
-    thisx->unk1D8 = 0;
-    thisx->velocity.y = 0.0f;
-    thisx->unk1E8 = 0.0f;
+    thisx->shape.rot.y = 0;
     thisx->gravity = -0.4f;
-    thisx->unk1E0 = 0.25f;
-    if (temp_a1_2 == 0) {
-        //New initials
-        osSyncPrintf((const char *) "ニュウ イニシャル[ %d ] ！！\n", temp_a1_2);
+    this->unk_1CC = 0;
+    this->unk_1CE = 2;
+    thisx->velocity.y = 0.0f;
+    this->unk_1D4 = 0xFF;
+    this->unk_1D8 = 0;
+    this->unk_1E8 = 0.0f;
+    this->unk_1E0 = 0.25f;
+    if (thisx->params == 0) {
+        // New initials
+        osSyncPrintf("ニュウ イニシャル[ %d ] ！！\n", thisx->params);
         thisx->colChkInfo.mass = 0;
-        thisx->unk1D4 = 0;
-        thisx->unk1D8 = 0xFF;
-        thisx->unk1E0 = 1.0f;
-        func_80ABCDBC(thisx);
+        this->unk_1D4 = 0;
+        this->unk_1D8 = 0xFF;
+        this->unk_1E0 = 1.0f;
+        func_80ABCDBC(this);
         return;
     }
 
-    //Dummy new initials
-    osSyncPrintf("ダミーニュウ イニシャル[ %d ] ！！\n", temp_a1_2);
-    sp28 = &func_80ABDBF8;
+    // Dummy new initials
+    osSyncPrintf("ダミーニュウ イニシャル[ %d ] ！！\n", thisx->params);
     osSyncPrintf("En_Ny_actor_move2[ %x ] ！！\n", func_80ABDBF8);
     thisx->colChkInfo.mass = 0xFF;
-    thisx->unk164 = (u8)9;
-    thisx->update = (void (*)(struct Actor *, struct GlobalContext *)) &func_80ABDBF8;
+    this->collider.base.type = COLTYPE_METAL_SHIELD;
+    thisx->update = &func_80ABDBF8;
 }
-
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ny/EnNy_Destroy.s")
 
@@ -117,7 +112,14 @@ void EnNy_Init(Actor *thisx, GlobalContext *globalCtx) {
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ny/func_80ABCDAC.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ny/func_80ABCDBC.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ny/func_80ABCDBC.s")
+void func_80ABCDBC(EnNy *this) {
+    this->unk_1F4 = 0.0f;
+    func_80ABCD40(this);
+    this->unk_1DC = 0xB4;
+    this->actionFunc = &func_80ABCF4C;
+}
+
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ny/func_80ABCDFC.s")
 
--- src/overlays/actors/ovl_En_Ny/z_en_ny.h
+++ src/overlays/actors/ovl_En_Ny/z_en_ny.h
@@ -13,9 +13,22 @@ typedef struct EnNy {
     /* 0x014C */ EnNyActionFunc actionFunc;
     /* 0x0150 */ ColliderJntSph collider;
     /* 0x0170 */ ColliderJntSphItem items;
-    /* 0x01B0 */ char unk_1B0[0x108];
-    ///* 0x014C */ char unk_150[0x168];
-    /* 0x01
+    /* 0x01B0 */ char unk_1B0[0x1A];
+    /* 0x01CA */ u16 unk_1CA;
+    /* 0x01CC */ u16 unk_1CC;
+    /* 0x01CE */ u16 unk_1CE;
+    /* 0x01D0 */ u8 unk_1D0;
+    /* 0x01D1 */ s8 unk_1D1;
+    /* 0x01D2 */ char unk_1D2[2];
+    /* 0x01D4 */ s32 unk_1D4;
+    /* 0x01D8 */ s32 unk_1D8;
+    /* 0x01DC */ s32 unk_1DC;
+    /* 0x01E0 */ f32 unk_1E0;
+    /* 0x01E4 */ char unk_1E4[4];
+    /* 0x01E8 */ f32 unk_1E8;
+    /* 0x01EC */ char unk_1EC[0x8];
+    /* 0x01F4 */ f32 unk_1F4;
+    /* 0x01F8 */ char unk_1F8[0xC0];
 } EnNy; // size = 0x02B8
 
 extern const ActorInit En_Ny_InitVars;
