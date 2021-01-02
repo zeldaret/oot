1027e7390b4b68e8c6f7316e921ca18d5c318209
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-01-02 14:19:33 +0000

    Functions and most variables named

--- src/overlays/actors/ovl_En_Ge3/z_en_ge3.c
+++ src/overlays/actors/ovl_En_Ge3/z_en_ge3.c
@@ -10,15 +10,41 @@
 
 #define THIS ((EnGe3*)thisx)
 
+typedef enum {
+    /* 00 */ GELDB_JOINT_ROOT,
+    /* 01 */ GELDB_JOINT_1,
+    /* 02 */ GELDB_JOINT_TORSO,
+    /* 03 */ GELDB_JOINT_3, // Neck?
+    /* 04 */ GELDB_JOINT_PONYTAIL,
+    /* 05 */ GELDB_JOINT_VEIL,
+    /* 06 */ GELDB_JOINT_HEAD,
+    /* 07 */ GELDB_JOINT_RIGHT_UPPER_ARM,
+    /* 08 */ GELDB_JOINT_RIGHT_FOREARM,
+    /* 09 */ GELDB_JOINT_9, // Right wrist?
+    /* 10 */ GELDB_JOINT_RIGHT_HAND,
+    /* 11 */ GELDB_JOINT_RIGHT_SWORD,
+    /* 12 */ GELDB_JOINT_LEFT_UPPER_ARM,
+    /* 13 */ GELDB_JOINT_LEFT_FOREARM,
+    /* 14 */ GELDB_JOINT_14, // Left wrist?
+    /* 15 */ GELDB_JOINT_LEFT_HAND,
+    /* 16 */ GELDB_JOINT_LEFT_SWORD,
+    /* 17 */ GELDB_JOINT_LEFT_THIGH,
+    /* 18 */ GELDB_JOINT_LEFT_LOWER_LEG,
+    /* 19 */ GELDB_JOINT_LEFT_FOOT,
+    /* 20 */ GELDB_JOINT_RIGHT_THIGH,
+    /* 21 */ GELDB_JOINT_RIGHT_LOWER_LEG,
+    /* 22 */ GELDB_JOINT_RIGHT_FOOT,
+    /* 23 */ GELDB_JOINT_HIPS
+} EnGeldBJoints;
+
 void EnGe3_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnGe3_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnGe3_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnGe3_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-void func_80A34A80(EnGe3* this, GlobalContext* globalCtx);
-void func_80A34B90(EnGe3* this, GlobalContext* globalCtx);
-void func_80A34D68(Actor* thisx, GlobalContext* globalCtx);
-
+void EnGe3_WaitLookAtPlayer(EnGe3* this, GlobalContext* globalCtx);
+void EnGe3_ForceTalk(EnGe3* this, GlobalContext* globalCtx);
+void EnGe3_UpdateWhenNotTalking(Actor* thisx, GlobalContext* globalCtx);
 
 const ActorInit En_Ge3_InitVars = {
     ACTOR_EN_GE3,
@@ -32,147 +58,130 @@ const ActorInit En_Ge3_InitVars = {
     (ActorFunc)EnGe3_Draw,
 };
 
-
-// D_80A35190;
-static ColliderCylinderInit sCylinderInit =
-{
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK10, 0x00, 0x09, 0x39, 0x10, COLSHAPE_CYLINDER },
     { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000722, 0x00, 0x00 }, 0x00, 0x01, 0x01 },
     { 20, 50, 0, { 0, 0, 0 } },
 };
-static EnGe3ActionFunc D_80A351BC[] = { func_80A34A80 };
+static EnGe3ActionFunc sActionFunc[] = { EnGe3_WaitLookAtPlayer };
 
-static AnimationHeader* D_80A351C0[] = { 0x0600B07C };
+static AnimationHeader* sAnimationHeader[] = { 0x0600B07C }; // Idle with right hand on hip and left over mouth
 
-static u8 D_80A351C4[] = { 0 };
+static u8 sAnimationMode[] = { ANIMMODE_LOOP };
 
 static Vec3f D_80A351C8 = { 600.0f, 700.0f, 0.0f };
 
-static Gfx* D_80A351D4[] = { 0x06005FE8, 0x060065A8, 0x06006D28 };
-
-// extern ColliderCylinderInit D_80A35190;
-// extern AnimationHeader* D_80A351C0[];
-// extern EnGe3ActionFunc D_80A351BC[];
-// extern u8 D_80A351C4[];
-// extern Vec3f D_80A351C8;
-// extern Gfx* D_80A351D4[];
-
+static u64* sEyeTextures[] = {
+    0x06005FE8, // Half-open
+    0x060065A8, // Quarter-open
+    0x06006D28, // Closed
+};
 
 extern FlexSkeletonHeader D_0600A458;
-extern AnimationHeader D_0600B07C;
+extern AnimationHeader D_0600B07C; // Idle with right hand on hip and left over mouth
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34620.s")
-void func_80A34620(EnGe3* this, s32 i) {
-    this->actionFunc = D_80A351BC[i];
-    SkelAnime_ChangeAnim(&this->skelAnime, D_80A351C0[i], 1.0f, 0.0f, (f32)SkelAnime_GetFrameCount(D_80A351C0[i]),
-                         D_80A351C4[i], -8.0f);
+void EnGe3_ChangeAction(EnGe3* this, s32 i) {
+    this->actionFunc = sActionFunc[i];
+    Animation_Change(&this->skelAnime, sAnimationHeader[i], 1.0f, 0.0f,
+                     (f32)Animation_GetLastFrame(sAnimationHeader[i]), sAnimationMode[i], -8.0f);
     this->unk_30C &= ~2;
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/EnGe3_Init.s")
 void EnGe3_Init(Actor* thisx, GlobalContext* globalCtx2) {
     EnGe3* this = THIS;
     GlobalContext* globalCtx = globalCtx2;
 
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawFunc_Circle, 36.0f);
-    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &D_0600A458, NULL, this->limbDrawTable, this->transitionDrawTable,
-                       0x18);
-    SkelAnime_ChangeAnimDefaultRepeat(&this->skelAnime, &D_0600B07C);
+    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &D_0600A458, NULL, this->jointTable, this->morphTable, 24);
+    Animation_PlayLoop(&this->skelAnime, &D_0600B07C);
     Collider_InitCylinder(globalCtx, &this->collider);
     Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
     this->actor.colChkInfo.mass = 0xFF;
     Actor_SetScale(&this->actor, 0.01f);
     this->actor.posRot.rot.z = 0;
     this->actor.shape.rot.z = 0;
-    func_80A34620(this, 0);
-    this->actionFunc = func_80A34B90;
-    this->unk_30C = (u16)0;
+    EnGe3_ChangeAction(this, 0);
+    this->actionFunc = EnGe3_ForceTalk;
+    this->unk_30C = 0;
     this->actor.unk_1F = 6;
     this->actor.minVelocityY = -4.0f;
     this->actor.gravity = -1.0f;
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/EnGe3_Destroy.s")
 void EnGe3_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     EnGe3* this = THIS;
 
     Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A347F4.s")
-void func_80A347F4(EnGe3* this, GlobalContext* globalCtx) {
+void EnGe3_TurnToFacePlayer(EnGe3* this, GlobalContext* globalCtx) {
     s32 pad;
-    s16 angle_diff = this->actor.yawTowardsLink - this->actor.shape.rot.y;
+    s16 angleDiff = this->actor.yawTowardsLink - this->actor.shape.rot.y;
 
-    if (ABS(angle_diff) < 0x4001) {
-        Math_SmoothScaleMaxMinS(&this->actor.shape.rot.y, this->actor.yawTowardsLink, 6, 4000, 100);
+    if (ABS(angleDiff) <= 0x4000) {
+        Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsLink, 6, 4000, 100);
         this->actor.posRot.rot.y = this->actor.shape.rot.y;
-        func_80038290(globalCtx, &this->actor, &this->unk_300, &this->unk_306, this->actor.posRot2.pos);
+        func_80038290(globalCtx, &this->actor, &this->headRot, &this->unk_306, this->actor.posRot2.pos);
     } else {
-        if (angle_diff < 0) {
-            Math_SmoothScaleMaxMinS(&this->unk_300.y, -0x2000, 6, 6200, 0x100);
+        if (angleDiff < 0) {
+            Math_SmoothStepToS(&this->headRot.y, -0x2000, 6, 6200, 0x100);
         } else {
-            Math_SmoothScaleMaxMinS(&this->unk_300.y, 0x2000, 6, 6200, 0x100);
+            Math_SmoothStepToS(&this->headRot.y, 0x2000, 6, 6200, 0x100);
         }
-        Math_SmoothScaleMaxMinS(&this->actor.shape.rot.y, this->actor.yawTowardsLink, 12, 1000, 100);
+
+        Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsLink, 12, 1000, 100);
         this->actor.posRot.rot.y = this->actor.shape.rot.y;
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A3490C.s")
-void func_80A3490C(EnGe3* this, GlobalContext* globalCtx) {
-    if ((ABS((s16)(this->actor.yawTowardsLink - this->actor.shape.rot.y)) < 0x2301) &&
+void EnGe3_LookAtPlayer(EnGe3* this, GlobalContext* globalCtx) {
+    if ((ABS((s16)(this->actor.yawTowardsLink - this->actor.shape.rot.y)) <= 0x2300) &&
         (this->actor.xzDistFromLink < 100.0f)) {
-        func_80038290(globalCtx, &this->actor, &this->unk_300, &this->unk_306, this->actor.posRot2.pos);
+        func_80038290(globalCtx, &this->actor, &this->headRot, &this->unk_306, this->actor.posRot2.pos);
     } else {
-        Math_SmoothScaleMaxMinS(&this->unk_300.x, 0, 6, 6200, 100);
-        Math_SmoothScaleMaxMinS(&this->unk_300.y, 0, 6, 6200, 100);
-        Math_SmoothScaleMaxMinS(&this->unk_306.x, 0, 6, 6200, 100);
-        Math_SmoothScaleMaxMinS(&this->unk_306.y, 0, 6, 6200, 100);
+        Math_SmoothStepToS(&this->headRot.x, 0, 6, 6200, 100);
+        Math_SmoothStepToS(&this->headRot.y, 0, 6, 6200, 100);
+        Math_SmoothStepToS(&this->unk_306.x, 0, 6, 6200, 100);
+        Math_SmoothStepToS(&this->unk_306.y, 0, 6, 6200, 100);
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34A20.s")
-void func_80A34A20(EnGe3* this, GlobalContext* globalCtx) {
+void EnGe3_Wait(EnGe3* this, GlobalContext* globalCtx) {
     if (func_8002F334(&this->actor, globalCtx)) {
-        this->actionFunc = func_80A34A80;
-        this->actor.update = func_80A34D68;
-        this->actor.flags = this->actor.flags & ~0x10000;
+        this->actionFunc = EnGe3_WaitLookAtPlayer;
+        this->actor.update = EnGe3_UpdateWhenNotTalking;
+        this->actor.flags &= ~0x10000;
     }
-    func_80A347F4(this, globalCtx);
+    EnGe3_TurnToFacePlayer(this, globalCtx);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34A80.s")
-void func_80A34A80(EnGe3* this, GlobalContext* globalCtx) {
-    func_80A3490C(this, globalCtx);
+void EnGe3_WaitLookAtPlayer(EnGe3* this, GlobalContext* globalCtx) {
+    EnGe3_LookAtPlayer(this, globalCtx);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34AA0.s")
-void func_80A34AA0(EnGe3* this, GlobalContext* globalCtx) {
+void EnGe3_WaitTillCardGiven(EnGe3* this, GlobalContext* globalCtx) {
     if (Actor_HasParent(&this->actor, globalCtx)) {
         this->actor.parent = NULL;
-        this->actionFunc = func_80A34A20;
+        this->actionFunc = EnGe3_Wait;
     } else {
         func_8002F434(&this->actor, globalCtx, GI_GERUDO_CARD, 10000.0f, 50.0f);
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34B00.s")
-void func_80A34B00(EnGe3* this, GlobalContext* globalCtx) {
+void EnGe3_GiveCard(EnGe3* this, GlobalContext* globalCtx) {
     if ((func_8010BDBC(&globalCtx->msgCtx) == 5) && (func_80106BC8(globalCtx) != 0)) {
         func_80106CCC(globalCtx);
         this->actor.flags &= ~0x10000;
-        this->actionFunc = func_80A34AA0;
+        this->actionFunc = EnGe3_WaitTillCardGiven;
         func_8002F434(&this->actor, globalCtx, GI_GERUDO_CARD, 10000.0f, 50.0f);
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34B90.s")
-void func_80A34B90(EnGe3* this, GlobalContext* globalCtx) {
+void EnGe3_ForceTalk(EnGe3* this, GlobalContext* globalCtx) {
     if (func_8002F194(&this->actor, globalCtx)) {
-        this->actionFunc = func_80A34B00;
+        this->actionFunc = EnGe3_GiveCard;
     } else {
-        if ((this->unk_30C & 4) == 0) {
+        if (!(this->unk_30C & 4)) {
             func_8002DF54(globalCtx, &this->actor, 7);
             this->unk_30C |= 4;
         }
@@ -180,50 +189,44 @@ void func_80A34B90(EnGe3* this, GlobalContext* globalCtx) {
         this->actor.flags |= 0x10000;
         func_8002F1C4(&this->actor, globalCtx, 300.0f, 300.0f, 0);
     }
-    func_80A3490C(this, globalCtx);
+    EnGe3_LookAtPlayer(this, globalCtx);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34C40.s")
-void func_80A34C40(EnGe3* this, GlobalContext* globalCtx) {
+void EnGe3_MaintainCollider(EnGe3* this, GlobalContext* globalCtx) {
     s32 pad;
     s32 pad2;
 
     Collider_CylinderUpdate(&this->actor, &this->collider);
     CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
     func_8002E4B4(globalCtx, &this->actor, 40.0f, 25.0f, 40.0f, 5);
-    if (((this->unk_30C & 2) == 0) && (SkelAnime_FrameUpdateMatrix(&this->skelAnime) != 0)) {
+
+    if (!(this->unk_30C & 2) && SkelAnime_Update(&this->skelAnime)) {
         this->unk_30C |= 2;
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34CE4.s")
-void func_80A34CE4(EnGe3* this, GlobalContext* globalCtx) {
-    s32 phi_v0;
+void EnGe3_MoveAndBlink(EnGe3* this, GlobalContext* globalCtx) {
 
     Actor_MoveForward(&this->actor);
-    if (this->unk_2FE == 0) {
-        phi_v0 = 0;
-    } else {
-        this->unk_2FE--;
-        phi_v0 = this->unk_2FE;
+
+    if (DECR(this->blinkTimer) == 0) {
+        this->blinkTimer = Rand_S16Offset(60, 60);
     }
-    if (phi_v0 == 0) {
-        this->unk_2FE = Math_Rand_S16Offset(60, 60);
-    }
-    this->unk_2FC = this->unk_2FE;
-    if (this->unk_2FC >= 3) {
-        this->unk_2FC = 0;
+    this->eyeIndex = this->blinkTimer;
+
+    if (this->eyeIndex >= 3) {
+        this->eyeIndex = 0;
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34D68.s")
-void func_80A34D68(Actor* thisx, GlobalContext* globalCtx) {
+void EnGe3_UpdateWhenNotTalking(Actor* thisx, GlobalContext* globalCtx) {
     EnGe3* this = THIS;
 
-    func_80A34C40(this, globalCtx);
+    EnGe3_MaintainCollider(this, globalCtx);
     this->actionFunc(this, globalCtx);
-    if (func_8002F194(&this->actor, globalCtx) != 0) {
-        this->actionFunc = func_80A34A20;
+
+    if (func_8002F194(&this->actor, globalCtx)) {
+        this->actionFunc = EnGe3_Wait;
         this->actor.update = EnGe3_Update;
     } else {
         this->actor.textId = 0x6005;
@@ -231,41 +234,44 @@ void func_80A34D68(Actor* thisx, GlobalContext* globalCtx) {
             func_8002F2CC(&this->actor, globalCtx, 100.0f);
         }
     }
-    func_80A34CE4(this, globalCtx);
+
+    EnGe3_MoveAndBlink(this, globalCtx);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/EnGe3_Update.s")
 void EnGe3_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnGe3* this = THIS;
 
-    func_80A34C40(this, globalCtx);
+    EnGe3_MaintainCollider(this, globalCtx);
     this->actionFunc(this, globalCtx);
-    func_80A34CE4(this, globalCtx);
+    EnGe3_MoveAndBlink(this, globalCtx);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A34E58.s")
-s32 func_80A34E58(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx) {
+s32 EnGe3_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx) {
     EnGe3* this = THIS;
 
     switch (limbIndex) {
-        case 5:
-        case 11:
-        case 16:
+        // Hide swords and veil from object_geldb
+        case GELDB_JOINT_VEIL:
+        case GELDB_JOINT_RIGHT_SWORD:
+        case GELDB_JOINT_LEFT_SWORD:
             *dList = NULL;
-            return 0;
-        case 6:
-            rot->x += this->unk_300.y;
+            return false;
+        // Turn head
+        case GELDB_JOINT_HEAD:
+            rot->x += this->headRot.y;
+
+        // This is a hack to fix the color-changing clothes this Gerudo has on N64 versions
         default:
             OPEN_DISPS(globalCtx->state.gfxCtx, "../z_en_ge3.c", 547);
             switch (limbIndex) {
-                case 3:
+                case GELDB_JOINT_3:
                     break;
-                case 6:
+                case GELDB_JOINT_HEAD:
                     gDPPipeSync(POLY_OPA_DISP++);
                     gDPSetEnvColor(POLY_OPA_DISP++, 80, 60, 10, 255);
                     break;
-                case 11:
-                case 16:
+                case GELDB_JOINT_RIGHT_SWORD:
+                case GELDB_JOINT_LEFT_SWORD:
                     gDPPipeSync(POLY_OPA_DISP++);
                     gDPSetEnvColor(POLY_OPA_DISP++, 140, 170, 230, 255);
                     gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 255, 255, 255, 255);
@@ -276,32 +282,28 @@ s32 func_80A34E58(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* p
                     break;
             }
             CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_en_ge3.c", 566);
-            return 0;
+            break;
     }
-
+    return false;
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/func_80A35004.s")
-void func_80A35004(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, void* thisx) {
+void EnGe3_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, void* thisx) {
     EnGe3* this = THIS;
-    Vec3f sp18;
-
-    sp18 = D_80A351C8;
-    if (limbIndex == 6) {
-        Matrix_MultVec3f(&sp18, &this->actor.posRot2.pos);
+    Vec3f src = D_80A351C8;
+    if (limbIndex == GELDB_JOINT_HEAD) {
+        Matrix_MultVec3f(&src, &this->actor.posRot2.pos);
     }
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ge3/EnGe3_Draw.s")
 void EnGe3_Draw(Actor* thisx, GlobalContext* globalCtx2) {
     EnGe3* this = THIS;
     GlobalContext* globalCtx = globalCtx2;
 
     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_en_ge3.c", 614);
     func_800943C8(globalCtx->state.gfxCtx);
-    gSPSegment(POLY_OPA_DISP++, 0x08, SEGMENTED_TO_VIRTUAL(D_80A351D4[this->unk_2FC]));
+    gSPSegment(POLY_OPA_DISP++, 0x08, SEGMENTED_TO_VIRTUAL(sEyeTextures[this->eyeIndex]));
     func_8002EBCC(&this->actor, globalCtx, 0);
-    SkelAnime_DrawFlexOpa(globalCtx, this->skelAnime.skeleton, this->skelAnime.limbDrawTbl,
-                          (s32)this->skelAnime.dListCount, func_80A34E58, func_80A35004, this);
+    SkelAnime_DrawFlexOpa(globalCtx, this->skelAnime.skeleton, this->skelAnime.jointTable, this->skelAnime.dListCount,
+                          EnGe3_OverrideLimbDraw, EnGe3_PostLimbDraw, this);
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_en_ge3.c", 631);
 }
--- src/overlays/actors/ovl_En_Ge3/z_en_ge3.h
+++ src/overlays/actors/ovl_En_Ge3/z_en_ge3.h
@@ -12,14 +12,13 @@ typedef struct EnGe3 {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ ColliderCylinder collider;
     /* 0x0198 */ SkelAnime skelAnime;
-    /* 0x01DC */ Vec3s limbDrawTable[0x18];
-    /* 0x026C */ Vec3s transitionDrawTable[0x18];
-    /* 0x02FC */ s16 unk_2FC;
-    /* 0x02FC */ s16 unk_2FE;
-    /* 0x0300 */ Vec3s unk_300;
+    /* 0x01DC */ Vec3s jointTable[24];
+    /* 0x026C */ Vec3s morphTable[24];
+    /* 0x02FC */ s16 eyeIndex;
+    /* 0x02FE */ s16 blinkTimer;
+    /* 0x0300 */ Vec3s headRot;
     /* 0x0306 */ Vec3s unk_306;
     /* 0x030C */ u16 unk_30C;
-    /* 0x014C */ char unk_30E[0x2];
     /* 0x0310 */ EnGe3ActionFunc actionFunc;
 } EnGe3; // size = 0x0314
 
