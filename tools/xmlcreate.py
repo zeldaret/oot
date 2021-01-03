3f06482780ef71cce0f7e722f0ee3ec7a2f9d458
petrie911 <pmontag@DESKTOP-LG8A167.localdomain>
2021-01-02 21:26:17 -0600

    fixes

--- tools/xmlcreate.py
+++ tools/xmlcreate.py
@@ -2,106 +2,70 @@
 import os
 import argparse
 
-dlist_count = 0
-collision_count = 0
-skeleton_count = 0
-animation_count = 0
-texture_count = 0
-unknown_count = 0
 offsets = set()
 replacements = {}
+global_name = ''
 
-name = ''
+name_fmt = 'g{0}{1}_{2}'
+spec_fmt = '    include "build/assets/objects/{0}/{0}.o"\n    number 6\n'
 
-dlist_xml = '<DList Name="g{0}DL{1}" Offset="0x{2}"/>'
-collision_xml = '<Collision Name="g{0}Col{1}" Offset="0x{2}"/>'
-animation_xml = '<Animation Name="g{0}Anim{1}" Offset="0x{2}"/>'
-skeleton_xml = '<Skeleton Name="g{0}Skel{1}" Type="{3}" LimbType="{4}" Offset="0x{2}"/>'
-unknown_xml = '<!-- <{3} Name=g{0}Unknown{1} Offset="0x{2}"/> -->'
+dlist_xml = '<DList Name="{0}" Offset="0x{1}"/>'
+collision_xml = '<Collision Name="{0}" Offset="0x{1}"/>'
+animation_xml = '<Animation Name="{0}" Offset="0x{1}"/>'
+skeleton_xml = '<Skeleton Name="{0}" Type="{2}" LimbType="{3}" Offset="0x{1}"/>'
+unknown_xml = '<!-- <{2} Name="{0}" Offset="0x{1}"/> -->'
 
 def set_globals(new_name):
-    global dlist_count
-    global collision_count
-    global skeleton_count
-    global animation_count
-    global texture_count
-    global unknown_count
     global offsets
-    global name
+    global global_name
     global replacements
 
-    dlist_count = 0
-    collision_count = 0
-    skeleton_count = 0
-    animation_count = 0
-    texture_count = 0
-    unknown_count = 0
     offsets = set()
     replacements = {}
-    name = new_name
+    global_name = new_name
     return 0
 
 
-def dlist_to_xml(offset):
-    global dlist_count
-    dlist_count += 1
+def dlist_to_xml(var_name,offset):
 
-    return dlist_xml.format(name,dlist_count,offset)
+    return dlist_xml.format(var_name,offset.lstrip('0'))
 
-def collision_to_xml(offset):
-    global collision_count
-    collision_count += 1
+def collision_to_xml(var_name,offset):
 
-    return collision_xml.format(name,collision_count,offset)
+    return collision_xml.format(var_name,offset.lstrip('0'))
 
-def animation_to_xml(offset):
-    global animation_count
-    animation_count += 1
+def animation_to_xml(var_name,offset):
 
-    return animation_xml.format(name,animation_count,offset)
+    return animation_xml.format(var_name,offset.lstrip('0'))
 
-def skeleton_to_xml(offset, type):
-    global skeleton_count
-    skeleton_count += 1
+def skeleton_to_xml(var_name,offset, type):
     skel_type = "Flex" if "Flex" in type else "Standard"
     limb_type = "Standard"
 
-    return skeleton_xml.format(name,skeleton_count,offset, skel_type, limb_type)
+    return skeleton_xml.format(var_name, offset.lstrip('0'), skel_type, limb_type)
 
-def unknown_to_xml(offset, type):
-    global unknown_count
-    unknown_count += 1
+def unknown_to_xml(var_name,offset, type):
 
-    return unknown_xml.format(name,unknown_count,offset,type)
-
-def make_replace(symbol, type):
-    global replacements
-
-    if 'Gfx' in type:
-        var_name = 'g' + name + 'DList' + str(dlist_count)
-    elif 'Col' in type:
-        var_name = 'g' + name + 'Collision' + str(collision_count)
-    elif 'Animation' in type:
-        var_name = 'g' + name + 'Anim' + str(animation_count)
-    elif 'Skeleton' in type:
-        var_name = 'g' + name + 'Skel' + str(skeleton_count)
-    else:
-        var_name = 'g' + name + 'Unknown' + str(unknown_count)
-    replacements[symbol] = var_name
-    return 1
+    return unknown_xml.format(var_name,offset.lstrip('0'),type)
 
 def make_xml_line(offset, type):
     if 'Gfx' in type:
-        xml_line = dlist_to_xml(offset)
+        var_name = name_fmt.format(global_name,'DL',offset)
+        xml_line = dlist_to_xml(var_name, offset)
     elif 'Col' in type:
-        xml_line = collision_to_xml(offset)
+        var_name = name_fmt.format(global_name,'Col',offset)
+        xml_line = collision_to_xml(var_name, offset)
     elif 'Animation' in type:
-        xml_line = animation_to_xml(offset)
+        var_name = name_fmt.format(global_name,'Anim',offset)
+        xml_line = animation_to_xml(var_name, offset)
     elif 'Skeleton' in type:
-        xml_line = skeleton_to_xml(offset, type)
+        var_name = name_fmt.format(global_name,'Skel',offset)
+        xml_line = skeleton_to_xml(var_name, offset, type)
     else:
-        xml_line = unknown_to_xml(offset, type)
+        var_name = name_fmt.format(global_name,'Unknown',offset)
+        xml_line = unknown_to_xml(var_name, offset, type)
         print('Unknown type at offset', offset)
+    replacements['06'+offset] = var_name
     return xml_line
 
 def extern_to_xml(line):
@@ -109,7 +73,7 @@ def extern_to_xml(line):
 
     type = line.split()[1]
     sym = line.split()[2]
-    offset = sym[5:10]
+    offset = sym[4:10]
     if(offset in offsets):
         return ''
     else:
@@ -117,7 +81,7 @@ def extern_to_xml(line):
 
     xml_line = ' ' * 8
     xml_line += make_xml_line(offset, type)
-    make_replace('060' + offset, type)
+    # make_replace(offset, type)
 
     return xml_line + '\n'
 
@@ -132,15 +96,17 @@ def find_type(srcdata, i):
 
 def other_to_xml(srcdata, i):
     xml_data = ''
-    index = srcdata[i].find('0x060')
-    while(index< len(srcdata[i]) and '0x060' in srcdata[i][index:]):
-        offset = srcdata[i][index+5:index+10]
+    line = srcdata[i]
+
+    index = line.find('0x060')
+    while(index < len(line) and '0x060' in line[index:]):
+        offset = line[index+4:index+10]
         type = find_type(srcdata, i)
         if(offset not in offsets):
             offsets.add(offset)
             xml_data += ' ' * 8 + make_xml_line(offset, type) + '\n'
-            make_replace('060' + offset, type)
-        index += srcdata[i][index+10:].find('0x060') + 10
+            # make_replace(offset, type)
+        index += line[index+10:].find('0x060') + 10
     return xml_data
 
 def find_object(src):
@@ -166,7 +132,7 @@ def create_xml(src, name):
         if '0x060' in line or 'D_060' in line:
             if 'extern' in line:
                 xml += extern_to_xml(line)
-            else:
+            elif '0x060' in line:
                 xml += other_to_xml(srcdata, i)
     xml += '    </File>\n</Root>\n'
     return xml
@@ -187,26 +153,51 @@ def add_header(src):
 
 def replace_src(src):
     global replacements
-    global name
+    global global_name
 
     add_header(src)
     with open(src,'r', encoding='utf-8') as srcfile:
         srcdata = srcfile.read()
     for key in list(replacements.keys()):
         srcdata = srcdata.replace(key, replacements.get(key))
-        srcdata = srcdata.replace('D_g' + name, 'g' + name)
+        srcdata = srcdata.replace('D_g' + global_name, 'g' + global_name)
         if('Gfx' in replacements.get(key)):
-            srcdata = srcdata.replace('0xg' + name, 'g' + name)
+            srcdata = srcdata.replace('0xg' + global_name, 'g' + global_name)
         else:
-            srcdata = srcdata.replace('0xg' + name, '&g' + name)
+            srcdata = srcdata.replace('0xg' + global_name, '&g' + global_name)
     with open(src,'w',encoding='utf-8', newline = '\n') as outfile:
         outfile.write(srcdata)
+    return 1
+
+def fix_spec(src, spec):
+    object = find_object(src)
+    fix = False
+    old = False
+    with open(spec, 'r') as specfile:
+        specdata = specfile.readlines()
+    for i, line in enumerate(specdata):
+        if ('"' + object + '"') in line:
+            if 'number' in specdata[i+3]:
+                old = True
+            else:
+                specdata[i+2] = spec_fmt.format(object)
+                fix = True
+            break
+    if old:
+        print('Already fixed', object, 'in', spec)
+        return 0
+    if not fix:
+        print('Could not find',object,'in', spec)
+        return -1
+    with open(spec, 'w', newline='\n') as outfile:
+        outfile.writelines(specdata)
     return 1
 
 parser = argparse.ArgumentParser(description="Generate an xml object file from a source file")
 parser.add_argument('file', help="overlay file to generate xml from")
 parser.add_argument('name', help='name to use for xml variables')
 parser.add_argument('-r',action='store_true', help="replace variables in overlay with the new names")
+parser.add_argument('-s',metavar = 'spec', dest = 'spec', help="spec file to update", default=None)
 parser.add_argument('-o', metavar = 'outfile', dest = 'outfile', help = 'file to write xml to', default = None)
 
 if __name__ == "__main__":
@@ -218,3 +209,5 @@ if __name__ == "__main__":
             outfile.write(create_xml(args.file, args.name))
     if(args.r):
         replace_src(args.file)
+    if(args.spec is not None):
+        fix_spec(args.file, args.spec)
