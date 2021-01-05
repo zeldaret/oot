fd8a6f59e80192f3afcf66c6353b354b6535a451
Zelllll <elijah@DESKTOP-NMP1I89.localdomain>
2021-01-04 20:25:05 -0600

    more functions matched

--- src/overlays/actors/ovl_Bg_Spot06_Objects/z_bg_spot06_objects.c
+++ src/overlays/actors/ovl_Bg_Spot06_Objects/z_bg_spot06_objects.c
@@ -14,14 +14,16 @@ void func_808AEE00(BgSpot06Objects* this, GlobalContext* globalCtx);
 void func_808AF450(BgSpot06Objects* this, GlobalContext* globalCtx);
 void func_808AEEFC(BgSpot06Objects* this, GlobalContext* globalCtx);
 void func_808AF7FC(BgSpot06Objects* this, GlobalContext* globalCtx);
+void func_808AF1D8(BgSpot06Objects* this, GlobalContext* globalCtx);
+void func_808AF524(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06000120;
-extern UNK_TYPE D_06000470;
-extern UNK_TYPE D_06000E10;
+extern Gfx D_06000120[];
+extern Gfx D_06000470[];
+extern Gfx D_06000E10[];
 extern UNK_TYPE D_06000EE8;
-extern UNK_TYPE D_06001160;
+extern Gfx D_06001160[];
 extern UNK_TYPE D_06001238;
-extern UNK_TYPE D_06002490;
+extern Gfx D_06002490[];
 
 const ActorInit Bg_Spot06_Objects_InitVars = {
     ACTOR_BG_SPOT06_OBJECTS,
@@ -134,7 +136,21 @@ void BgSpot06Objects_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot06_Objects/BgSpot06Objects_Destroy.s")
+void BgSpot06Objects_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgSpot06Objects* this = THIS;
+
+    switch (this->dyna.actor.params) {
+        case 0:
+        case 3:
+            DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+            break;
+        case 1:
+            Collider_DestroyJntSph(globalCtx, &this->collider.base);
+            break;
+        case 2:
+            break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot06_Objects/func_808AEBC0.s")
 
@@ -160,9 +176,59 @@ void BgSpot06Objects_Init(Actor* thisx, GlobalContext* globalCtx) {
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot06_Objects/BgSpot06Objects_Update.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot06_Objects/func_808AF524.s")
+void func_808AF524(Actor* thisx, GlobalContext* globalCtx) {
+    BgSpot06Objects* this = THIS;
+    s32 gameplayFrames;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot06_Objects/BgSpot06Objects_Draw.s")
+    OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot06_objects.c", 844);
+
+    func_80093D84(globalCtx->state.gfxCtx);
+
+    gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot06_objects.c", 850),
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+
+    gameplayFrames = globalCtx->state.frames;
+
+    gSPSegment(POLY_XLU_DISP++, 0x08,
+               Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, -gameplayFrames, gameplayFrames, 32, 32, 1, gameplayFrames,
+                                gameplayFrames, 32, 32));
+    gSPSegment(POLY_XLU_DISP++, 0x09,
+               Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, -gameplayFrames, gameplayFrames * 6, 32, 32, 1,
+                                gameplayFrames, gameplayFrames * 6, 32, 32));
+
+    gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, 255, 128);
+
+    if ((this->lakeHyliaWaterBoxYPos < -680.0f) && (gSaveContext.sceneSetupIndex < 4)) {
+        gSPDisplayList(POLY_XLU_DISP++, D_06000120);
+    } else {
+        gSPDisplayList(POLY_XLU_DISP++, D_06000470);
+    }
+
+    CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot06_objects.c", 879);
+}
+
+void BgSpot06Objects_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    BgSpot06Objects* this = THIS;
+
+    switch (this->dyna.actor.params) {
+        case 0:
+            Gfx_DrawDListOpa(globalCtx, D_06000E10);
+            break;
+        case 1:
+            Gfx_DrawDListOpa(globalCtx, D_06002490);
+
+            if (this->actionFunc == func_808AF1D8) {
+                func_800628A4(1, &this->collider);
+            }
+            break;
+        case 2:
+            func_808AF524(thisx, globalCtx);
+            break;
+        case 3:
+            Gfx_DrawDListOpa(globalCtx, D_06001160);
+            break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Spot06_Objects/func_808AF7FC.s")
 
