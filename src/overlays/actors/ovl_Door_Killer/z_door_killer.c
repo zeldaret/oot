9ec073b9dae752e3a8e347994a665a5c2b260063
adam <adamkiddle23@gmail.com>
2021-01-10 20:49:26 +0000

    Init done

--- src/overlays/actors/ovl_Door_Killer/z_door_killer.c
+++ src/overlays/actors/ovl_Door_Killer/z_door_killer.c
@@ -4,11 +4,24 @@
 
 #define THIS ((DoorKiller*)thisx)
 
+
+
 void DoorKiller_Init(Actor* thisx, GlobalContext* globalCtx);
 void DoorKiller_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void DoorKiller_Update(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06001BC8;
+//prototypes
+void func_80995D6C();
+
+extern FlexSkeletonHeader D_06001BC8;
+
+extern temp_struct D_80996010[];
+
+extern UNK_TYPE D_80996014;
+
+extern ColliderCylinderInit D_80995FB0;
+
+extern ColliderJntSphInit D_80996000;
 
 /*
 const ActorInit Door_Killer_InitVars = {
@@ -23,7 +36,71 @@ const ActorInit Door_Killer_InitVars = {
     NULL,
 };
 */
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Door_Killer/DoorKiller_Init.s")
+
+void DoorKiller_Init(Actor *thisx, GlobalContext *globalCtx) {
+    DoorKiller *this = THIS;
+    f32 sp50;
+    temp_struct *phi_s1;
+    s32 phi_s0;
+    s32 temp_v0;
+    f32 sp40;
+    GlobalContext *globalCtx2 = globalCtx;
+    
+    for (temp_v0 = -1, phi_s0 = 0; temp_v0 < 0 ; phi_s0++){
+        temp_v0 = Object_GetIndex(&globalCtx2->objectCtx, D_80996010[phi_s0].objectID);
+        this->unk21D = phi_s0;
+    }
+
+    osSyncPrintf((const char *) "bank_ID = %d\n", temp_v0);
+    osSyncPrintf((const char *) "status = %d\n", this->unk21D);
+    this->unk21C = temp_v0;
+    this->unk214 = D_80996010[this->unk21D].seg_address;
+    ActorShape_Init(&thisx->shape, 0.0f, NULL, 0.0f);
+    Actor_SetScale(thisx, 0.01f);
+
+    this->unk191 = this->unk190 = this->unk218 = this->unk21A = 0;
+
+    switch ((thisx->params & 0xFFFF) & 0xFF){
+        case 0:
+            SkelAnime_InitFlex(globalCtx2, &this->unk14C, &D_06001BC8, NULL, &this->unk192, &this->unk192, 9);
+            this->unk280 = &func_80995D6C;
+            func_80995D6C(thisx, globalCtx2);
+            this->unk198 = this->unk19C = 0x4000;
+            Collider_InitCylinder(globalCtx, &this->unk1C8);
+            Collider_SetCylinder(globalCtx, &this->unk1C8, thisx, &D_80995FB0);
+            Collider_InitJntSph(globalCtx, &this->unk220);
+            Collider_SetJntSph(globalCtx, &this->unk220, thisx, &D_80996000, &this->unk240);
+            this->unk220.list[0].dim.worldSphere.radius = 80;
+            this->unk220.list[0].dim.worldSphere.center.x = (s16)thisx->posRot.pos.x;
+            this->unk220.list[0].dim.worldSphere.center.y = (s16)thisx->posRot.pos.y + 50;
+            this->unk220.list[0].dim.worldSphere.center.z = (s16)thisx->posRot.pos.z;
+            if ((((thisx->params >> 8) & 0x3F) != 0x3F) && Flags_GetSwitch(globalCtx, ((thisx->params >> 8) & 0x3F))) {
+                Actor_Kill(thisx);
+                return;
+            }
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            this->unk280 = &func_80995D6C;
+            func_80995D6C(thisx, globalCtx);
+            thisx->gravity = -0.6f;
+            thisx->minVelocityY = -6.0f;
+            sp50 = Rand_CenteredFloat(8.0f);
+            thisx->velocity.z = Rand_ZeroFloat(8.0f);
+            sp40 = Math_CosS(thisx->posRot.rot.y);
+            thisx->velocity.x = (Math_SinS(thisx->posRot.rot.y) * thisx->velocity.z) + (sp40 * sp50);
+            sp40 = Math_SinS(thisx->posRot.rot.y);
+            thisx->velocity.z = (Math_CosS(thisx->posRot.rot.y) * thisx->velocity.z) + (-sp40 * sp50);
+            thisx->velocity.y = Rand_ZeroFloat(4.0f) + 4.0f;
+            thisx->posRot.rot.x = (s16) (s32) Rand_CenteredFloat(4096.0f);
+            thisx->posRot.rot.y = (s16) (s32) Rand_CenteredFloat(4096.0f);
+            thisx->posRot.rot.z = (s16) (s32) Rand_CenteredFloat(4096.0f);
+            this->unk21A = (u16)80;
+            break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Door_Killer/DoorKiller_Destroy.s")
 
--- src/overlays/actors/ovl_Door_Killer/z_door_killer.h
+++ src/overlays/actors/ovl_Door_Killer/z_door_killer.h
@@ -4,11 +4,35 @@
 #include "ultra64.h"
 #include "global.h"
 
+typedef struct{
+    /* 0x00 */s16 objectID;
+    /* 0x02 */char pad[0x2];
+    /* 0x04 */s32 *seg_address;
+} temp_struct; // size 0x8
+
 struct DoorKiller;
 
 typedef struct DoorKiller {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x138];
+    /* 0x014C */ SkelAnime unk14C;
+    /* 0x0190 */ u8 unk190;
+    /* 0x0191 */ u8 unk191;
+    /* 0x0192 */ Vec3s unk192;
+    /* 0x0198 */ s16 unk198;
+    /* 0x019A */ char unk19A[0x2];
+    /* 0x019C */ s16 unk19C;
+    /* 0x019E */ char unk19E[0x2A];
+    /* 0x01C8 */ ColliderCylinder unk1C8;
+    /* 0x0214 */ s32 *unk214;
+    /* 0x0218 */ u16 unk218;
+    /* 0x021A */ u16 unk21A;
+    /* 0x021C */ s8 unk21C;
+    /* 0x021D */ u8 unk21D;
+    /* 0x021E */ char unk_21E[0x2];
+    /* 0x0220 */ ColliderJntSph unk220;
+    /* 0x0240 */ ColliderJntSphItem* unk240;
+    /* 0x0244 */ char unk_244[0x3C];
+    /* 0x0280 */ void *unk280;
 } DoorKiller; // size = 0x0284
 
 extern const ActorInit Door_Killer_InitVars;
