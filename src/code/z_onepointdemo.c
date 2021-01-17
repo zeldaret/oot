14fec2eb91d2404a0093854deeb4419d4ff03604
petrie911 <pmontag@DESKTOP-LG8A167.localdomain>
2021-01-17 17:46:56 -0600

    more matches

--- src/code/z_onepointdemo.c
+++ src/code/z_onepointdemo.c
@@ -14,9 +14,6 @@ void func_8007C680(Vec3f *vec1, Vec3f *vec2, VecSph *vecSph) {
     *vec1 = sp24;
 }
 
-s16 func_8007C704(Vec3f* vec1, Vec3f* vec2);
-// #pragma GLOBAL_ASM("asm/non_matchings/code/z_onepointdemo/func_8007C704.s")
-
 s16 func_8007C704(Vec3f *vec1, Vec3f *vec2) {
     return DEGF_TO_BINANG(RADF_TO_DEGF(Math_FAtan2F(vec2->x - vec1->x, vec2->z - vec1->z)));
 }
@@ -27,7 +24,13 @@ void func_8007C76C(Vec3f* src, Vec3s* dst) {
     dst->z = src->z;
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_onepointdemo/func_8007C7A8.s")
+void func_8007C7A8(CollisionContext *colCtx, Vec3f *vec1, Vec3f *vec2) {
+    Vec3f sp3C;
+    s32 sp38;
+    CollisionPoly* sp34 = NULL;
+
+    BgCheck_CameraLineTest1(colCtx, vec1, vec2, &sp3C, &sp34, 1, 1, 1, 0, &sp38);
+}
 
 void func_8007C7F8(CollisionContext* colCtx, Vec3f* pos) {
     CollisionPoly* poly;
@@ -44,6 +47,7 @@ void func_8007C820(OnePointDemoCamera* onePointCamera, s16 actionParameters, s16
     onePointCamera->initTimer = initTimer;
 }
 
+void func_8007C850(GlobalContext* globalCtx, s16 arg1, s16 arg2, Actor* actor, s16 timer);
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_onepointdemo/func_8007C850.s")
 
 s16 func_8007FFE0(GlobalContext *globalCtx, s16 arg1, s16 arg2) {
@@ -73,79 +77,79 @@ s32 func_80080024(GlobalContext *globalCtx, s16 arg1) {
     return phi_v1;
 }
 
-// s16 func_800800F8(GlobalContext *globalCtx, s16 csID, s16 timer, Actor *actor, s16 camIdx) {
-//     s16 sp42;
-//     Camera *temp_v1;
-//     s16 temp_a2;
-//     s16 temp_a3;
-//     s16 temp_s0;
-//     s16 temp_s2;
-//     s16 temp_s4;
-//     s16 temp_s5;
-//     s16 temp_v0;
-//     s16 temp_v0_2;
-//     void *temp_s1;
-//     s16 phi_s1;
-//     s16 phi_s0;
-//     Camera *phi_s1_2;
-//     s16 phi_s2;
+#ifdef NON_MATCHING
+s16 func_800800F8(GlobalContext *globalCtx, s16 csID, s16 timer, Actor *actor, s16 camIdx) {
+    Camera *temp_v1;
+    s16 sp42;
+    s16 temp_a2;
+    s16 temp_a3;
+    s16 phi_s0_2;
+    s16 phi_s0;
+    s16 phi_s2;
+    Camera* tempC;
 
+    if (camIdx == -1) {
+        camIdx = globalCtx->activeCamera;
+    }
+    sp42 = Gameplay_CreateSubCamera(globalCtx);
+    if (sp42 == -1) {
+        osSyncPrintf("\x1b[41;37monepoint demo: error: too many cameras ... give up! type=%d\n\x1b[m", csID);
+        return -1;
+    }
+    phi_s0 = 7;
+    phi_s2 = globalCtx->cameraPtrs[camIdx]->childCamIdx;
+    if (phi_s2 > 0) {
+        func_8007FFE0(globalCtx, phi_s2, sp42);
+        phi_s0 = 1;
+    } else {
+        Interface_ChangeAlpha(2);
+    }
+    func_8007FFE0(globalCtx, sp42, camIdx);
+    tempC = globalCtx->cameraPtrs[sp42];
+    tempC->timer = timer;
+    tempC->target = actor;
+    
+    tempC->at = globalCtx->view.lookAt;
+    tempC->eye = globalCtx->view.eye;
+    
 
-//     phi_s1 = camIdx;
-//     if (camIdx == -1) {
-//         phi_s1 = globalCtx->activeCamera;
-//     }
-//     sp42 = Gameplay_CreateSubCamera(globalCtx);
-//     if (sp42 == -1) {
-//         osSyncPrintf("\x1b[41;37monepoint demo: error: too many cameras ... give up! type=%d\n\x1b[m", temp_s4);
-//         return -1;
-//     }
-//     if (globalCtx->cameraPtrs[phi_s1]->childCamIdx > 0) {
-//         func_8007FFE0(globalCtx, globalCtx->cameraPtrs[phi_s1]->childCamIdx, sp42);
-//         phi_s0 = 1;
-//     } else {
-//         Interface_ChangeAlpha(2);
-//         phi_s0 =7;
-//     }
-//     func_8007FFE0(globalCtx, sp42, phi_s1);
+    
+    tempC->fov = globalCtx->view.fovy;
+    tempC->unk_168 = csID;
+    if (camIdx == 0) {
+        Gameplay_ChangeCameraStatus(globalCtx, camIdx, 3);
+    } else {
+        Gameplay_ChangeCameraStatus(globalCtx, camIdx, 1);
+    }
+    func_8007C850(globalCtx, sp42, csID, actor, timer);
+    Gameplay_ChangeCameraStatus(globalCtx, sp42, phi_s0);
 
-//     globalCtx->cameraPtrs[sp42]->timer = timer;
-//     globalCtx->cameraPtrs[sp42]->target = actor;
-//     globalCtx->cameraPtrs[sp42]->at = globalCtx->view.lookAt;
-//     globalCtx->cameraPtrs[sp42]->eye = globalCtx->view.eye;
-//     globalCtx->cameraPtrs[sp42]->unk_168 = csID;
-//     globalCtx->cameraPtrs[sp42]->fov = globalCtx->view.fovy;
-//     if (phi_s1 == 0) {
-//         Gameplay_ChangeCameraStatus(globalCtx, phi_s1, 3);
-//     } else {
-//         Gameplay_ChangeCameraStatus(globalCtx, phi_s1, 1);
-//     }
-//     func_8007C850(globalCtx, sp42, csID, actor, timer);
-//     Gameplay_ChangeCameraStatus(globalCtx, sp42, phi_s0);
-//     temp_s0 = globalCtx->cameraPtrs[sp42]->childCamIdx;
-//     phi_s2 = sp42;
-//     while(temp_s0 > 0) {
-//         temp_a2 = globalCtx->cameraPtrs[temp_s0]->unk_168;
-//         temp_a3 = globalCtx->cameraPtrs[sp42]->unk_168;
-//         if ((temp_a2 / 100) < (temp_a3 / 100)) {
-//             osSyncPrintf( "\x1b[43;30monepointdemo camera[%d]: killed 'coz low priority (%d < %d)\n\x1b[m", temp_s0, temp_a2, temp_a3);
-//             if (globalCtx->cameraPtrs[temp_s0]->unk_168 != 0x1392) {
-//                 temp_v0_2 = func_80080024(globalCtx, temp_s0);
-//                 if (temp_v0_2 != -1) {
-//                     Gameplay_ChangeCameraStatus(globalCtx, temp_v0_2, 7);
-//                 }
-//             } else {
-//                 func_800803F0(globalCtx, temp_s0);
-//             }
-//         } else {
-
-//         }
-//         temp_s0 = globalCtx->cameraPtrs[phi_s2]->childCamIdx;
-//     }
-//     return sp42;
-// }
-
+    phi_s0_2 = globalCtx->cameraPtrs[sp42]->childCamIdx;
+    phi_s2 = sp42;
+    while (phi_s0_2 > 0) {
+        temp_a2 = globalCtx->cameraPtrs[phi_s0_2]->unk_168;
+        temp_a3 = globalCtx->cameraPtrs[sp42]->unk_168;
+        if ((temp_a2 / 0x64) < (temp_a3 / 0x64)) {
+            osSyncPrintf("\x1b[43;30monepointdemo camera[%d]: killed 'coz low priority (%d < %d)\n\x1b[m", phi_s0_2, temp_a2, temp_a3);
+            if (globalCtx->cameraPtrs[phi_s0_2]->unk_168 != 0x1392) {
+                phi_s0_2 = func_80080024(globalCtx, phi_s0_2);
+                if (phi_s0_2 != -1) {
+                    Gameplay_ChangeCameraStatus(globalCtx, phi_s0_2, 7);
+                }
+            } else {
+                phi_s2 = phi_s0_2;
+                func_800803F0(globalCtx, phi_s0_2);
+            }
+        } else {
+            phi_s2 = phi_s0_2;
+        }
+        phi_s0_2 = globalCtx->cameraPtrs[phi_s2]->childCamIdx;
+    }
+    return sp42;
+}
+#else
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_onepointdemo/func_800800F8.s")
+#endif
 
 s16 func_800803F0(GlobalContext *globalCtx, s16 camId) {
     if (camId == -1) {
