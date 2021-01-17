226b733654a8296642c8e98865d6a7f529b3790d
adam <adamkiddle23@gmail.com>
2021-01-17 19:45:35 +0000

    EnFireRock_Init done (but will only match once initvars are uncommented

--- src/overlays/actors/ovl_En_Fire_Rock/z_en_fire_rock.c
+++ src/overlays/actors/ovl_En_Fire_Rock/z_en_fire_rock.c
@@ -1,4 +1,5 @@
 #include "z_en_fire_rock.h"
+#include "vt.h"
 
 #define FLAGS 0x00000030
 
@@ -9,9 +10,14 @@ void EnFireRock_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnFireRock_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnFireRock_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06000DE0;
+void func_80A125B8(EnFireRock* this, GlobalContext* globalCtx);
+void func_80A12730(EnFireRock* this, GlobalContext* globalCtx);
+void func_80A120CC(EnFireRock* this, GlobalContext* globalCtx);
 
-/*
+extern UNK_TYPE D_06000DE0;
+extern ColliderCylinderInit D_80A12CA0[];
+extern ColliderCylinderInit D_80A12CCC[];
+
 const ActorInit En_Fire_Rock_InitVars = {
     ACTOR_EN_FIRE_ROCK,
     ACTORTYPE_ENEMY,
@@ -23,8 +29,83 @@ const ActorInit En_Fire_Rock_InitVars = {
     (ActorFunc)EnFireRock_Update,
     (ActorFunc)EnFireRock_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Fire_Rock/EnFireRock_Init.s")
+
+void EnFireRock_Init(Actor *thisx, GlobalContext *globalCtx) {
+    
+    GlobalContext* globalCtx2 = globalCtx;
+    Player* player = PLAYER;
+    EnFireRock* this = THIS;
+    s16 temp;
+
+    this->unk18C = this->actor.params;
+    if (this->unk18C != 5){
+        ActorShape_Init(&thisx->shape, 0.0f, ActorShadow_DrawFunc_Circle, 15.0f);
+        if (this->unk18C != 6){
+            this->unk14C.x = (Rand_ZeroFloat(10.0f) + 15.0f);
+            this->unk14C.y = (Rand_ZeroFloat(10.0f) + 15.0f);
+            this->unk14C.z = (Rand_ZeroFloat(10.0f) + 15.0f);
+        }
+    }
+    switch(this->unk18C){
+        case 5:
+            this->actor.draw = 0;
+            osSyncPrintf("\x1b[33m☆☆☆☆☆ 天井待ち岩 ☆☆☆☆☆ \n\x1b[m");
+            this->unk168 = &func_80A125B8;
+            break;
+        case 6:
+            Actor_SetScale(&this->actor, 0.03f);
+            Collider_InitCylinder(globalCtx, &this->unk194);
+            Collider_SetCylinder(globalCtx, &this->unk194, &this->actor, D_80A12CCC);
+            osSyncPrintf("\x1b[33m☆☆☆☆☆ 床岩 ☆☆☆☆☆ \n\x1b[m");
+            this->unk194.dim.radius = 0x17;
+            this->unk194.dim.height = 0x25;
+            this->unk194.dim.yShift = -0xA;
+            Actor_ChangeType(globalCtx, &globalCtx->actorCtx, &this->actor, (u8)6U);
+            this->actor.colChkInfo.mass = 0xFF;
+            this->unk168 = &func_80A12730;
+            break;
+        case 0:
+            this->unk17C.x = (f32) (Rand_CenteredFloat(50.0f) + player->actor.posRot.pos.x);
+            this->unk17C.z = (f32) (Rand_CenteredFloat(50.0f) + player->actor.posRot.pos.z);
+        case 3:
+            this->unk16C = (Rand_ZeroFloat(2.0f) / 100.0f) + 0.02f;
+            Actor_SetScale(&this->actor, this->unk16C);
+            Collider_InitCylinder(globalCtx, &this->unk194);
+            Collider_SetCylinder(globalCtx, &this->unk194, &this->actor, D_80A12CA0);
+            this->actor.posRot.rot.y = this->actor.shape.rot.y = Rand_CenteredFloat(65535.0f);
+            this->unk168 = &func_80A120CC;
+            this->actor.shape.unk_10 = 15.0f;
+            break;
+        case 1:
+            this->actor.velocity.y = Rand_ZeroFloat(3.0f) + 4.0f;
+            this->actor.speedXZ = Rand_ZeroFloat(3.0f) + 3.0f;
+            this->unk16C = (Rand_ZeroFloat(1.0f) / 100.0f) + 0.02f;
+            Actor_SetScale(&this->actor, this->unk16C);
+            this->actor.gravity = -1.5f;
+            Collider_InitCylinder(globalCtx, &this->unk194);
+            Collider_SetCylinder(globalCtx, &this->unk194, &this->actor, D_80A12CA0);
+            this->actor.shape.unk_10 = 10.0f;
+            this->actor.posRot.rot.y = this->actor.shape.rot.y = Rand_CenteredFloat(65535.0f);
+            this->unk168 = &func_80A120CC;
+            break;
+        case 2:
+            this->actor.velocity.y = Rand_ZeroFloat(3.0f) + 4.0f;
+            this->actor.speedXZ = Rand_ZeroFloat(3.0f) + 2.0f;
+            this->unk16C = (Rand_ZeroFloat(1.0f) / 500.0f) + 0.01f;
+            Actor_SetScale(&this->actor, this->unk16C);
+            this->actor.gravity = -1.2f;
+            this->actor.shape.unk_10 = 5.0f;
+            this->actor.posRot.rot.y = this->actor.shape.rot.y = Rand_CenteredFloat(65535.0f);
+            this->unk168 = &func_80A120CC;
+            break;
+        default:
+            osSyncPrintf(VT_FGCOL(YELLOW)"☆☆☆☆☆ そんな岩はねぇ！ERR!!!!!! ☆☆☆☆☆ \n" VT_RST);
+            Actor_Kill(&this->actor);
+            break;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Fire_Rock/EnFireRock_Init.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Fire_Rock/EnFireRock_Destroy.s")
 
--- src/overlays/actors/ovl_En_Fire_Rock/z_en_fire_rock.h
+++ src/overlays/actors/ovl_En_Fire_Rock/z_en_fire_rock.h
@@ -8,9 +8,24 @@ struct EnFireRock;
 
 typedef struct EnFireRock {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x94];
+    /* 0x014C */ Vec3f unk14C;
+    /* 0x0158 */ char unk_158[0x10];
+    /* 0x0168 */ void* unk168;
+    /* 0x016C */ f32 unk16C;
+    /* 0x0170 */ char unk_170[0xC];
+    /* 0x017C */ Vec3f unk17C;
+    /* 0x0188 */ char unk_188[0x4];
+    /* 0x018C */ s16 unk18C;
+    /* 0x0190 */ char unk_190[0x4];
+    /* 0x0194 */ ColliderCylinder unk194;
 } EnFireRock; // size = 0x01E0
 
 extern const ActorInit En_Fire_Rock_InitVars;
 
 #endif
+/*
+     0x0000  s16 radius;
+    0x0002  s16 height;
+     0x0004 s16 yShift;
+     0x0006  Vec3s pos;
+    */
