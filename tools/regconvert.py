6d5f5f29fa3995b130e9527e5c4d7f90569e940c
Anghelo <angheloalf95@gmail.com>
2021-01-27 10:53:13 -0300

    add --offset and --hex options
    Signed-off-by: Anghelo <angheloalf95@gmail.com>

--- tools/regconvert.py
+++ tools/regconvert.py
@@ -13,21 +13,25 @@ def get_reg_macro(offset):
     reg_in_group = reg % GROUP_SIZE
     return "%cREG(%d)\n" % (REGISTER_NAMES[group], reg_in_group)
 
-def parse_number(string):
-    if string.lower().startswith("0x"):
+def parse_number(string, force_hex):
+    if string.lower().startswith("0x") or force_hex:
         return int(string, 16)
     return int(string, 10)
 
 def index_to_offset(index):
-    return (index + 10) * 2
+    return index * 2 + DATA_OFFSET
 
 def main():
     parser = argparse.ArgumentParser(description="Converts a GameInfo->data index to a REG macro.")
     parser.add_argument("index", help="index of GameInfo->data in decimal (or hexadecimal if starts with 0x)")
+    parser.add_argument("--offset", help="treat `index` argument as an offset to GameInfo instead", action="store_true")
+    parser.add_argument("--hex", help="treat `index` argument as hexadecimal, even without the 0x", action="store_true")
     args = parser.parse_args()
 
-    index = parse_number(args.index)
+    index = parse_number(args.index, args.hex)
     offset = index_to_offset(index)
+    if args.offset:
+        offset = index
 
     print(get_reg_macro(offset))
 
