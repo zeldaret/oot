d3413d1158ddd1916294a0817d4771fd5f180e21
angie <angheloalf95@gmail.com>
2021-01-29 23:27:08 -0300

    the script now should be able to parse the files in the build folder
    also added some flags to ignore an actor list and parse the non-matching like before
    
    Signed-off-by: angie <angheloalf95@gmail.com>

--- tools/get_actor_sizes.py
+++ tools/get_actor_sizes.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 import os
-import pprint
+#import pprint
+import re
+import argparse
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = script_dir + "/../"
 asm_dir = root_dir + "asm/non_matchings/overlays/actors"
+build_dir = root_dir + "build/src/overlays/actors"
 
-overlays = {}
 
 
 def get_num_instructions(f_path):
@@ -20,7 +22,9 @@ def get_num_instructions(f_path):
     return sum
 
 
-def main():
+def count_non_matching():
+    overlays = {}
+
     for root, dirs, files in os.walk(asm_dir):
         for dir in dirs:
             total_size = 0
@@ -36,14 +40,125 @@ def main():
                     if file_size > max_size:
                         max_size = file_size
             overlays[dir] = (num_files, max_size, total_size,
-                             total_size / num_files)
+                             total_size / num_files, "", "")
     
-    sorted_actors = {k: v for k, v in sorted(
+    return overlays
+
+    """sorted_actors = {k: v for k, v in sorted(
         overlays.items(), key=lambda item: item[1][2])
     }
     for actor in sorted_actors.items():
         print(
-            f"{actor[0]}, {actor[1][0]}, {actor[1][1]}, {actor[1][2]}, {actor[1][3]}")
+            f"{actor[0]}, {actor[1][0]}, {actor[1][1]}, {actor[1][2]}, {actor[1][3]}")"""
+
+
+pattern_function = re.compile("^[0-9a-fA-F]+ <(.+)>:")
+pattern_switchcase = re.compile("L[0-9a-fA-F]{8}")
+
+def count_builded_funcs_and_instructions(f_path):
+    f_lines = ""
+    with open(f_path) as f:
+        f_lines = f.readlines()
+    
+    current = ""
+    funcs = {}
+    for line in f_lines:
+        if line.strip() == "":
+            continue
+        match_function = pattern_function.match(line)
+        if match_function:
+            func_name = match_function.group(1)
+            if pattern_switchcase.match(func_name):
+                # this is not a real function tag.
+                # probably a case from a switch
+                # for example: <L80979A80>
+                continue
+            #if "<" in line and ">:" in line:
+            #    current = line.split("<")[1].split(">:")[0]
+            #    funcs[current] = 0
+            current = func_name
+            funcs[current] = 0
+        elif current != "":
+            funcs[current] += 1
+    return funcs
+
+
+def count_build():
+    overlays = {}
+
+    for root, dirs, files in os.walk(build_dir):
+        for dir in dirs:
+            total_size = 0
+            max_size = -1
+            ovl_path = os.path.join(root, dir)
+            num_files = 0
+            for f_name in os.listdir(ovl_path):
+                if not f_name.endswith(".s"):
+                    continue
+                if f_name.endswith("_reloc.s"):
+                    continue
+
+                funcs = count_builded_funcs_and_instructions(
+                    os.path.join(ovl_path, f_name))
+                
+                num_files = len(funcs)
+                if num_files > 0:
+                    total_size = sum(funcs.values())
+                    max_size = max(funcs.values())
+                    # every function is in one file so,
+                    # if the file with the functions is
+                    # found, we should not keep searching
+                    break
+            overlays[dir] = (num_files, max_size, total_size,
+                             total_size / num_files, "", "")
+
+    return overlays
+
+    """sorted_actors = [(k, v) for k, v in overlays.items()]
+    sorted_actors.sort()
+
+    row = "{},{},{},{},{},{},{}"
+    print(row.format("Overlay", "Num files", "Max size", "Total size", "Average size", "Description", "Status"))
+
+    for actor in sorted_actors:
+        name = actor[0]
+        other = actor[1]
+        print(row.format(name, *other))"""
+
+
+def get_ignored(filename):
+    ignored = []
+    if filename is not None:
+        with open(filename) as f:
+            ignored = f.read().split("\n")
+    return ignored
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Collects actor's functions sizes, and print them in csv format.")
+    parser.add_argument("--non-matching", help="Collect data of the non-matching actors instead.", action="store_true")
+    parser.add_argument("--ignore", help="Path to file that contains overlays names which should be ignored.")
+    args = parser.parse_args()
+
+    if args.non_matching:
+        overlays = count_non_matching()
+    else:
+        overlays = count_build()
+
+    ignored = get_ignored(args.ignore)
+
+    sorted_actors = [(k, v) for k, v in overlays.items()]
+    sorted_actors.sort()
+
+    row = "{},{},{},{},{},{},{}"
+    print(row.format("Overlay", "Num files", "Max size", "Total size", "Average size", "Description", "Status"))
+
+    for actor in sorted_actors:
+        name = actor[0]
+        other = actor[1]
+        if name in ignored:
+            continue
+        print(row.format(name, *other))
 
 
 main()
