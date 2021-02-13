fc1faa592618623bd7b9aed2cf9ac03a92ea6485
mzxrules <mzxrules@gmail.com>
2021-02-13 15:19:53 -0500

    implement requested changes

--- src/overlays/actors/ovl_Bg_Jya_Ironobj/z_bg_jya_ironobj.c
+++ src/overlays/actors/ovl_Bg_Jya_Ironobj/z_bg_jya_ironobj.c
@@ -24,7 +24,7 @@ void func_808992E8(BgJyaIronobj* this, GlobalContext* globalCtx);
 void BgJyaIronobj_SpawnPillarParticles(BgJyaIronobj* this, GlobalContext* globalCtx, EnIk* enIk);
 void BgJyaIronobj_SpawnThoneParticles(BgJyaIronobj* this, GlobalContext* arg1, EnIk* enIk);
 
-static int unused = 0;
+static int sUnused = 0;
 
 const ActorInit Bg_Jya_Ironobj_InitVars = {
     ACTOR_BG_JYA_IRONOBJ,
@@ -63,14 +63,14 @@ static ColliderCylinderInit sCylinderInit = {
 static s16 D_808994D8[] = { 0x8700, 0x4000, 0xC000, 0x0000 };
 
 // Pillar
-static s16 D_808994E0[] = { 0x0005, 0x0008, 0x000B, 0x000E, 0x0011, 0x0014, 0x0017, 0x001A };
-static s16 D_808994F0[] = { 0x0012, 0x001A, 0x0022, 0x002A, 0x0032, 0x003C, 0x0046, 0x0050 };
-static s16 D_80899500[] = { 0x0030, 0x002A, 0x0024, 0x0020, 0x001C, 0x0018, 0x0014, 0x0010 };
+static s16 D_808994E0[] = { 5, 8, 11, 14, 17, 20, 23, 26 };
+static s16 D_808994F0[] = { 18, 26, 34, 42, 50, 60, 70, 80 };
+static s16 D_80899500[] = { 48, 42, 36, 32, 28, 24, 20, 16 };
 
 // Throne
-static s16 D_80899510[] = { 0x0005, 0x0008, 0x000B, 0x000E, 0x0011, 0x0014, 0x0017, 0x001A };
-static s16 D_80899520[] = { 0x0012, 0x001A, 0x0022, 0x002A, 0x0032, 0x003C, 0x0046, 0x0050 };
-static s16 D_80899530[] = { 0x0030, 0x002A, 0x0024, 0x0020, 0x001C, 0x0018, 0x0014, 0x0010 };
+static s16 D_80899510[] = { 5, 8, 11, 14, 17, 20, 23, 26 };
+static s16 D_80899520[] = { 18, 26, 34, 42, 50, 60, 70, 80 };
+static s16 D_80899530[] = { 48, 42, 36, 32, 28, 24, 20, 16 };
 
 static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale.x, 100, ICHAIN_CONTINUE),
@@ -82,9 +82,8 @@ static InitChainEntry sInitChain[] = {
 static CollisionHeader* sCollisionHeaders[] = { &gPillarCol, &gThroneCol };
 
 void BgJyaIronobj_InitCylinder(BgJyaIronobj* this, GlobalContext* globalCtx) {
-    ColliderCylinder* colCylinder;
+    ColliderCylinder* colCylinder = &this->colCylinder;
 
-    colCylinder = &this->colCylinder;
     Collider_InitCylinder(globalCtx, colCylinder);
     Collider_SetCylinder(globalCtx, colCylinder, &this->dyna.actor, &sCylinderInit);
     if ((this->dyna.actor.params & 1) == 1) {
@@ -95,8 +94,8 @@ void BgJyaIronobj_InitCylinder(BgJyaIronobj* this, GlobalContext* globalCtx) {
 }
 
 /*
-* Spawns particles for the destroyed pillar
-*/
+ * Spawns particles for the destroyed pillar
+ */
 void BgJyaIronobj_SpawnPillarParticles(BgJyaIronobj* this, GlobalContext* globalCtx, EnIk* enIk) {
     s32 i;
     s32 j;
@@ -120,7 +119,7 @@ void BgJyaIronobj_SpawnPillarParticles(BgJyaIronobj* this, GlobalContext* global
         Actor* actor =
             Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_BG_JYA_HAHENIRON, this->dyna.actor.world.pos.x,
                         Rand_ZeroOne() * 80.0f + this->dyna.actor.world.pos.y + 20.0f, this->dyna.actor.world.pos.z,
-                        0, ((s16)(s32)(Rand_ZeroOne() * 0x4000) + rotY) - 0x2000, 0, 0);
+                        0, (s16)(Rand_ZeroOne() * 0x4000) + rotY - 0x2000, 0, 0);
         if (actor != NULL) {
             actor->speedXZ = Rand_ZeroOne() * 8.0f + 9.0f;
             actor->velocity.y = Rand_ZeroOne() * 10.0f + 6.0f;
@@ -148,17 +147,17 @@ void BgJyaIronobj_SpawnPillarParticles(BgJyaIronobj* this, GlobalContext* global
         vel.x = 2.0f * ((Rand_ZeroOne() * 6.0f) - 3.0f) + (Rand_ZeroOne() * sins * 8.0f * temp_f22);
         vel.y = (Rand_ZeroOne() * 8.0f) - 3.0f;
         vel.z = 2.0f * ((Rand_ZeroOne() * 6.0f) - 3.0f) + (Rand_ZeroOne() * coss * 8.0f * temp_f22);
-        EffectSsKakera_Spawn(globalCtx, &pos, &vel, &pos, -0x15E, unkArg5, D_80899500[j & 7], 4, 0,
-                             D_808994E0[j & 7], 0, 5, D_808994F0[j & 7], -1, 0x16C, gObjectJyaIronDL_000880);
+        EffectSsKakera_Spawn(globalCtx, &pos, &vel, &pos, -350, unkArg5, D_80899500[j & 7], 4, 0,
+                             D_808994E0[j & 7], 0, 5, D_808994F0[j & 7], -1, OBJECT_JYA_IRON, gObjectJyaIronDL_000880);
         if (Rand_ZeroOne() < 0.26f) {
-            func_80033480(globalCtx, &pos, 200.0f, 1, D_808994E0[j & 7] * 4 + 0x3C, D_808994E0[j & 7] * 4 + 0x50, 1);
+            func_80033480(globalCtx, &pos, 200.0f, 1, D_808994E0[j & 7] * 4 + 60, D_808994E0[j & 7] * 4 + 80, 1);
         }
     }
 }
 
 /*
-* Spawns particles for the destroyed throne
-*/
+ * Spawns particles for the destroyed throne
+ */
 void BgJyaIronobj_SpawnThoneParticles(BgJyaIronobj* this, GlobalContext* arg1, EnIk* enIk) {
     s32 i;
     s32 j;
@@ -206,10 +205,10 @@ void BgJyaIronobj_SpawnThoneParticles(BgJyaIronobj* this, GlobalContext* arg1, E
         vel.x = 2.0f * (Rand_ZeroOne() * 6.0f - 3.0f) + (Rand_ZeroOne() * sins * 8.0f * temp_f22);
         vel.y = Rand_ZeroOne() * 8.0f - 3.0f;
         vel.z = 2.0f * (Rand_ZeroOne() * 6.0f - 3.0f) + (Rand_ZeroOne() * coss * 8.0f * temp_f22);
-        EffectSsKakera_Spawn(arg1, &pos, &vel, &pos, -0x15E, unkArg5, D_80899530[j & 7], 4, 0, D_80899510[j & 7], 0,
-                             5, D_80899520[j & 7], -1, 0x16C, gObjectJyaIronDL_000880);
+        EffectSsKakera_Spawn(arg1, &pos, &vel, &pos, -350, unkArg5, D_80899530[j & 7], 4, 0, D_80899510[j & 7], 0,
+                             5, D_80899520[j & 7], -1, OBJECT_JYA_IRON, gObjectJyaIronDL_000880);
         if (Rand_ZeroOne() < 0.26f) {
-            func_80033480(arg1, &pos, 200.0f, 1, D_80899510[j & 7] * 4 + 0x3C, D_80899510[j & 7] * 4 + 0x50, 1);
+            func_80033480(arg1, &pos, 200.0f, 1, D_80899510[j & 7] * 4 + 60, D_80899510[j & 7] * 4 + 80, 1);
         }
     }
 }
@@ -254,7 +253,7 @@ void func_808992E8(BgJyaIronobj* this, GlobalContext* globalCtx) {
             dropPos.y = this->dyna.actor.world.pos.y + 20.0f;
             dropPos.z = this->dyna.actor.world.pos.z;
             for (i = 0; i < 3; i++) {
-                Item_DropCollectible(globalCtx, &dropPos, 0x0003);
+                Item_DropCollectible(globalCtx, &dropPos, ITEM00_HEART);
                 dropPos.y += 18.0f;
             }
             Actor_Kill(&this->dyna.actor);
@@ -267,6 +266,7 @@ void func_808992E8(BgJyaIronobj* this, GlobalContext* globalCtx) {
 
 void BgJyaIronobj_Update(Actor* thisx, GlobalContext* globalCtx) {
     BgJyaIronobj* this = THIS;
+
     this->actionFunc(this, globalCtx);
 }
 
