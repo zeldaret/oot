fc7de1e32a28c16052951051f96f607203c7aaf4
Thar0 <maximilianc64@gmail.com>
2021-02-13 20:28:54 +0000

    2 more matches, some other improvements

--- src/overlays/actors/ovl_En_Ganon_Mant/z_en_ganon_mant.c
+++ src/overlays/actors/ovl_En_Ganon_Mant/z_en_ganon_mant.c
@@ -49,122 +49,91 @@ s16 D_80A24D54[] = {
 Struct_80A24D64 D_80A24D64[] = {
     { D_80A24D20, ARRAY_COUNT(D_80A24D20) },
     { D_80A24D20, ARRAY_COUNT(D_80A24D20) },
-    { D_80A24D34, ARRAY_COUNT(D_80A24D34)-1 },
-    { D_80A24D54, ARRAY_COUNT(D_80A24D54)-1 },
+    { D_80A24D34, ARRAY_COUNT(D_80A24D34) - 1 },
+    { D_80A24D54, ARRAY_COUNT(D_80A24D54) - 1 },
 };
 
 f32 D_80A24D84[] = {
-    0.0f, 1.0f, 0.5f, 0.25f, 
-    0.1f, 0.0f, 0.0f, 0.0f, 
-    0.0f, 0.0f, 0.0f, 0.0f, 
-    0.0f, 0.0f, 0.0f, 0.0f, 
-    0.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.5f, 0.25f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
 };
 
 f32 D_80A24DD0[] = {
-    0.0f, 1.0f, 0.9f, 0.8f, 
-    0.7f, 0.6f, 0.5f, 0.4f, 
-    0.3f, 0.2f, 0.1f, 0.0f, 
-    0.0f, 0.0f, 0.0f, 0.0f, 
-    0.0f, 0.0f,
+    0.0f, 1.0f, 0.9f, 0.8f, 0.7f, 0.6f, 0.5f, 0.4f, 0.3f, 0.2f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
 };
 
 f32 D_80A24E18[] = {
-    0.0f, 1.0f, 1.0f, 1.0f, 
-    1.0f, 1.1f, 1.2f, 1.3f, 
-    1.4f, 1.5f, 1.6f, 1.7f, 
-    1.8f,
+    0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f,
 };
 
 u16 D_80A24E4C[] = {
-    0x000B, 0x0017, 0x0023, 0x002F, 0x003B, 0x0047, 0x0053, 0x005F, 
-    0x006B, 0x0077, 0x0083, 0x008F, 0x000A, 0x0016, 0x0022, 0x002E, 
-    0x003A, 0x0046, 0x0052, 0x005E, 0x006A, 0x0076, 0x0082, 0x008E, 
-    0x0009, 0x0015, 0x0021, 0x002D, 0x0039, 0x0045, 0x0051, 0x005D, 
-    0x0069, 0x0075, 0x0081, 0x008D, 0x0008, 0x0014, 0x0020, 0x002C, 
-    0x0038, 0x0044, 0x0050, 0x005C, 0x0068, 0x0074, 0x0080, 0x008C, 
-    0x0007, 0x0013, 0x001F, 0x002B, 0x0037, 0x0043, 0x004F, 0x005B, 
-    0x0067, 0x0073, 0x007F, 0x008B, 0x0006, 0x0012, 0x001E, 0x002A, 
-    0x0036, 0x0042, 0x004E, 0x005A, 0x0066, 0x0072, 0x007E, 0x008A, 
-    0x0005, 0x0011, 0x001D, 0x0029, 0x0035, 0x0041, 0x004D, 0x0059, 
-    0x0065, 0x0071, 0x007D, 0x0089, 0x0004, 0x0010, 0x001C, 0x0028, 
-    0x0034, 0x0040, 0x004C, 0x0058, 0x0064, 0x0070, 0x007C, 0x0088, 
-    0x0003, 0x000F, 0x001B, 0x0027, 0x0033, 0x003F, 0x004B, 0x0057, 
-    0x0063, 0x006F, 0x007B, 0x0087, 0x0002, 0x000E, 0x001A, 0x0026, 
-    0x0032, 0x003E, 0x004A, 0x0056, 0x0062, 0x006E, 0x007A, 0x0086, 
-    0x0001, 0x000D, 0x0019, 0x0025, 0x0031, 0x003D, 0x0049, 0x0055, 
-    0x0061, 0x006D, 0x0079, 0x0085, 0x0000, 0x000C, 0x0018, 0x0024, 
-    0x0030, 0x003C, 0x0048, 0x0054, 0x0060, 0x006C, 0x0078, 0x0084, 
-    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+    0x000B, 0x0017, 0x0023, 0x002F, 0x003B, 0x0047, 0x0053, 0x005F, 0x006B, 0x0077, 0x0083, 0x008F, 0x000A, 0x0016,
+    0x0022, 0x002E, 0x003A, 0x0046, 0x0052, 0x005E, 0x006A, 0x0076, 0x0082, 0x008E, 0x0009, 0x0015, 0x0021, 0x002D,
+    0x0039, 0x0045, 0x0051, 0x005D, 0x0069, 0x0075, 0x0081, 0x008D, 0x0008, 0x0014, 0x0020, 0x002C, 0x0038, 0x0044,
+    0x0050, 0x005C, 0x0068, 0x0074, 0x0080, 0x008C, 0x0007, 0x0013, 0x001F, 0x002B, 0x0037, 0x0043, 0x004F, 0x005B,
+    0x0067, 0x0073, 0x007F, 0x008B, 0x0006, 0x0012, 0x001E, 0x002A, 0x0036, 0x0042, 0x004E, 0x005A, 0x0066, 0x0072,
+    0x007E, 0x008A, 0x0005, 0x0011, 0x001D, 0x0029, 0x0035, 0x0041, 0x004D, 0x0059, 0x0065, 0x0071, 0x007D, 0x0089,
+    0x0004, 0x0010, 0x001C, 0x0028, 0x0034, 0x0040, 0x004C, 0x0058, 0x0064, 0x0070, 0x007C, 0x0088, 0x0003, 0x000F,
+    0x001B, 0x0027, 0x0033, 0x003F, 0x004B, 0x0057, 0x0063, 0x006F, 0x007B, 0x0087, 0x0002, 0x000E, 0x001A, 0x0026,
+    0x0032, 0x003E, 0x004A, 0x0056, 0x0062, 0x006E, 0x007A, 0x0086, 0x0001, 0x000D, 0x0019, 0x0025, 0x0031, 0x003D,
+    0x0049, 0x0055, 0x0061, 0x006D, 0x0079, 0x0085, 0x0000, 0x000C, 0x0018, 0x0024, 0x0030, 0x003C, 0x0048, 0x0054,
+    0x0060, 0x006C, 0x0078, 0x0084, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
 };
 
 #include "ovl_en_ganon_mant_gfx.c"
 
-void EnGanonMant_Init(Actor *thisx, GlobalContext *globalCtx) {
+void EnGanonMant_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnGanonMant* this = THIS;
 
     this->actor.flags &= ~1;
 }
 
-
-void EnGanonMant_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+void EnGanonMant_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
-#ifdef NON_MATCHING
-// Regalloc differences, Stack
+/**
+ * Randomly zeros portions of the cloak texture
+ */
 void func_80A23D84(EnGanonMant* this) {
-    f32 sp50;
-    f32 sp4C;
-    f32 temp_f0;
-    f32 temp_f20;
-    f32 temp_f22;
-    Struct_80A24D64 *temp_v0;
-    s16 phi_s0;
-    s16 phi_a0;
-    s16 phi_t0;
-    s16 phi_v1;
-    s16* temp;
-    s16 temp_;
+    s32 pad;
+    s16 i;
+    s16 txi;
+    s16 tyi;
+    s16 texIdx;
+    f32 tx = Rand_ZeroFloat(32.0f);
+    f32 ty = Rand_ZeroFloat(64.0f);
+    f32 temp_f0 = Rand_ZeroFloat(2 * M_PI);
+    f32 temp_f20 = sinf(temp_f0);
+    f32 temp_f22 = cosf(temp_f0);
+    Struct_80A24D64* coeffs = &D_80A24D64[(s16)Rand_ZeroFloat(3.99f)];
+    s16 len = coeffs->unk_4;
+    s16* data = coeffs->unk_0;
 
-    sp4C = Rand_ZeroFloat(32.0f);
-    sp50 = Rand_ZeroFloat(64.0f);
-    temp_f0 = Rand_ZeroFloat(2 * M_PI);
-
-    temp_f20 = sinf(temp_f0);
-    temp_f22 = cosf(temp_f0);
-
-    temp_v0 = &D_80A24D64[(s16)Rand_ZeroFloat(3.99f)];
-
-    temp_ = temp_v0->unk_4;
-    temp = temp_v0->unk_0;
-
-    for (phi_s0 = 0; phi_s0 < temp_; phi_s0++) {
-        if ((0.0f <= sp50 && sp50 < 32.0f) && (0.0f <= sp4C && sp4C < 64.0f)) {
-            for (phi_t0 = 0; phi_t0 <= temp[phi_s0]; phi_t0++) {
-                for (phi_v1 = 0; phi_v1 <= temp[phi_s0]; phi_v1++) {
-                    phi_a0 = (s16)((s32)sp50 + ((s32)sp4C * 32)) + (phi_t0 + (phi_v1 * 32));
-                    if (phi_a0 < 0x800) {
-                        D_80A24F78[phi_a0] = 0;
+    for (i = 0; i < len; i++) {
+        if ((0.0f <= tx && tx < 32.0f) && (0.0f <= ty && ty < 64.0f)) {
+            for (txi = 0; txi <= data[i]; txi++) {
+                texIdx = 0;
+                if (1) {}
+                for (tyi = 0; tyi <= data[i]; tyi++) {
+                    texIdx = (s16)((s16)tx + ((s16)ty * 32)) + ((s16)txi + ((s16)tyi * 32));
+                    if (texIdx < ARRAY_COUNT(D_80A24F78)) {
+                        D_80A24F78[texIdx] = 0;
                     }
                 }
             }
         }
-        sp50 += temp_f20;
-        sp4C += temp_f22;
+        tx += temp_f20;
+        ty += temp_f22;
     }
 
-    for (phi_s0 = 0; phi_s0 < 4; phi_s0++) {
+    for (i = 0; i < 4; i++) {
         this->unk_14C[(s16)Rand_ZeroFloat(11.9f)].unk_1B0[(s16)Rand_ZeroFloat(11.9f)] = 1;
     }
 }
-#else
-void func_80A23D84(EnGanonMant*);
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ganon_Mant/func_80A23D84.s")
-#endif
 
 #ifdef NON_MATCHING
 // Stack only
-void func_80A23FE0(GlobalContext *globalCtx, EnGanonMant *this, Vec3f *arg2, Vec3f *arg3, Vec3f *arg4, Vec3f *arg5, Vec3f *arg6, s16 arg7) {
+void func_80A23FE0(GlobalContext* globalCtx, EnGanonMant* this, Vec3f* arg2, Vec3f* arg3, Vec3f* arg4, Vec3f* arg5,
+                   Vec3f* arg6, s16 arg7) {
     f32 temp_f12;
     f32 temp_f20;
     f32 temp_f22;
@@ -233,7 +202,8 @@ void func_80A23FE0(GlobalContext *globalCtx, EnGanonMant *this, Vec3f *arg2, Vec
             arg3->y = (arg3 - 1)->y + sp94.y;
             arg3->z = (arg3 - 1)->z + sp94.z;
 
-            if (sqrtf(SQ(arg3->x - this->actor.world.pos.x) + SQ(arg3->z - this->actor.world.pos.z)) < (D_80A24E18[phi_s4] * this->unk_16D0)) {
+            if (sqrtf(SQ(arg3->x - this->actor.world.pos.x) + SQ(arg3->z - this->actor.world.pos.z)) <
+                (D_80A24E18[phi_s4] * this->unk_16D0)) {
                 temp_f12 = Math_Atan2F(arg3->z - this->actor.world.pos.z, arg3->x - this->actor.world.pos.x);
                 spA0.z = this->unk_16D0 * D_80A24E18[phi_s4];
                 Matrix_RotateY(temp_f12, MTXMODE_NEW);
@@ -277,52 +247,53 @@ void func_80A23FE0(GlobalContext *globalCtx, EnGanonMant *this, Vec3f *arg2, Vec
     arg5[11].x = arg5[10].x;
 }
 #else
-void func_80A23FE0(GlobalContext *globalCtx, EnGanonMant *this, Vec3f *arg2, Vec3f *arg3, Vec3f *arg4, Vec3f *arg5, Vec3f *arg6, s16 arg7);
+void func_80A23FE0(GlobalContext* globalCtx, EnGanonMant* this, Vec3f* arg2, Vec3f* arg3, Vec3f* arg4, Vec3f* arg5,
+                   Vec3f* arg6, s16 arg7);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ganon_Mant/func_80A23FE0.s")
 #endif
 
 void func_80A245A4(EnGanonMant* this) {
-    s16 sp76;
-    Vtx_tn* temp_s0;
-    Vtx_tn* sp6C;
+    s16 i;
+    Vtx_tn* vtx;
+    Vtx_tn* vertices;
     MantSub14C* sub;
-    s16 phi_s2;
-    s16 phi_s3;
-    Vec3f sp58;
-    Vec3f sp4C;
+    s16 j;
+    s16 k;
+    Vec3f up;
+    Vec3f normal;
 
-    if ((this->unk_1706 & 1) != 0) {
-        sp6C = (Vtx_tn*)SEGMENTED_TO_VIRTUAL(D_80A26778);
+    if (this->frameTimer % 2 != 0) {
+        vertices = (Vtx_tn*)SEGMENTED_TO_VIRTUAL(D_80A26778);
     } else {
-        sp6C = (Vtx_tn*)SEGMENTED_TO_VIRTUAL(D_80A27578);
+        vertices = (Vtx_tn*)SEGMENTED_TO_VIRTUAL(D_80A27578);
     }
-    sp58.x = 0.0f;
-    sp58.y = 30.0f;
-    sp58.z = 0.0f;
+    up.x = 0.0f;
+    up.y = 30.0f;
+    up.z = 0.0f;
 
     sub = &this->unk_14C[0];
-    for (sp76 = 0; sp76 < 12; sp76++, sub++) {
-        for (phi_s2 = 0, phi_s3 = 0; phi_s2 < 12; phi_s2++, phi_s3 += 0xC) {
-            temp_s0 = &sp6C[D_80A24E4C[sp76 + phi_s3]];
-            temp_s0->ob[0] = sub->unk_C[phi_s2].x;
-            temp_s0->ob[1] = sub->unk_C[phi_s2].y;
-            temp_s0->ob[2] = sub->unk_C[phi_s2].z;
-            Matrix_RotateY(sub->unk_90[phi_s2].y, MTXMODE_NEW);
-            Matrix_RotateX(sub->unk_90[phi_s2].x, MTXMODE_APPLY);
-            Matrix_MultVec3f(&sp58, &sp4C);
-            temp_s0->n[0] = sp4C.x;
-            temp_s0->n[1] = sp4C.y;
-            temp_s0->n[2] = sp4C.z;
+    for (i = 0; i < ARRAY_COUNT(sub->unk_C); i++, sub++) {
+        for (j = 0, k = 0; j < ARRAY_COUNT(sub->unk_C); j++, k += ARRAY_COUNT(sub->unk_C)) {
+            vtx = &vertices[D_80A24E4C[i + k]];
+            vtx->ob[0] = sub->unk_C[j].x;
+            vtx->ob[1] = sub->unk_C[j].y;
+            vtx->ob[2] = sub->unk_C[j].z;
+            Matrix_RotateY(sub->unk_90[j].y, MTXMODE_NEW);
+            Matrix_RotateX(sub->unk_90[j].x, MTXMODE_APPLY);
+            Matrix_MultVec3f(&up, &normal);
+            vtx->n[0] = normal.x;
+            vtx->n[1] = normal.y;
+            vtx->n[2] = normal.z;
         }
     }
 }
 
-void EnGanonMant_Update(Actor *thisx, GlobalContext *globalCtx) {
+void EnGanonMant_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnGanonMant* this = THIS;
     BossGanon* ganon = (BossGanon*)this->actor.parent;
 
     this->unk_1705 = 1;
-    this->unk_1706++;
+    this->frameTimer++;
 
     if (this->unk_16C0 == 0.0f) {
         // lol
@@ -344,40 +315,45 @@ void EnGanonMant_Update(Actor *thisx, GlobalContext *globalCtx) {
     }
 }
 
-void func_80A24884(GlobalContext* globalCtx, EnGanonMant *this) {
+void func_80A24884(GlobalContext* globalCtx, EnGanonMant* this) {
     s32 pad;
 
     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_en_ganon_mant.c", 564);
 
     Matrix_Translate(0.0f, 0.0f, 0.0f, MTXMODE_NEW);
-    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_ganon_mant.c", 572), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+
+    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_ganon_mant.c", 572),
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+
+    // set texture
     gSPDisplayList(POLY_OPA_DISP++, D_80A27078);
-    if ((this->unk_1706 & 1) != 0) {
+
+    // set vertices
+    if (this->frameTimer % 2 != 0) {
         gSPSegment(POLY_OPA_DISP++, 0x0C, D_80A26778);
     } else {
         gSPSegment(POLY_OPA_DISP++, 0x0C, D_80A27578);
     }
-    gSPDisplayList(POLY_OPA_DISP++, D_80A27128);
+
+    gSPDisplayList(POLY_OPA_DISP++, D_80A27128); // draw vertices
 
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_en_ganon_mant.c", 584);
 }
 
-#ifdef NON_MATCHING
-// Stack only
-void EnGanonMant_Draw(Actor *thisx, GlobalContext *globalCtx) {
+void EnGanonMant_Draw(Actor* thisx, GlobalContext* globalCtx) {
     EnGanonMant* this = THIS;
-    Vec3f spC0;
-    Vec3f spB4;
-    Vec3f sp8C;
     f32 temp_f20;
     f32 temp_f20_2;
+    Vec3f spC0;
+    Vec3f spB4;
     f32 temp_f22;
     f32 temp_f22_2;
     f32 temp_f24;
     f32 temp_f26;
-    Vec3f *phi_v0;
-    Vec3f *phi_v1;
+    Vec3f* phi_v0;
+    Vec3f* phi_v1;
     s16 phi_s0;
+    Vec3f sp8C;
     s16 idx;
 
     if (this->unk_1705 != 0) {
@@ -412,7 +388,7 @@ void EnGanonMant_Draw(Actor *thisx, GlobalContext *globalCtx) {
 
         Matrix_RotateY(temp_f26, MTXMODE_NEW);
         Matrix_RotateX(temp_f20_2, MTXMODE_APPLY);
-        this->unk_16CC = temp_f26 - M_PI/2.0f;
+        this->unk_16CC = temp_f26 - M_PI / 2.0f;
 
         for (phi_s0 = 0; phi_s0 < 12; phi_s0++) {
             Matrix_Push();
@@ -429,9 +405,9 @@ void EnGanonMant_Draw(Actor *thisx, GlobalContext *globalCtx) {
                 idx = phi_s0 - 1;
             }
 
-            func_80A23FE0(globalCtx, this, &this->unk_14C[phi_s0].unk_0, 
-                        this->unk_14C[phi_s0].unk_C, this->unk_14C[idx].unk_C, 
-                        &this->unk_14C[phi_s0].unk_90[1], &this->unk_14C[phi_s0].unk_120[1], phi_s0);
+            func_80A23FE0(globalCtx, this, &this->unk_14C[phi_s0].unk_0, this->unk_14C[phi_s0].unk_C,
+                          this->unk_14C[idx].unk_C, &this->unk_14C[phi_s0].unk_90[0], &this->unk_14C[phi_s0].unk_120[0],
+                          phi_s0);
             Matrix_Pull();
         }
         func_80A245A4(this);
@@ -439,6 +415,3 @@ void EnGanonMant_Draw(Actor *thisx, GlobalContext *globalCtx) {
     }
     func_80A24884(globalCtx, this);
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Ganon_Mant/EnGanonMant_Draw.s")
-#endif
--- src/overlays/actors/ovl_En_Ganon_Mant/z_en_ganon_mant.h
+++ src/overlays/actors/ovl_En_Ganon_Mant/z_en_ganon_mant.h
@@ -7,33 +7,33 @@
 struct EnGanonMant;
 
 typedef struct {
-    /* 0x000 */ Vec3f unk_0;        // 14C
-    /* 0x00C */ Vec3f unk_C[12];    // 158
-    /* 0x090 */ Vec3f unk_90[12];   // 1E8
-    /* 0x120 */ Vec3f unk_120[12];  // 278
+    /* 0x000 */ Vec3f unk_0;        // 14C  points along dorf's collar, the "roots" of unK_C
+    /* 0x00C */ Vec3f unk_C[12];    // 158  points down the cape, the "skeleton"
+    /* 0x090 */ Vec3f unk_90[12];   // 1E8  normal vector rotations, z is unused
+    /* 0x120 */ Vec3f unk_120[12];  // 278  in the range [-5,5], approaches 0, is the difference between previous points
     /* 0x1B0 */ u8 unk_1B0[12];     // 308
 } MantSub14C; // size = 0x1C8
 
 typedef struct EnGanonMant {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ MantSub14C unk_14C[12];
-    /* 0x16AC */ f32 unk_16AC;
-    /* 0x16B0 */ f32 unk_16B0;
-    /* 0x16B4 */ f32 unk_16B4;
-    /* 0x16B8 */ f32 unk_16B8;
-    /* 0x16BC */ f32 unk_16BC;
-    /* 0x16C0 */ f32 unk_16C0;
-    /* 0x16C4 */ f32 unk_16C4;
-    /* 0x16C8 */ f32 unk_16C8;
+    /* 0x16AC */ f32 unk_16AC; // set by ganondorf to 57.0f, 2.0f
+    /* 0x16B0 */ f32 unk_16B0; // " to -2.0f, -3.0f, -3.0f, -3.0f, -9.0f
+    /* 0x16B4 */ f32 unk_16B4; // " to 0.25f, 0.25f, 0.25f, 1.25f, 0.25f
+    /* 0x16B8 */ f32 unk_16B8; // " to -1.0f, -3.0f, -3.0f, -2.0f, -2.0f, -13.0f
+    /* 0x16BC */ f32 unk_16BC; // " to 20.0f, D_808E4D40[random], 0
+    /* 0x16C0 */ f32 unk_16C0; // " to 15.0f, 10, 0
+    /* 0x16C4 */ f32 unk_16C4; // " to 18.0f, 18.0f, 26.0f, 15.0f, 18.0f
+    /* 0x16C8 */ f32 unk_16C8; // " to -1.0f
     /* 0x16CC */ f32 unk_16CC;
-    /* 0x16D0 */ f32 unk_16D0;
+    /* 0x16D0 */ f32 unk_16D0; // " to 0.0f, 20.0f, 10.0f, 13.0f
     /* 0x16D4 */ Vec3f unk_16D4;
     /* 0x16E0 */ Vec3f unk_16E0;
     /* 0x16EC */ Vec3f unk_16EC;
     /* 0x16F8 */ Vec3f unk_16F8;
     /* 0x1704 */ u8 unk_1704;
     /* 0x1705 */ u8 unk_1705;
-    /* 0x1706 */ u8 unk_1706;
+    /* 0x1706 */ u8 frameTimer;
 } EnGanonMant; // size = 0x1708
 
 extern const ActorInit En_Ganon_Mant_InitVars;
