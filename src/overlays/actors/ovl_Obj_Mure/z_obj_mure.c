2e474d1aa9f065e817d3d4b9fda5cdb4cd0faeb6
Sam Lyon <sam.c.lyon@gmail.com>
2021-02-15 20:43:38 -0500

    almost matching init, matching data

--- src/overlays/actors/ovl_Obj_Mure/z_obj_mure.c
+++ src/overlays/actors/ovl_Obj_Mure/z_obj_mure.c
@@ -10,11 +10,13 @@
 
 #define THIS ((ObjMure*)thisx)
 
+// s32 func_80B98B1C(ObjMure* this, s32 arg1, s32 params);
 void ObjMure_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure_Update(Actor* thisx, GlobalContext* globalCtx);
+// s32 func_80B98C88(ObjMure* this);
+void func_80B992E4(ObjMure* this, GlobalContext* globalCtx);
 
-/*
 const ActorInit Obj_Mure_InitVars = {
     ACTOR_OBJ_MURE,
     ACTORCAT_ITEMACTION,
@@ -26,12 +28,60 @@ const ActorInit Obj_Mure_InitVars = {
     (ActorFunc)ObjMure_Update,
     NULL,
 };
-*/
+
+s32 D_80B99A50[] = 
+    { 0x44C80000, 0x44C80000, 0x447A0000, 0x447A0000, 0x447A0000 };
+s32 D_80B99A64[] = 
+    { 0x0000000C, 0x00000009, 0x00000008, 0x00000000 };
+s32 D_80B99A74[] = 
+    { 0x01250000, 0x00210020, 0x001E0000 };
+s32 D_80B99A80[] = 
+    { 0x00000002, 0xFFFF0000, 0xFFFF0000 };
+s32 D_80B99A8C[] = 
+    { 0xB0F404B0, 0xB0F800C8, 0x30FC04B0 };
+s32 D_80B99A98[] = 
+    { 0x00000000, 0x00000000 };
+
+s32 unk_FP1 = 0x80B995A4; // &func_80B995A4;
+s32 unk_FP2 = 0x80B995A4; // &func_80B995A4;
+s32 unk_FP3 = 0x80B997CC; //&func_80B997CC;
+s32 unk_1 = 0x00000000;
+
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/func_80B98AA0.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/func_80B98B1C.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/ObjMure_Init.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/ObjMure_Init.s")
+void ObjMure_Init(Actor *thisx, GlobalContext *globalCtx) {
+    ObjMure* this = THIS;
+
+    s32 temp_1 = 0xED;
+    this->unk_152 = (thisx->params >> 8) & 0x07;
+    this->unk_150 = (thisx->params >> 0xC) & 0x0F;
+    this->unk_154 = (thisx->params >> 5) & 0x03;
+    this->unk_156 = thisx->params & 0x1F;
+
+    if (this->unk_152 >= 4) {
+        osSyncPrintf("Error 群れな敵 (%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", temp_1, thisx->params);
+        Actor_Kill(thisx);
+        return;
+    }
+    if ((s32) this->unk_156 >= 5) {
+        osSyncPrintf("Error 群れな敵 (%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0xF5, thisx->params);
+        Actor_Kill(thisx);
+        return;
+    }
+    
+    if (func_80B98B1C(this, 0xF5, thisx->params) == 0) {
+        Actor_Kill(thisx);
+        return;
+    }
+    this->actionFunc = func_80B992E4;
+    osSyncPrintf("群れな敵 (arg_data 0x%04x)(chNum(%d) ptn(%d) svNum(%d) type(%d))\n", thisx->params, this->unk_150, this->unk_152, this->unk_154, this->unk_156);
+    if (func_80B98C88(this) <= 0) {
+        osSyncPrintf("Warning : 個体数が設定されていません(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0x10C, thisx->params);
+    }   
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/ObjMure_Destroy.s")
 
--- src/overlays/actors/ovl_Obj_Mure/z_obj_mure.h
+++ src/overlays/actors/ovl_Obj_Mure/z_obj_mure.h
@@ -6,9 +6,16 @@
 
 struct ObjMure;
 
+typedef void (*ObjMureActionFunc)(struct ObjMure*, GlobalContext*);
+
 typedef struct ObjMure {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x60];
+    /* 0x014C */ ObjMureActionFunc actionFunc; // function
+    /* 0x0150 */ s16 unk_150;
+    /* 0x0152 */ s16 unk_152;
+    /* 0x0154 */ s16 unk_154;
+    /* 0x0156 */ s16 unk_156;
+    /* 0x0158 */ char unk_158[0x54];
 } ObjMure; // size = 0x01AC
 
 extern const ActorInit Obj_Mure_InitVars;
