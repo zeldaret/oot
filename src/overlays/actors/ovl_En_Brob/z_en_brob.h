768fb61a38c6a996df056a98a3ea31173e21e784
Emil Lenngren <emil.lenngren@gmail.com>
2021-02-20 13:16:16 +0100

    ovl_En_Brob OK

--- src/overlays/actors/ovl_En_Brob/z_en_brob.h
+++ src/overlays/actors/ovl_En_Brob/z_en_brob.h
@@ -6,9 +6,19 @@
 
 struct EnBrob;
 
+typedef void (*EnBrobUpdateFunc)(struct EnBrob* this, GlobalContext* globalCtx);
+
 typedef struct EnBrob {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x174];
+    /* 0x0000 */ DynaPolyActor dyna;
+    /* 0x0164 */ SkelAnime skelAnime;
+    /* 0x01A8 */ EnBrobUpdateFunc updateFunc;
+    /* 0x01AC */ s16 timer;
+    /* 0x01AE */ s16 unk_1AE;
+    /* 0x01B0 */ Vec3s jointTable;
+    /* 0x01B6 */ char unk_1B6[0x36];
+    /* 0x01EC */ Vec3s morphTable;
+    /* 0x01F2 */ char unk_1F2[0x36];
+    /* 0x0228 */ ColliderCylinder colliders[2];
 } EnBrob; // size = 0x02C0
 
 extern const ActorInit En_Brob_InitVars;
