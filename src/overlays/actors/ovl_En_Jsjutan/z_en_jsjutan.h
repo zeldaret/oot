41b07dfae9dadcf825a62f13acd80c475001c693
angie <angheloalf95@gmail.com>
2021-02-20 00:10:42 -0300

    nonmatching draw
    Signed-off-by: angie <angheloalf95@gmail.com>

--- src/overlays/actors/ovl_En_Jsjutan/z_en_jsjutan.h
+++ src/overlays/actors/ovl_En_Jsjutan/z_en_jsjutan.h
@@ -9,9 +9,12 @@ struct EnJsjutan;
 typedef struct EnJsjutan {
     /* 0x0000 */ DynaPolyActor dyna;
     /* 0x0165 */ u8 unk_164;
-    /* 0x0165 */ char unk_165[0x07];
+    /* 0x0165 */ char unk_165[0x03];
+    /* 0x0168 */ f32 unk_168;
     /* 0x016C */ f32 unk_16C;
-    /* 0x0170 */ char unk_170[0x08];
+    /* 0x0170 */ char unk_170[0x05];
+    /* 0x0175 */ u8 unk_175;
+    /* 0x0176 */ char unk_176[0x02];
 } EnJsjutan; // size = 0x0178
 
 extern const ActorInit En_Jsjutan_InitVars;
