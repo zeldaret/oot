efa05e6519c45d391a06bf0e4c0cd92bdfe631c7
angie <angheloalf95@gmail.com>
2021-03-04 21:33:46 -0300

    compiler complain that the symbol doesn't exists
    Signed-off-by: angie <angheloalf95@gmail.com>

--- src/overlays/actors/ovl_En_Jsjutan/z_en_jsjutan.h
+++ src/overlays/actors/ovl_En_Jsjutan/z_en_jsjutan.h
@@ -9,13 +9,11 @@ struct EnJsjutan;
 typedef struct EnJsjutan {
     /* 0x0000 */ DynaPolyActor dyna;
     /* 0x0165 */ u8 unk_164; // flag to set carpet shadow textures.
-    /* 0x0165 */ char unk_165[0x03]; // padding (?)
     /* 0x0168 */ f32 unk_168; // actorYPos
     /* 0x016C */ f32 shadowAlpha;
     /* 0x0170 */ f32 unk_170;
     /* 0x0174 */ u8 unk_174; // flag?
     /* 0x0175 */ u8 unk_175; // setup flag?
-    /* 0x0176 */ char unk_176[0x02]; // padding (?)
 } EnJsjutan; // size = 0x0178
 
 extern const ActorInit En_Jsjutan_InitVars;
