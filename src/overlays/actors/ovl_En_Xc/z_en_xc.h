5f42ed3801e93998b7d96fd03958e2128570a73b
Lucas Shaw <lucas.shaw1123@gmail.com>
2021-03-29 13:28:32 -0700

    Remove unnecessary comment

--- src/overlays/actors/ovl_En_Xc/z_en_xc.h
+++ src/overlays/actors/ovl_En_Xc/z_en_xc.h
@@ -120,17 +120,17 @@ typedef struct EnXc {
     /* 0x014C */ SkelAnime skelAnime;
     /* 0x0190 */ Vec3s jointTable[17];
     /* 0x01F6 */ Vec3s morphTable[17];
-    /* 0x025C */ s16 eyeIdx; // segment idx
-    /* 0x025E */ s16 blinkTimer; // eye related
+    /* 0x025C */ s16 eyeIdx;
+    /* 0x025E */ s16 blinkTimer;
     /* 0x0260 */ s32 action;
     /* 0x0264 */ s32 drawMode;
     /* 0x0268 */ f32 timer;
-    /* 0x026C */ s32 unk_26C; // prev custscene command action?
+    /* 0x026C */ s32 unk_26C;
     /* 0x0270 */ s32 unk_270; // some sort of flag
     /* 0x0274 */ s32 unk_274;
     /* 0x0278 */ s32 triforcePrimColor[4];
     /* 0x0288 */ s32 triforceEnvColor[4];
-    /* 0x0298 */ f32 triforceScale[3]; // some sort of scale
+    /* 0x0298 */ f32 triforceScale[3];
     /* 0x02A4 */ s16 unk_2A4; // some sort of angle
     /* 0x02A8 */ s32 unk_2A8; // sound related
     /* 0x02AC */ s32 unk_2AC; // sound related
