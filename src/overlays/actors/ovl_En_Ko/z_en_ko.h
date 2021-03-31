d75d24c5873b52d6c475ae6bd8a802461b86bcf5
Louis <35883445+louist103@users.noreply.github.com>
2021-03-30 22:13:59 -0400

    lets try this again

--- src/overlays/actors/ovl_En_Ko/z_en_ko.h
+++ src/overlays/actors/ovl_En_Ko/z_en_ko.h
@@ -1,5 +1,5 @@
-#ifndef _Z_EN_SKJ_H_
-#define _Z_EN_SKJ_H_
+#ifndef _Z_EN_KO_H_
+#define _Z_EN_KO_H_
 
 #include "ultra64.h"
 #include "global.h"
@@ -9,8 +9,8 @@ struct EnKo;
 typedef struct EnKo {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ char unk_14C[0x1D8];
-} EnKo; // size = 0x0300
+} EnKo; // size = 0x0324
 
-extern const ActorInit En_Skj_InitVars;
+extern const ActorInit En_Ko_InitVars;
 
 #endif
