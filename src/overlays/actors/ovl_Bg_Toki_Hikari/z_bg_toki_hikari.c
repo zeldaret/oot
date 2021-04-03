cc05d4ca6006f5a2435b689670c83ae3f9cd082b
Parker Burnett <burnettparker@gmail.com>
2021-04-03 00:27:07 -0400

    init and draw matched, one to go

--- src/overlays/actors/ovl_Bg_Toki_Hikari/z_bg_toki_hikari.c
+++ src/overlays/actors/ovl_Bg_Toki_Hikari/z_bg_toki_hikari.c
@@ -10,10 +10,11 @@ void BgTokiHikari_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgTokiHikari_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void func_808B9F98(BgTokiHikari* this, GlobalContext* globalCtx);
-
+void func_808BA018(BgTokiHikari* this, GlobalContext* globalCtx);
 void func_808BA204(BgTokiHikari* this, GlobalContext* globalCtx);
 void func_808BA22C(BgTokiHikari* this, GlobalContext* globalCtx);
 void func_808BA274(BgTokiHikari* this, GlobalContext* globalCtx);
+void func_808BA2CC(BgTokiHikari* this, GlobalContext* globalCtx);
 
 extern Gfx D_06000880[];
 extern Gfx D_060009C0[];
@@ -34,60 +35,57 @@ const ActorInit Bg_Toki_Hikari_InitVars = {
     (ActorFunc)BgTokiHikari_Draw,
 };
 
-s32 D_808BAC70[] = { 0x485003E8, 0x00000000, 0x00000000, 0x00000000 };
+static InitChainEntry sInitChain[] = {
+    ICHAIN_VEC3F_DIV1000(scale, 1000, ICHAIN_STOP),
+};
 
-// static InitChainEntry sInitChain[] = {
-//     ICHAIN_VEC3F_DIV1000(scale, 1000, ICHAIN_STOP),
-// };
+void BgTokiHikari_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiHikari* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Init.s")
-// void BgTokiHikari_Init(Actor* thisx, GlobalContext* globalCtx) {
-//     BgTokiHikari* this = THIS;
+    switch (this->actor.params) {
+        case 0:
+            Actor_ProcessInitChain(&this->actor, sInitChain);
+            this->actionFunc = func_808B9F98;
+            break;
+        case 1:
+            if (!(gSaveContext.eventChkInf[4] & 0x800)) {
+                this->actionFunc = func_808BA204;
+                this->unk_14C = 0.0f;
+            } else {
+                Actor_Kill(&this->actor);
+            }
+            break;
+    }
+}
 
-//     switch (this->actor.params) {
-//         case 0:
-//             Actor_ProcessInitChain(&this->actor, sInitChain);
-//             this->actionFunc = func_808B9F98;
-//             break;
-
-//         case 1:
-//             break;
-//     }
-
-//     if ((gSaveContext.eventChkInf[4] & 0x800) == 0) {
-//         this->actionFunc = func_808BA204;
-//         this->unk_14C = 0.0f;
-//     } else {
-
-//         Actor_Kill(thisx);
-//     }
-// }
-
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Destroy.s")
 void BgTokiHikari_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
-// matches
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/func_808B9F98.s")
 void func_808B9F98(BgTokiHikari* this, GlobalContext* globalCtx) {
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Update.s")
 void BgTokiHikari_Update(Actor* thisx, GlobalContext* globalCtx) {
     BgTokiHikari* this = THIS;
     this->actionFunc(this, globalCtx);
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Draw.s")
+void BgTokiHikari_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiHikari* this = THIS;
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/func_808BA018.s")
+    switch (this->actor.params) {
+        case 0:
+            func_808BA018(this, globalCtx);
+            break;
+        case 1:
+            func_808BA2CC(this, globalCtx);
+            break;
+    }
+}
 void func_808BA018(BgTokiHikari* this, GlobalContext* globalCtx) {
     s32 pad;
 
     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_toki_hikari.c", 0xF6);
-
     func_80093D18(globalCtx->state.gfxCtx);
-
     gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_toki_hikari.c", 0xFC),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
 
@@ -96,7 +94,6 @@ void func_808BA018(BgTokiHikari* this, GlobalContext* globalCtx) {
     } else {
         gSPDisplayList(POLY_OPA_DISP++, D_06007E20);
         func_80093D84(globalCtx->state.gfxCtx);
-
         gSPSegment(POLY_XLU_DISP++, 0x08,
                    Gfx_TexScroll(globalCtx->state.gfxCtx, 0, globalCtx->gameplayFrames & 0x7F, 0x40, 0x20));
 
@@ -113,7 +110,6 @@ void func_808BA018(BgTokiHikari* this, GlobalContext* globalCtx) {
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/func_808BA204.s")
 void func_808BA204(BgTokiHikari* this, GlobalContext* globalCtx) {
-
     if (globalCtx->unk_11D30[1] != 0) {
         this->actionFunc = func_808BA22C;
     }
@@ -121,9 +117,8 @@ void func_808BA204(BgTokiHikari* this, GlobalContext* globalCtx) {
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/func_808BA22C.s")
 void func_808BA22C(BgTokiHikari* this, GlobalContext* globalCtx) {
-
     if (this->unk_14C < 1.0f) {
-        this->unk_14C = (this->unk_14C + 0.05f);
+        this->unk_14C += 0.05f;
     } else {
         this->unk_14C = 1.0f;
         this->actionFunc = func_808BA274;
@@ -132,7 +127,6 @@ void func_808BA22C(BgTokiHikari* this, GlobalContext* globalCtx) {
 
 // #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/func_808BA274.s")
 void func_808BA274(BgTokiHikari* this, GlobalContext* globalCtx) {
-
     if (this->unk_14C > 0.2f) {
         this->unk_14C -= 0.025f;
     } else {
@@ -177,7 +171,7 @@ void func_808BA274(BgTokiHikari* this, GlobalContext* globalCtx) {
 //     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_toki_hikari.c", 0x15E);
 //     Matrix_Translate(0.0f, 276.0f, 1122.0f, 0);
 //     Matrix_Scale(0.32f, 0.32f, this->unk_14C * 7.0f, 1);
-//     Matrix_RotateZ(3.1415927f, 1);
+//     Matrix_RotateZ(M_PI, 1);
 //     func_80093D18(globalCtx->state.gfxCtx);
 //     Matrix_Push();
 
@@ -199,8 +193,8 @@ void func_808BA274(BgTokiHikari* this, GlobalContext* globalCtx) {
 //               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
 
 //     gSPSegment(POLY_XLU_DISP++, 0x08,
-//                Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, (0 - globalCtx->gameplayFrames & 0x7F) * 2, 0U, 0x20, 0x40,
-//                                 1, globalCtx->gameplayFrames & 0x7F * 4, 0, 0x20, 0x40));
+//                Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, (0 - globalCtx->gameplayFrames & 0x7F) * 2, 0U, 0x20,
+//                0x40, 1, globalCtx->gameplayFrames & 0x7F * 4, 0, 0x20, 0x40));
 
 //     gSPDisplayList(POLY_XLU_DISP++, D_06000880);
 //     Matrix_Pull();
