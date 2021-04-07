7ef38bc8dcbbef9f6874253d58c1b7ce7bc71ac6
mzxrules <mzxrules@gmail.com>
2021-04-07 16:33:11 -0400

    clean up code

--- src/overlays/actors/ovl_Bg_Spot07_Taki/z_bg_spot07_taki.c
+++ src/overlays/actors/ovl_Bg_Spot07_Taki/z_bg_spot07_taki.c
@@ -15,13 +15,15 @@ void BgSpot07Taki_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot07Taki_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot07Taki_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06000460;
-extern UNK_TYPE D_06000BE0;
-extern UNK_TYPE D_06001CF0;
-extern UNK_TYPE D_06001F68;
+void BgSpot07Taki_DoNothing(BgSpot07Taki* this, GlobalContext* globalCtx);
+
+extern Gfx D_06000460[];
+extern Gfx D_06000BE0[];
+extern Gfx D_06001CF0[];
+extern Gfx D_06001F68[];
 extern CollisionHeader D_06002590;
-extern UNK_TYPE D_06003210;
-extern UNK_TYPE D_060032D8;
+extern Gfx D_06003210[];
+extern Gfx D_060032D8[];
 extern CollisionHeader D_060038FC;
 
 const ActorInit Bg_Spot07_Taki_InitVars = {
@@ -40,26 +42,21 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
 };
 
-void func_808AFD08(BgSpot07Taki* this, GlobalContext* globalCtx);
-
 void BgSpot07Taki_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgSpot07Taki* this = THIS;
     s32 pad;
-    CollisionHeader* sp24;
-
-    sp24 = NULL;
+    CollisionHeader* colHeader = NULL;
     DynaPolyActor_Init(&this->dyna, DPM_PLAYER);
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     if (gSaveContext.linkAge == 0) {
         if (thisx->params == 0) {
-            CollisionHeader_GetVirtual(&D_06002590, &sp24);
+            CollisionHeader_GetVirtual(&D_06002590, &colHeader);
+        } else {
+            CollisionHeader_GetVirtual(&D_060038FC, &colHeader);
         }
-        else {
-            CollisionHeader_GetVirtual(&D_060038FC, &sp24);
-        }
-        this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, thisx, sp24);
+        this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, thisx, colHeader);
     }
-    this->actionFunc = func_808AFD08;
+    this->actionFunc = BgSpot07Taki_DoNothing;
 }
 
 void BgSpot07Taki_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -67,8 +64,7 @@ void BgSpot07Taki_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
 }
 
-void func_808AFD08(BgSpot07Taki* this, GlobalContext* globalCtx) {
-
+void BgSpot07Taki_DoNothing(BgSpot07Taki* this, GlobalContext* globalCtx) {
 }
 
 void BgSpot07Taki_Update(Actor* thisx, GlobalContext* globalCtx) {
@@ -78,63 +74,51 @@ void BgSpot07Taki_Update(Actor* thisx, GlobalContext* globalCtx) {
 
 void BgSpot07Taki_Draw(Actor* thisx, GlobalContext* globalCtx) {
     u32 pad;
-    u32 sp98;
+    u32 frames;
     OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot07_taki.c", 169);
-    sp98 = globalCtx->gameplayFrames;
+    frames = globalCtx->gameplayFrames;
     if (gSaveContext.linkAge == 0) {
         func_80093D18(globalCtx->state.gfxCtx);
         gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot07_taki.c", 177),
-            G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+                  G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         if (thisx->params == 0) {
-            gSPDisplayList(POLY_OPA_DISP++, &D_06001CF0);
-        }
-        else {
-            gSPDisplayList(POLY_OPA_DISP++, &D_06003210);
+            gSPDisplayList(POLY_OPA_DISP++, D_06001CF0);
+        } else {
+            gSPDisplayList(POLY_OPA_DISP++, D_06003210);
         }
     }
     func_80093D84(globalCtx->state.gfxCtx);
 
     gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_spot07_taki.c", 191),
-        G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
 
-#define sp44 (sp98 * 1) & 0x7F
-#define sp48 ( - 1 * sp98) & 0x7F
-#define temp_a3_2 (( - 3 * sp98) & 0xFF)
-#define temp_a3_3 ((sp98 * 3) & 0x1FF)
+    gSPSegment(POLY_XLU_DISP++, 0x08,
+               Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, ((frames * -1) & 0x7F), ((frames * 1) & 0x7F), 32, 32, 1,
+                                ((frames * 1) & 0x7F), ((frames * 1) & 0x7F), 32, 32));
 
-#define temp_a3_4 ((sp98 * 3) & 0x1FF)
-#define sp44_2 sp98 * 0
-
-    gSPSegment(POLY_XLU_DISP++, 0x08, 
-        Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, sp48, sp44, 32, 32, 1, sp44, sp44, 32, 32)
-    );
-    
     if (gSaveContext.linkAge != 0) {
-        gDPSetEnvColor(POLY_XLU_DISP++, 0xFF, 0xFF, 0xFF, 0x80);
+        gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, 255, 128);
         if (thisx->params == 0) {
             gSPSegment(POLY_XLU_DISP++, 0x09,
-                Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, sp48, temp_a3_2, 64, 64, 1, sp44, temp_a3_2, 64, 64)
-            );
+                       Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, ((frames * -1) & 0x7F), ((frames * -3) & 0xFF), 64,
+                                        64, 1, ((frames * 1) & 0x7F), ((frames * -3) & 0xFF), 64, 64));
             gSPSegment(POLY_XLU_DISP++, 0x0A,
-                Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, sp44_2, temp_a3_3, 32, 128, 1, sp44_2, temp_a3_3, 32, 128)
-            );
-            gSPDisplayList(POLY_XLU_DISP++, &D_06000460);
-        }
-        else {
+                       Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, frames * 0, ((frames * 3) & 0x1FF), 32, 128, 1,
+                                        frames * 0, ((frames * 3) & 0x1FF), 32, 128));
+            gSPDisplayList(POLY_XLU_DISP++, D_06000460);
+        } else {
             gSPSegment(POLY_XLU_DISP++, 0x09,
-                Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, sp44_2, sp48, 32, 32, 1, sp44_2, sp48, 32, 32)
-            );
+                       Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, frames * 0, ((frames * -1) & 0x7F), 32, 32, 1,
+                                        frames * 0, ((frames * -1) & 0x7F), 32, 32));
             gSPSegment(POLY_XLU_DISP++, 0x0A,
-                Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, sp44_2, temp_a3_4, 32, 128, 1, sp44_2, temp_a3_4, 32, 128)
-            );
+                       Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, frames * 0, ((frames * 3) & 0x1FF), 32, 128, 1,
+                                        frames * 0, ((frames * 3) & 0x1FF), 32, 128));
             gSPDisplayList(POLY_XLU_DISP++, &D_06000BE0);
         }
-    }
-    else if (thisx->params == 0) {
-        gSPDisplayList(POLY_XLU_DISP++, &D_06001F68);
-    }
-    else {
-        gSPDisplayList(POLY_XLU_DISP++, &D_060032D8);
+    } else if (thisx->params == 0) {
+        gSPDisplayList(POLY_XLU_DISP++, D_06001F68);
+    } else {
+        gSPDisplayList(POLY_XLU_DISP++, D_060032D8);
     }
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_spot07_taki.c", 272);
 }
