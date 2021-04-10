aa201c6a90d57af611c20361dea21eb2e3849878
angie <angheloalf95@gmail.com>
2021-04-10 15:09:26 -0400

    Only process touched files and abort the extraction if one file wasn't extracted properly
    Signed-off-by: angie <angheloalf95@gmail.com>

--- extract_assets.py
+++ extract_assets.py
@@ -1,54 +1,94 @@
 #!/usr/bin/env python3
 
 import argparse
+from multiprocessing import Pool, cpu_count, Event
 import os
-from shutil import copyfile
-from multiprocessing import Pool
-from multiprocessing import cpu_count
+
+# Returns True if outFile doesn't exists
+# or if inFile has been modified after the last modification of outFile
+def checkTouchedFile(inFile: str, outFile: str) -> bool:
+    if not os.path.exists(outFile):
+        return True
+    return os.path.getmtime(inFile) > os.path.getmtime(outFile)
 
 def Extract(xmlPath, outputPath):
-	ExtractFile(xmlPath, outputPath, 1, 0)	
+    ExtractFile(xmlPath, outputPath, 1, 0)
 
 def ExtractScene(xmlPath, outputPath):
-	ExtractFile(xmlPath, outputPath, 1, 1)
+    ExtractFile(xmlPath, outputPath, 1, 1)
 
 def ExtractFile(xmlPath, outputPath, genSrcFile, incFilePrefix):
-	execStr = "tools/ZAPD/ZAPD.out e -eh -i %s -b baserom/ -o %s -gsf %i -ifp %i -sm tools/ZAPD/SymbolMap_OoTMqDbg.txt" % (xmlPath, outputPath, genSrcFile, incFilePrefix)
+    if globalAbort.is_set():
+        # Don't extract if another file wasn't extracted properly.
+        return
 
-	print(execStr)
-	os.system(execStr)
+    execStr = "tools/ZAPD/ZAPD.out e -eh -i %s -b baserom/ -o %s -gsf %i -ifp %i -sm tools/ZAPD/SymbolMap_OoTMqDbg.txt" % (xmlPath, outputPath, genSrcFile, incFilePrefix)
+
+    print(execStr)
+    exitValue = os.system(execStr)
+    if exitValue != 0:
+        globalAbort.set()
+        print("\n")
+        print("Error when extracting from file " + xmlPath, file=os.sys.stderr)
+        print("Aborting...", file=os.sys.stderr)
+        print("\n")
 
 def ExtractFunc(fullPath):
-	outPath = ("assets/" + fullPath.split("assets/xml/")[1]).split(".xml")[0]
+    *pathList, xmlName = fullPath.split(os.sep)
+    objectName = os.path.splitext(xmlName)[0]
 
-	if (fullPath.startswith("assets/xml/scenes/")):
-		ExtractScene(fullPath, outPath)
-	else:
-		Extract(fullPath, outPath)
+    outPath = os.path.join("assets", *pathList[2:], objectName)
+
+    isScene = fullPath.startswith("assets/xml/scenes/")
+    if isScene:
+        objectName += "_scene"
+
+    if not globalForce:
+        cFile = os.path.join(outPath, objectName + ".c")
+        hFile = os.path.join(outPath, objectName + ".h")
+        if not checkTouchedFile(fullPath, cFile) and not checkTouchedFile(fullPath, hFile):
+            return
+
+    if isScene:
+        ExtractScene(fullPath, outPath)
+    else:
+        Extract(fullPath, outPath)
+
+def initializeWorker(force, abort):
+    global globalForce
+    global globalAbort
+    globalForce = force
+    globalAbort = abort
 
 def main():
     parser = argparse.ArgumentParser(description="baserom asset extractor")
     parser.add_argument("-s", "--single", help="asset path relative to assets/, e.g. objects/gameplay_keep")
+    parser.add_argument("-f", "--force", help="Force the extraction of every xml instead of checking the touched ones.", action="store_true")
     args = parser.parse_args()
+
+    abort = Event()
 
     asset_path = args.single
     if asset_path is not None:
-        if asset_path.endswith("/"):
-            asset_path = asset_path[0:-1]
-        Extract(f"assets/xml/{asset_path}.xml", f"assets/{asset_path}/")
+        # Always force if -s is used.
+        initializeWorker(True, abort)
+        fullPath = os.path.join("assets", "xml", asset_path + ".xml")
+        ExtractFunc(fullPath)
     else:
         xmlFiles = []
-        for currentPath, folders, files in os.walk("assets"):
-                for file in files:
-                        fullPath = os.path.join(currentPath, file)
-                        if file.endswith(".xml") and currentPath.startswith("assets/xml/"):
-                                outPath = ("assets/" + fullPath.split("assets/xml/")[1]).split(".xml")[0]
-                                xmlFiles.append(fullPath)
+        for currentPath, folders, files in os.walk(os.path.join("assets", "xml")):
+            for file in files:
+                fullPath = os.path.join(currentPath, file)
+                if file.endswith(".xml"):
+                    xmlFiles.append(fullPath)
 
         numCores = cpu_count()
         print("Extracting assets with " + str(numCores) + " CPU cores.")
-        p = Pool(numCores)
-        p.map(ExtractFunc, xmlFiles)
+        with Pool(numCores,  initializer=initializeWorker, initargs=(args.force, abort)) as p:
+            p.map(ExtractFunc, xmlFiles)
+
+    if abort.is_set():
+        exit(1)
 
 if __name__ == "__main__":
     main()
