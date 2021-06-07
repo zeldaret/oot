868271650a9302d987fdecc5492e261228e92ccd
mzxrules <mzxrules@gmail.com>
2021-06-07 02:52:43 -0400

    pick up some OKs

--- src/overlays/actors/ovl_Obj_Mure/z_obj_mure.c
+++ src/overlays/actors/ovl_Obj_Mure/z_obj_mure.c
@@ -10,9 +10,8 @@
 
 #define THIS ((ObjMure*)thisx)
 
-// not sure about signatures
-s32 func_80B98AA0(Actor* thisx, s32 globalCtx);
-s32 func_80B98B1C(Actor* thisx, s32 globalCtx);
+s32 func_80B98AA0(Actor* thisx, GlobalContext* globalCtx);
+s32 func_80B98B1C(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure_Update(Actor* thisx, GlobalContext* globalCtx);
@@ -26,6 +25,8 @@ void func_80B99204(ObjMure* this, GlobalContext* globalCtx);
 void func_80B99224(ObjMure* this, GlobalContext* globalCtx);
 void func_80B992E4(ObjMure* this, GlobalContext* globalCtx);
 void func_80B992F8(ObjMure* this, GlobalContext* globalCtx);
+void func_80B995A4(ObjMure* this, GlobalContext* globalCtx);
+void func_80B997CC(ObjMure* this, GlobalContext* globalCtx);
 void func_80B99950(ObjMure* this, GlobalContext* globalCtx);
 
 const ActorInit Obj_Mure_InitVars = {
@@ -42,12 +43,12 @@ const ActorInit Obj_Mure_InitVars = {
 
 f32 D_80B99A50[] = { 1600.0f, 1600.0f, 1000.0f, 1000.0f, 1000.0f };
 
-s32 D_80B99A64[] = { 0x0000000C, 0x00000009, 0x00000008, 0x00000000 };
+s32 D_80B99A64[] = { 12, 9, 8, 0 };
 
 // used to set actorID and params (?) in spawn child
-s16 D_80B99A74[] = { 293, 0, 33, 32, 30, 0 };
+s16 D_80B99A74[] = { ACTOR_EN_KUSA, 0, ACTOR_EN_FISH, ACTOR_EN_INSECT, ACTOR_EN_BUTTE };
 
-s16 D_80B99A80[] = { 0, 2, -1, 0, -1, 0 };
+s16 D_80B99A80[] = { 0, 2, -1, 0, -1 };
 
 // sInitChain
 InitChainEntry D_80B99A8C[] = {
@@ -56,38 +57,34 @@ InitChainEntry D_80B99A8C[] = {
     ICHAIN_F32(uncullZoneDownward, 1200, ICHAIN_STOP),
 };
 
-ObjMureActionFunc D_80B99A98[] = { 0x00000000, 0x00000000, 0x80B995A4, 0x80B995A4, 0x80B997CC, 0x00000000 };
+ObjMureActionFunc D_80B99A98[] = { NULL, NULL, func_80B995A4, func_80B995A4, func_80B997CC };
 
-// one branch instruction missing
-#ifdef NON_MATCHING
-s32 func_80B98AA0(Actor* thisx, s32 globalCtx) {
+s32 func_80B98AA0(Actor* thisx, GlobalContext* globalCtx) {
     ObjMure* this = THIS;
+    s32 result;
 
     switch (this->type) {
-        case 2:
-        case 3:
-        case 4:
-            Actor_ProcessInitChain(thisx, D_80B99A8C);
-            return 1;
-        default:
-            osSyncPrintf("Error : カリングの設定がされていません。(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0xCC,
-                         thisx->params);
-            return 0;
+    case 2:
+    case 3:
+    case 4:
+        Actor_ProcessInitChain(&this->actor, D_80B99A8C);
+        result = true;
+        break;
+    default:
+        osSyncPrintf("Error : カリングの設定がされていません。(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 204,
+            this->actor.params);
+        return false;
     }
+    return result;
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/func_80B98AA0.s")
-#endif
 
-s32 func_80B98B1C(Actor* thisx, s32 globalCtx) {
+s32 func_80B98B1C(Actor* thisx, GlobalContext* globalCtx) {
     if (func_80B98AA0(thisx, globalCtx) == 0) {
         return 0;
     }
     return 1;
 }
 
-// almost matching
-#ifdef NON_MATCHING
 void ObjMure_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjMure* this = THIS;
 
@@ -97,44 +94,41 @@ void ObjMure_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->type = thisx->params & 0x1F;
 
     if (this->ptn >= 4) {
-        osSyncPrintf("Error 群れな敵 (%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0xED, thisx->params);
-        Actor_Kill(thisx);
+        osSyncPrintf("Error 群れな敵 (%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 237, thisx->params);
+        Actor_Kill(&this->actor);
         return;
     } else if (this->type >= 5) {
-        osSyncPrintf("Error 群れな敵 (%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0xF5, thisx->params);
-        Actor_Kill(thisx);
+        osSyncPrintf("Error 群れな敵 (%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 245, thisx->params);
+        Actor_Kill(&this->actor);
         return;
-    } else if (func_80B98B1C(thisx, 0xF5) == 0) {
-        Actor_Kill(thisx);
+    } else if (func_80B98B1C(thisx, globalCtx) == 0) {
+        Actor_Kill(&this->actor);
         return;
     }
     this->actionFunc = func_80B992E4;
     osSyncPrintf("群れな敵 (arg_data 0x%04x)(chNum(%d) ptn(%d) svNum(%d) type(%d))\n", thisx->params, this->chNum,
                  this->ptn, this->svNum, this->type);
     if (func_80B98C88(this) <= 0) {
-        osSyncPrintf("Warning : 個体数が設定されていません(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0x10C,
+        osSyncPrintf("Warning : 個体数が設定されていません(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 268,
                      thisx->params);
     }
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/ObjMure_Init.s")
-#endif
 
 void ObjMure_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
-// gets the number of spawned actors? or something else
+// gets the number of spawned actors
 s32 func_80B98C88(ObjMure* this) {
     if (this->chNum == 0) {
         return D_80B99A64[this->ptn];
     }
-    return (s32)this->chNum;
+    return this->chNum;
 }
 
 // ObjMure_SetActorPos
 void func_80B98CB4(PosRot* this, PosRot* arg1, s32 ptn, s32 arg3) {
     if (ptn >= 4) {
-        osSyncPrintf("おかしなの (%s %d)\n", "../z_obj_mure.c", 0x133);
+        osSyncPrintf("おかしなの (%s %d)\n", "../z_obj_mure.c", 307);
     }
     this->pos = arg1->pos;
 }
@@ -161,7 +155,7 @@ void func_80B98D18(ObjMure* this, GlobalContext* globalCtx) {
                     &globalCtx->actorCtx, globalCtx, D_80B99A74[this->type], sp74.pos.x, sp74.pos.y, sp74.pos.z,
                     this->actor.world.rot.x, this->actor.world.rot.y, this->actor.world.rot.z, D_80B99A80[this->type]);
                 if (this->actorSpawnPtrList[i] != NULL) {
-                    this->actorSpawnPtrList[i]->flags = this->actorSpawnPtrList[i]->flags | 0x800;
+                    this->actorSpawnPtrList[i]->flags |= 0x800;
                     this->actorSpawnPtrList[i]->room = this->actor.room;
                 } else {
                     osSyncPrintf("warning 発生失敗 (%s %d)\n", "../z_obj_mure.c", 0x167);
@@ -189,29 +183,32 @@ void func_80B98F38(ObjMure* this, GlobalContext* globalCtx) {
     PosRot sp7C;
     s32 temp_v0;
     s32 i;
-    s16 phi_t0;
+    GlobalContext* g = (GlobalContext*)globalCtx;
 
     temp_v0 = func_80B98C88(this);
     for (i = 0; i < temp_v0; i++) {
+        //s16 phi_t0;
         if (this->actorSpawnPtrList[i] != NULL) {
-            osSyncPrintf("Error : 既に子供がいる(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 0x197,
+            osSyncPrintf("Error : 既に子供がいる(%s %d)(arg_data 0x%04x)\n", "../z_obj_mure.c", 407,
                          this->actor.params);
         }
         func_80B98CB4(&sp7C, &this->actor.world, this->ptn, i);
-        if (this->type == 4 && i == 0) {
-            phi_t0 = 1;
-        } else {
-            phi_t0 = D_80B99A80[this->type];
-        }
+        //phi_t0 = (this->type == 4 && i == 0) ? 1 : D_80B99A80[this->type];
+        //if (this->type == 4 && i == 0) {
+        //    phi_t0 = 1;
+        //} else {
+        //    phi_t0 = D_80B99A80[this->type];
+        //}
         this->actorSpawnPtrList[i] =
-            Actor_Spawn(&globalCtx->actorCtx, globalCtx, D_80B99A74[this->type], sp7C.pos.x, sp7C.pos.y, sp7C.pos.z,
-                        this->actor.world.rot.x, this->actor.world.rot.y, this->actor.world.rot.z, phi_t0);
+            Actor_Spawn(&g->actorCtx, g, D_80B99A74[this->type], sp7C.pos.x, sp7C.pos.y, sp7C.pos.z,
+                this->actor.world.rot.x, this->actor.world.rot.y, this->actor.world.rot.z, //phi_t0);
+                (this->type == 4 && i == 0) ? 1 : D_80B99A80[this->type]);
         if (this->actorSpawnPtrList[i] != NULL) {
             this->unk_194[i] = 0;
             this->actorSpawnPtrList[i]->room = this->actor.room;
         } else {
             this->unk_194[i] = 1;
-            osSyncPrintf("warning 発生失敗 (%s %d)\n", "../z_obj_mure.c", 0x1B6);
+            osSyncPrintf("warning 発生失敗 (%s %d)\n", "../z_obj_mure.c", 438);
         }
     }
 }
@@ -296,8 +293,8 @@ void func_80B992E4(ObjMure* this, GlobalContext* globalCtx) {
 void func_80B992F8(ObjMure* this, GlobalContext* globalCtx) {
     if (fabsf(this->actor.projectedPos.z) < D_80B99A50[this->type]) {
         this->actionFunc = func_80B99950;
-        this->actor.flags = this->actor.flags | 0x10;
-        func_80B990BC(this, globalCtx); // most likely the args
+        this->actor.flags |= 0x10;
+        func_80B990BC(this, globalCtx);
     }
 }
 
@@ -311,7 +308,7 @@ void func_80B99358(ObjMure* this, f32 arg1) {
         if (this->actorSpawnPtrList[i] != NULL) {
             this->actorSpawnPtrList[i]->child = NULL;
             if (Rand_ZeroOne() <= arg1) {
-                temp_f16 = (s32)(Rand_ZeroOne() * ((f32)temp_v0 - 0.5f));
+                temp_f16 = (s32)(Rand_ZeroOne() * (temp_v0 - 0.5f));
                 if (i != temp_f16) {
                     this->actorSpawnPtrList[i]->child = this->actorSpawnPtrList[temp_f16];
                 }
@@ -320,70 +317,69 @@ void func_80B99358(ObjMure* this, f32 arg1) {
     }
 }
 
-// almost matching
-#ifdef NON_MATCHING
 void func_80B9942C(ObjMure* this, s32 arg1) {
     s32 temp_v0;
     s32 i;
+    s32 i2;
     s32 j;
 
     temp_v0 = func_80B98C88(this);
-    for (i = 0; i < temp_v0; i++) {
-        if (this->actorSpawnPtrList[i] != NULL && i < arg1) {
-            this->actorSpawnPtrList[i]->child = this->actorSpawnPtrList[i];
-            for (j = i; j < temp_v0; j++) {
-                if (i != j && this->actorSpawnPtrList[j]->child == this->actorSpawnPtrList[i]) {
-                    this->actorSpawnPtrList[j]->child = NULL;
+    i = 0;
+    i2 = 0;
+
+    while (i < temp_v0) {
+        if (this->actorSpawnPtrList[i] != NULL) {
+            if (i2 < arg1) {
+                i2++;
+                this->actorSpawnPtrList[i]->child = this->actorSpawnPtrList[i];
+                for (j = 0; j < temp_v0; j++) {
+                    if (i != j && this->actorSpawnPtrList[j]->child == this->actorSpawnPtrList[i]) {
+                        this->actorSpawnPtrList[j]->child = NULL;
+                    }
                 }
             }
-        } else if (this->actorSpawnPtrList[i]->child == this->actorSpawnPtrList[i]) {
-            this->actorSpawnPtrList[i]->child = NULL;
-        }
+            else if (this->actorSpawnPtrList[i]->child == this->actorSpawnPtrList[i]) {
+                this->actorSpawnPtrList[i]->child = NULL;
+            }
+        } 
+        i++;
     }
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/func_80B9942C.s")
-#endif
 
-// stack/regalloc
-#ifdef NON_MATCHING
-void func_80B995A4(ObjMure* this, GlobalContext* GlobalContext) {
+void func_80B995A4(ObjMure* this, GlobalContext* globalCtx) {
     if (this->unk_1A4 <= 0) {
-        if (this->unk_1A6 != 0) {
-            this->unk_1A6 = 0;
+        if (this->unk_1A6) {
+            this->unk_1A6 = false;
             func_80B99358(this, (Rand_ZeroOne() * 0.5f) + 0.1f);
             if (this->actor.xzDistToPlayer < 60.0f) {
-                this->unk_1A4 = (s16)((s32)(Rand_ZeroOne() * 5.5f) + 4);
+                this->unk_1A4 = (s16)(Rand_ZeroOne() * 5.5f) + 4;
             } else {
-                this->unk_1A4 = (s16)((s32)(Rand_ZeroOne() * 40.5f) + 4);
+                this->unk_1A4 = (s16)(Rand_ZeroOne() * 40.5f) + 4;
             }
         } else {
-            this->unk_1A6 = 1;
+            this->unk_1A6 = true;
             if (this->actor.xzDistToPlayer < 60.0f) {
-                this->unk_1A4 = (s16)((s32)(Rand_ZeroOne() * 10.5f) + 4);
+                this->unk_1A4 = (s16)(Rand_ZeroOne() * 10.5f) + 4;
                 func_80B99358(this, (Rand_ZeroOne() * 0.2f) + 0.8f);
             } else {
-                this->unk_1A4 = (s16)((s32)(Rand_ZeroOne() * 10.5f) + 4);
+                this->unk_1A4 = (s16)(Rand_ZeroOne() * 10.5f) + 4;
                 func_80B99358(this, (Rand_ZeroOne() * 0.2f) + 0.6f);
             }
         }
     }
     if (this->actor.xzDistToPlayer < 120.0f) {
-        this->unk_1A8 = this->unk_1A8 + 1;
+        this->unk_1A8++;
     } else {
         this->unk_1A8 = 0;
     }
-    if (this->unk_1A8 >= 0x50) {
+    if (this->unk_1A8 >= 80) {
         func_80B9942C(this, 1);
         return;
     }
     func_80B9942C(this, 0);
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Obj_Mure/func_80B995A4.s")
-#endif
 
-// stack diff
+// regalloc
 #ifdef NON_MATCHING
 void func_80B997CC(ObjMure* this, GlobalContext* globalCtx) {
     s32 temp_v0;
@@ -391,25 +387,26 @@ void func_80B997CC(ObjMure* this, GlobalContext* globalCtx) {
 
     if (this->unk_1A4 <= 0) {
         if (this->unk_1A6 != 0) {
-            this->unk_1A6 = 0;
+            this->unk_1A6 = false;
             func_80B99358(this, Rand_ZeroOne() * 0.2f);
             if (this->actor.xzDistToPlayer < 60.0f) {
-                this->unk_1A4 = (s32)(Rand_ZeroOne() * 5.5f) + 4;
+                this->unk_1A4 = (s16)(Rand_ZeroOne() * 5.5f) + 4;
             } else {
-                this->unk_1A4 = (s32)(Rand_ZeroOne() * 40.5f) + 4;
+                this->unk_1A4 = (s16)(Rand_ZeroOne() * 40.5f) + 4;
             }
         } else {
-            this->unk_1A6 = 1;
+            this->unk_1A6 = true;
             func_80B99358(this, Rand_ZeroOne() * 0.7f);
-            this->unk_1A4 = (s32)(Rand_ZeroOne() * 10.5f) + 4;
+            this->unk_1A4 = (s16)(Rand_ZeroOne() * 10.5f) + 4;
         }
     }
 
     temp_v0 = func_80B98C88(this);
     for (i = 0; i < temp_v0; i++) {
+        Actor* a = this->actorSpawnPtrList[i];
         if (this->actorSpawnPtrList[i] != NULL) {
-            if ((this->actorSpawnPtrList[i]->child != NULL) && (this->actorSpawnPtrList[i]->update == NULL)) {
-                this->actorSpawnPtrList[i]->child = NULL;
+            if ((a->child != NULL) && (a->update == NULL)) {
+                a->child = NULL;
             }
         }
     }
@@ -435,7 +432,7 @@ void ObjMure_Update(Actor* thisx, GlobalContext* globalCtx) {
     ObjMure* this = THIS;
 
     if (this->unk_1A4 > 0) {
-        this->unk_1A4 = this->unk_1A4 - 1;
+        this->unk_1A4 -= 1;
     }
 
     this->actionFunc(this, globalCtx);
--- src/overlays/actors/ovl_Obj_Mure/z_obj_mure.h
+++ src/overlays/actors/ovl_Obj_Mure/z_obj_mure.h
@@ -10,18 +10,16 @@ typedef void (*ObjMureActionFunc)(struct ObjMure*, GlobalContext*);
 
 typedef struct ObjMure {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ ObjMureActionFunc actionFunc; // function
+    /* 0x014C */ ObjMureActionFunc actionFunc;
     /* 0x0150 */ s16 chNum;
     /* 0x0152 */ s16 ptn;
     /* 0x0154 */ s16 svNum;
     /* 0x0156 */ s16 type;
-    /* 0x0158 */ Actor* actorSpawnPtrList[0xF];
-    /* 0x0194 */ u8 unk_194[0xF];
-    /* 0x01A3 */ char unk_1A3; // unused padding?
+    /* 0x0158 */ Actor* actorSpawnPtrList[15];
+    /* 0x0194 */ u8 unk_194[15];
     /* 0x01A4 */ s16 unk_1A4;
     /* 0x01A6 */ s16 unk_1A6;
     /* 0x01A8 */ s16 unk_1A8;
-    /* 0x01AA */ char unk_1AA[0x2];
 } ObjMure; // size = 0x01AC
 
 extern const ActorInit Obj_Mure_InitVars;
