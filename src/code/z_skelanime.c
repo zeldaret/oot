8a31890e66363eac9737b4c302e03a600bebb1a6
angie <angheloalf95@gmail.com>
2021-06-10 01:26:30 -0400

    Simplify SkelAnime_GetFrameDataLegacy

--- src/code/z_skelanime.c
+++ src/code/z_skelanime.c
@@ -732,53 +732,19 @@ s32 SkelAnime_GetFrameDataLegacy(LegacyAnimationHeader* animation, s32 frame, Ve
     s16* dynamicData = &frameData[frame];
     s32 i;
 
-    /**
-     *Equivalent to the following, but the compiler optimizes the loop in a way I can't replicate
-     */
-
-    // for(i = 0, frameTable++, key++; i < limbCount + 1; i++, key++, frameTable++) {
-    //     frameTable->x = frame < key->xMax ? dynamicData[key->x] : staticData[key->x];
-    //     frameTable->y = frame < key->yMax ? dynamicData[key->y] : staticData[key->y];
-    //     frameTable->z = frame < key->zMax ? dynamicData[key->z] : staticData[key->z];
-    // }
-
     frameTable->x = frame < key->xMax ? dynamicData[key->x] : staticData[key->x];
     frameTable->y = frame < key->yMax ? dynamicData[key->y] : staticData[key->y];
     frameTable->z = frame < key->zMax ? dynamicData[key->z] : staticData[key->z];
 
-    i = 1;
     frameTable++;
     key++;
 
-    if (limbCount & 1) {}
-
-    if (limbCount > 0) {
-        if (limbCount & 1) {
-            i++;
-            frameTable->x = frame < key->xMax ? dynamicData[key->x] : staticData[key->x];
-            frameTable->y = frame < key->yMax ? dynamicData[key->y] : staticData[key->y];
-            frameTable->z = frame < key->zMax ? dynamicData[key->z] : staticData[key->z];
-            key++;
-            frameTable++;
-            if (limbCount + 1 == i) {
-                goto ret;
-            }
-        }
-        do {
-            i += 2;
-            frameTable->x = frame < key->xMax ? dynamicData[key->x] : staticData[key->x];
-            frameTable->y = frame < key->yMax ? dynamicData[key->y] : staticData[key->y];
-            frameTable->z = frame < key->zMax ? dynamicData[key->z] : staticData[key->z];
-            key++;
-            frameTable++;
-            frameTable->x = frame < key->xMax ? dynamicData[key->x] : staticData[key->x];
-            frameTable->y = frame < key->yMax ? dynamicData[key->y] : staticData[key->y];
-            frameTable->z = frame < key->zMax ? dynamicData[key->z] : staticData[key->z];
-            key++;
-            frameTable++;
-        } while (i != limbCount + 1);
+    for(i = 1; i <= limbCount; i++, key++, frameTable++) {
+        frameTable->x = frame < key->xMax ? dynamicData[key->x] : staticData[key->x];
+        frameTable->y = frame < key->yMax ? dynamicData[key->y] : staticData[key->y];
+        frameTable->z = frame < key->zMax ? dynamicData[key->z] : staticData[key->z];
     }
-ret:
+
     return limbCount;
 }
 
