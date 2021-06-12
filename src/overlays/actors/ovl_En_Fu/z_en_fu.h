4039324458334d1b55259aeafef813b48ee7c6b1
louist103 <35883445+louist103@users.noreply.github.com>
2021-06-12 14:19:55 -0400

    Update z_en_fu.h

--- src/overlays/actors/ovl_En_Fu/z_en_fu.h
+++ src/overlays/actors/ovl_En_Fu/z_en_fu.h
@@ -8,21 +8,6 @@ struct EnFu;
 
 typedef void (*EnFuActionFunc)(struct EnFu*, GlobalContext*);
 
-typedef struct EnFu {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ ColliderCylinder collider;
-    /* 0x0198 */ SkelAnime skelanime;
-    /* 0x01DC */ Vec3s jointTable[FU_LIMB_MAX];
-    /* 0x023C */ Vec3s morphTable[FU_LIMB_MAX];
-    /* 0x029C */ Vec3s lookAngleOffset; // offset applied to neck rotation to look at link when he is close
-    /* 0x02A2 */ Vec3s unk_2A2; 
-    /* 0x02A8 */ u16 behaviorFlags;
-    /* 0x02AA */ u16 facialExpression;
-    /* 0x02AC */ EnFuActionFunc actionFunc;
-} EnFu; // size = 0x02B0
-
-extern const ActorInit En_Fu_InitVars;
-
 typedef enum {
     /* 0 */ FU_ROOT_LIMB,
     /* 1 */ FU_TORSO_LIMB,
@@ -42,5 +27,20 @@ typedef enum {
     /* F */ FU_HORN_LIMB,
     /* 10 */ FU_LIMB_MAX
 } EnFuLimb;
+
+typedef struct EnFu {
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ ColliderCylinder collider;
+    /* 0x0198 */ SkelAnime skelanime;
+    /* 0x01DC */ Vec3s jointTable[FU_LIMB_MAX];
+    /* 0x023C */ Vec3s morphTable[FU_LIMB_MAX];
+    /* 0x029C */ Vec3s lookAngleOffset; // offset applied to neck rotation to look at link when he is close
+    /* 0x02A2 */ Vec3s unk_2A2; 
+    /* 0x02A8 */ u16 behaviorFlags;
+    /* 0x02AA */ u16 facialExpression;
+    /* 0x02AC */ EnFuActionFunc actionFunc;
+} EnFu; // size = 0x02B0
+
+extern const ActorInit En_Fu_InitVars;
 
 #endif
