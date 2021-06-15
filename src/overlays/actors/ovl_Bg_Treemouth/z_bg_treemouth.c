f14b5ed1cb9977f123321314835440872f502a94
Lucas Shaw <lucas.shaw1123@gmail.com>
2021-06-14 21:59:55 -0700

    object_spot04_objects fix

--- src/overlays/actors/ovl_Bg_Treemouth/z_bg_treemouth.c
+++ src/overlays/actors/ovl_Bg_Treemouth/z_bg_treemouth.c
@@ -64,7 +64,7 @@ void BgTreemouth_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     Actor_ProcessInitChain(thisx, sInitChain);
     DynaPolyActor_Init(&this->dyna, DPM_UNK);
-    CollisionHeader_GetVirtual(&gBgTreemouthCol, &colHeader);
+    CollisionHeader_GetVirtual(&gDekuTreeMouthCol, &colHeader);
     this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, thisx, colHeader);
     ActorShape_Init(&thisx->shape, 0.0f, NULL, 0.0f);
     Actor_SetFocus(thisx, 50.0f);
@@ -244,7 +244,7 @@ void BgTreemouth_Draw(Actor* thisx, GlobalContext* globalCtx) {
     gDPSetEnvColor(POLY_OPA_DISP++, 128, 128, 128, alpha * 0.1f);
     gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_treemouth.c", 932),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(POLY_OPA_DISP++, &gBgTreemouthDL);
+    gSPDisplayList(POLY_OPA_DISP++, &gDekuTreeMouthDL);
 
     CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_treemouth.c", 937);
 }
