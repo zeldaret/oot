d1a5ea5110ee3a8e7d2f0ed54921570fdccee65c
krimtonz <33664508+krimtonz@users.noreply.github.com>
2021-07-27 18:44:58 -0500

    Audio WIP (#836)
    * being code_800EC960
    
    * wip
    
    * wip
    
    * more audio progress
    
    * migrate data in code_800E11F0
    
    * wip
    
    * make ok
    
    * remove asm
    
    * wip
    
    * move some variables outside of gAudioContext to the AudioContext structure due to the size used in func_800E3094
    
    * more progress
    
    * wip
    
    * wip
    
    * split code_800E11F0
    
    * migrate rodata in code_800E11F0
    
    * match functions that couldn't because of data issues
    
    * move code_800E4FE0 asm files
    
    * wip
    
    * more wip
    
    * fix global functions, and forward declarations
    
    * wip
    
    * wip
    
    * wip
    
    * ocarina wip
    
    * match a couple functions
    
    * some progress
    
    * separate some bss
    
    * match func_800EDA3C
    
    * some matching
    
    * more matches
    
    * migrate audio rodata
    
    * some matches
    
    * more matchess
    
    * start on synthesis
    
    * work on synthesis
    
    * fix function declaration
    
    * Merge branch 'master' into audio
    
    * match a few more functions
    
    * wip
    
    * wip
    
    * more matching, rename Audio_SetBGM to Audio_QueueSeqCmd
    
    * name several audio functions, and audiocontext members
    
    * more naming, rename code_800E11F0 to audio_load, code_800DAAC0 to audio_synthesis
    
    * audio wip
    
    * match a few more functions.
    
    * wip
    
    * add missing NON_MATCHING directive
    
    * wip
    
    * some matching, data reogranization
    
    * match cursed function
    
    * wip
    
    * wip
    
    * formatting
    
    * remove prefix from struct memebers
    
    * missed function rename
    
    * review
    
    Co-authored-by: fig02 <fig02srl@gmail.com>

--- include/z64.h
+++ include/z64.h
@@ -510,7 +510,7 @@ typedef struct {
     /* 0x0168 */ Vtx*   cursorVtx;
     /* 0x016C */ Vtx*   saveVtx;
     /* 0x0170 */ char   unk_170[0x24];
-    /* 0x0194 */ UnkAudioStruct* unk_194;
+    /* 0x0194 */ OcarinaStaff* unk_194;
     /* 0x0198 */ char   unk_198[0x20];
     /* 0x01B8 */ OSMesgQueue loadQueue;
     /* 0x01D0 */ OSMesg loadMsg;
@@ -1506,23 +1506,11 @@ typedef struct {
 #define OS_SC_YIELDED           0x0020
 
 typedef struct {
-    struct {
-    /* 0x0000 */ s32          unk_0[0x10]; // not char to avoid generating lwl/lwr swl/swr in a struct copy
-    } unk_0;
-    /* 0x0040 */ OSMesgQueue*  unk_40;
-} Sub_AudioMgr_18; // size = 0x44
-
-typedef struct {
     /* 0x0000 */ IrqMgr*       irqMgr;
     /* 0x0004 */ SchedContext* sched;
-    /* 0x0008 */ OSMesg        unk_8;
-    /* 0x000C */ char          unk_C[0x04];
-    /* 0x0010 */ s32           unk_10;
-    /* 0x0014 */ s32           unk_14;
-    /* 0x0018 */ Sub_AudioMgr_18 unk_18;
-    /* 0x005C */ UNK_PTR       unk_5C;
+    /* 0x0008 */ OSScTask      audioTask;
     /* 0x0060 */ char          unk_60[0x10];
-    /* 0x0070 */ Sub_AudioMgr_18* unk_70;
+    /* 0x0070 */ AudioTask*    rspTask;
     /* 0x0074 */ OSMesgQueue   unk_74;
     /* 0x008C */ OSMesg        unk_8C;
     /* 0x0090 */ OSMesgQueue   unk_90;
@@ -1534,7 +1522,6 @@ typedef struct {
     /* 0x00E4 */ char          unk_E4[0x04];
     /* 0x00E8 */ OSThread      unk_E8;
 } AudioMgr; // size = 0x298
-
 
 struct ArenaNode;
 
