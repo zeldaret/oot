2013330aea887b70eaf85b51f329e29dc6aa955f
Louis <35883445+louist103@users.noreply.github.com>
2021-07-31 20:45:38 -0400

    added the comment Fig requested

--- src/code/z_en_item00.c
+++ src/code/z_en_item00.c
@@ -716,7 +716,7 @@ void func_8001E5C8(EnItem00* this, GlobalContext* globalCtx) {
         this->actor.world.pos.y += 20.0f;
     }
 }
-
+/* The BSS in the function acted weird in the past. It is matching now but mighy cause issues in the future*/
 void EnItem00_Update(Actor* thisx, GlobalContext* globalCtx) {
     static u32 D_80157D90;
     static s16 D_80157D94[1];
@@ -744,7 +744,7 @@ void EnItem00_Update(Actor* thisx, GlobalContext* globalCtx) {
     this->actor.scale.y = this->actor.scale.x;
     if (this->actor.gravity) {
         if (this->actor.bgCheckFlags & 0x0003) {
-            if ((*temp) != globalCtx->gameplayFrames) {
+            if (*temp != globalCtx->gameplayFrames) {
                 D_80157D90 = globalCtx->gameplayFrames;
                 D_80157D94[0] = 0;
                 for (i = 0; i < 50; i++) {
