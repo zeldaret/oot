3db532e88db008c2c7ea285d025b15629c456aea
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-08-16 12:30:18 +0200

    document bombchu actor

--- src/overlays/actors/ovl_En_Bom_Chu/z_en_bom_chu.h
+++ src/overlays/actors/ovl_En_Bom_Chu/z_en_bom_chu.h
@@ -12,10 +12,10 @@ typedef struct EnBomChu {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ EnBomChuActionFunc actionFunc;
     /* 0x0150 */ s16 timer;
-    /* 0x0154 */ Vec3f unk_154;
-    /* 0x0160 */ Vec3f unk_160;
-    /* 0x016C */ Vec3f unk_16C;
-    /* 0x0178 */ f32 unk_178;
+    /* 0x0154 */ Vec3f axisForwards;
+    /* 0x0160 */ Vec3f axisUp;
+    /* 0x016C */ Vec3f axisLeft;
+    /* 0x0178 */ f32 visualJitter;
     /* 0x017C */ s32 blure1Index;
     /* 0x0180 */ s32 blure2Index;
     /* 0x0184 */ ColliderJntSph collider;
