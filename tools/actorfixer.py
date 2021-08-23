e8ae17f2e1c9a7f211446532efb835cd0ab0b6af
Zelllll <56516451+Zelllll@users.noreply.github.com>
2021-08-23 07:07:55 -0500

    Update tools/actorfixer.py
    Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>

--- tools/actorfixer.py
+++ tools/actorfixer.py
@@ -100,7 +100,7 @@ wordReplace = {
     "actorCtx.unk_00": "actorCtx.freezeFlashTimer",
     "func_800339B8": "Actor_TestFloorInDirection",
     "Matrix_Reverse": "Matrix_Transpose",
-    "Matrix_MtxFToYXZRotS": "Matrix_MtxFToYXZRotS",
+    "func_800D20CC": "Matrix_MtxFToYXZRotS",
     "func_800D2264": "Matrix_MtxFToZYXRotS",
     "func_800D23FC": "Matrix_RotateAxis",
 }
