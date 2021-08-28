9c8ef2aeb93cf39ba7b85be743874d2368011b39
angie <angheloalf95@gmail.com>
2021-08-28 17:58:21 -0400

    Add designated initializers for cutscene macros

--- include/command_macros_base.h
+++ include/command_macros_base.h
@@ -6,16 +6,22 @@
  * Each macro packs bytes (B), halfowrds (H) and words (W, for consistency) into a single word
  */
 
-#define CMD_BBBB(a, b, c, d) (_SHIFTL(a, 24, 8) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 8, 8) | _SHIFTL(d, 0, 8))
+#define CMD_BBBB(a, b, c, d) {(_SHIFTL(a, 24, 8) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 8, 8) | _SHIFTL(d, 0, 8))}
 
-#define CMD_BBH(a, b, c) (_SHIFTL(a, 24, 8) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 0, 16))
+#define CMD_BBH(a, b, c) {(_SHIFTL(a, 24, 8) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 0, 16))}
 
-#define CMD_HBB(a, b, c) (_SHIFTL(a, 16, 16) | _SHIFTL(b, 8, 8) | _SHIFTL(c, 0, 8))
+#define CMD_HBB(a, b, c) {(_SHIFTL(a, 16, 16) | _SHIFTL(b, 8, 8) | _SHIFTL(c, 0, 8))}
 
-#define CMD_HH(a, b) (_SHIFTL(a, 16, 16) | _SHIFTL(b, 0, 16))
+#define CMD_HH(a, b) {(_SHIFTL(a, 16, 16) | _SHIFTL(b, 0, 16))}
 
-#define CMD_W(a) (a)
+#define CMD_W(a) {(a)}
 
-#define CMD_PTR(a) (u32)(a)
+#ifdef __GNUC__
+#define CMD_F(a) {.f = (a)}
+#else
+#define CMD_F(a) {(a)}
+#endif
+
+#define CMD_PTR(a) {(u32)(a)}
 
 #endif
