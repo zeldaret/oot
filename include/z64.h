9977da5bd25ece7b70ea5eaa516e38aced4511a0
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-09-21 09:50:53 +0200

    Use defines instead of magic values in head/tail magic comments

--- include/z64.h
+++ include/z64.h
@@ -67,13 +67,13 @@ typedef struct {
 } GameInfo; // size = 0x15D4
 
 typedef struct {
-    /* 0x00000 */ u16 headMagic; // 1234
+    /* 0x00000 */ u16 headMagic; // GFXPOOL_HEAD_MAGIC
     /* 0x00008 */ Gfx polyOpaBuffer[0x17E0];
     /* 0x0BF08 */ Gfx polyXluBuffer[0x800];
     /* 0x0FF08 */ Gfx overlayBuffer[0x400];
     /* 0x11F08 */ Gfx workBuffer[0x80];
     /* 0x11308 */ Gfx unusedBuffer[0x20];
-    /* 0x12408 */ u16 tailMagic; // 5678
+    /* 0x12408 */ u16 tailMagic; // GFXPOOL_TAIL_MAGIC
 } GfxPool; // size = 0x12410
 
 typedef struct {
