19f9c31392cb0555300c4a864041588c1ae4f125
Roman971 <romanlasnier@hotmail.com>
2021-11-26 00:10:35 +0100

    Improve gfxprint documentation and strings

--- src/code/z_moji.c
+++ src/code/z_moji.c
@@ -82,7 +82,7 @@ void Moji_DrawChar(GraphicsContext* gfxCtx, char c) {
 /**
  * Does not work as is in most cases.
  * Can work if the render mode, combiner and possibly other settings are set correctly.
- * For example this works with the render mode used in `GfxPrint_InitDlist`,
+ * For example this works with the render mode used in `GfxPrint_Setup`,
  * and `G_CC_MODULATEI_PRIM` for both combiner cycles.
  */
 void Moji_DrawString(GraphicsContext* gfxCtx, const char* str) {
