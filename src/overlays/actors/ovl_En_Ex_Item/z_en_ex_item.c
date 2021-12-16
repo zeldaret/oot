88474d7cb5d35c7fd0e0eb09d548dbb5ee7553c8
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-12-16 17:42:23 +0100

    `sgiDrawIds` -> `giDrawIds`

--- src/overlays/actors/ovl_En_Ex_Item/z_en_ex_item.c
+++ src/overlays/actors/ovl_En_Ex_Item/z_en_ex_item.c
@@ -488,10 +488,10 @@ void EnExItem_DrawHeartPiece(EnExItem* this, GlobalContext* globalCtx) {
 }
 
 void EnExItem_DrawMagic(EnExItem* this, GlobalContext* globalCtx, s16 magicIndex) {
-    static s16 sgiDrawIds[] = { GID_DINS_FIRE, GID_FARORES_WIND, GID_NAYRUS_LOVE };
+    static s16 giDrawIds[] = { GID_DINS_FIRE, GID_FARORES_WIND, GID_NAYRUS_LOVE };
 
     func_8002ED80(&this->actor, globalCtx, 0);
-    GetItem_Draw(globalCtx, sgiDrawIds[magicIndex]);
+    GetItem_Draw(globalCtx, giDrawIds[magicIndex]);
 }
 
 void EnExItem_DrawKey(EnExItem* this, GlobalContext* globalCtx, s32 index) {
