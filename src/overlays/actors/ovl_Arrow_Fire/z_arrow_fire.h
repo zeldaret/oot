ab68e17b8d0cdb4f32c618bfd1a7ee57e80a246c
jw2476 <32795849+jw2476@users.noreply.github.com>
2021-12-16 21:18:11 +0000

    Fix type in z_arrow_fire.h

--- src/overlays/actors/ovl_Arrow_Fire/z_arrow_fire.h
+++ src/overlays/actors/ovl_Arrow_Fire/z_arrow_fire.h
@@ -12,7 +12,7 @@ typedef struct ArrowFire {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ Vec3f trailEnd;
     /* 0x0158 */ f32 trailLen;
-    /* 0x015C */ f32 hitAnimItensity;
+    /* 0x015C */ f32 hitAnimIntensity;
     /* 0x0160 */ ArrowFireActionFunc actionFunc;
     /* 0x0164 */ s16 radius;
     /* 0x0166 */ u16 timer;
