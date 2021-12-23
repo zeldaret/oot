5349b0bf39f2572f198ec54ba9b8e0186f9b27d3
Louis <louist103@pop-os.localdomain>
2021-12-23 16:11:06 -0500

    Change 2 ssize to size

--- src/code/code_800E6840.c
+++ src/code/code_800E6840.c
@@ -1,13 +1,13 @@
 #include "global.h"
 
-void Audio_InvalDCache(void* buf, ssize_t size) {
+void Audio_InvalDCache(void* buf, size_t size) {
     OSIntMask prevMask = osSetIntMask(1);
 
     osInvalDCache(buf, size);
     osSetIntMask(prevMask);
 }
 
-void Audio_WritebackDCache(void* buf, ssize_t size) {
+void Audio_WritebackDCache(void* buf, size_t size) {
     OSIntMask prevMask = osSetIntMask(1);
 
     osWritebackDCache(buf, size);
