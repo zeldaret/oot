4d660f93bc3342695ad8315cc02e9d696e7ba55c
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-01-14 20:30:41 +0100

    Make it more clear "CFB" means Color Frame Buffer

--- src/code/z_vismono.c
+++ src/code/z_vismono.c
@@ -4,7 +4,7 @@
 
 #include "global.h"
 
-// Height of the fragments the color frame buffer is split into.
+// Height of the fragments the color frame buffer (CFB) is split into.
 // It is the maximum amount of lines such that all rgba16 SCREEN_WIDTH-long lines fit into
 // the half of tmem (0x800 bytes) dedicated to color-indexed data.
 #define VISMONO_CFBFRAG_HEIGHT (0x800 / (SCREEN_WIDTH * G_IM_SIZ_16b_BYTES))
