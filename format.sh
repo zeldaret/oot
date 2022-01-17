9b67778a000940e00609146f7510738527d54818
EllipticEllipsis <73679967+EllipticEllipsis@users.noreply.github.com>
2022-01-17 00:43:07 +0000

    Building on Macs (#1086)
    * git subrepo pull (merge) tools/ZAPD
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "945e6ca1a"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "50242eca9"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * Fix extract_assets.py multithreading
    
    * Update binutils doc a bit
    
    * Remove * import, add multiprocessing option
    and way to pass arguments to ZAPD
    
    * Update format.sh to be more platform-independent
    
    * git subrepo pull --force tools/ZAPD
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "fd5a7f434"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "fd5a7f434"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * Remove ;
    
    * Update formatting script to not just use 11
    
    * Add Python requirements,
    move the Mac stuff in the README into its own doc
    
    * Fix readme link
    
    * Minor format thing
    
    * .
    
    * Move ZAPDArgs into its own function
    
    * Update readme and remove requirements.txt
    
    * Dragorn-inspired rewrite of processZAPDArgs
    
    * git subrepo pull --force tools/ZAPD
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "a0d3f7b68"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "a0d3f7b68"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * Fix function definition
    
    * Building docs overhaul
    
    * Add Python packages to Mac and Cygwin
    
    * Heading number
    
    * format format.sh (!)
    
    * Replace "currently"
    
    * Remove Debian
    
    * git subrepo pull (merge) --force tools/ZAPD
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "0ba781304"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "0ba781304"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"

--- format.sh
+++ format.sh
@@ -1,29 +1,57 @@
 #!/usr/bin/env bash
 
+FORMAT_VER="11"
 FORMAT_OPTS="-i -style=file"
 TIDY_OPTS="-p . --fix --fix-errors"
 COMPILER_OPTS="-fno-builtin -std=gnu90 -Iinclude -Isrc -D_LANGUAGE_C -DNON_MATCHING"
 
+# https://backreference.org/2010/05/23/sanitizing-files-with-no-trailing-newline/index.html
+# "gets the last character of the file pipes it into read, which will exit with
+# a nonzero exit code if it encounters EOF before newline (so, if the last 
+# character of the file isn't a newline). If read exits nonzero, then append a
+# newline onto the file using echo (if read exits 0, that satisfies the ||, so 
+# the echo command isn't run)." (https://stackoverflow.com/a/34865616)
+function add_final_newline () {
+    for file in "$@"
+    do
+        tail -c1 $file | read -r _ || echo >> $file
+    done
+}
+export -f add_final_newline
+
 shopt -s globstar
+
+if [ $(command -v clang-format) ]
+then
+    CLANG_FORMAT="clang-format"
+else
+    if [ $(command -v clang-format-${FORMAT_VER}) ]
+    then
+        CLANG_FORMAT="clang-format-${FORMAT_VER}"
+    else
+        echo "Neither clang-format nor clang-format-${FORMAT_VER} found. Exiting."
+        exit 1
+    fi
+fi
 
 if (( $# > 0 )); then
     echo "Formatting file(s) $*"
     echo "Running clang-format..."
-    clang-format ${FORMAT_OPTS} "$@"
+    ${CLANG_FORMAT} ${FORMAT_OPTS} "$@"
     echo "Running clang-tidy..."
     clang-tidy ${TIDY_OPTS} "$@" -- ${COMPILER_OPTS} &> /dev/null
     echo "Adding missing final new lines..."
-    sed -i -e '$a\' "$@"
+    add_final_newline "$@"
     echo "Done formatting file(s) $*"
     exit
 fi
 
 echo "Formatting C files. This will take a bit"
 echo "Running clang-format..."
-clang-format ${FORMAT_OPTS} src/**/*.c
+${CLANG_FORMAT} ${FORMAT_OPTS} src/**/*.c
 echo "Running clang-tidy..."
 clang-tidy ${TIDY_OPTS} src/**/*.c -- ${COMPILER_OPTS} &> /dev/null
 echo "Adding missing final new lines..."
-find src/ -type f -name "*.c" -exec sed -i -e '$a\' {} \;
-find assets/xml/ -type f -name "*.xml" -exec sed -i -e '$a\' {} \;
+find src/ -type f -name "*.c" -exec bash -c 'add_final_newline "$@"' bash {} +
+find assets/xml/ -type f -name "*.xml" -exec bash -c 'add_final_newline "$@"' bash {} +
 echo "Done formatting all files."
