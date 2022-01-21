b053e3d74832a2dcea5022e09db1609cf1c1ff25
MNGoldenEagle <17274702+MNGoldenEagle@users.noreply.github.com>
2022-01-20 22:18:12 -0600

    Split code audio tables out to a separate file

--- data/rsp.rodata.s
+++ data/rsp.rodata.s
@@ -9,18 +9,6 @@
 
 .balign 16
 
-glabel gSoundFontTable
-    .incbin "baserom.z64", 0xBCC270, 0x270
-
-glabel gSequenceFontTable
-    .incbin "baserom.z64", 0xBCC4E0, 0x1C0
-
-glabel gSequenceTable
-    .incbin "baserom.z64", 0xBCC6A0, 0x6F0
-
-glabel gSampleBankTable
-    .incbin "baserom.z64", 0xBCCD90, 0x80
-
 glabel rspAspMainDataStart
     .incbin "baserom.z64", 0xBCCE10, 0x2E0
 glabel rspAspMainDataEnd
