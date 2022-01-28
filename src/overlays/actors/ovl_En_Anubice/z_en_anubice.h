9ded0c6ecd1a55c8d209c76a64d77d8180d602d6
Milo van der Tier <milo@vdtier.nl>
2022-01-28 20:37:57 +0100

    Fix offset comment in EnAnubice header

--- src/overlays/actors/ovl_En_Anubice/z_en_anubice.h
+++ src/overlays/actors/ovl_En_Anubice/z_en_anubice.h
@@ -35,7 +35,7 @@ typedef struct EnAnubice {
     /* 0x0298 */ Vec3f home;
     /* 0x02A4 */ Vec3f knockbackRecoveryVelocity;
     /* 0x02B0 */ BgHidanCurtain* flameCircles[5];
-    /* 0x02C0 */ char unk_2C0[0x4];
+    /* 0x02C4 */ char unk_2C0[0x4];
     /* 0x02C8 */ ColliderCylinder col;
 } EnAnubice; // size = 0x0314
 
