af0b6717abf2e1dfe3deff23f27a802110eaa7f4
angie <angheloalf95@gmail.com>
2022-02-06 15:36:54 -0300

    force_structure_alignment on each Font buffer

--- include/z64.h
+++ include/z64.h
@@ -521,12 +521,22 @@ typedef enum {
 typedef struct {
     /* 0x0000 */ u32    msgOffset;
     /* 0x0004 */ u32    msgLength;
+    union {
     /* 0x0008 */ u8     charTexBuf[FONT_CHAR_TEX_SIZE * 120];
+    /* 0x0008 */ u64    force_structure_alignment_charTex;
+    };
+    union {
     /* 0x3C08 */ u8     iconBuf[FONT_CHAR_TEX_SIZE];
+    /* 0x3C08 */ u64    force_structure_alignment_icon;
+    };
+    union {
     /* 0x3C88 */ u8     fontBuf[FONT_CHAR_TEX_SIZE * 320];
+    /* 0x3C88 */ u64    force_structure_alignment_font;
+    };
     union {
     /* 0xDC88 */ char   msgBuf[1280];
     /* 0xDC88 */ u16    msgBufWide[640];
+    /* 0xDC88 */ u64    force_structure_alignment_msg;
     };
 } Font; // size = 0xE188
 
