deb09606c4fdd9f13484f43db906bc6c9ca2a493
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-02-19 17:53:55 +0100

    `bossLimbDl` -> `bossLimbDL`

--- src/overlays/actors/ovl_En_Goma/z_en_goma.h
+++ src/overlays/actors/ovl_En_Goma/z_en_goma.h
@@ -72,7 +72,7 @@ typedef struct EnGoma {
     /* 0x2F4 */ s32 unk_2F4;
     /* 0x2F8 */ s16 stunTimer;
     /* 0x2FC */ Vec3f shieldKnockbackVel;
-    /* 0x308 */ Gfx* bossLimbDl; // set by z_boss_goma
+    /* 0x308 */ Gfx* bossLimbDL; // set by z_boss_goma
     /* 0x30C */ ColliderCylinder colCyl1;
     /* 0x358 */ ColliderCylinder colCyl2;
 } EnGoma; // size = 0x03A4
