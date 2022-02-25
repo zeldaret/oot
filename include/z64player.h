e21f188551e467d08f2688a4aa4e785863dbda2c
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-02-25 12:12:53 +0100

    Move `PlayerSwordAnimation` from `z_en_torch2.c` to `z64player.h`

--- include/z64player.h
+++ include/z64player.h
@@ -151,6 +151,37 @@ typedef enum {
 } PlayerLimb;
 
 typedef enum {
+    /*  0 */ FORWARD_SLASH_1H,
+    /*  1 */ FORWARD_SLASH_2H,
+    /*  2 */ FORWARD_COMBO_1H,
+    /*  3 */ FORWARD_COMBO_2H,
+    /*  4 */ RIGHT_SLASH_1H,
+    /*  5 */ RIGHT_SLASH_2H,
+    /*  6 */ RIGHT_COMBO_1H,
+    /*  7 */ RIGHT_COMBO_2H,
+    /*  8 */ LEFT_SLASH_1H,
+    /*  9 */ LEFT_SLASH_2H,
+    /* 10 */ LEFT_COMBO_1H,
+    /* 11 */ LEFT_COMBO_2H,
+    /* 12 */ STAB_1H,
+    /* 13 */ STAB_2H,
+    /* 14 */ STAB_COMBO_1H,
+    /* 15 */ STAB_COMBO_2H,
+    /* 16 */ FLIPSLASH_START,
+    /* 17 */ JUMPSLASH_START,
+    /* 18 */ FLIPSLASH_FINISH,
+    /* 19 */ JUMPSLASH_FINISH,
+    /* 20 */ BACKSLASH_RIGHT,
+    /* 21 */ BACKSLASH_LEFT,
+    /* 22 */ HAMMER_FORWARD,
+    /* 23 */ HAMMER_SIDE,
+    /* 24 */ SPIN_ATTACK_1H,
+    /* 25 */ SPIN_ATTACK_2H,
+    /* 26 */ BIG_SPIN_1H,
+    /* 27 */ BIG_SPIN_2H
+} PlayerSwordAnimation;
+
+typedef enum {
     /* -1 */ PLAYER_DOORTYPE_AJAR = -1,
     /*  0 */ PLAYER_DOORTYPE_NONE,
     /*  1 */ PLAYER_DOORTYPE_HANDLE,
--- src/overlays/actors/ovl_En_Torch2/z_en_torch2.c
+++ src/overlays/actors/ovl_En_Torch2/z_en_torch2.c
@@ -16,37 +16,6 @@ typedef enum {
     /* 3 */ ENTORCH2_DAMAGE
 } EnTorch2ActionStates;
 
-typedef enum {
-    /*  0 */ FORWARD_SLASH_1H,
-    /*  1 */ FORWARD_SLASH_2H,
-    /*  2 */ FORWARD_COMBO_1H,
-    /*  3 */ FORWARD_COMBO_2H,
-    /*  4 */ RIGHT_SLASH_1H,
-    /*  5 */ RIGHT_SLASH_2H,
-    /*  6 */ RIGHT_COMBO_1H,
-    /*  7 */ RIGHT_COMBO_2H,
-    /*  8 */ LEFT_SLASH_1H,
-    /*  9 */ LEFT_SLASH_2H,
-    /* 10 */ LEFT_COMBO_1H,
-    /* 11 */ LEFT_COMBO_2H,
-    /* 12 */ STAB_1H,
-    /* 13 */ STAB_2H,
-    /* 14 */ STAB_COMBO_1H,
-    /* 15 */ STAB_COMBO_2H,
-    /* 16 */ FLIPSLASH_START,
-    /* 17 */ JUMPSLASH_START,
-    /* 18 */ FLIPSLASH_FINISH,
-    /* 19 */ JUMPSLASH_FINISH,
-    /* 20 */ BACKSLASH_RIGHT,
-    /* 21 */ BACKSLASH_LEFT,
-    /* 22 */ HAMMER_FORWARD,
-    /* 23 */ HAMMER_SIDE,
-    /* 24 */ SPIN_ATTACK_1H,
-    /* 25 */ SPIN_ATTACK_2H,
-    /* 26 */ BIG_SPIN_1H,
-    /* 27 */ BIG_SPIN_2H
-} PlayerSwordAnimation;
-
 void EnTorch2_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnTorch2_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnTorch2_Update(Actor* thisx, GlobalContext* globalCtx);
