9012970bb3eb1eda101188a556282c8425142ce0
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-02-26 12:10:27 +0100

    Run formatter

--- src/code/z_scene_table.c
+++ src/code/z_scene_table.c
@@ -2313,9 +2313,9 @@ void func_8009FE58(GlobalContext* globalCtx) {
 
         temp = 0.020000001f;
         View_SetDistortionOrientation(&globalCtx->view,
-                                 ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_CosS(D_8012A39C),
-                                 ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A39C),
-                                 ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A3A0));
+                                      ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_CosS(D_8012A39C),
+                                      ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A39C),
+                                      ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A3A0));
         View_SetDistortionScale(&globalCtx->view, 1.f + (0.79999995f * temp * Math_SinS(D_8012A3A0)),
                                 1.f + (0.39999998f * temp * Math_CosS(D_8012A3A0)),
                                 1.f + (1 * temp * Math_CosS(D_8012A39C)));
