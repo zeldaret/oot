71c356ffbe344c1505cd3ae3d5e43a217d39b592
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-02-28 13:20:08 +0000

    Rename functions and files

--- src/code/__osMemmove.c
+++ src/code/__osMemmove.c
@@ -1,7 +1,7 @@
 #include "global.h"
 
 // memmove used in __osMalloc.c
-void* func_801068B0(void* dst, void* src, size_t size) {
+void* __osMemmove(void* dst, void* src, size_t size) {
     u8* byteDst = dst;
     u8* byteSrc = src;
 
--- src/code/__osMemset.c
+++ src/code/__osMemset.c
@@ -1,7 +1,7 @@
 #include "global.h"
 
 // memset used in __osMalloc, z_quake, z_view, and z_camera
-void* func_80106860(void* ptr, s32 val, size_t size) {
+void* __osMemset(void* ptr, s32 val, size_t size) {
     u8* bytePtr = ptr;
 
     while (size--) {
