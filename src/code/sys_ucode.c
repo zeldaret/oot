bc67668b63761f210ce0726bda2b22b980e23e3e
Thar0 <17233964+Thar0@users.noreply.github.com>
2022-03-03 14:15:20 +0000

    Change capitalization of sys_ucode variables

--- src/code/sys_ucode.c
+++ src/code/sys_ucode.c
@@ -1,7 +1,7 @@
 #include "global.h"
 
-u64* sDefaultGSPUcodeText = gspF3DZEX2_NoN_PosLight_fifoTextStart;
-u64* sDefaultGSPUcodeData = gspF3DZEX2_NoN_PosLight_fifoDataStart;
+u64* sDefaultGSPUCodeText = gspF3DZEX2_NoN_PosLight_fifoTextStart;
+u64* sDefaultGSPUCodeData = gspF3DZEX2_NoN_PosLight_fifoDataStart;
 
 u64* SysUcode_GetUCodeBoot(void) {
     return rspbootTextStart;
@@ -12,9 +12,9 @@ size_t SysUcode_GetUCodeBootSize(void) {
 }
 
 u64* SysUcode_GetUCode(void) {
-    return sDefaultGSPUcodeText;
+    return sDefaultGSPUCodeText;
 }
 
 u64* SysUcode_GetUCodeData(void) {
-    return sDefaultGSPUcodeData;
+    return sDefaultGSPUCodeData;
 }
