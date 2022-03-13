f6904a231401f57c43459d9ed43ff055b7eb4bbf
louist103 <louist103@gmail.com>
2022-03-13 19:05:03 -0400

    pr fixes

--- include/libc/stdint.h
+++ include/libc/stdint.h
@@ -9,19 +9,19 @@ typedef unsigned int uintptr_t;
 #define INT32_MIN   (-0x80000000)
 #define INT64_MIN   (-0x8000000000000000)
 
-#define INT8_MAX    0x7f
-#define INT16_MAX   0x7fff
-#define INT32_MAX   0x7fffffff
-#define INT64_MAX   0x7fffffffffffffff
+#define INT8_MAX    0x7F
+#define INT16_MAX   0x7FFF
+#define INT32_MAX   0x7FFFFFFF
+#define INT64_MAX   0x7FFFFFFFffffffff
 
-#define UINT8_MAX   0xff
-#define UINT16_MAX  0xffff
-#define UINT32_MAX  0xffffffff
-#define UINT64_MAX  0xffffffffffffffff
+#define UINT8_MAX   0xFF
+#define UINT16_MAX  0xFFFF
+#define UINT32_MAX  0xFFFFFFFF
+#define UINT64_MAX  0xFFFFFFFFffffffff
 
 #define INTPTR_MIN  (-0x80000000)
-#define INTPTR_MAX  0x7fffffff
-#define UINTPTR_MAX 0xffffffff
+#define INTPTR_MAX  0x7FFFFFFF
+#define UINTPTR_MAX 0xFFFFFFFF
 
 
 #endif /* STDINT_H */
