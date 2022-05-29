b483a7d92f6cb3f5462de93fa6df26fa8d9c2a3f
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-05-29 22:39:36 +0200

    `SCENE_DRAW_CONFIG_` -> `SDC_`

--- include/z64scene.h
+++ include/z64scene.h
@@ -339,60 +339,60 @@ typedef enum {
 #undef DEFINE_ENTRANCE
 
 typedef enum {
-    /*  0 */ SCENE_DRAW_CONFIG_DEFAULT,
-    /*  1 */ SCENE_DRAW_CONFIG_SPOT00,
-    /*  2 */ SCENE_DRAW_CONFIG_SPOT01,
-    /*  3 */ SCENE_DRAW_CONFIG_SPOT03,
-    /*  4 */ SCENE_DRAW_CONFIG_SPOT04,
-    /*  5 */ SCENE_DRAW_CONFIG_SPOT06,
-    /*  6 */ SCENE_DRAW_CONFIG_SPOT07,
-    /*  7 */ SCENE_DRAW_CONFIG_SPOT08,
-    /*  8 */ SCENE_DRAW_CONFIG_SPOT09,
-    /*  9 */ SCENE_DRAW_CONFIG_SPOT10,
-    /* 10 */ SCENE_DRAW_CONFIG_SPOT11,
-    /* 11 */ SCENE_DRAW_CONFIG_SPOT12,
-    /* 12 */ SCENE_DRAW_CONFIG_SPOT13,
-    /* 13 */ SCENE_DRAW_CONFIG_SPOT15,
-    /* 14 */ SCENE_DRAW_CONFIG_SPOT16,
-    /* 15 */ SCENE_DRAW_CONFIG_SPOT17,
-    /* 16 */ SCENE_DRAW_CONFIG_SPOT18,
-    /* 17 */ SCENE_DRAW_CONFIG_SPOT20,
-    /* 18 */ SCENE_DRAW_CONFIG_HIDAN, // used for Fire Temple and its boss scene
-    /* 19 */ SCENE_DRAW_CONFIG_YDAN,
-    /* 20 */ SCENE_DRAW_CONFIG_DDAN,
-    /* 21 */ SCENE_DRAW_CONFIG_BDAN, // used for Jabu-Jabu and its boss scene
-    /* 22 */ SCENE_DRAW_CONFIG_BMORI1,
-    /* 23 */ SCENE_DRAW_CONFIG_MIZUSIN,
-    /* 24 */ SCENE_DRAW_CONFIG_HAKADAN, // used for Bottom Of The Well, Shadow Temple and its boss scene
-    /* 25 */ SCENE_DRAW_CONFIG_JYASINZOU,
-    /* 26 */ SCENE_DRAW_CONFIG_GANONTIKA,
-    /* 27 */ SCENE_DRAW_CONFIG_MEN,
-    /* 28 */ SCENE_DRAW_CONFIG_YDAN_BOSS,
-    /* 29 */ SCENE_DRAW_CONFIG_MIZUSIN_BS,
-    /* 30 */ SCENE_DRAW_CONFIG_TOKINOMA,
-    /* 31 */ SCENE_DRAW_CONFIG_KAKUSIANA,
-    /* 32 */ SCENE_DRAW_CONFIG_KENJYANOMA,
-    /* 33 */ SCENE_DRAW_CONFIG_GREAT_FAIRY_FOUNTAIN, // used for both Great Fairy Fountains scenes
-    /* 34 */ SCENE_DRAW_CONFIG_SYATEKIJYOU,
-    /* 35 */ SCENE_DRAW_CONFIG_HAIRAL_NIWA, // used for Castle Courtyard (with the hedges) (day, night and debug scenes)
-    /* 36 */ SCENE_DRAW_CONFIG_GANON_CASTLE_EXTERIOR, // used for two scenes
-    /* 37 */ SCENE_DRAW_CONFIG_ICE_DOUKUTO,
-    /* 38 */ SCENE_DRAW_CONFIG_GANON_FINAL,
-    /* 39 */ SCENE_DRAW_CONFIG_FAIRY_FOUNTAIN, // used for both Fairy Fountains scenes
-    /* 40 */ SCENE_DRAW_CONFIG_GERUDOWAY,
-    /* 41 */ SCENE_DRAW_CONFIG_BOWLING,
-    /* 42 */ SCENE_DRAW_CONFIG_HAKAANA_OUKE,
-    /* 43 */ SCENE_DRAW_CONFIG_HYLIA_LABO,
-    /* 44 */ SCENE_DRAW_CONFIG_SOUKO,
-    /* 45 */ SCENE_DRAW_CONFIG_MIHARIGOYA,
-    /* 46 */ SCENE_DRAW_CONFIG_MAHOUYA,
-    /* 47 */ SCENE_DRAW_CONFIG_CALM_WATER, // used for Castle Courtyard (with Zelda), Sacred Forest Meadow, and debug Jungle Gym
-    /* 48 */ SCENE_DRAW_CONFIG_GRAVE_EXIT_LIGHT_SHINING, // used for one grave scene and the Dampé race scene
-    /* 49 */ SCENE_DRAW_CONFIG_BESITU,
-    /* 50 */ SCENE_DRAW_CONFIG_TURIBORI,
-    /* 51 */ SCENE_DRAW_CONFIG_GANON_SONOGO,
-    /* 52 */ SCENE_DRAW_CONFIG_GANONTIKA_SONOGO,
-    /* 53 */ SCENE_DRAW_CONFIG_MAX
+    /*  0 */ SDC_DEFAULT,
+    /*  1 */ SDC_SPOT00,
+    /*  2 */ SDC_SPOT01,
+    /*  3 */ SDC_SPOT03,
+    /*  4 */ SDC_SPOT04,
+    /*  5 */ SDC_SPOT06,
+    /*  6 */ SDC_SPOT07,
+    /*  7 */ SDC_SPOT08,
+    /*  8 */ SDC_SPOT09,
+    /*  9 */ SDC_SPOT10,
+    /* 10 */ SDC_SPOT11,
+    /* 11 */ SDC_SPOT12,
+    /* 12 */ SDC_SPOT13,
+    /* 13 */ SDC_SPOT15,
+    /* 14 */ SDC_SPOT16,
+    /* 15 */ SDC_SPOT17,
+    /* 16 */ SDC_SPOT18,
+    /* 17 */ SDC_SPOT20,
+    /* 18 */ SDC_HIDAN, // used for Fire Temple and its boss scene
+    /* 19 */ SDC_YDAN,
+    /* 20 */ SDC_DDAN,
+    /* 21 */ SDC_BDAN, // used for Jabu-Jabu and its boss scene
+    /* 22 */ SDC_BMORI1,
+    /* 23 */ SDC_MIZUSIN,
+    /* 24 */ SDC_HAKADAN, // used for Bottom Of The Well, Shadow Temple and its boss scene
+    /* 25 */ SDC_JYASINZOU,
+    /* 26 */ SDC_GANONTIKA,
+    /* 27 */ SDC_MEN,
+    /* 28 */ SDC_YDAN_BOSS,
+    /* 29 */ SDC_MIZUSIN_BS,
+    /* 30 */ SDC_TOKINOMA,
+    /* 31 */ SDC_KAKUSIANA,
+    /* 32 */ SDC_KENJYANOMA,
+    /* 33 */ SDC_GREAT_FAIRY_FOUNTAIN, // used for both Great Fairy Fountains scenes
+    /* 34 */ SDC_SYATEKIJYOU,
+    /* 35 */ SDC_HAIRAL_NIWA, // used for Castle Courtyard (with the hedges) (day, night and debug scenes)
+    /* 36 */ SDC_GANON_CASTLE_EXTERIOR, // used for two scenes
+    /* 37 */ SDC_ICE_DOUKUTO,
+    /* 38 */ SDC_GANON_FINAL,
+    /* 39 */ SDC_FAIRY_FOUNTAIN, // used for both Fairy Fountains scenes
+    /* 40 */ SDC_GERUDOWAY,
+    /* 41 */ SDC_BOWLING,
+    /* 42 */ SDC_HAKAANA_OUKE,
+    /* 43 */ SDC_HYLIA_LABO,
+    /* 44 */ SDC_SOUKO,
+    /* 45 */ SDC_MIHARIGOYA,
+    /* 46 */ SDC_MAHOUYA,
+    /* 47 */ SDC_CALM_WATER, // used for Castle Courtyard (with Zelda), Sacred Forest Meadow, and debug Jungle Gym
+    /* 48 */ SDC_GRAVE_EXIT_LIGHT_SHINING, // used for one grave scene and the Dampé race scene
+    /* 49 */ SDC_BESITU,
+    /* 50 */ SDC_TURIBORI,
+    /* 51 */ SDC_GANON_SONOGO,
+    /* 52 */ SDC_GANONTIKA_SONOGO,
+    /* 53 */ SDC_MAX
 } SceneDrawConfig;
 
 // Scene commands
