051757253f6b7affb2014ddf8becf48ad6d6860c
Roman971 <romanlasnier@hotmail.com>
2022-06-12 19:08:13 +0200

    Prioritize using clang-tidy 6 and improve format.sh
    * Use clang-tidy-6.0 when available since it's better at applying 'readability-inconsistent-declaration-parameter-name'
    * Add -m32 and -Wno-everything to compiler options to avoid unwanted errors and warnings
    * Remove -fsyntax-only since it serves no purpose in the context of clang-tidy

--- format.sh
+++ format.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+# Clang-Format version and options (see .clang-format for rules applied)
+# Version 11 is used when available for more consistency between contributors
 FORMAT_VER="11"
 FORMAT_OPTS="-i -style=file"
+
+# Clang-Tidy version and options (see .clang-tidy for checks enabled)
+# Version 6 is used when available because it's better at finding issues with argument names
+TIDY_VER="6.0"
 TIDY_OPTS="-p . --fix --fix-errors"
-COMPILER_OPTS="-fno-builtin -fsyntax-only -std=gnu90 -Iinclude -Isrc -Ibuild -I. -D_LANGUAGE_C -DNON_MATCHING"
+
+# Compiler options used with Clang-Tidy
+# Normal warnings are disabled with -Wno-everything to focus only on tidying
+INCLUDES="-Iinclude -Isrc -Ibuild -I."
+DEFINES="-D_LANGUAGE_C -DNON_MATCHING"
+COMPILER_OPTS="-fno-builtin -std=gnu90 -m32 -Wno-everything ${INCLUDES} ${DEFINES}"
 
 # https://backreference.org/2010/05/23/sanitizing-files-with-no-trailing-newline/index.html
 # "gets the last character of the file pipes it into read, which will exit with
@@ -34,10 +45,17 @@ else
     fi
 fi
 
-if [ ! $(command -v clang-tidy) ]
+if [ $(command -v clang-tidy-${TIDY_VER}) ]
 then
-    echo "clang-tidy not found. Exiting."
-    exit 1
+    CLANG_TIDY="clang-tidy-${TIDY_VER}"
+else
+    if [ $(command -v clang-tidy) ]
+    then
+        CLANG_TIDY="clang-tidy"
+    else
+        echo "Neither clang-tidy nor clang-tidy-${TIDY_VER} found. Exiting."
+        exit 1
+    fi
 fi
 
 if (( $# > 0 )); then
@@ -45,7 +63,7 @@ if (( $# > 0 )); then
     echo "Running clang-format..."
     ${CLANG_FORMAT} ${FORMAT_OPTS} "$@"
     echo "Running clang-tidy..."
-    clang-tidy ${TIDY_OPTS} "$@" -- ${COMPILER_OPTS} &> /dev/null
+    ${CLANG_TIDY} ${TIDY_OPTS} "$@" -- ${COMPILER_OPTS} &> /dev/null
     echo "Adding missing final new lines..."
     add_final_newline "$@"
     echo "Done formatting file(s) $*"
@@ -56,7 +74,7 @@ echo "Formatting C files. This will take a bit"
 echo "Running clang-format..."
 ${CLANG_FORMAT} ${FORMAT_OPTS} src/**/*.c
 echo "Running clang-tidy..."
-clang-tidy ${TIDY_OPTS} src/**/*.c -- ${COMPILER_OPTS} &> /dev/null
+${CLANG_TIDY} ${TIDY_OPTS} src/**/*.c -- ${COMPILER_OPTS} &> /dev/null
 echo "Adding missing final new lines..."
 find src/ -type f -name "*.c" -exec bash -c 'add_final_newline "$@"' bash {} +
 find assets/xml/ -type f -name "*.xml" -exec bash -c 'add_final_newline "$@"' bash {} +
