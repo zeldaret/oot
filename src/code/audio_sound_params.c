05200464ada1aff2f3fe5f98630af478053bb468
engineer124 <47598039+engineer124@users.noreply.github.com>
2022-06-14 03:50:52 +1000

    Update src/code/audio_sound_params.c
    Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>

--- src/code/audio_sound_params.c
+++ src/code/audio_sound_params.c
@@ -3,8 +3,8 @@
 
 #define SFX_PARAMS(importance, distParam, randParam, flags)                                 \
     {                                                                                       \
-        importance, (((distParam) << SFX_PARAM_01_SHIFT) & SFX_PARAM_01_MASK) |             \
-                        (((randParam) << SFX_PARAM_67_SHIFT) & SFX_PARAM_67_MASK) | (flags) \
+        importance, ((((distParam) << SFX_PARAM_01_SHIFT) & SFX_PARAM_01_MASK) |          \
+                     (((randParam) << SFX_PARAM_67_SHIFT) & SFX_PARAM_67_MASK) | (flags)) \
     }
 
 SoundParams sEnemyBankParams[] = {
