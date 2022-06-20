98612df731bda672bb8cd345258e0147f06e2727
engineer124 <engineer124engineer124@gmail.com>
2022-06-21 08:10:15 +1000

    rename

--- src/code/z_sfx_source.c
+++ src/code/z_sfx_source.c
@@ -1,19 +1,19 @@
 #include "global.h"
 
 void SfxSource_InitAll(PlayState* play) {
-    SoundSource* sources = &play->soundSources[0];
+    SfxSource* sources = &play->sfxSources[0];
     s32 i;
 
     // clang-format off
-    for (i = 0; i < ARRAY_COUNT(play->soundSources); i++) { sources[i].countdown = 0; }
+    for (i = 0; i < ARRAY_COUNT(play->sfxSources); i++) { sources[i].countdown = 0; }
     // clang-format on
 }
 
 void SfxSource_UpdateAll(PlayState* play) {
-    SoundSource* source = &play->soundSources[0];
+    SfxSource* source = &play->sfxSources[0];
     s32 i;
 
-    for (i = 0; i < ARRAY_COUNT(play->soundSources); i++) {
+    for (i = 0; i < ARRAY_COUNT(play->sfxSources); i++) {
         if (source->countdown != 0) {
             if (DECR(source->countdown) == 0) {
                 Audio_StopSfxByPos(&source->projectedPos);
@@ -28,18 +28,18 @@ void SfxSource_UpdateAll(PlayState* play) {
 
 void SfxSource_PlaySfxAtFixedWorldPos(PlayState* play, Vec3f* worldPos, s32 duration, u16 sfxId) {
     s32 countdown;
-    SoundSource* source;
+    SfxSource* source;
     s32 smallestCountdown = 0xFFFF;
-    SoundSource* backupSource;
+    SfxSource* backupSource;
     s32 i;
 
-    source = &play->soundSources[0];
-    for (i = 0; i < ARRAY_COUNT(play->soundSources); i++) {
+    source = &play->sfxSources[0];
+    for (i = 0; i < ARRAY_COUNT(play->sfxSources); i++) {
         if (source->countdown == 0) {
             break;
         }
 
-        // Store the sound source with the smallest remaining countdown
+        // Store the sfx source with the smallest remaining countdown
         countdown = source->countdown;
         if (countdown < smallestCountdown) {
             smallestCountdown = countdown;
@@ -48,8 +48,8 @@ void SfxSource_PlaySfxAtFixedWorldPos(PlayState* play, Vec3f* worldPos, s32 dura
         source++;
     }
 
-    // If no sound source is available, replace the sound source with the smallest remaining countdown
-    if (i >= ARRAY_COUNT(play->soundSources)) {
+    // If no sfx source is available, replace the sfx source with the smallest remaining countdown
+    if (i >= ARRAY_COUNT(play->sfxSources)) {
         source = backupSource;
         Audio_StopSfxByPos(&source->projectedPos);
     }
