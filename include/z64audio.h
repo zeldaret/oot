5dda2f9ffc53b01d1eaeeb28da1ef94b46cb2e5c
engineer124 <47598039+engineer124@users.noreply.github.com>
2022-06-21 04:42:02 -0400

    audio_seqplayer.c cleanup (#1274)
    * Audio Seqplayer Cleanup
    
    * More
    
    * Edit description
    
    * PR Suggestions
    
    * while true
    
    * Cleanup function declarations
    
    * Extra space, oops
    
    * Small fix
    
    * PR Suggestions
    
    * tatums per beat
    
    * Edit comment

--- include/z64audio.h
+++ include/z64audio.h
@@ -8,6 +8,7 @@
 #define TATUMS_PER_BEAT 48
 
 #define IS_SEQUENCE_CHANNEL_VALID(ptr) ((u32)(ptr) != (u32)&gAudioContext.sequenceChannelNone)
+#define SEQ_NUM_CHANNELS 16
 
 #define MAX_CHANNELS_PER_BANK 3
 
