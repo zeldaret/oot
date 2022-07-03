b26e56dc4d2cfef4fd7cda117046cfce5d9cac32
engineer124 <engineer124engineer124@gmail.com>
2022-07-04 04:38:50 +1000

    Associate it with AudioMgr to separate it from the audio library

--- src/code/code_800C3C20.c
+++ src/code/code_800C3C20.c
@@ -4,7 +4,7 @@ u8 sSfxBankIds[] = {
     BANK_PLAYER, BANK_ITEM, BANK_ENV, BANK_ENEMY, BANK_SYSTEM, BANK_OCARINA, BANK_VOICE,
 };
 
-void Audio_StopAllSfx(void) {
+void AudioMgr_StopAllSfx(void) {
     u32 i;
 
     for (i = 0; (u32)(i < ARRAY_COUNT(sSfxBankIds)); i++) {
