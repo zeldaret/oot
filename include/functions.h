f2d4c3b10bed4f5b515f6396baf49e5c24eef684
engineer124 <47598039+engineer124@users.noreply.github.com>
2022-07-04 11:51:38 -0400

    Document code_800C3C20: Audio_StopAllSfx (#1309)
    * Document code_800C3C20
    
    * Associate it with AudioMgr to separate it from the audio library

--- include/functions.h
+++ include/functions.h
@@ -1557,7 +1557,7 @@ u32 THA_IsCrash(TwoHeadArena* tha);
 void THA_Init(TwoHeadArena* tha);
 void THA_Ct(TwoHeadArena* tha, void* ptr, u32 size);
 void THA_Dt(TwoHeadArena* tha);
-void func_800C3C20(void);
+void AudioMgr_StopAllSfx(void);
 void func_800C3C80(AudioMgr* audioMgr);
 void AudioMgr_HandleRetrace(AudioMgr* audioMgr);
 void AudioMgr_HandlePreNMI(AudioMgr* audioMgr);
