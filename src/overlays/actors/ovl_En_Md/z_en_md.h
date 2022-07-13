5b5db113e427b355361597cccaefd6e53f7533b8
Charles Averill <charlesaverill20@gmail.com>
2022-07-13 11:02:57 -0500

    Name EnMd limb indices

--- src/overlays/actors/ovl_En_Md/z_en_md.h
+++ src/overlays/actors/ovl_En_Md/z_en_md.h
@@ -6,6 +6,27 @@
 
 struct EnMd;
 
+typedef enum {
+    ENMD_LIMB_NONE,
+    ENMD_LIMB_ROOT,
+    ENMD_LIMB_WAIST,
+    ENMD_LIMB_LEFT_THIGH,
+    ENMD_LIMB_LEFT_LEG,
+    ENMD_LIMB_LEFT_FOOT,
+    ENMD_LIMB_RIGHT_THIGH,
+    ENMD_LIMB_RIGHT_LEG,
+    ENMD_LIMB_RIGHT_FOOT,
+    ENMD_LIMB_TORSO,
+    ENMD_LIMB_LEFT_UPPER_ARM,
+    ENMD_LIMB_LEFT_FOREARM,
+    ENMD_LIMB_LEFT_HAND,
+    ENMD_LIMB_RIGHT_UPPER_ARM,
+    ENMD_LIMB_RIGHT_FOREARM,
+    ENMD_LIMB_RIGHT_HAND,
+    ENMD_LIMB_HEAD,
+    ENMD_LIMB_MAX
+} EnMdLimb;
+
 typedef void (*EnMdActionFunc)(struct EnMd*, PlayState*);
 
 typedef struct EnMd {
@@ -22,10 +43,10 @@ typedef struct EnMd {
     /* 0x020E */ s16 eyeIdx;
     /* 0x0210 */ s16 alpha;
     /* 0x0212 */ s16 waypoint;
-    /* 0x0214 */ s16 unk_214[17];
-    /* 0x0236 */ s16 unk_236[17];
-    /* 0x0258 */ Vec3s jointTable[17];
-    /* 0x02BE */ Vec3s morphTable[17];
+    /* 0x0214 */ s16 unk_214[ENMD_LIMB_MAX];
+    /* 0x0236 */ s16 unk_236[ENMD_LIMB_MAX];
+    /* 0x0258 */ Vec3s jointTable[ENMD_LIMB_MAX];
+    /* 0x02BE */ Vec3s morphTable[ENMD_LIMB_MAX];
 } EnMd; // size = 0x0324
 
 #endif
