ad1d05493727cd76b0ec4b25dd8cc9c91dd38e21
Roman971 <romanlasnier@hotmail.com>
2022-07-18 22:19:19 +0200

    Use intptr types in overlays and dmadata

--- src/overlays/actors/ovl_player_actor/z_player.c
+++ src/overlays/actors/ovl_player_actor/z_player.c
@@ -9311,7 +9311,7 @@ void Player_Init(Actor* thisx, PlayState* play2) {
     Player_SetEquipmentData(play, this);
     this->prevBoots = this->currentBoots;
     Player_InitCommon(this, play, gPlayerSkelHeaders[((void)0, gSaveContext.linkAge)]);
-    this->giObjectSegment = (void*)(((u32)ZeldaArena_MallocDebug(0x3008, "../z_player.c", 17175) + 8) & ~0xF);
+    this->giObjectSegment = (void*)(((uintptr_t)ZeldaArena_MallocDebug(0x3008, "../z_player.c", 17175) + 8) & ~0xF);
 
     respawnFlag = gSaveContext.respawnFlag;
 
