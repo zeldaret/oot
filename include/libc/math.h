e545de1dde717aa7785384895dc0a19ab6efafc0
Thar0 <17233964+Thar0@users.noreply.github.com>
2022-12-18 22:35:59 +0000

    More libc header changes

--- include/libc/math.h
+++ include/libc/math.h
@@ -3,8 +3,37 @@
 
 #define M_PI 3.14159265358979323846f
 #define M_SQRT2 1.41421356237309504880f
-#define FLT_MAX 340282346638528859811704183484516925440.0f
+#define MAXFLOAT 3.40282347e+38f
 #define SHT_MAX 32767.0f
 #define SHT_MINV (1.0f / SHT_MAX)
+
+float floorf(float);
+double floor(double);
+long lfloorf(float);
+long lfloor(double);
+
+float ceilf(float);
+double ceil(double);
+long lceilf(float);
+long lceil(double);
+
+float truncf(float);
+double trunc(double);
+long ltruncf(float);
+long ltrunc(double);
+
+float nearbyintf(float);
+double nearbyint(double);
+long lnearbyintf(float);
+long lnearbyint(double);
+
+float roundf(float);
+double round(double);
+long lroundf(float);
+long lround(double);
+
+extern float qNaN0x3FFFFF;
+extern float qNaN0x10000;
+extern float sNaN0x3FFFFF;
 
 #endif
--- include/libc/string.h
+++ include/libc/string.h
@@ -10,7 +10,7 @@ void* memcpy(void*, const void*, size_t);
 
 // TODO move
 void bzero(void* __s, size_t __n);
-int bcmp(void* __sl, void* __s2, size_t __n);
-void* bcopy(void* __src, void* __dest, size_t __n);
+int bcmp(const void* __sl, const void* __s2, size_t __n);
+void bcopy(const void* __src, void* __dest, size_t __n);
 
 #endif
