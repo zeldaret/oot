618f2f20cf9d353640db9acc4999806b4fad9b91
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-12-30 14:44:52 +0100

    PauseBgPreRenderState back to z64.h

--- include/z64pause.h
+++ include/z64pause.h
@@ -7,14 +7,6 @@
 
 struct OcarinaStaff;
 
-typedef enum {
-    /* 0 */ PAUSE_BG_PRERENDER_OFF, // Inactive, do nothing.
-    /* 1 */ PAUSE_BG_PRERENDER_SETUP, // The current frame is only drawn for the purpose of serving as the pause background.
-    /* 2 */ PAUSE_BG_PRERENDER_PROCESS, // The previous frame was PAUSE_BG_PRERENDER_SETUP, now apply prerender filters.
-    /* 3 */ PAUSE_BG_PRERENDER_READY, // The pause background is ready to be used.
-    /* 4 */ PAUSE_BG_PRERENDER_MAX
-} PauseBgPreRenderState;
-
 #define PAUSE_ITEM_NONE 999
 
 #define PAUSE_CURSOR_PAGE_LEFT 10
