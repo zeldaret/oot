c1e948e3acc8b91d72042a07384f7e6ee77ee0c8
cadmic <cadmic24@gmail.com>
2023-11-26 10:12:22 -0800

    actor update function -> actor update

--- src/code/z_actor.c
+++ src/code/z_actor.c
@@ -3398,7 +3398,7 @@ Actor* func_80033684(PlayState* play, Actor* explosiveActor) {
  */
 void Actor_ChangeCategory(PlayState* play, ActorContext* actorCtx, Actor* actor, u8 actorCategory) {
     //! @bug Calling this function immediately moves an actor from one category list to the other.
-    //! So, if Actor_ChangeCategory is called during an actor update function, the inner loop in
+    //! So, if Actor_ChangeCategory is called during an actor update, the inner loop in
     //! Actor_UpdateAll will continue from the next actor in the new category, rather than the next
     //! actor in the old category. This will cause any actors after this one in the old category to
     //! be skipped over and not updated, and any actors in the new category to be updated more than
