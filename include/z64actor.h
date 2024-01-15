bfe874fa16f69dab6a660a2f22b8b13ca920804e
cadmic <cadmic24@gmail.com>
2024-01-15 11:19:40 -0800

    Remove actor padding in non-debug versions

--- include/z64actor.h
+++ include/z64actor.h
@@ -305,7 +305,9 @@ typedef struct Actor {
     /* 0x130 */ ActorFunc update; // Update Routine. Called by `Actor_UpdateAll`
     /* 0x134 */ ActorFunc draw; // Draw Routine. Called by `Actor_Draw`
     /* 0x138 */ ActorOverlay* overlayEntry; // Pointer to the overlay table entry for this actor
+#ifdef OOT_DEBUG
     /* 0x13C */ char dbgPad[0x10]; // Padding that only exists in the debug rom
+#endif
 } Actor; // size = 0x14C
 
 typedef enum {
