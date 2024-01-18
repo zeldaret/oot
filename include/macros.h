90a6268460e9032733c708eb00d124b291256be9
cadmic <cadmic24@gmail.com>
2024-01-18 05:47:22 -0800

    Test for __sgi instead of __GNUC__ for PRINTF macro (#1612)

--- include/macros.h
+++ include/macros.h
@@ -103,14 +103,14 @@
 #ifdef OOT_DEBUG
 #define PRINTF osSyncPrintf
 #else
-#ifdef __GNUC__
-#define PRINTF(format, ...) (void)0
-#else
+#ifdef __sgi /* IDO compiler */
 // IDO doesn't support variadic macros, but it merely throws a warning for the
 // number of arguments not matching the definition (warning 609) instead of
 // throwing an error. We suppress this warning and rely on GCC to catch macro
 // argument errors instead.
 #define PRINTF(args) (void)0
+#else
+#define PRINTF(format, ...) (void)0
 #endif
 #endif
 
