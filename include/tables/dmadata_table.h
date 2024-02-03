44419b803f17c23cbb0118e0ea4c466579f3e1c6
cadmic <cadmic24@gmail.com>
2024-02-02 17:34:04 -0800

    Typo

--- include/tables/dmadata_table.h
+++ include/tables/dmadata_table.h
@@ -2,7 +2,7 @@
  * Select dmadata table for version
  */
 #if !OOT_DEBUG || NON_MATCHING
-// For retails version and non-matching builds, dmadata is generated from the specfile segments
+// For retail versions and non-matching builds, dmadata is generated from the specfile segments
 #include "dmadata_table_spec.h"
 #else
 #include "dmadata_table_mqdbg.h"
