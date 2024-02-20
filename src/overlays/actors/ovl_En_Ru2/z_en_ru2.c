2b79710e4071a8d8aba96dad950df0729f6ad315
cadmic <cadmic24@gmail.com>
2024-02-19 20:26:34 -0800

    Match entity actors O-T

--- src/overlays/actors/ovl_En_Ru2/z_en_ru2.c
+++ src/overlays/actors/ovl_En_Ru2/z_en_ru2.c
@@ -137,6 +137,7 @@ s32 func_80AF26A0(EnRu2* this) {
     return params & 0xFF;
 }
 
+#if OOT_DEBUG
 void func_80AF26AC(EnRu2* this) {
     this->action = 7;
     this->drawConfig = 0;
@@ -164,6 +165,7 @@ void func_80AF26D0(EnRu2* this, PlayState* play) {
         }
     }
 }
+#endif
 
 void func_80AF2744(EnRu2* this, PlayState* play) {
     Actor_UpdateBgCheckInfo(play, &this->actor, 75.0f, 30.0f, 30.0f, UPDBGCHECKINFO_FLAG_2);
@@ -450,7 +452,9 @@ void func_80AF30AC(EnRu2* this, PlayState* play) {
 
 void func_80AF3144(EnRu2* this, PlayState* play) {
     func_80AF2F04(this, play);
+#if OOT_DEBUG
     func_80AF26D0(this, play);
+#endif
 }
 
 void func_80AF3174(EnRu2* this, PlayState* play) {
@@ -458,7 +462,9 @@ void func_80AF3174(EnRu2* this, PlayState* play) {
     EnRu2_UpdateSkelAnime(this);
     func_80AF2608(this);
     func_80AF2F58(this, play);
+#if OOT_DEBUG
     func_80AF26D0(this, play);
+#endif
 }
 
 void func_80AF31C8(EnRu2* this, PlayState* play) {
@@ -466,7 +472,9 @@ void func_80AF31C8(EnRu2* this, PlayState* play) {
     EnRu2_UpdateSkelAnime(this);
     func_80AF2608(this);
     func_80AF30AC(this, play);
+#if OOT_DEBUG
     func_80AF26D0(this, play);
+#endif
 }
 
 void func_80AF321C(EnRu2* this, PlayState* play) {
--- src/overlays/actors/ovl_En_Skj/z_en_skj.c
+++ src/overlays/actors/ovl_En_Skj/z_en_skj.c
@@ -1513,7 +1513,8 @@ void EnSkj_WaitForOfferResponse(EnSkj* this, PlayState* play) {
 
                 player->stateFlags3 |= PLAYER_STATE3_5; // makes player take ocarina out right away after closing box
                 this->actionFunc = EnSkj_SetupWaitForOcarina;
-            } break;
+                break;
+            }
             case 1: // no
                 this->actionFunc = EnSkj_CleanupOcarinaGame;
                 break;
