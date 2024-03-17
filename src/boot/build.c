ff817f4d6611c07e2770e4a8247395e6e7d2ab17
cadmic <cadmic24@gmail.com>
2024-03-17 11:39:58 -0700

    Match retail build date

--- src/boot/build.c
+++ src/boot/build.c
@@ -1,3 +1,10 @@
 const char gBuildTeam[] = "zelda@srd022j";
+
+// TODO: Use per-version preprocessor defines
+#if OOT_DEBUG // gc-eu-mq-dbg
 const char gBuildDate[] = "03-02-21 00:16:31";
+#else // gc-eu-mq
+const char gBuildDate[] = "03-02-21 20:37:19";
+#endif
+
 const char gBuildMakeOption[] = "";
