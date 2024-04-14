f643499462f8cf908ddf1ee77b5625ebaff2cf25
cadmic <cadmic24@gmail.com>
2024-04-14 14:51:32 -0700

    Match retail BSS ordering (#1927)
    * Match retail BSS ordering
    
    * Revert moving some global variables to headers
    
    * Adjust block numbers after header changes
    
    * Fix debug build
    
    * Overlay bss ordering
    
    * Fix BSS ordering after header changes
    
    * gc-eu-mq OK
    
    * Implement preprocessor for #pragma increment_block_number
    
    * Transfer usage comment from reencode.sh
    
    * Use temporary directory instead of temporary file
    
    * Move ColChkMassType back

--- src/code/z_camera.c
+++ src/code/z_camera.c
@@ -4,6 +4,10 @@
 #include "terminal.h"
 #include "overlays/actors/ovl_En_Horse/z_en_horse.h"
 
+// For retail BSS ordering, the block number of D_8015BD7C
+// must be between 88 and 123 inclusive.
+#pragma increment_block_number 30
+
 s16 Camera_RequestSettingImpl(Camera* camera, s16 requestedSetting, s16 flags);
 s32 Camera_RequestModeImpl(Camera* camera, s16 requestedMode, u8 forceModeChange);
 s32 Camera_UpdateWater(Camera* camera);
@@ -3627,6 +3631,8 @@ s32 Camera_KeepOn3(Camera* camera) {
     }
     return 1;
 }
+
+#pragma increment_block_number 100
 
 s32 Camera_KeepOn4(Camera* camera) {
     static Vec3f D_8015BD50;
