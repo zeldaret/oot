a1c7937dba74f265e0bdc07518784ce82a7fd0d7
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-07-18 01:32:55 +0200

    make format.py clean up extra whitespace, also in .h files (#1991)
    * make format.py clean up extra whitespace, also in .h files
    
    * fixup
    
    * cleanup whitespace msg
    
    * Update check_format.py to also check non-src files touched by format.py
    
    * format

--- include/z64actor.h
+++ include/z64actor.h
@@ -140,22 +140,22 @@ typedef struct {
     /* 0x18 */ Vec3f feetPos[2]; // Update by using `Actor_SetFeetPos` in PostLimbDraw
 } ActorShape; // size = 0x30
 
-// 
+//
 #define ACTOR_FLAG_0 (1 << 0)
 
-// 
+//
 #define ACTOR_FLAG_2 (1 << 2)
 
-// 
+//
 #define ACTOR_FLAG_3 (1 << 3)
 
-// 
+//
 #define ACTOR_FLAG_4 (1 << 4)
 
-// 
+//
 #define ACTOR_FLAG_5 (1 << 5)
 
-// 
+//
 #define ACTOR_FLAG_6 (1 << 6)
 
 // hidden or revealed by Lens of Truth (depending on room lensMode)
@@ -166,64 +166,64 @@ typedef struct {
 // Actor will retain this flag until `Actor_TalkOfferAccepted` is called or manually turned off by the actor
 #define ACTOR_FLAG_TALK (1 << 8)
 
-// 
+//
 #define ACTOR_FLAG_9 (1 << 9)
 
-// 
+//
 #define ACTOR_FLAG_10 (1 << 10)
 
-// 
+//
 #define ACTOR_FLAG_ENKUSA_CUT (1 << 11)
 
 // Actor will not shake when a quake occurs
 #define ACTOR_FLAG_IGNORE_QUAKE (1 << 12)
 
-// 
+//
 #define ACTOR_FLAG_13 (1 << 13)
 
-// 
+//
 #define ACTOR_FLAG_14 (1 << 14)
 
-// 
+//
 #define ACTOR_FLAG_15 (1 << 15)
 
-// 
+//
 #define ACTOR_FLAG_16 (1 << 16)
 
-// 
+//
 #define ACTOR_FLAG_17 (1 << 17)
 
-// 
+//
 #define ACTOR_FLAG_18 (1 << 18)
 
-// 
+//
 #define ACTOR_FLAG_19 (1 << 19)
 
-// 
+//
 #define ACTOR_FLAG_20 (1 << 20)
 
-// 
+//
 #define ACTOR_FLAG_21 (1 << 21)
 
 // ignores point lights but not directional lights (such as environment lights)
 #define ACTOR_FLAG_IGNORE_POINT_LIGHTS (1 << 22)
 
-// 
+//
 #define ACTOR_FLAG_23 (1 << 23)
 
-// 
+//
 #define ACTOR_FLAG_24 (1 << 24)
 
-// 
+//
 #define ACTOR_FLAG_25 (1 << 25)
 
-// 
+//
 #define ACTOR_FLAG_26 (1 << 26)
 
-// 
+//
 #define ACTOR_FLAG_27 (1 << 27)
 
-// 
+//
 #define ACTOR_FLAG_28 (1 << 28)
 
 #define COLORFILTER_GET_COLORINTENSITY(colorFilterParams) (((colorFilterParams) & 0x1F00) >> 5)
