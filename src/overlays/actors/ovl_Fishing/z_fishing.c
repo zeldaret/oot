65b4c9b4de6f2c6b5a46cb786a037d80ac415e66
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-08-01 20:09:59 +0200

    fix bss

--- src/overlays/actors/ovl_Fishing/z_fishing.c
+++ src/overlays/actors/ovl_Fishing/z_fishing.c
@@ -12,7 +12,7 @@
 #include "terminal.h"
 
 // For retail BSS ordering, the block number of sStreamSfxProjectedPos must be 0.
-#pragma increment_block_number 206
+#pragma increment_block_number 202
 
 #define FLAGS ACTOR_FLAG_4
 
