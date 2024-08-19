40bd0db213f22e55e768f8ac8c41192be0d16461
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-08-19 21:10:40 +0200

    add n64dd segment using baserom

--- src/code/code_n64dd_800AD410.c
+++ src/code/code_n64dd_800AD410.c
@@ -1,12 +1,19 @@
 #include "ultra64.h"
 #include "z64dma.h"
+#include "segment_symbols.h"
 #include "n64dd.h"
+
+DECLARE_SEGMENT(n64dd)
+DECLARE_ROM_SEGMENT(n64dd)
+DECLARE_BSS_SEGMENT(n64dd)
 
 s8 func_801C7740_unknown(void);
 
 void func_800ADA80(void) {
     if (!B_80121AE0) {
-        DmaMgr_RequestSync(func_801C7740_unknown, (uintptr_t)D_00B8ADA0, D_00B9DA70 - D_00B8ADA0);
+        DmaMgr_RequestSync(_n64ddSegmentStart, (uintptr_t)_n64ddSegmentRomStart,
+                           _n64ddSegmentRomEnd - _n64ddSegmentRomStart);
+        // TODO use _n64ddSegmentBssStart, _n64ddSegmentBssEnd
         bzero(&D_801DA410, &D_801E8090 - &D_801DA410);
         B_80121AE0 = true;
         B_80121AE1 = func_801C7740_unknown();
