24d1cf36bb787a18b75ed25263b6e1899add9b36
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-08-21 16:52:07 +0200

    bss

--- src/overlays/actors/ovl_Fishing/z_fishing.c
+++ src/overlays/actors/ovl_Fishing/z_fishing.c
@@ -14,7 +14,7 @@
 #include "cic6105.h"
 #endif
 
-#pragma increment_block_number "gc-eu:167 gc-eu-mq:165 gc-jp:167 gc-jp-ce:167 gc-jp-mq:167 gc-us:167 gc-us-mq:167"
+#pragma increment_block_number "gc-eu:165 gc-eu-mq:165 gc-jp:167 gc-jp-ce:167 gc-jp-mq:167 gc-us:167 gc-us-mq:167"
 
 #define FLAGS ACTOR_FLAG_4
 
