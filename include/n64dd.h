365d2dd518b9a087b80f1a4f5b51573e9de7aa08
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-08-23 14:39:00 +0200

    Match z_kaleido_debug.c, z_lmap_mark.c

--- include/n64dd.h
+++ include/n64dd.h
@@ -1,6 +1,7 @@
 #ifndef N64DD_H
 #define N64DD_H
 
+#include "z64pause.h"
 #include "z64scene.h"
 
 struct RegEditor;
@@ -22,7 +23,9 @@ struct PlayState;
 typedef struct n64ddStruct_80121AF0 {
     void (*unk_00)(n64ddStruct_800FF4B0_pointers*, struct n64ddStruct_80121AF0*);
     void (*unk_04)(void);
-    char unk_08[0x34];
+    char unk_08[0x2C];
+    void (*unk_34)(PauseMapMarksData**);
+    void (*unk_38)(PauseMapMarksData**);
     void (*unk_3C)(void);
     void (*unk_40)(void);
     char unk_44[0x28];
