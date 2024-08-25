0546db8651f43ddd3eef858545047def022268a8
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-08-25 17:57:10 +0200

    bss

--- src/boot/z_std_dma.c
+++ src/boot/z_std_dma.c
@@ -26,8 +26,6 @@
 
 #pragma increment_block_number "gc-eu:128 gc-eu-mq:128 gc-jp:128 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128"
 
-#pragma increment_block_number "gc-eu:128 gc-eu-mq:128 gc-jp:128 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128"
-
 StackEntry sDmaMgrStackInfo;
 OSMesgQueue sDmaMgrMsgQueue;
 OSMesg sDmaMgrMsgBuf[32];
