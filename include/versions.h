887326005a359a368e7c98be3b3937a0c3031e6e
cadmic <cadmic24@gmail.com>
2024-08-25 17:51:10 -0700

    Add all N64 ROMs to versions.h

--- include/versions.h
+++ include/versions.h
@@ -2,14 +2,18 @@
 #define VERSIONS_H
 
 // OOT versions in build order
-#define OOT_NTSC_1_2 1
-#define OOT_GC_JP 2
-#define OOT_GC_JP_MQ 3
-#define OOT_GC_US 4
-#define OOT_GC_US_MQ 5
-#define OOT_GC_EU_MQ_DBG 6
-#define OOT_GC_EU 7
-#define OOT_GC_EU_MQ 8
-#define OOT_GC_JP_CE 9
+#define OOT_NTSC_1_0 1
+#define OOT_NTSC_1_1 2
+#define OOT_PAL_1_0 3
+#define OOT_NTSC_1_2 4
+#define OOT_PAL_1_1 5
+#define OOT_GC_JP 6
+#define OOT_GC_JP_MQ 7
+#define OOT_GC_US 8
+#define OOT_GC_US_MQ 9
+#define OOT_GC_EU_MQ_DBG 10
+#define OOT_GC_EU 11
+#define OOT_GC_EU_MQ 12
+#define OOT_GC_JP_CE 13
 
 #endif
