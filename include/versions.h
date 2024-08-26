c9e74274db64a9ac2d554dcd65d4397170f51830
cadmic <cadmic24@gmail.com>
2024-08-25 18:11:43 -0700

    Make it pretty

--- include/versions.h
+++ include/versions.h
@@ -2,18 +2,18 @@
 #define VERSIONS_H
 
 // OOT versions in build order
-#define NTSC_1_0 1
-#define NTSC_1_1 2
-#define PAL_1_0 3
-#define NTSC_1_2 4
-#define PAL_1_1 5
-#define GC_JP 6
-#define GC_JP_MQ 7
-#define GC_US 8
-#define GC_US_MQ 9
-#define GC_EU_MQ_DBG 10
-#define GC_EU 11
-#define GC_EU_MQ 12
-#define GC_JP_CE 13
+#define NTSC_1_0       1
+#define NTSC_1_1       2
+#define PAL_1_0        3
+#define NTSC_1_2       4
+#define PAL_1_1        5
+#define GC_JP          6
+#define GC_JP_MQ       7
+#define GC_US          8
+#define GC_US_MQ       9
+#define GC_EU_MQ_DBG  10
+#define GC_EU         11
+#define GC_EU_MQ      12
+#define GC_JP_CE      13
 
 #endif
