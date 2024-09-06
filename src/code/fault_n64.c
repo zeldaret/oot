d46c61b811e5d76e5bdc55bb3db6047afdd1a294
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-09-06 10:18:24 +0200

    push fault key combo T()

--- src/code/fault_n64.c
+++ src/code/fault_n64.c
@@ -449,11 +449,14 @@ void Fault_WaitForButtonCombo(void) {
     s32 count;
     s32 pad[4];
 
-    // "KeyWaitB (LRZ Up Down Up Down Left Left Right Right B A START)"
-    osSyncPrintf(
-        VT_FGCOL(WHITE) "KeyWaitB (ＬＲＺ " VT_FGCOL(WHITE) "上" VT_FGCOL(YELLOW) "下 " VT_FGCOL(YELLOW) "上" VT_FGCOL(WHITE) "下 " VT_FGCOL(WHITE) "左" VT_FGCOL(
-            YELLOW) "左 " VT_FGCOL(YELLOW) "右" VT_FGCOL(WHITE) "右 " VT_FGCOL(GREEN) "Ｂ" VT_FGCOL(BLUE) "Ａ" VT_FGCOL(RED) "START" VT_FGCOL(WHITE) ")" VT_RST
-                                                                                                                                                     "\n");
+    // "KeyWaitB (L R Z Up Down Up Down Left Left Right Right B A START)"
+    osSyncPrintf(VT_FGCOL(WHITE) T("KeyWaitB (ＬＲＺ ", "KeyWaitB (L R Z ") VT_FGCOL(WHITE) T("上", "Up ")
+                     VT_FGCOL(YELLOW) T("下 ", "Down ") VT_FGCOL(YELLOW) T("上", "Up ") VT_FGCOL(WHITE)
+                         T("下 ", "Down ") VT_FGCOL(WHITE) T("左", "Left ") VT_FGCOL(YELLOW) T("左 ", "Left ")
+                             VT_FGCOL(YELLOW) T("右", "Right ") VT_FGCOL(WHITE) T("右 ", "Right ") VT_FGCOL(GREEN)
+                                 T("Ｂ", "B ") VT_FGCOL(BLUE) T("Ａ", "A ")
+                                     VT_FGCOL(RED) "START" VT_FGCOL(WHITE) ")" VT_RST "\n");
+
     x = 0;
     y = 0;
     count = 0;
