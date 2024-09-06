5734608e3005db7f56a4dbd9ff2b96a4d3e09bb3
cadmic <cadmic24@gmail.com>
2024-09-05 18:40:12 -0700

    Fix English n64dd error textures

--- src/n64dd/n64dd_error_textures.c
+++ src/n64dd/n64dd_error_textures.c
@@ -7,7 +7,7 @@ u64 gN64DDError41Texs[2][0x600 / sizeof(u64)] = {
 #include "assets/n64dd/error_textures/n64dd_error_41_jpn.i4.inc.c"
     },
     {
-#include "assets/n64dd/error_textures/n64dd_error_41_jpn.i4.inc.c"
+#include "assets/n64dd/error_textures/n64dd_error_41_eng.i4.inc.c"
     },
 };
 
@@ -16,7 +16,7 @@ u64 gN64DDPleaseReadManualTexs[2][0x2800 / sizeof(u64)] = {
 #include "assets/n64dd/error_textures/n64dd_please_read_manual_jpn.i4.inc.c"
     },
     {
-#include "assets/n64dd/error_textures/n64dd_please_read_manual_jpn.i4.inc.c"
+#include "assets/n64dd/error_textures/n64dd_please_read_manual_eng.i4.inc.c"
     },
 };
 
