62b30e2ebe285d78012c053a157068a4c55ab22b
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-09-07 12:30:55 +0200

    Move `Graph_Alloc` and open/close Disps to `gfx.h` (#1976)
    * move graph_alloc and open/close Disps to gfx.h
    
    * zcolchk bss reeeeeee
    
    * fix matching gc-eu-mq (bss)
    
    * remove __gfxCtx extern
    
    * revert makefile -Werror=implicit-function-declaration
    
    * bss

--- src/overlays/actors/ovl_Fishing/z_fishing.c
+++ src/overlays/actors/ovl_Fishing/z_fishing.c
@@ -14,7 +14,7 @@
 #include "cic6105.h"
 #endif
 
-#pragma increment_block_number "gc-eu:147 gc-eu-mq:147 gc-jp:149 gc-jp-ce:149 gc-jp-mq:149 gc-us:149 gc-us-mq:149"
+#pragma increment_block_number "gc-eu:149 gc-eu-mq:149 gc-jp:151 gc-jp-ce:151 gc-jp-mq:151 gc-us:151 gc-us-mq:151"
 
 #define FLAGS ACTOR_FLAG_4
 
