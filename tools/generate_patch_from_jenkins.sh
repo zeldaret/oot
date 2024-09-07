a5d5c1f8658852ffd6c861f1429957efd93298da
cadmic <cadmic24@gmail.com>
2024-09-07 16:00:32 -0700

    Run fix_bss.py in CI and output a patch

---
+++ tools/generate_patch_from_jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+PATCH=$(git diff | base64 -w 0)
+if [ -n "$PATCH" ]; then
+    echo "Jenkins made some changes to your PR. To apply these fixes, copy and run"
+    echo "the following command (triple-click to select the entire line):"
+    echo
+    echo "echo -n $PATCH | base64 -d | git apply -"
+    echo
+fi
