a30f130eb9e48d55d8d7c935fb708af1156fee42
cadmic <cadmic24@gmail.com>
2024-09-07 05:10:52 -0700

    Fix some boot and code data (#2155)

--- src/boot/z_std_dma.c
+++ src/boot/z_std_dma.c
@@ -33,8 +33,11 @@ OSMesgQueue sDmaMgrMsgQueue;
 OSMesg sDmaMgrMsgBuf[32];
 OSThread sDmaMgrThread;
 STACK(sDmaMgrStack, 0x500);
+
+#if OOT_DEBUG
 const char* sDmaMgrCurFileName;
 s32 sDmaMgrCurFileLine;
+#endif
 
 u32 gDmaMgrVerbose = 0;
 size_t gDmaMgrDmaBuffSize = DMAMGR_DEFAULT_BUFSIZE;
