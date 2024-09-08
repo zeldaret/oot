acaa1d0004ad6b228be105bab50c5fbedebb752e
cadmic <cadmic24@gmail.com>
2024-09-07 18:53:14 -0700

    Wording tweaks

--- tools/generate_patch_from_jenkins.sh
+++ tools/generate_patch_from_jenkins.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 PATCH=$(git diff | base64 -w 0)
 if [ -n "$PATCH" ]; then
-    echo "Jenkins made some changes to your PR. To apply these fixes, copy and run"
-    echo "the following command (triple-click to select the entire line):"
+    echo "Jenkins made some fixes to your PR. To apply these changes to your working directory,"
+    echo "copy and run the following command (triple-click to select the entire line):"
     echo
     echo "echo -n $PATCH | base64 -d | git apply -"
     echo
