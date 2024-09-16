7335c03a46140940d2aafd115a193246d997ab87
fig02 <fig02srl@gmail.com>
2024-09-16 17:23:14 -0400

    PLAYER_STATE1_DEAD

--- src/overlays/actors/ovl_Door_Shutter/z_door_shutter.c
+++ src/overlays/actors/ovl_Door_Shutter/z_door_shutter.c
@@ -906,7 +906,7 @@ void DoorShutter_Update(Actor* thisx, PlayState* play) {
     DoorShutter* this = (DoorShutter*)thisx;
     Player* player = GET_PLAYER(play);
 
-    if (!(player->stateFlags1 & (PLAYER_STATE1_6 | PLAYER_STATE1_7 | PLAYER_STATE1_10 | PLAYER_STATE1_28)) ||
+    if (!(player->stateFlags1 & (PLAYER_STATE1_6 | PLAYER_STATE1_DEAD | PLAYER_STATE1_10 | PLAYER_STATE1_28)) ||
         (this->actionFunc == DoorShutter_WaitForObject)) {
         this->actionFunc(this, play);
     }
