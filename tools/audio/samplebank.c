effd26ec5562cd7f41ea6d62ab3994299a8720a1
cadmic <cadmic24@gmail.com>
2024-10-01 21:14:42 -0700

    Fix build on certain Linux distributions

--- tools/audio/samplebank.c
+++ tools/audio/samplebank.c
@@ -7,6 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 #include <assert.h>
+#include <stdlib.h>
 
 #include "xml.h"
 #include "samplebank.h"
--- tools/audio/samplebank_compiler.c
+++ tools/audio/samplebank_compiler.c
@@ -8,6 +8,7 @@
  */
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "xml.h"
