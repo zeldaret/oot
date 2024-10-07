62144af6b88abd0b933b31760fd551d05ea0bea2
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-10-07 11:04:09 +0200

    doc fixup ish in KaleidoScope_SetPageVertices

--- src/overlays/misc/ovl_kaleido_scope/z_kaleido_scope.c
+++ src/overlays/misc/ovl_kaleido_scope/z_kaleido_scope.c
@@ -2581,9 +2581,9 @@ s16 KaleidoScope_SetPageVertices(PlayState* play, Vtx* vtx, s16 vtxPage, s16 num
         }
 
         if (vtxPage == VTX_PAGE_MAP_WORLD) {
-            // For world map page, initialize vtx beyond VTX_PAGE_MAP_WORLD_QUADS
+            // WORLD_MAP_QUAD_TRADE_QUEST_MARKER
 
-            bufI -= 12;
+            bufI -= ((VTX_PAGE_MAP_WORLD_QUADS - WORLD_MAP_QUAD_TRADE_QUEST_MARKER) * 4);
 
             j = gSaveContext.worldMapArea;
 
@@ -2597,7 +2597,7 @@ s16 KaleidoScope_SetPageVertices(PlayState* play, Vtx* vtx, s16 vtxPage, s16 num
 
             vtx[bufI + 2].v.ob[1] = vtx[bufI + 3].v.ob[1] = vtx[bufI + 0].v.ob[1] - sVtxMapWorldAreaHeight[j];
 
-            bufI += 12;
+            bufI += ((VTX_PAGE_MAP_WORLD_QUADS - WORLD_MAP_QUAD_TRADE_QUEST_MARKER) * 4);
 
             if (pauseCtx->tradeQuestMarker != TRADE_QUEST_MARKER_NONE) {
                 if (sTradeQuestMarkerBobTimer == 0) {
