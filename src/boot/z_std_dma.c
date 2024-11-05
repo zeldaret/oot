b8953fd0b901f13af7bc6a2f0a08d1e102c9f858
feacur <feacur@gmail.com>
2024-11-05 13:08:27 +0100

    apply Jenkins fixes
    https://jenkins.deco.mp/job/OOT/job/PR-2286/4/console
    
    hmm, i do need this automation locally to

--- src/boot/z_std_dma.c
+++ src/boot/z_std_dma.c
@@ -27,7 +27,7 @@
 #endif
 
 #pragma increment_block_number "gc-eu:128 gc-eu-mq:128 gc-jp:128 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128" \
-                               "ntsc-1.2:92 pal-1.0:90 pal-1.1:90"
+                               "ntsc-1.2:88 pal-1.0:86 pal-1.1:86"
 
 StackEntry sDmaMgrStackInfo;
 OSMesgQueue sDmaMgrMsgQueue;
