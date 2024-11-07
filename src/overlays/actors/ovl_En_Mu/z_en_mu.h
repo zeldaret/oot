e2578f456e239b8a84e59d10fe8911b646fbad9a
feacur <feacur@gmail.com>
2024-11-07 23:13:43 +0100

    remove an unused `struct EnMu` field @ `0x024A`
    a continuation to the 211263295cd8768d63c1426025cf913ba6bedc18
    automatic padding commpensates its absence

--- src/overlays/actors/ovl_En_Mu/z_en_mu.h
+++ src/overlays/actors/ovl_En_Mu/z_en_mu.h
@@ -17,7 +17,6 @@ typedef struct EnMu {
     /* 0x0208 */ u16 defaultTextId;
     /* 0x020A */ s16 fidgetTableY[16];
     /* 0x022A */ s16 fidgetTableZ[16];
-    /* 0x024A */ s16 unk_24A;
 } EnMu; // size = 0x024C
 
 #endif
