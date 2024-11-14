35c635781dd1137d19312cc4e256040c4c6c8ee1
feacur <feacur@gmail.com>
2024-11-14 19:25:50 +0100

    make the comment multiline "officially"

--- include/z64actor.h
+++ include/z64actor.h
@@ -14,9 +14,10 @@
 #define MASS_IMMOVABLE 0xFF // Cannot be pushed by OC colliders
 #define MASS_HEAVY 0xFE // Can only be pushed by OC colliders from actors with IMMOVABLE or HEAVY mass.
 
-// These constants are the default parameters for the ubiqutous "fidgeting" formulas.
-// Some actors call `Actor_UpdateFidgetTables` function and cache their results in `fidgetTable*` fields.
-// Others compute them on the fly. Both variants are applied inside `*_OverrideLimbDraw` as input angles.
+/* These constants are the default parameters for the ubiqutous "fidgeting" formulas.
+ * Some actors call `Actor_UpdateFidgetTables` function and cache their results in `fidgetTable*` fields.
+ * Others compute them on the fly. Both variants are applied inside `*_OverrideLimbDraw` as input angles.
+ */
 #define FIDGET_FREQ_Y 0x814
 #define FIDGET_FREQ_Z 0x940
 #define FIDGET_FREQ_LIMB 0x32
