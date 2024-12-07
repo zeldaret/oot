6735eb25d6e568a4fc74ea3688dabee8422775eb
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2024-12-07 17:01:21 -0500

    Rename EVENTCHKINF_02 (#2322)
    * Rename EVENTCHKINF_02
    
    * Rename to EVENTCHKINF_MIDO_DENIED_DEKU_TREE_ACCESS

--- include/z64save.h
+++ include/z64save.h
@@ -464,7 +464,7 @@ typedef enum LinkAge {
  * SaveContext.eventChkInf
  */
 
-#define EVENTCHKINF_02 0x02
+#define EVENTCHKINF_MIDO_DENIED_DEKU_TREE_ACCESS 0x02
 #define EVENTCHKINF_03 0x03
 #define EVENTCHKINF_04 0x04
 #define EVENTCHKINF_05 0x05
