e9b165b6e556ced73f708f4846a1698dc3f32d55
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2024-12-08 20:45:02 -0500

    Add names for Medallion cutscene data (#2319)
    * Add names for Medallion cutscene data
    
    * Rename to Cs at end

--- src/overlays/actors/ovl_Demo_Du/z_demo_du.c
+++ src/overlays/actors/ovl_Demo_Du/z_demo_du.c
@@ -201,7 +201,7 @@ void DemoDu_CsFireMedallion_AdvanceTo01(DemoDu* this, PlayState* play) {
         Player* player = GET_PLAYER(play);
 
         this->updateIndex = CS_FIREMEDALLION_SUBSCENE(1);
-        play->csCtx.script = D_8096C1A4;
+        play->csCtx.script = gFireMedallionCs;
         gSaveContext.cutsceneTrigger = 2;
         Item_Give(play, ITEM_MEDALLION_FIRE);
 
