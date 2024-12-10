769c7f9fced1cdd596e1ecb6b03926bdbbeed1be
JordanLongstaff <jrdnlngstff@gmail.com>
2024-12-09 22:27:22 -0500

    Renamed to Nabooru Knuckle

--- src/overlays/actors/ovl_En_Ik/z_en_ik.c
+++ src/overlays/actors/ovl_En_Ik/z_en_ik.c
@@ -1528,7 +1528,7 @@ void EnIk_StartDefeatCutscene(Actor* thisx, PlayState* play) {
         Cutscene_SetScript(play, gSpiritBossNabooruKnuckleDefeatCs);
         gSaveContext.cutsceneTrigger = 1;
         Actor_SetScale(&this->actor, 0.01f);
-        SET_EVENTCHKINF(EVENTCHKINF_DEFEATED_NABOORU);
+        SET_EVENTCHKINF(EVENTCHKINF_DEFEATED_NABOORU_KNUCKLE);
         EnIk_SetupCsAction3(this, play);
     }
 }
@@ -1537,7 +1537,8 @@ void EnIk_Init(Actor* thisx, PlayState* play) {
     EnIk* this = (EnIk*)thisx;
     s32 upperParams = IK_GET_UPPER_PARAMS(&this->actor);
 
-    if (((IK_GET_ARMOR_TYPE(&this->actor) == IK_TYPE_NABOORU) && GET_EVENTCHKINF(EVENTCHKINF_DEFEATED_NABOORU)) ||
+    if (((IK_GET_ARMOR_TYPE(&this->actor) == IK_TYPE_NABOORU) &&
+        GET_EVENTCHKINF(EVENTCHKINF_DEFEATED_NABOORU_KNUCKLE)) ||
         (upperParams != 0 && Flags_GetSwitch(play, upperParams >> 8))) {
         Actor_Kill(&this->actor);
     } else {
