ce238ea8683c2a9c3d7752eac3903aa5b9655163
JordanLongstaff <jrdnlngstff@gmail.com>
2024-12-09 20:10:10 -0500

    Fix Jenkins errors, round 2

--- src/overlays/actors/ovl_En_Go2/z_en_go2.c
+++ src/overlays/actors/ovl_En_Go2/z_en_go2.c
@@ -399,9 +399,9 @@ u16 EnGo2_GetTextIdGoronDmtDcEntrance(PlayState* play, EnGo2* this) {
     } else if (CHECK_QUEST_ITEM(QUEST_GORON_RUBY)) {
         return 0x3027;
     } else {
-        return GET_EVENTCHKINF(EVENTCHKINF_DESTROYED_DODONGOS_CAVERN_BOULDER) ? 0x3021
-             : GET_INFTABLE(INFTABLE_TALKED_TO_DODONGOS_CAVERN_ENTRANCE_GORON) ? 0x302A
-             : 0x3008;
+        return GET_EVENTCHKINF(EVENTCHKINF_DESTROYED_DODONGOS_CAVERN_BOULDER)    ? 0x3021
+               : GET_INFTABLE(INFTABLE_TALKED_TO_DODONGOS_CAVERN_ENTRANCE_GORON) ? 0x302A
+                                                                                 : 0x3008;
     }
 }
 
@@ -464,10 +464,10 @@ u16 EnGo2_GetTextIdGoronCityLowestFloor(PlayState* play, EnGo2* this) {
     } else if (CHECK_QUEST_ITEM(QUEST_GORON_RUBY)) {
         return 0x3027;
     } else {
-        return CUR_UPG_VALUE(UPG_STRENGTH) != 0                    ? 0x302C
-             : !Flags_GetSwitch(play, 0x1B)                        ? 0x3017
-             : GET_INFTABLE(INFTABLE_TALKED_TO_DARUNIA_DOOR_GORON) ? 0x3019
-             : 0x3018;
+        return CUR_UPG_VALUE(UPG_STRENGTH) != 0                      ? 0x302C
+               : !Flags_GetSwitch(play, 0x1B)                        ? 0x3017
+               : GET_INFTABLE(INFTABLE_TALKED_TO_DARUNIA_DOOR_GORON) ? 0x3019
+                                                                     : 0x3018;
     }
 }
 
