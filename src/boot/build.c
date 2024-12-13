1d75550e3ab015a257313339fc0c85e82274e428
Thar0 <17233964+Thar0@users.noreply.github.com>
2024-12-13 02:09:48 +0000

    Better fix for formatting problems

--- src/boot/build.c
+++ src/boot/build.c
@@ -1,17 +1,5 @@
 #include "versions.h"
 
-#ifndef BUILD_CREATOR
-#define BUILD_CREATOR ""
-#endif
-
-#ifndef BUILD_DATE
-#define BUILD_DATE __DATE__
-#endif
-
-#ifndef BUILD_TIME
-#define BUILD_TIME __DATE__
-#endif
-
 const char gBuildCreator[] = BUILD_CREATOR;
 const char gBuildDate[] = BUILD_DATE " " BUILD_TIME;
 const char gBuildMakeOption[] = "";
