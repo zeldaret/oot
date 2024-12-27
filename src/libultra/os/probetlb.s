e591378317aa40e0990ec67a9128222e1d2e1946
Thar0 <17233964+Thar0@users.noreply.github.com>
2024-12-27 21:38:24 +0000

    Wrap all of __osProbeTLB in noreorder

--- src/libultra/os/probetlb.s
+++ src/libultra/os/probetlb.s
@@ -13,56 +13,53 @@
  *  Returns the physical address if found, or -1 if not found.
  */
 LEAF(__osProbeTLB)
+.set noreorder
     /* Set C0_ENTRYHI based on supplied vaddr */
-    MFC0(   t0, C0_ENTRYHI)
+    mfc0    t0, C0_ENTRYHI
     and     t1, t0, TLBHI_PIDMASK
-.set noreorder; .set reorder
     and     t2, a0, TLBHI_VPN2MASK
     or      t1, t1, t2
-    MTC0(   t1, C0_ENTRYHI)
-    NOP
-    NOP
-    NOP
+    mtc0    t1, C0_ENTRYHI
+    nop
+    nop
+    nop
     /* TLB probe, sets C0_INX to a value matching C0_ENTRYHI. */
     /* If no match is found the TLBINX_PROBE bit is set to indicate this. */
-    TLBP
-    NOP
-    NOP
+    tlbp
+    nop
+    nop
     /* Read result */
-    MFC0(   t3, C0_INX)
+    mfc0    t3, C0_INX
     and     t3, t3, TLBINX_PROBE
     /* Branch if no match was found */
     bnez    t3, 3f
+     nop
 
     /* Read TLB, sets C0_ENTRYHI, C0_ENTRYLO0, C0_ENTRYLO1 and C0_PAGEMASK for the TLB */
     /* entry indicated by C0_INX */
-    TLBR
-    NOP
-    NOP
-    NOP
+    tlbr
+    nop
+    nop
+    nop
     /* Calculate page size = (page mask + 0x2000) >> 1 */
-    MFC0(   t3, C0_PAGEMASK)
+    mfc0    t3, C0_PAGEMASK
     add     t3, t3, 0x2000
     srl     t3, t3, 1
     /* & with vaddr */
     and     t4, t3, a0
     /* Select C0_ENTRYLO0 or C0_ENTRYLO1 */
-    add     t3, t3, -1 /* make bitmask out of page size */
-
     bnez    t4, 1f
-    MFC0(   v0, C0_ENTRYLO0)
-
+     add    t3, t3, -1 /* make bitmask out of page size */
+    mfc0    v0, C0_ENTRYLO0
     b       2f
-
+     nop
 1:
-    MFC0(   v0, C0_ENTRYLO1)
+    mfc0    v0, C0_ENTRYLO1
 2:
     /* Check valid bit and branch if not valid */
     and     t5, v0, TLBLO_V
-.set noreorder
     beqz    t5, 3f
      nop
-.set reorder
 
     /* Extract the Page Frame Number from the entry */
     and     v0, v0, TLBLO_PFNMASK
@@ -71,17 +68,16 @@ LEAF(__osProbeTLB)
     and     t5, a0, t3
     /* Add masked vaddr to pfn to obtain the physical address */
     add     v0, v0, t5
-.set noreorder
     b       4f
      nop
-.set reorder
-
 3:
     /* No physical address for the supplied virtual address was found, */
     /* return -1 */
     li      v0, -1
 4:
     /* Restore original C0_ENTRYHI value before returning */
-    MTC0(   t0, C0_ENTRYHI)
+    mtc0    t0, C0_ENTRYHI
     jr      ra
+     nop
+.set reorder
 END(__osProbeTLB)
