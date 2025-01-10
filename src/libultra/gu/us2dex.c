62c0effccdf56a02f5df741fd31c93080854a150
Tharo <17233964+Thar0@users.noreply.github.com>
2025-01-10 20:24:37 +0000

    [iQue] Match libultra/gu (#2402)
    * [iQue] Match libultra/gu
    
    * Match sqrtf

--- src/libultra/gu/us2dex.c
+++ src/libultra/gu/us2dex.c
@@ -1,25 +1,25 @@
 #include "global.h"
 
 void guS2DInitBg(uObjBg* bg) {
-    u32 size;
-    s32 tmem = (bg->b.imageFmt == G_IM_FMT_CI) ? 0x100 : 0x200;
-    u16 shift = (6 - bg->b.imageSiz);
+    u16 tmem = (bg->b.imageFmt == G_IM_FMT_CI) ? 0x100 : 0x200;
+    u16 shift = 6 - bg->b.imageSiz;
+    u32 tsize;
 
     if (bg->b.imageLoad == G_BGLT_LOADBLOCK) {
         bg->b.tmemW = bg->b.imageW >> shift;
-        bg->b.tmemH = (tmem / bg->b.tmemW) * 4;
-        bg->b.tmemSizeW = bg->b.tmemW * 2;
+        bg->b.tmemH = (tmem / bg->b.tmemW) << 2;
+        bg->b.tmemSizeW = bg->b.tmemW << 1;
         bg->b.tmemSize = bg->b.tmemH * bg->b.tmemSizeW;
         bg->b.tmemLoadSH = (bg->b.tmemSize >> 1) - 1;
-        bg->b.tmemLoadTH = (0x7FF / bg->b.tmemW) + 1;
+        bg->b.tmemLoadTH = GS_CALC_DXT(bg->b.tmemW);
     } else { // G_BGLT_LOADTILE
         bg->b.tmemW = (bg->b.frameW >> shift) + 3;
-        bg->b.tmemH = (tmem / bg->b.tmemW) * 4;
-        bg->b.tmemSizeW = (bg->b.imageW >> shift) * 2;
+        bg->b.tmemH = (tmem / bg->b.tmemW) << 2;
+        bg->b.tmemSizeW = (bg->b.imageW >> shift) << 1;
 
-        size = bg->b.tmemH * bg->b.tmemSizeW;
-        bg->b.tmemSize = (size >> 16);
-        bg->b.tmemLoadSH = (size >> 0) & 0xFFFF;
+        tsize = bg->b.tmemH * bg->b.tmemSizeW;
+        bg->b.tmemSize = tsize >> 16;
+        bg->b.tmemLoadSH = tsize & 0xFFFF;
         bg->b.tmemLoadTH = bg->b.tmemH - 1;
     }
 }
