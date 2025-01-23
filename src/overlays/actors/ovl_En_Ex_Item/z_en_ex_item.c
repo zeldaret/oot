68a2d145589b4be0e89efdf2b092f2ed84ea8181
mzxrules <mzxrules@gmail.com>
2025-01-23 02:57:43 -0500

    add some missing dependencies

--- src/overlays/actors/ovl_En_Ex_Item/z_en_ex_item.c
+++ src/overlays/actors/ovl_En_Ex_Item/z_en_ex_item.c
@@ -14,6 +14,9 @@
 #include "sfx.h"
 #include "sys_matrix.h"
 #include "terminal.h"
+#include "z_lib.h"
+#include "z64play.h"
+#include "z64player.h"
 
 #include "global.h"
 #include "assets/objects/gameplay_keep/gameplay_keep.h"
