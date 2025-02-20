d280c104bb1ea723279f51736b9f737a2c9b44c3
fig02 <fig02srl@gmail.com>
2025-02-19 23:40:01 -0500

    fixes 2

--- include/prenmi_state.h
+++ include/prenmi_state.h
@@ -3,12 +3,11 @@
 
 #include "ultra64.h"
 #include "z64game.h"
-#include "unk.h"
 
 typedef struct PreNMIState {
     /* 0x00 */ GameState state;
-    /* 0xA4 */ u32       timer;
-    /* 0xA8 */ UNK_TYPE4 unk_A8;
+    /* 0xA4 */ u32 timer;
+    /* 0xA8 */ s32 unk_A8;
 } PreNMIState; // size = 0xAC
 
 void PreNMI_Init(GameState* thisx);
