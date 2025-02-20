10c1f7edd2d30c8a46ba77fd33c6d6618dd6e8e1
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-02-20 14:23:11 +0100

    remove duplicate declarations

--- include/functions.h
+++ include/functions.h
@@ -131,7 +131,7 @@ ListAlloc* ListAlloc_Init(ListAlloc* this);
 void* ListAlloc_Alloc(ListAlloc* this, u32 size);
 void ListAlloc_Free(ListAlloc* this, void* data);
 void ListAlloc_FreeAll(ListAlloc* this);
-void Main(void* arg);
+
 void SysCfb_Init(s32 n64dd);
 void* SysCfb_GetFbPtr(s32 idx);
 void* SysCfb_GetFbEnd(void);
