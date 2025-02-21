5b10cb5fc4901724383b1f4f64cf95270d739881
fig02 <fig02srl@gmail.com>
2025-02-21 11:32:51 -0500

    add back forward declare

--- src/code/main.c
+++ src/code/main.c
@@ -7,6 +7,7 @@ extern uintptr_t gSegments[NUM_SEGMENTS];
 #pragma increment_block_number "gc-eu:252 gc-eu-mq:252 gc-jp:252 gc-jp-ce:252 gc-jp-mq:252 gc-us:252 gc-us-mq:252" \
 "ique-cn:252 ntsc-1.0:128 ntsc-1.1:128 ntsc-1.2:128 pal-1.0:128 pal-1.1:128"
 
+extern struct PreNmiBuff* gAppNmiBufferPtr;
 extern struct Scheduler gScheduler;
 extern struct PadMgr gPadMgr;
 extern struct IrqMgr gIrqMgr;
