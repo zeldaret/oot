39e5df71be9fb954ff893ba52b1d90bd1d568053
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-02-22 00:15:11 +0100

    "Lon Lon"

--- src/overlays/actors/ovl_Bg_Spot15_Rrbox/z_bg_spot15_rrbox.c
+++ src/overlays/actors/ovl_Bg_Spot15_Rrbox/z_bg_spot15_rrbox.c
@@ -143,7 +143,7 @@ void BgSpot15Rrbox_Init(Actor* thisx, PlayState* play) {
     } else {
         func_808B4084(this, play);
     }
-    PRINTF("(spot15 " T("ロンロン木箱", "Lonlon Wooden Box") ")(arg_data 0x%04x)\n", this->dyna.actor.params);
+    PRINTF("(spot15 " T("ロンロン木箱", "Lon Lon Wooden Box") ")(arg_data 0x%04x)\n", this->dyna.actor.params);
 }
 
 void BgSpot15Rrbox_Destroy(Actor* thisx, PlayState* play) {
@@ -330,7 +330,7 @@ void func_808B43D0(BgSpot15Rrbox* this, PlayState* play) {
     Actor_MoveXZGravity(actor);
 
     if (actor->world.pos.y <= BGCHECK_Y_MIN + 10.0f) {
-        PRINTF("Warning : " T("ロンロン木箱落ちすぎた", "Lonlon Wooden Box fell too far") "(%s %d)(arg_data 0x%04x)\n",
+        PRINTF("Warning : " T("ロンロン木箱落ちすぎた", "Lon Lon Wooden Box fell too far") "(%s %d)(arg_data 0x%04x)\n",
                "../z_bg_spot15_rrbox.c", 599, actor->params);
 
         Actor_Kill(actor);
