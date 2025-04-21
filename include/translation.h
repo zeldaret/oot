b93a5dcb7c09a87d106711fb9dde8d79e64f3fc6
fig02 <fig02srl@gmail.com>
2025-04-21 11:32:09 -0400

    create printf and translation headers

---
+++ include/translation.h
@@ -0,0 +1,11 @@
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+/**
+ * The T macro holds translations in English for original debug strings written in Japanese.
+ * The translated strings match the original debug strings, they are only direct translations.
+ * For example, any original name is left as is rather than being replaced with the name in the codebase.
+ */
+#define T(jp, en) jp
+
+#endif
