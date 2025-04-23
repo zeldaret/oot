dc346c961b152f70c0d13828e8a8a69670669941
fig02 <fig02srl@gmail.com>
2025-04-23 03:26:40 -0400

    edit comment

--- include/translation.h
+++ include/translation.h
@@ -2,14 +2,14 @@
 #define TRANSLATION_H
 
 /**
- * Note: This translation macro exists for quality of life purposes.
- *       This does not refect anything from the original game
- *
  * The "T" macro holds translations in English for original debug strings written in Japanese.
  * The translated strings are only direct translations. Certain names or terms may not reflect
  * their in-game localized counterparts.
  *
- * To use translated English strings in the build, change the definition below from to "en".
+ * To use translated English strings in the build, change the definition below to "en".
+ *
+ * Note: This translation macro exists for quality of life purposes.
+ *       The original game would not have had a macro like this.
  */
 #define T(jp, en) jp
 
