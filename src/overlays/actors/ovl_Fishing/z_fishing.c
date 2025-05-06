e31ff1e6e6c71d5f9ec3bcfc23e645a868889171
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-05-07 00:09:42 +0200

    bss

--- src/overlays/actors/ovl_Fishing/z_fishing.c
+++ src/overlays/actors/ovl_Fishing/z_fishing.c
@@ -37,8 +37,8 @@
 #include "cic6105.h"
 #endif
 
-#pragma increment_block_number "gc-eu:192 gc-eu-mq:192 gc-jp:192 gc-jp-ce:192 gc-jp-mq:192 gc-us:192 gc-us-mq:192" \
-                               "ntsc-1.0:128 ntsc-1.1:128 ntsc-1.2:128 pal-1.0:128 pal-1.1:128"
+#pragma increment_block_number "gc-eu:0 gc-eu-mq:0 gc-jp:0 gc-jp-ce:0 gc-jp-mq:0 gc-us:0 gc-us-mq:0 ntsc-1.0:0" \
+                               "ntsc-1.1:0 ntsc-1.2:0 pal-1.0:0 pal-1.1:0"
 
 #define FLAGS ACTOR_FLAG_UPDATE_CULLING_DISABLED
 
