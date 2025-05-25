ded6a2383bc008f873ef7f560cc94045d3256711
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-05-25 10:28:06 +0200

    review

--- include/tex_len.h
+++ include/tex_len.h
@@ -3,9 +3,9 @@
 
 /**
  * Compute a length for an array holding a texture.
- * `type` is the array's element type
- * `width`, `height` are the texture dimensions
- * `bpp` is the texture's format's bits-per-pixels
+ * `type` is the array's element type.
+ * `width`, `height` are the texture dimensions.
+ * `bpp` is the texture's pixel size, in bits per pixels.
  * The calculation computes the size of the texture in bits `width * height * bpp`,
  * then divides by 8 to get the size in bytes, then divides by the element type size.
  */
