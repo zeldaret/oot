03540c7db9a2d624a014d749f1825b95316a82c8
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-05-29 14:01:49 +0200

    bss

--- src/code/z_player_lib.c
+++ src/code/z_player_lib.c
@@ -17,8 +17,8 @@
 #include "assets/objects/object_link_boy/object_link_boy.h"
 #include "assets/objects/object_link_child/object_link_child.h"
 
-#pragma increment_block_number "gc-eu:0 gc-eu-mq:0 gc-jp:128 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128" \
-                               "pal-1.0:0 pal-1.1:0"
+#pragma increment_block_number "gc-eu:0 gc-eu-mq:0 gc-jp:0 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128 pal-1.0:0" \
+                               "pal-1.1:0"
 
 typedef struct BowSlingshotStringData {
     /* 0x00 */ Gfx* dList;
