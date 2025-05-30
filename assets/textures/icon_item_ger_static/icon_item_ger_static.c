2ab30fe161bc8a6171516990fb2b26638d146e14
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-05-30 15:17:22 +0200

    commit icon_item_LANG_static for LANG in fra,ger,jpn,nes

---
+++ assets/textures/icon_item_ger_static/icon_item_ger_static.c
@@ -0,0 +1,131 @@
+#include "icon_item_ger_static.h"
+
+u64 gPauseDekuTitleGERTex[TEX_LEN(u64, gPauseDekuTitleGERTex_WIDTH, gPauseDekuTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseDekuTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseDodongoTitleGERTex[TEX_LEN(u64, gPauseDodongoTitleGERTex_WIDTH, gPauseDodongoTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseDodongoTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseJabuTitleGERTex[TEX_LEN(u64, gPauseJabuTitleGERTex_WIDTH, gPauseJabuTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseJabuTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseForestTitleGERTex[TEX_LEN(u64, gPauseForestTitleGERTex_WIDTH, gPauseForestTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseForestTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseFireTitleGERTex[TEX_LEN(u64, gPauseFireTitleGERTex_WIDTH, gPauseFireTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseFireTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseWaterTitleGERTex[TEX_LEN(u64, gPauseWaterTitleGERTex_WIDTH, gPauseWaterTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseWaterTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseSpiritTitleGERTex[TEX_LEN(u64, gPauseSpiritTitleGERTex_WIDTH, gPauseSpiritTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSpiritTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseShadowTitleGERTex[TEX_LEN(u64, gPauseShadowTitleGERTex_WIDTH, gPauseShadowTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseShadowTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseBotWTitleGERTex[TEX_LEN(u64, gPauseBotWTitleGERTex_WIDTH, gPauseBotWTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseBotWTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseIceCavernTitleGERTex[TEX_LEN(u64, gPauseIceCavernTitleGERTex_WIDTH, gPauseIceCavernTitleGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseIceCavernTitleGERTex.ia8.inc.c"
+};
+
+u64 gPauseToEquipGERTex[TEX_LEN(u64, gPauseToEquipGERTex_WIDTH, gPauseToEquipGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToEquipGERTex.ia8.inc.c"
+};
+
+u64 gPauseToDecideGERTex[TEX_LEN(u64, gPauseToDecideGERTex_WIDTH, gPauseToDecideGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToDecideGERTex.ia8.inc.c"
+};
+
+u64 gPauseToPlayMelodyGERTex[TEX_LEN(u64, gPauseToPlayMelodyGERTex_WIDTH, gPauseToPlayMelodyGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToPlayMelodyGERTex.ia8.inc.c"
+};
+
+u64 gPauseToSelectItemGERTex[TEX_LEN(u64, gPauseToSelectItemGERTex_WIDTH, gPauseToSelectItemGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToSelectItemGERTex.ia8.inc.c"
+};
+
+u64 gPauseToMapGERTex[TEX_LEN(u64, gPauseToMapGERTex_WIDTH, gPauseToMapGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToMapGERTex.ia8.inc.c"
+};
+
+u64 gPauseToQuestStatusGERTex[TEX_LEN(u64, gPauseToQuestStatusGERTex_WIDTH, gPauseToQuestStatusGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToQuestStatusGERTex.ia8.inc.c"
+};
+
+u64 gPauseToEquipmentGERTex[TEX_LEN(u64, gPauseToEquipmentGERTex_WIDTH, gPauseToEquipmentGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseToEquipmentGERTex.ia8.inc.c"
+};
+
+u64 gPauseSavePromptGERTex[TEX_LEN(u64, gPauseSavePromptGERTex_WIDTH, gPauseSavePromptGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSavePromptGERTex.ia8.inc.c"
+};
+
+u64 gPauseSaveConfirmationGERTex[TEX_LEN(u64, gPauseSaveConfirmationGERTex_WIDTH, gPauseSaveConfirmationGERTex_HEIGHT,
+                                         8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSaveConfirmationGERTex.ia8.inc.c"
+};
+
+u64 gPauseYesGERTex[TEX_LEN(u64, gPauseYesGERTex_WIDTH, gPauseYesGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseYesGERTex.ia8.inc.c"
+};
+
+u64 gPauseNoGERTex[TEX_LEN(u64, gPauseNoGERTex_WIDTH, gPauseNoGERTex_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseNoGERTex.ia8.inc.c"
+};
+
+u64 gPauseCurrentPositionGERTex[TEX_LEN(u64, gPauseCurrentPositionGERTex_WIDTH, gPauseCurrentPositionGERTex_HEIGHT,
+                                        4)] = {
+#include "assets/textures/icon_item_ger_static/gPauseCurrentPositionGERTex.i4.inc.c"
+};
+
+u64 gPauseEquipment00GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseEquipment00GERTex.ia8.inc.c"
+};
+
+u64 gPauseEquipment10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseEquipment10GERTex.ia8.inc.c"
+};
+
+u64 gPauseEquipment20GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseEquipment20GERTex.ia8.inc.c"
+};
+
+u64 gPauseSelectItem00GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSelectItem00GERTex.ia8.inc.c"
+};
+
+u64 gPauseSelectItem10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSelectItem10GERTex.ia8.inc.c"
+};
+
+u64 gPauseSelectItem20GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSelectItem20GERTex.ia8.inc.c"
+};
+
+u64 gPauseMap10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseMap10GERTex.ia8.inc.c"
+};
+
+u64 gPauseQuestStatus10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseQuestStatus10GERTex.ia8.inc.c"
+};
+
+u64 gPauseSave10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSave10GERTex.ia8.inc.c"
+};
+
+u64 gPauseSave20GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)] = {
+#include "assets/textures/icon_item_ger_static/gPauseSave20GERTex.ia8.inc.c"
+};
