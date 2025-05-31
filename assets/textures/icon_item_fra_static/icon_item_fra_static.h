fd5fbe138d29a5e0501e0728d9c2e348d7992b06
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-05-31 10:26:04 +0200

    Commit `icon_item_24_static`, `icon_item_{fra,ger,jpn,nes}_static`, `item_name_static` (#2562)
    * commit icon_item_24_static
    
    * commit icon_item_LANG_static for LANG in fra,ger,jpn,nes
    
    * commit item_name_static

---
+++ assets/textures/icon_item_fra_static/icon_item_fra_static.h
@@ -0,0 +1,87 @@
+#ifndef ICON_ITEM_FRA_STATIC_H
+#define ICON_ITEM_FRA_STATIC_H
+
+#include "ultra64.h"
+#include "tex_len.h"
+#include "src/overlays/misc/ovl_kaleido_scope/z_kaleido_scope.h"
+
+#define gPauseDekuTitleFRATex_WIDTH 96
+#define gPauseDekuTitleFRATex_HEIGHT 16
+extern u64 gPauseDekuTitleFRATex[TEX_LEN(u64, gPauseDekuTitleFRATex_WIDTH, gPauseDekuTitleFRATex_HEIGHT, 8)];
+#define gPauseDodongoTitleFRATex_WIDTH 96
+#define gPauseDodongoTitleFRATex_HEIGHT 16
+extern u64 gPauseDodongoTitleFRATex[TEX_LEN(u64, gPauseDodongoTitleFRATex_WIDTH, gPauseDodongoTitleFRATex_HEIGHT, 8)];
+#define gPauseJabuTitleFRATex_WIDTH 96
+#define gPauseJabuTitleFRATex_HEIGHT 16
+extern u64 gPauseJabuTitleFRATex[TEX_LEN(u64, gPauseJabuTitleFRATex_WIDTH, gPauseJabuTitleFRATex_HEIGHT, 8)];
+#define gPauseForestTitleFRATex_WIDTH 96
+#define gPauseForestTitleFRATex_HEIGHT 16
+extern u64 gPauseForestTitleFRATex[TEX_LEN(u64, gPauseForestTitleFRATex_WIDTH, gPauseForestTitleFRATex_HEIGHT, 8)];
+#define gPauseFireTitleFRATex_WIDTH 96
+#define gPauseFireTitleFRATex_HEIGHT 16
+extern u64 gPauseFireTitleFRATex[TEX_LEN(u64, gPauseFireTitleFRATex_WIDTH, gPauseFireTitleFRATex_HEIGHT, 8)];
+#define gPauseWaterTitleFRATex_WIDTH 96
+#define gPauseWaterTitleFRATex_HEIGHT 16
+extern u64 gPauseWaterTitleFRATex[TEX_LEN(u64, gPauseWaterTitleFRATex_WIDTH, gPauseWaterTitleFRATex_HEIGHT, 8)];
+#define gPauseSpiritTitleFRATex_WIDTH 96
+#define gPauseSpiritTitleFRATex_HEIGHT 16
+extern u64 gPauseSpiritTitleFRATex[TEX_LEN(u64, gPauseSpiritTitleFRATex_WIDTH, gPauseSpiritTitleFRATex_HEIGHT, 8)];
+#define gPauseShadowTitleFRATex_WIDTH 96
+#define gPauseShadowTitleFRATex_HEIGHT 16
+extern u64 gPauseShadowTitleFRATex[TEX_LEN(u64, gPauseShadowTitleFRATex_WIDTH, gPauseShadowTitleFRATex_HEIGHT, 8)];
+#define gPauseBotWTitleFRATex_WIDTH 96
+#define gPauseBotWTitleFRATex_HEIGHT 16
+extern u64 gPauseBotWTitleFRATex[TEX_LEN(u64, gPauseBotWTitleFRATex_WIDTH, gPauseBotWTitleFRATex_HEIGHT, 8)];
+#define gPauseIceCavernTitleFRATex_WIDTH 96
+#define gPauseIceCavernTitleFRATex_HEIGHT 16
+extern u64 gPauseIceCavernTitleFRATex[TEX_LEN(u64, gPauseIceCavernTitleFRATex_WIDTH, gPauseIceCavernTitleFRATex_HEIGHT, 8)];
+#define gPauseToEquipFRATex_WIDTH 80
+#define gPauseToEquipFRATex_HEIGHT 16
+extern u64 gPauseToEquipFRATex[TEX_LEN(u64, gPauseToEquipFRATex_WIDTH, gPauseToEquipFRATex_HEIGHT, 8)];
+#define gPauseToDecideFRATex_WIDTH 72
+#define gPauseToDecideFRATex_HEIGHT 16
+extern u64 gPauseToDecideFRATex[TEX_LEN(u64, gPauseToDecideFRATex_WIDTH, gPauseToDecideFRATex_HEIGHT, 8)];
+#define gPauseToPlayMelodyFRATex_WIDTH 112
+#define gPauseToPlayMelodyFRATex_HEIGHT 16
+extern u64 gPauseToPlayMelodyFRATex[TEX_LEN(u64, gPauseToPlayMelodyFRATex_WIDTH, gPauseToPlayMelodyFRATex_HEIGHT, 8)];
+#define gPauseToSelectItemFRATex_WIDTH 128
+#define gPauseToSelectItemFRATex_HEIGHT 16
+extern u64 gPauseToSelectItemFRATex[TEX_LEN(u64, gPauseToSelectItemFRATex_WIDTH, gPauseToSelectItemFRATex_HEIGHT, 8)];
+#define gPauseToMapFRATex_WIDTH 128
+#define gPauseToMapFRATex_HEIGHT 16
+extern u64 gPauseToMapFRATex[TEX_LEN(u64, gPauseToMapFRATex_WIDTH, gPauseToMapFRATex_HEIGHT, 8)];
+#define gPauseToQuestStatusFRATex_WIDTH 128
+#define gPauseToQuestStatusFRATex_HEIGHT 16
+extern u64 gPauseToQuestStatusFRATex[TEX_LEN(u64, gPauseToQuestStatusFRATex_WIDTH, gPauseToQuestStatusFRATex_HEIGHT, 8)];
+#define gPauseToEquipmentFRATex_WIDTH 128
+#define gPauseToEquipmentFRATex_HEIGHT 16
+extern u64 gPauseToEquipmentFRATex[TEX_LEN(u64, gPauseToEquipmentFRATex_WIDTH, gPauseToEquipmentFRATex_HEIGHT, 8)];
+#define gPauseSavePromptFRATex_WIDTH 152
+#define gPauseSavePromptFRATex_HEIGHT 16
+extern u64 gPauseSavePromptFRATex[TEX_LEN(u64, gPauseSavePromptFRATex_WIDTH, gPauseSavePromptFRATex_HEIGHT, 8)];
+#define gPauseSaveConfirmationFRATex_WIDTH 152
+#define gPauseSaveConfirmationFRATex_HEIGHT 16
+extern u64 gPauseSaveConfirmationFRATex[TEX_LEN(u64, gPauseSaveConfirmationFRATex_WIDTH, gPauseSaveConfirmationFRATex_HEIGHT, 8)];
+#define gPauseYesFRATex_WIDTH 48
+#define gPauseYesFRATex_HEIGHT 16
+extern u64 gPauseYesFRATex[TEX_LEN(u64, gPauseYesFRATex_WIDTH, gPauseYesFRATex_HEIGHT, 8)];
+#define gPauseNoFRATex_WIDTH 48
+#define gPauseNoFRATex_HEIGHT 16
+extern u64 gPauseNoFRATex[TEX_LEN(u64, gPauseNoFRATex_WIDTH, gPauseNoFRATex_HEIGHT, 8)];
+#define gPauseCurrentPositionFRATex_WIDTH 64
+#define gPauseCurrentPositionFRATex_HEIGHT 8
+extern u64 gPauseCurrentPositionFRATex[TEX_LEN(u64, gPauseCurrentPositionFRATex_WIDTH, gPauseCurrentPositionFRATex_HEIGHT, 4)];
+
+extern u64 gPauseEquipment00FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseEquipment10FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseEquipment20FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSelectItem00FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSelectItem10FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSelectItem20FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseMap10FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseQuestStatus10FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSave00FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSave10FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSave20FRATex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+
+#endif
---
+++ assets/textures/icon_item_ger_static/icon_item_ger_static.h
@@ -0,0 +1,86 @@
+#ifndef ICON_ITEM_GER_STATIC_H
+#define ICON_ITEM_GER_STATIC_H
+
+#include "ultra64.h"
+#include "tex_len.h"
+#include "src/overlays/misc/ovl_kaleido_scope/z_kaleido_scope.h"
+
+#define gPauseDekuTitleGERTex_WIDTH 96
+#define gPauseDekuTitleGERTex_HEIGHT 16
+extern u64 gPauseDekuTitleGERTex[TEX_LEN(u64, gPauseDekuTitleGERTex_WIDTH, gPauseDekuTitleGERTex_HEIGHT, 8)];
+#define gPauseDodongoTitleGERTex_WIDTH 96
+#define gPauseDodongoTitleGERTex_HEIGHT 16
+extern u64 gPauseDodongoTitleGERTex[TEX_LEN(u64, gPauseDodongoTitleGERTex_WIDTH, gPauseDodongoTitleGERTex_HEIGHT, 8)];
+#define gPauseJabuTitleGERTex_WIDTH 96
+#define gPauseJabuTitleGERTex_HEIGHT 16
+extern u64 gPauseJabuTitleGERTex[TEX_LEN(u64, gPauseJabuTitleGERTex_WIDTH, gPauseJabuTitleGERTex_HEIGHT, 8)];
+#define gPauseForestTitleGERTex_WIDTH 96
+#define gPauseForestTitleGERTex_HEIGHT 16
+extern u64 gPauseForestTitleGERTex[TEX_LEN(u64, gPauseForestTitleGERTex_WIDTH, gPauseForestTitleGERTex_HEIGHT, 8)];
+#define gPauseFireTitleGERTex_WIDTH 96
+#define gPauseFireTitleGERTex_HEIGHT 16
+extern u64 gPauseFireTitleGERTex[TEX_LEN(u64, gPauseFireTitleGERTex_WIDTH, gPauseFireTitleGERTex_HEIGHT, 8)];
+#define gPauseWaterTitleGERTex_WIDTH 96
+#define gPauseWaterTitleGERTex_HEIGHT 16
+extern u64 gPauseWaterTitleGERTex[TEX_LEN(u64, gPauseWaterTitleGERTex_WIDTH, gPauseWaterTitleGERTex_HEIGHT, 8)];
+#define gPauseSpiritTitleGERTex_WIDTH 96
+#define gPauseSpiritTitleGERTex_HEIGHT 16
+extern u64 gPauseSpiritTitleGERTex[TEX_LEN(u64, gPauseSpiritTitleGERTex_WIDTH, gPauseSpiritTitleGERTex_HEIGHT, 8)];
+#define gPauseShadowTitleGERTex_WIDTH 96
+#define gPauseShadowTitleGERTex_HEIGHT 16
+extern u64 gPauseShadowTitleGERTex[TEX_LEN(u64, gPauseShadowTitleGERTex_WIDTH, gPauseShadowTitleGERTex_HEIGHT, 8)];
+#define gPauseBotWTitleGERTex_WIDTH 96
+#define gPauseBotWTitleGERTex_HEIGHT 16
+extern u64 gPauseBotWTitleGERTex[TEX_LEN(u64, gPauseBotWTitleGERTex_WIDTH, gPauseBotWTitleGERTex_HEIGHT, 8)];
+#define gPauseIceCavernTitleGERTex_WIDTH 96
+#define gPauseIceCavernTitleGERTex_HEIGHT 16
+extern u64 gPauseIceCavernTitleGERTex[TEX_LEN(u64, gPauseIceCavernTitleGERTex_WIDTH, gPauseIceCavernTitleGERTex_HEIGHT, 8)];
+#define gPauseToEquipGERTex_WIDTH 88
+#define gPauseToEquipGERTex_HEIGHT 16
+extern u64 gPauseToEquipGERTex[TEX_LEN(u64, gPauseToEquipGERTex_WIDTH, gPauseToEquipGERTex_HEIGHT, 8)];
+#define gPauseToDecideGERTex_WIDTH 88
+#define gPauseToDecideGERTex_HEIGHT 16
+extern u64 gPauseToDecideGERTex[TEX_LEN(u64, gPauseToDecideGERTex_WIDTH, gPauseToDecideGERTex_HEIGHT, 8)];
+#define gPauseToPlayMelodyGERTex_WIDTH 104
+#define gPauseToPlayMelodyGERTex_HEIGHT 16
+extern u64 gPauseToPlayMelodyGERTex[TEX_LEN(u64, gPauseToPlayMelodyGERTex_WIDTH, gPauseToPlayMelodyGERTex_HEIGHT, 8)];
+#define gPauseToSelectItemGERTex_WIDTH 128
+#define gPauseToSelectItemGERTex_HEIGHT 16
+extern u64 gPauseToSelectItemGERTex[TEX_LEN(u64, gPauseToSelectItemGERTex_WIDTH, gPauseToSelectItemGERTex_HEIGHT, 8)];
+#define gPauseToMapGERTex_WIDTH 128
+#define gPauseToMapGERTex_HEIGHT 16
+extern u64 gPauseToMapGERTex[TEX_LEN(u64, gPauseToMapGERTex_WIDTH, gPauseToMapGERTex_HEIGHT, 8)];
+#define gPauseToQuestStatusGERTex_WIDTH 128
+#define gPauseToQuestStatusGERTex_HEIGHT 16
+extern u64 gPauseToQuestStatusGERTex[TEX_LEN(u64, gPauseToQuestStatusGERTex_WIDTH, gPauseToQuestStatusGERTex_HEIGHT, 8)];
+#define gPauseToEquipmentGERTex_WIDTH 128
+#define gPauseToEquipmentGERTex_HEIGHT 16
+extern u64 gPauseToEquipmentGERTex[TEX_LEN(u64, gPauseToEquipmentGERTex_WIDTH, gPauseToEquipmentGERTex_HEIGHT, 8)];
+#define gPauseSavePromptGERTex_WIDTH 152
+#define gPauseSavePromptGERTex_HEIGHT 16
+extern u64 gPauseSavePromptGERTex[TEX_LEN(u64, gPauseSavePromptGERTex_WIDTH, gPauseSavePromptGERTex_HEIGHT, 8)];
+#define gPauseSaveConfirmationGERTex_WIDTH 152
+#define gPauseSaveConfirmationGERTex_HEIGHT 16
+extern u64 gPauseSaveConfirmationGERTex[TEX_LEN(u64, gPauseSaveConfirmationGERTex_WIDTH, gPauseSaveConfirmationGERTex_HEIGHT, 8)];
+#define gPauseYesGERTex_WIDTH 48
+#define gPauseYesGERTex_HEIGHT 16
+extern u64 gPauseYesGERTex[TEX_LEN(u64, gPauseYesGERTex_WIDTH, gPauseYesGERTex_HEIGHT, 8)];
+#define gPauseNoGERTex_WIDTH 48
+#define gPauseNoGERTex_HEIGHT 16
+extern u64 gPauseNoGERTex[TEX_LEN(u64, gPauseNoGERTex_WIDTH, gPauseNoGERTex_HEIGHT, 8)];
+#define gPauseCurrentPositionGERTex_WIDTH 64
+#define gPauseCurrentPositionGERTex_HEIGHT 8
+extern u64 gPauseCurrentPositionGERTex[TEX_LEN(u64, gPauseCurrentPositionGERTex_WIDTH, gPauseCurrentPositionGERTex_HEIGHT, 4)];
+
+extern u64 gPauseEquipment00GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseEquipment10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseEquipment20GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSelectItem00GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSelectItem10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSelectItem20GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseMap10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseQuestStatus10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSave10GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+extern u64 gPauseSave20GERTex[TEX_LEN(u64, PAGE_BG_QUAD_TEX_WIDTH, PAGE_BG_QUAD_TEX_HEIGHT, 8)];
+
+#endif
