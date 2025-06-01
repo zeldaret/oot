f7c6d31a751c9d26ee02710197ede5265f7bfed3
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-01 17:41:10 +0200

    bss

--- src/code/game.c
+++ src/code/game.c
@@ -32,7 +32,7 @@
 #include "z64play.h"
 #include "z64vis.h"
 
-#pragma increment_block_number "gc-eu:128 gc-eu-mq:128 gc-jp:128 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128"
+#pragma increment_block_number "gc-eu:0 gc-eu-mq:0 gc-jp:0 gc-jp-ce:0 gc-jp-mq:0 gc-us:0 gc-us-mq:0"
 
 SpeedMeter D_801664D0;
 VisCvg sVisCvg;
