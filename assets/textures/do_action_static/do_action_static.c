01f33f575aee34f0bc167f7385073731a87091b0
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-06 09:17:37 +0200

    Commit `do_action_static`

---
+++ assets/textures/do_action_static/do_action_static.c
@@ -0,0 +1,481 @@
+#include "do_action_static.h"
+
+#if OOT_NTSC
+
+// Japanese
+
+u64 gAttackDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gAttackDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gCheckDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gCheckDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gEnterDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gEnterDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gReturnDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReturnDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gOpenDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gOpenDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gJumpDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gJumpDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gDecideDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDecideDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gDiveDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDiveDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gFasterDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gFasterDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gThrowDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gThrowDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gUnusedNaviDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gUnusedNaviDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gClimbDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gClimbDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gDropDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDropDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gDownDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDownDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gSaveDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSaveDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gSpeakDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSpeakDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNextDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNextDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gGrabDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gGrabDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gStopDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gStopDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gPutAwayDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gPutAwayDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gReelDoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReelDoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum1DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum1DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum2DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum2DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum3DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum3DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum4DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum4DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum5DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum5DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum6DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum6DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum7DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum7DoActionJPNTex.ia4.inc.c"
+};
+
+u64 gNum8DoActionJPNTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum8DoActionJPNTex.ia4.inc.c"
+};
+
+#endif
+
+// English
+
+u64 gAttackDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gAttackDoActionENGTex.ia4.inc.c"
+};
+
+u64 gCheckDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gCheckDoActionENGTex.ia4.inc.c"
+};
+
+u64 gEnterDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gEnterDoActionENGTex.ia4.inc.c"
+};
+
+u64 gReturnDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReturnDoActionENGTex.ia4.inc.c"
+};
+
+u64 gOpenDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gOpenDoActionENGTex.ia4.inc.c"
+};
+
+u64 gJumpDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gJumpDoActionENGTex.ia4.inc.c"
+};
+
+u64 gDecideDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDecideDoActionENGTex.ia4.inc.c"
+};
+
+u64 gDiveDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDiveDoActionENGTex.ia4.inc.c"
+};
+
+u64 gFasterDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gFasterDoActionENGTex.ia4.inc.c"
+};
+
+u64 gThrowDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gThrowDoActionENGTex.ia4.inc.c"
+};
+
+u64 gUnusedNaviDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gUnusedNaviDoActionENGTex.ia4.inc.c"
+};
+
+u64 gClimbDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gClimbDoActionENGTex.ia4.inc.c"
+};
+
+u64 gDropDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDropDoActionENGTex.ia4.inc.c"
+};
+
+u64 gDownDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDownDoActionENGTex.ia4.inc.c"
+};
+
+u64 gSaveDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSaveDoActionENGTex.ia4.inc.c"
+};
+
+u64 gSpeakDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSpeakDoActionENGTex.ia4.inc.c"
+};
+
+u64 gNextDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNextDoActionENGTex.ia4.inc.c"
+};
+
+u64 gGrabDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gGrabDoActionENGTex.ia4.inc.c"
+};
+
+u64 gStopDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gStopDoActionENGTex.ia4.inc.c"
+};
+
+u64 gPutAwayDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gPutAwayDoActionENGTex.ia4.inc.c"
+};
+
+u64 gReelDoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReelDoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum1DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum1DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum2DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum2DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum3DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum3DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum4DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum4DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum5DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum5DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum6DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum6DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum7DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum7DoActionENGTex.ia4.inc.c"
+};
+
+u64 gNum8DoActionENGTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum8DoActionENGTex.ia4.inc.c"
+};
+
+#if OOT_PAL
+
+// German
+
+u64 gAttackDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gAttackDoActionGERTex.ia4.inc.c"
+};
+
+u64 gCheckDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gCheckDoActionGERTex.ia4.inc.c"
+};
+
+u64 gEnterDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gEnterDoActionGERTex.ia4.inc.c"
+};
+
+u64 gReturnDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReturnDoActionGERTex.ia4.inc.c"
+};
+
+u64 gOpenDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gOpenDoActionGERTex.ia4.inc.c"
+};
+
+u64 gJumpDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gJumpDoActionGERTex.ia4.inc.c"
+};
+
+u64 gDecideDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDecideDoActionGERTex.ia4.inc.c"
+};
+
+u64 gDiveDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDiveDoActionGERTex.ia4.inc.c"
+};
+
+u64 gFasterDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gFasterDoActionGERTex.ia4.inc.c"
+};
+
+u64 gThrowDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gThrowDoActionGERTex.ia4.inc.c"
+};
+
+u64 gUnusedNaviDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gUnusedNaviDoActionGERTex.ia4.inc.c"
+};
+
+u64 gClimbDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gClimbDoActionGERTex.ia4.inc.c"
+};
+
+u64 gDropDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDropDoActionGERTex.ia4.inc.c"
+};
+
+u64 gDownDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDownDoActionGERTex.ia4.inc.c"
+};
+
+u64 gSaveDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSaveDoActionGERTex.ia4.inc.c"
+};
+
+u64 gSpeakDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSpeakDoActionGERTex.ia4.inc.c"
+};
+
+u64 gNextDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNextDoActionGERTex.ia4.inc.c"
+};
+
+u64 gGrabDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gGrabDoActionGERTex.ia4.inc.c"
+};
+
+u64 gStopDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gStopDoActionGERTex.ia4.inc.c"
+};
+
+u64 gPutAwayDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gPutAwayDoActionGERTex.ia4.inc.c"
+};
+
+u64 gReelDoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReelDoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum1DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum1DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum2DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum2DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum3DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum3DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum4DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum4DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum5DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum5DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum6DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum6DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum7DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum7DoActionGERTex.ia4.inc.c"
+};
+
+u64 gNum8DoActionGERTex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum8DoActionGERTex.ia4.inc.c"
+};
+
+// French
+
+u64 gAttackDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gAttackDoActionFRATex.ia4.inc.c"
+};
+
+u64 gCheckDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gCheckDoActionFRATex.ia4.inc.c"
+};
+
+u64 gEnterDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gEnterDoActionFRATex.ia4.inc.c"
+};
+
+u64 gReturnDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReturnDoActionFRATex.ia4.inc.c"
+};
+
+u64 gOpenDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gOpenDoActionFRATex.ia4.inc.c"
+};
+
+u64 gJumpDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gJumpDoActionFRATex.ia4.inc.c"
+};
+
+u64 gDecideDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDecideDoActionFRATex.ia4.inc.c"
+};
+
+u64 gDiveDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDiveDoActionFRATex.ia4.inc.c"
+};
+
+u64 gFasterDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gFasterDoActionFRATex.ia4.inc.c"
+};
+
+u64 gThrowDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gThrowDoActionFRATex.ia4.inc.c"
+};
+
+u64 gUnusedNaviDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gUnusedNaviDoActionFRATex.ia4.inc.c"
+};
+
+u64 gClimbDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gClimbDoActionFRATex.ia4.inc.c"
+};
+
+u64 gDropDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDropDoActionFRATex.ia4.inc.c"
+};
+
+u64 gDownDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gDownDoActionFRATex.ia4.inc.c"
+};
+
+u64 gSaveDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSaveDoActionFRATex.ia4.inc.c"
+};
+
+u64 gSpeakDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gSpeakDoActionFRATex.ia4.inc.c"
+};
+
+u64 gNextDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNextDoActionFRATex.ia4.inc.c"
+};
+
+u64 gGrabDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gGrabDoActionFRATex.ia4.inc.c"
+};
+
+u64 gStopDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gStopDoActionFRATex.ia4.inc.c"
+};
+
+u64 gPutAwayDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gPutAwayDoActionFRATex.ia4.inc.c"
+};
+
+u64 gReelDoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gReelDoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum1DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum1DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum2DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum2DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum3DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum3DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum4DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum4DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum5DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum5DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum6DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum6DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum7DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum7DoActionFRATex.ia4.inc.c"
+};
+
+u64 gNum8DoActionFRATex[DO_ACTION_TEX_SIZE] = {
+#include "assets/textures/do_action_static/gNum8DoActionFRATex.ia4.inc.c"
+};
+
+#endif
---
+++ assets/textures/do_action_static/do_action_static.h
@@ -0,0 +1,140 @@
+#ifndef DO_ACTION_STATIC_H
+#define DO_ACTION_STATIC_H
+
+#include "ultra64.h"
+#include "tex_len.h"
+#include "versions.h"
+
+#define DO_ACTION_TEX_WIDTH 48
+#define DO_ACTION_TEX_HEIGHT 16
+#define DO_ACTION_TEX_SIZE TEX_LEN(u64, DO_ACTION_TEX_WIDTH, DO_ACTION_TEX_HEIGHT, 4)
+
+#if OOT_NTSC
+
+extern u64 gAttackDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gCheckDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gEnterDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gReturnDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gOpenDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gJumpDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gDecideDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gDiveDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gFasterDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gThrowDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gUnusedNaviDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gClimbDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gDropDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gDownDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gSaveDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gSpeakDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNextDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gGrabDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gStopDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gPutAwayDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gReelDoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum1DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum2DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum3DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum4DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum5DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum6DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum7DoActionJPNTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum8DoActionJPNTex[DO_ACTION_TEX_SIZE];
+
+#endif
+
+extern u64 gAttackDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gCheckDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gEnterDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gReturnDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gOpenDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gJumpDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gDecideDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gDiveDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gFasterDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gThrowDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gUnusedNaviDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gClimbDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gDropDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gDownDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gSaveDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gSpeakDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNextDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gGrabDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gStopDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gPutAwayDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gReelDoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum1DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum2DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum3DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum4DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum5DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum6DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum7DoActionENGTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum8DoActionENGTex[DO_ACTION_TEX_SIZE];
+
+#if OOT_PAL
+
+extern u64 gAttackDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gCheckDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gEnterDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gReturnDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gOpenDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gJumpDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gDecideDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gDiveDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gFasterDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gThrowDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gUnusedNaviDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gClimbDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gDropDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gDownDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gSaveDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gSpeakDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNextDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gGrabDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gStopDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gPutAwayDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gReelDoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum1DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum2DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum3DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum4DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum5DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum6DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum7DoActionGERTex[DO_ACTION_TEX_SIZE];
+extern u64 gNum8DoActionGERTex[DO_ACTION_TEX_SIZE];
+
+extern u64 gAttackDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gCheckDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gEnterDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gReturnDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gOpenDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gJumpDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gDecideDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gDiveDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gFasterDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gThrowDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gUnusedNaviDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gClimbDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gDropDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gDownDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gSaveDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gSpeakDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNextDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gGrabDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gStopDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gPutAwayDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gReelDoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum1DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum2DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum3DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum4DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum5DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum6DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum7DoActionFRATex[DO_ACTION_TEX_SIZE];
+extern u64 gNum8DoActionFRATex[DO_ACTION_TEX_SIZE];
+
+#endif
+
+#endif
