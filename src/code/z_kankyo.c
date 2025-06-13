0b5243e936da764f8cb083b8d26528ce22ea7bff
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-13 15:38:43 +0200

    bss

--- src/code/z_kankyo.c
+++ src/code/z_kankyo.c
@@ -244,7 +244,7 @@ s16 sSunDepthTestX;
 s16 sSunDepthTestY;
 
 #pragma increment_block_number "gc-eu:224 gc-eu-mq:224 gc-jp:224 gc-jp-ce:224 gc-jp-mq:224 gc-us:224 gc-us-mq:224" \
-                               "ique-cn:224 ntsc-1.0:224 ntsc-1.1:224 ntsc-1.2:224 pal-1.0:224 pal-1.1:224"
+                               "ique-cn:224 ntsc-1.0:208 ntsc-1.1:208 ntsc-1.2:208 pal-1.0:224 pal-1.1:224"
 
 LightNode* sNGameOverLightNode;
 LightInfo sNGameOverLightInfo;
