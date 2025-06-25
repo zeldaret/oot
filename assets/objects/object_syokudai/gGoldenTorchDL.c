fb146a06cf620c03e1b399013982ed8c7e42d300
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-25 13:41:03 +0200

    commit object_syokudai

---
+++ assets/objects/object_syokudai/gGoldenTorchDL.c
@@ -0,0 +1,10 @@
+#include "object_syokudai.h"
+#include "gfx.h"
+
+Vtx gGoldenTorchVtx[] = {
+#include "assets/objects/object_syokudai/gGoldenTorchVtx.inc.c"
+};
+
+Gfx gGoldenTorchDL[58] = {
+#include "assets/objects/object_syokudai/gGoldenTorchDL.inc.c"
+};
---
+++ assets/objects/object_syokudai/gTimedTorchDL.c
@@ -0,0 +1,10 @@
+#include "object_syokudai.h"
+#include "gfx.h"
+
+Vtx gTimedTorchVtx[] = {
+#include "assets/objects/object_syokudai/gTimedTorchVtx.inc.c"
+};
+
+Gfx gTimedTorchDL[32] = {
+#include "assets/objects/object_syokudai/gTimedTorchDL.inc.c"
+};
