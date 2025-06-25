b62b278e664c2b8ec322eb97217fbb1d990df423
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-25 13:34:10 +0200

    commit object_ta

---
+++ assets/objects/object_ta/gTalonSitSleepingAnim.c
@@ -0,0 +1,13 @@
+#include "object_ta.h"
+
+s16 gTalonSitSleepingFrameData[] = {
+#include "assets/objects/object_ta/gTalonSitSleepingFrameData.inc.c"
+};
+
+JointIndex gTalonSitSleepingJointIndices[] = {
+#include "assets/objects/object_ta/gTalonSitSleepingJointIndices.inc.c"
+};
+
+AnimationHeader gTalonSitSleepingAnim = {
+#include "assets/objects/object_ta/gTalonSitSleepingAnim.inc.c"
+};
