c3d8189e42687186852713410c04cf46a739d822
Gabriel Ravier <gabravier@gmail.com>
2026-02-06 18:51:23 +0100

    Incorporate feedback from PR review.
    (see also https://github.com/zeldaret/oot/pull/2667)

--- src/n64dd/n64dd_error_headers.c
+++ src/n64dd/n64dd_error_headers.c
@@ -1,10 +1,11 @@
 #include "n64dd.h"
 
 // Padding in .rodata suggests that this array is in a separate file
+
 /**
  * Region-specific "error number" headers used in N64DD error handling.
  * The first one is used if the current region is Japan (gCurrentRegion == REGION_JP),
  * the second otherwise.
- * You can probably accurately guess what the japanese one means (it means "Error Number" :p)
+ * The Japanese version of the header, as you might expect, translates to "Error Number"
  */
 const char* gN64DDLocalizedErrorNumberHeaders[2] = { "エラー番号　　　", "Error Number    " };
