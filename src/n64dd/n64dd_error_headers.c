759b87155534635ae12b238fc72e7e27499de0cf
Gabriel Ravier <gabravier@gmail.com>
2026-02-18 18:27:11 +0100

    Put translation of gN64DDLocalizedErrorNumberHeaders[0] in a line comment
    (From feedback to https://github.com/zeldaret/oot/pull/2667)

--- src/n64dd/n64dd_error_headers.c
+++ src/n64dd/n64dd_error_headers.c
@@ -6,6 +6,8 @@
  * Region-specific "error number" headers used in N64DD error handling.
  * The first one is used if the current region is Japan (gCurrentRegion == REGION_JP),
  * the second otherwise.
- * The Japanese version of the header, as you might expect, translates to "Error Number"
  */
-const char* gN64DDLocalizedErrorNumberHeaders[2] = { "エラー番号　　　", "Error Number    " };
+const char* gN64DDLocalizedErrorNumberHeaders[2] = {
+    "エラー番号　　　", // "Error Number"
+    "Error Number    ",
+};
