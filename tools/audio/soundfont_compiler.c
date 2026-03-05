2058a68397012777db20503918bb356f68e682af
Tharo <17233964+Thar0@users.noreply.github.com>
2026-03-05 21:16:31 +0000

    Audio: Fix note values
    Due to the layout of the pitch_frequencies lookup table, note values
    computed in extraction were reflected around middle C (midi note
    number 60). This didn't matter for matching, values would successfully
    roundtrip. However when using samples in external programs or
    converting soundfonts to standard formats the note values would lead
    to incorrect playback of sounds. This change corrects the note values
    so that external programs correctly infer the pitch of the sound when
    played at a particular MIDI key.

--- tools/audio/soundfont_compiler.c
+++ tools/audio/soundfont_compiler.c
@@ -199,7 +199,8 @@ calc_tuning(float sample_rate, int basenote, int8_t finetune)
         /* 0x7F */ 0.099213f,   // PITCH_AF0
     };
 
-    float tuning = (sample_rate / playback_sample_rate) * pitch_frequencies[basenote];
+    // Due to the way the lookup table is arranged, the note needs to be reflected about middle C (z64 note value 39)
+    float tuning = (sample_rate / playback_sample_rate) * pitch_frequencies[(78u - (unsigned)basenote) % 128u];
     if (finetune == 0)
         return tuning;
 
@@ -1446,10 +1447,10 @@ emit_c_drums(FILE *out, soundfont *sf)
             ptr_table[ptr_offset].n = note_offset;
 
             // wrap note on overflow
-            int note = drum->base_note + note_offset;
-            if (note > 127)
-                note -= 128;
-
+            // drum frequencies increase with drum offset, corresponding to a decrease in note number
+            int note = drum->base_note - note_offset;
+            if (note < 0)
+                note += 128;
             float tuning = calc_tuning(drum->sample_rate, note, drum->fine_tune);
 
             fprintf(out, "    SF%d_%s_ENTRY(" F32_FMT "f),\n", sf->info.index, drum->name, tuning);
