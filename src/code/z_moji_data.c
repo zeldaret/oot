79e0eda7d5f7475a4e1a8166ed939f8aa2acaf2a
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-05-21 07:38:19 +0200

    add src/code/z_moji_data.c, extracting the binary blobs with the assets system instead of incbins

---
+++ src/code/z_moji_data.c
@@ -0,0 +1,9 @@
+#include "ultra64.h"
+
+u8 gMojiFontTLUTs[] = {
+#include "assets/code/z_moji_data/gMojiFontTLUTs.bin.inc.c"
+};
+
+u8 gMojiFontTex[] = {
+#include "assets/code/z_moji_data/gMojiFontTex.bin.inc.c"
+};
