6e2681ca12c825f275a8d8da8e64b4cefe258e2d
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-05-21 07:56:34 +0200

    extract z_moji_data data as textures instead of bin blobs for alignment reasons

--- src/code/z_moji_data.c
+++ src/code/z_moji_data.c
@@ -1,9 +1,21 @@
 #include "ultra64.h"
+#include "gfx.h"
 
-u8 gMojiFontTLUTs[] = {
-#include "assets/code/z_moji_data/gMojiFontTLUTs.bin.inc.c"
+u64 gMojiFontTLUTs[4][4] = {
+    {
+#include "assets/code/z_moji_data/gMojiFontTLUT0.rgba16.inc.c"
+    },
+    {
+#include "assets/code/z_moji_data/gMojiFontTLUT1.rgba16.inc.c"
+    },
+    {
+#include "assets/code/z_moji_data/gMojiFontTLUT2.rgba16.inc.c"
+    },
+    {
+#include "assets/code/z_moji_data/gMojiFontTLUT3.rgba16.inc.c"
+    },
 };
 
-u8 gMojiFontTex[] = {
-#include "assets/code/z_moji_data/gMojiFontTex.bin.inc.c"
+u64 gMojiFontTex[] = {
+#include "assets/code/z_moji_data/gMojiFontTex.i4.inc.c"
 };
