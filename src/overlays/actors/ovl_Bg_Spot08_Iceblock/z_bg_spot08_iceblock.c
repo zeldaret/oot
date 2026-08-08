ebce5c02861ea7533a8cb6daa93b24d692ffa77e
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-08-09 01:17:27 +0200

    partial docs for BgSpot08Iceblock

--- src/overlays/actors/ovl_Bg_Spot08_Iceblock/z_bg_spot08_iceblock.c
+++ src/overlays/actors/ovl_Bg_Spot08_Iceblock/z_bg_spot08_iceblock.c
@@ -6,7 +6,6 @@
 #include "printf.h"
 #include "sys_math3d.h"
 #include "sys_matrix.h"
-#include "translation.h"
 #include "z_lib.h"
 #include "play_state.h"
 #include "player.h"
@@ -21,13 +20,13 @@ void BgSpot08Iceblock_Destroy(Actor* thisx, PlayState* play);
 void BgSpot08Iceblock_Update(Actor* thisx, PlayState* play);
 void BgSpot08Iceblock_Draw(Actor* thisx, PlayState* play);
 
-void func_808B1388(BgSpot08Iceblock* this);
-void func_808B13AC(BgSpot08Iceblock* this, PlayState* play);
-void func_808B13FC(BgSpot08Iceblock* this);
-void func_808B1420(BgSpot08Iceblock* this, PlayState* play);
-void func_808B147C(BgSpot08Iceblock* this);
-void func_808B14A0(BgSpot08Iceblock* this, PlayState* play);
-void func_808B1574(BgSpot08Iceblock* this);
+void BgSpot08Iceblock_SetupIcebergStatic(BgSpot08Iceblock* this);
+void BgSpot08Iceblock_IcebergStatic(BgSpot08Iceblock* this, PlayState* play);
+void BgSpot08Iceblock_SetupIcebergSpin(BgSpot08Iceblock* this);
+void BgSpot08Iceblock_IcebergSpin(BgSpot08Iceblock* this, PlayState* play);
+void BgSpot08Iceblock_SetupActionIcebergDuo(BgSpot08Iceblock* this);
+void BgSpot08Iceblock_IcebergDuo(BgSpot08Iceblock* this, PlayState* play);
+void BgSpot08Iceblock_SetupDoNothing(BgSpot08Iceblock* this);
 
 ActorProfile Bg_Spot08_Iceblock_Profile = {
     /**/ ACTOR_BG_SPOT08_ICEBLOCK,
@@ -41,7 +40,7 @@ ActorProfile Bg_Spot08_Iceblock_Profile = {
     /**/ BgSpot08Iceblock_Draw,
 };
 
-static Vec3f D_808B16B0 = { 0.0f, 1.0f, 0.0f };
+static Vec3f sUpVec = { 0.0f, 1.0f, 0.0f };
 static Vec3f D_808B16BC = { 0.0f, 0.0f, 0.0f };
 static f32 D_808B16C8[3] = { 1.4285714e-8f, 5.714286e-9f, 1.4285715e-9f };
 static f32 D_808B16D4[3] = { 0.96f, 0.96f, 0.98f };
@@ -53,18 +52,18 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_F32(cullingVolumeDownward, 2200, ICHAIN_STOP),
 };
 
-void func_808B0960(BgSpot08Iceblock* this, void (*arg1)(BgSpot08Iceblock*, PlayState*)) {
-    this->unk164 = arg1;
+void BgSpot08Iceblock_SetupAction(BgSpot08Iceblock* this, void (*actionFunc)(BgSpot08Iceblock*, PlayState*)) {
+    this->actionFunc = actionFunc;
 }
 
-void func_808B0968(BgSpot08Iceblock* this, PlayState* play, CollisionHeader* arg2, s32 arg3) {
+void BgSpot08Iceblock_InitDynapoly(BgSpot08Iceblock* this, PlayState* play, CollisionHeader* colHeaderSeg,
+                                   s32 transformFlags) {
     s32 pad;
-    CollisionHeader* sp30;
+    CollisionHeader* colHeader = NULL;
 
-    sp30 = NULL;
-    DynaPolyActor_Init(&this->dyna, arg3);
-    CollisionHeader_GetVirtual(arg2, &sp30);
-    this->dyna.bgId = DynaPoly_SetBgActor(play, &play->colCtx.dyna, &this->dyna.actor, sp30);
+    DynaPolyActor_Init(&this->dyna, transformFlags);
+    CollisionHeader_GetVirtual(colHeaderSeg, &colHeader);
+    this->dyna.bgId = DynaPoly_SetBgActor(play, &play->colCtx.dyna, &this->dyna.actor, colHeader);
 
 #if DEBUG_FEATURES
     if (this->dyna.bgId == BG_ACTOR_MAX) {
@@ -76,7 +75,7 @@ void func_808B0968(BgSpot08Iceblock* this, PlayState* play, CollisionHeader* arg
 #endif
 }
 
-void func_808B09F0(BgSpot08Iceblock* this) {
+void BgSpot08Iceblock_CheckParams(BgSpot08Iceblock* this) {
     switch (PARAMS_GET_U(this->dyna.actor.params, 0, 8)) {
         case 0xFF:
             this->dyna.actor.params = 0x10;
@@ -101,186 +100,186 @@ void func_808B09F0(BgSpot08Iceblock* this) {
     }
 }
 
-void func_808B0A84(BgSpot08Iceblock* this) {
-    this->unk198 = (Math_SinS(this->unk18C) * 4.0f) + (Math_SinS(this->unk18E) * 3.0f);
+void BgSpot08Iceblock_UpdateYOscillation(BgSpot08Iceblock* this) {
+    this->yOscillation = (Math_SinS(this->yOscillationPhase1) * 4.0f) + (Math_SinS(this->yOscillationPhase2) * 3.0f);
 }
 
-void func_808B0AE0(BgSpot08Iceblock* this) {
-    f32 var_fv0;
-    f32 sp18;
+void BgSpot08Iceblock_UpdateSinkHeight(BgSpot08Iceblock* this) {
+    f32 target;
+    f32 step;
 
     switch (PARAMS_GET_NOSHIFT(this->dyna.actor.params, 4, 4)) {
         case 0:
-            sp18 = 0.15f;
+            step = 0.15f;
             break;
 
         case 0x10:
-            sp18 = 0.2f;
+            step = 0.2f;
             break;
 
         case 0x20:
-            sp18 = 0.4f;
+            step = 0.4f;
             break;
     }
-    if (DynaPolyActor_IsActorOnTop(&this->dyna) != 0) {
-        var_fv0 = -4.0f;
+    if (DynaPolyActor_IsActorOnTop(&this->dyna)) {
+        target = -4.0f;
     } else {
-        var_fv0 = 0.0f;
+        target = 0.0f;
     }
-    Math_StepToF(&this->unk194, var_fv0, sp18);
+    Math_StepToF(&this->sinkHeight, target, step);
 }
 
-void func_808B0B8C(BgSpot08Iceblock* this) {
-    this->dyna.actor.world.pos.y = this->unk194 + this->unk198 + this->dyna.actor.home.pos.y;
+void BgSpot08Iceblock_UpdatePosY(BgSpot08Iceblock* this) {
+    this->dyna.actor.world.pos.y = this->sinkHeight + this->yOscillation + this->dyna.actor.home.pos.y;
 }
 
-void func_808B0BA8(Vec3f* arg0, Vec3f* arg1, f32 arg2) {
-    arg0->x = arg1->x * arg2;
-    arg0->y = arg1->y * arg2;
-    arg0->z = arg1->z * arg2;
+void BgSpot08Iceblock_VecScale(Vec3f* dest, Vec3f* src, f32 fac) {
+    dest->x = src->x * fac;
+    dest->y = src->y * fac;
+    dest->z = src->z * fac;
 }
 
-void func_808B0BD4(Vec3f* arg0, Vec3f* arg1, Vec3f* arg2) {
-    arg0->x = (arg1->y * arg2->z) - (arg1->z * arg2->y);
-    arg0->y = (arg1->z * arg2->x) - (arg1->x * arg2->z);
-    arg0->z = (arg1->x * arg2->y) - (arg1->y * arg2->x);
+void BgSpot08Iceblock_VecCross(Vec3f* dest, Vec3f* a, Vec3f* b) {
+    dest->x = (a->y * b->z) - (a->z * b->y);
+    dest->y = (a->z * b->x) - (a->x * b->z);
+    dest->z = (a->x * b->y) - (a->y * b->x);
 }
 
-s32 func_808B0C44(Vec3f* arg0, Vec3f* arg1) {
-    f32 temp_fv0;
-    f32 temp_fv1;
+s32 BgSpot08Iceblock_VecNormalize(Vec3f* dest, Vec3f* src) {
+    f32 len;
+    f32 invLen;
 
-    temp_fv0 = Math3D_Vec3fMagnitude(arg1);
-    if (temp_fv0 < 0.001f) {
-        arg0->y = 0.0f;
-        arg0->x = 0.0f;
-        arg0->z = 1.0f;
+    len = Math3D_Vec3fMagnitude(src);
+    if (len < 0.001f) {
+        dest->y = 0.0f;
+        dest->x = 0.0f;
+        dest->z = 1.0f;
         return false;
     } else {
-        temp_fv1 = 1.0f / temp_fv0;
-        arg0->x = arg1->x * temp_fv1;
-        arg0->y = arg1->y * temp_fv1;
-        arg0->z = arg1->z * temp_fv1;
+        invLen = 1.0f / len;
+        dest->x = src->x * invLen;
+        dest->y = src->y * invLen;
+        dest->z = src->z * invLen;
         return true;
     }
 }
 
-void func_808B0CE0(BgSpot08Iceblock* this, PlayState* play) {
+void BgSpot08Iceblock_UpdateSwaying(BgSpot08Iceblock* this, PlayState* play) {
     Player* player = GET_PLAYER(play);
     s32 pad;
     Vec3f spD4;
     Vec3f spC8;
     Vec3f spBC;
     Vec3f spB0;
-    Vec3f spA4;
+    Vec3f randVec;
     Vec3f sp98;
-    Vec3f sp8C;
+    Vec3f swayAxis;
     f32 sp88;
-    s32 sp84;
-    MtxF sp44;
+    s32 physicsSet;
+    MtxF mf;
     f32 temp_fv0_2;
     f32 temp_fv1;
 
     switch (PARAMS_GET_U(this->dyna.actor.params, 0, 8)) {
         case 0x11:
-            sp84 = 0;
+            physicsSet = 0;
             break;
 
         case 1:
-            sp84 = 1;
+            physicsSet = 1;
             break;
 
         default:
-            sp84 = 2;
+            physicsSet = 2;
             break;
     }
     Math_Vec3f_Diff(&player->actor.world.pos, &this->dyna.actor.world.pos, &spC8);
     spC8.y -= 150.0f * this->dyna.actor.scale.y;
     sp88 = Math3D_Vec3fMagnitude(&spC8);
-    spA4.x = (Rand_ZeroOne() - 0.5f) * 0.0016f;
-    spA4.y = 0.0f;
-    spA4.z = (Rand_ZeroOne() - 0.5f) * 0.0016f;
-    spD4.x = this->unk168.x;
+    randVec.x = (Rand_ZeroOne() - 0.5f) * 0.0016f;
+    randVec.y = 0.0f;
+    randVec.z = (Rand_ZeroOne() - 0.5f) * 0.0016f;
+    spD4.x = this->unk_168.x;
     spD4.y = 0.0f;
-    spD4.z = this->unk168.z;
-    if ((DynaPolyActor_IsPlayerOnTop(&this->dyna) != 0) && (sp88 > 3.0f)) {
+    spD4.z = this->unk_168.z;
+    if (DynaPolyActor_IsPlayerOnTop(&this->dyna) && (sp88 > 3.0f)) {
         Math_Vec3f_Diff(&spC8, &spD4, &spBC);
-        func_808B0BA8(&spBC, &spBC, (D_808B16C8[sp84] * sp88) / this->dyna.actor.scale.x);
+        BgSpot08Iceblock_VecScale(&spBC, &spBC, (D_808B16C8[physicsSet] * sp88) / this->dyna.actor.scale.x);
     } else {
         spBC = D_808B16BC;
     }
-    func_808B0BA8(&spB0, &spD4, -0.01f);
-    Math_Vec3f_Sum(&this->unk174, &spBC, &this->unk174);
-    Math_Vec3f_Sum(&this->unk174, &spB0, &this->unk174);
-    Math_Vec3f_Sum(&this->unk174, &spA4, &this->unk174);
-    this->unk174.y = 0.0f;
-    Math_Vec3f_Sum(&this->unk168, &this->unk174, &sp98);
-    sp98.x *= D_808B16D4[sp84];
-    sp98.z *= D_808B16D4[sp84];
-    if (func_808B0C44(&this->unk168, &sp98)) {
-        temp_fv0_2 = Math3D_Dist1DSq(this->unk168.z, this->unk168.x);
-        if (D_808B16E0[sp84] < temp_fv0_2) {
-            temp_fv1 = D_808B16E0[sp84] / temp_fv0_2;
-            this->unk168.x *= temp_fv1;
-            this->unk168.y = D_808B16EC[sp84];
-            this->unk168.z *= temp_fv1;
+    BgSpot08Iceblock_VecScale(&spB0, &spD4, -0.01f);
+    Math_Vec3f_Sum(&this->unk_174, &spBC, &this->unk_174);
+    Math_Vec3f_Sum(&this->unk_174, &spB0, &this->unk_174);
+    Math_Vec3f_Sum(&this->unk_174, &randVec, &this->unk_174);
+    this->unk_174.y = 0.0f;
+    Math_Vec3f_Sum(&this->unk_168, &this->unk_174, &sp98);
+    sp98.x *= D_808B16D4[physicsSet];
+    sp98.z *= D_808B16D4[physicsSet];
+    if (BgSpot08Iceblock_VecNormalize(&this->unk_168, &sp98)) {
+        temp_fv0_2 = Math3D_Dist1DSq(this->unk_168.z, this->unk_168.x);
+        if (D_808B16E0[physicsSet] < temp_fv0_2) {
+            temp_fv1 = D_808B16E0[physicsSet] / temp_fv0_2;
+            this->unk_168.x *= temp_fv1;
+            this->unk_168.y = D_808B16EC[physicsSet];
+            this->unk_168.z *= temp_fv1;
         }
-        func_808B0BD4(&sp98, &D_808B16B0, &this->unk168);
-        if (func_808B0C44(&sp8C, &sp98)) {
-            this->unk180 = sp8C;
+        BgSpot08Iceblock_VecCross(&sp98, &sUpVec, &this->unk_168);
+        if (BgSpot08Iceblock_VecNormalize(&swayAxis, &sp98)) {
+            this->swayAxis = swayAxis;
         }
     } else {
-        this->unk168 = D_808B16B0;
+        this->unk_168 = sUpVec;
     }
-    Matrix_RotateAxis(Math_FAcosF(Math3D_Cos(&D_808B16B0, &this->unk168)), &this->unk180, MTXMODE_NEW);
+    Matrix_RotateAxis(Math_FAcosF(Math3D_Cos(&sUpVec, &this->unk_168)), &this->swayAxis, MTXMODE_NEW);
     Matrix_RotateY(BINANG_TO_RAD(this->dyna.actor.shape.rot.y), MTXMODE_APPLY);
-    Matrix_Get(&sp44);
-    Matrix_MtxFToYXZRotS(&sp44, &this->dyna.actor.shape.rot, 0);
+    Matrix_Get(&mf);
+    Matrix_MtxFToYXZRotS(&mf, &this->dyna.actor.shape.rot, 0);
 }
 
-void func_808B1054(BgSpot08Iceblock* this, PlayState* play) {
+void BgSpot08Iceblock_SetupIcebergDuo(BgSpot08Iceblock* this, PlayState* play) {
     s32 pad[2];
-    f32 sp44;
-    f32 temp_fv1;
+    f32 dx;
+    f32 dz;
 
-    sp44 = Math_SinS(this->dyna.actor.home.rot.y) * 100.0f;
-    temp_fv1 = Math_CosS(this->dyna.actor.home.rot.y) * 100.0f;
+    dx = Math_SinS(this->dyna.actor.home.rot.y) * 100.0f;
+    dz = Math_CosS(this->dyna.actor.home.rot.y) * 100.0f;
     if (!PARAMS_GET_NOSHIFT(this->dyna.actor.params, 8, 1)) {
         Actor_SpawnAsChild(&play->actorCtx, &this->dyna.actor, play, ACTOR_BG_SPOT08_ICEBLOCK,
                            this->dyna.actor.home.pos.x, this->dyna.actor.home.pos.y, this->dyna.actor.home.pos.z,
                            this->dyna.actor.home.rot.x, this->dyna.actor.home.rot.y, this->dyna.actor.home.rot.z,
                            0x123);
-        this->dyna.actor.world.pos.x += sp44;
-        this->dyna.actor.world.pos.z += temp_fv1;
+        this->dyna.actor.world.pos.x += dx;
+        this->dyna.actor.world.pos.z += dz;
     } else {
-        this->dyna.actor.world.pos.x -= sp44;
-        this->dyna.actor.world.pos.z -= temp_fv1;
+        this->dyna.actor.world.pos.x -= dx;
+        this->dyna.actor.world.pos.z -= dz;
     }
-    func_808B147C(this);
+    BgSpot08Iceblock_SetupActionIcebergDuo(this);
 }
 
 void BgSpot08Iceblock_Init(Actor* thisx, PlayState* play) {
     s32 pad;
-    CollisionHeader* var_a2;
-    s32 temp_v0_2;
+    CollisionHeader* colHeader;
+    s32 behaviorType;
     BgSpot08Iceblock* this = (BgSpot08Iceblock*)thisx;
 
     PRINTF("(spot08 流氷)(arg_data 0x%04x)\n", this->dyna.actor.params);
-    func_808B09F0(this);
+    BgSpot08Iceblock_CheckParams(this);
     switch (PARAMS_GET_NOSHIFT(this->dyna.actor.params, 9, 1)) {
         case 0:
-            var_a2 = &gZorasFountainIcebergCol;
+            colHeader = &gZorasFountainIcebergCol;
             break;
 
         case 0x200:
-            var_a2 = &gZorasFountainIceRampCol;
+            colHeader = &gZorasFountainIceRampCol;
             break;
     }
-    temp_v0_2 = PARAMS_GET_U(this->dyna.actor.params, 0, 4);
-    if ((temp_v0_2 == 2) || (temp_v0_2 == 3)) {
-        func_808B0968(this, play, var_a2, DYNA_TRANSFORM_POS | DYNA_TRANSFORM_ROT_Y);
+    behaviorType = PARAMS_GET_U(this->dyna.actor.params, 0, 4);
+    if ((behaviorType == 2) || (behaviorType == 3)) {
+        BgSpot08Iceblock_InitDynapoly(this, play, colHeader, DYNA_TRANSFORM_POS | DYNA_TRANSFORM_ROT_Y);
     } else {
-        func_808B0968(this, play, var_a2, 0);
+        BgSpot08Iceblock_InitDynapoly(this, play, colHeader, 0);
     }
     if (LINK_AGE_IN_YEARS == YEARS_CHILD) {
         Actor_Kill(&this->dyna.actor);
@@ -300,25 +299,26 @@ void BgSpot08Iceblock_Init(Actor* thisx, PlayState* play) {
             Actor_SetScale(&this->dyna.actor, 0.05f);
             break;
     }
-    this->unk18C = (s16)(s32)(Rand_ZeroOne() * 65535.5f);
-    this->unk18E = (s16)(s32)(Rand_ZeroOne() * 65535.5f);
-    this->unk168.y = 1.0f;
-    this->unk180.x = 1.0f;
+    this->yOscillationPhase1 = TRUNCF_BINANG(Rand_ZeroOne() * 65535.5f);
+    this->yOscillationPhase2 = TRUNCF_BINANG(Rand_ZeroOne() * 65535.5f);
+    this->unk_168.y = 1.0f;
+    this->swayAxis.x = 1.0f;
     switch (PARAMS_GET_U(this->dyna.actor.params, 0, 4)) {
         case 0:
         case 1:
-            func_808B1388(this);
+            BgSpot08Iceblock_SetupIcebergStatic(this);
             break;
 
         case 2:
-            func_808B13FC(this);
+            BgSpot08Iceblock_SetupIcebergSpin(this);
             break;
 
         case 3:
-            func_808B1054(this, play);
+            BgSpot08Iceblock_SetupIcebergDuo(this, play);
             break;
+
         case 4:
-            func_808B1574(this);
+            BgSpot08Iceblock_SetupDoNothing(this);
             break;
 
         default:
@@ -332,87 +332,87 @@ void BgSpot08Iceblock_Destroy(Actor* thisx, PlayState* play) {
     DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
 }
 
-void func_808B1388(BgSpot08Iceblock* this) {
-    func_808B0960(this, func_808B13AC);
+void BgSpot08Iceblock_SetupIcebergStatic(BgSpot08Iceblock* this) {
+    BgSpot08Iceblock_SetupAction(this, BgSpot08Iceblock_IcebergStatic);
 }
 
-void func_808B13AC(BgSpot08Iceblock* this, PlayState* play) {
-    func_808B0A84(this);
-    func_808B0AE0(this);
-    func_808B0B8C(this);
+void BgSpot08Iceblock_IcebergStatic(BgSpot08Iceblock* this, PlayState* play) {
+    BgSpot08Iceblock_UpdateYOscillation(this);
+    BgSpot08Iceblock_UpdateSinkHeight(this);
+    BgSpot08Iceblock_UpdatePosY(this);
     this->dyna.actor.shape.rot.y = this->dyna.actor.home.rot.y;
-    func_808B0CE0(this, play);
+    BgSpot08Iceblock_UpdateSwaying(this, play);
 }
 
-void func_808B13FC(BgSpot08Iceblock* this) {
-    func_808B0960(this, func_808B1420);
+void BgSpot08Iceblock_SetupIcebergSpin(BgSpot08Iceblock* this) {
+    BgSpot08Iceblock_SetupAction(this, BgSpot08Iceblock_IcebergSpin);
 }
 
-void func_808B1420(BgSpot08Iceblock* this, PlayState* play) {
-    func_808B0A84(this);
-    func_808B0AE0(this);
-    func_808B0B8C(this);
+void BgSpot08Iceblock_IcebergSpin(BgSpot08Iceblock* this, PlayState* play) {
+    BgSpot08Iceblock_UpdateYOscillation(this);
+    BgSpot08Iceblock_UpdateSinkHeight(this);
+    BgSpot08Iceblock_UpdatePosY(this);
     this->dyna.actor.world.rot.y += 0x190;
     this->dyna.actor.shape.rot.y = this->dyna.actor.world.rot.y;
-    func_808B0CE0(this, play);
+    BgSpot08Iceblock_UpdateSwaying(this, play);
 }
 
-void func_808B147C(BgSpot08Iceblock* this) {
-    func_808B0960(this, func_808B14A0);
+void BgSpot08Iceblock_SetupActionIcebergDuo(BgSpot08Iceblock* this) {
+    BgSpot08Iceblock_SetupAction(this, BgSpot08Iceblock_IcebergDuo);
 }
 
-void func_808B14A0(BgSpot08Iceblock* this, PlayState* play) {
-    f32 temp_fa0;
-    f32 temp_fv1;
+void BgSpot08Iceblock_IcebergDuo(BgSpot08Iceblock* this, PlayState* play) {
+    f32 dz;
+    f32 dx;
 
-    func_808B0A84(this);
-    func_808B0AE0(this);
-    func_808B0B8C(this);
+    BgSpot08Iceblock_UpdateYOscillation(this);
+    BgSpot08Iceblock_UpdateSinkHeight(this);
+    BgSpot08Iceblock_UpdatePosY(this);
     if (!PARAMS_GET_NOSHIFT(this->dyna.actor.params, 8, 1)) {
         this->dyna.actor.world.rot.y += 0x190;
-        temp_fv1 = Math_SinS(this->dyna.actor.world.rot.y) * 100.0f;
-        temp_fa0 = Math_CosS(this->dyna.actor.world.rot.y) * 100.0f;
-        this->dyna.actor.world.pos.x = this->dyna.actor.home.pos.x + temp_fv1;
-        this->dyna.actor.world.pos.z = this->dyna.actor.home.pos.z + temp_fa0;
+        dx = Math_SinS(this->dyna.actor.world.rot.y) * 100.0f;
+        dz = Math_CosS(this->dyna.actor.world.rot.y) * 100.0f;
+        this->dyna.actor.world.pos.x = this->dyna.actor.home.pos.x + dx;
+        this->dyna.actor.world.pos.z = this->dyna.actor.home.pos.z + dz;
         if (this->dyna.actor.child != NULL) {
-            this->dyna.actor.child->world.pos.x = this->dyna.actor.home.pos.x - temp_fv1;
-            this->dyna.actor.child->world.pos.z = this->dyna.actor.home.pos.z - temp_fa0;
+            this->dyna.actor.child->world.pos.x = this->dyna.actor.home.pos.x - dx;
+            this->dyna.actor.child->world.pos.z = this->dyna.actor.home.pos.z - dz;
         }
     }
     this->dyna.actor.shape.rot.y = this->dyna.actor.home.rot.y;
-    func_808B0CE0(this, play);
+    BgSpot08Iceblock_UpdateSwaying(this, play);
 }
 
-void func_808B1574(BgSpot08Iceblock* this) {
-    func_808B0960(this, NULL);
+void BgSpot08Iceblock_SetupDoNothing(BgSpot08Iceblock* this) {
+    BgSpot08Iceblock_SetupAction(this, NULL);
 }
 
 void BgSpot08Iceblock_Update(Actor* thisx, PlayState* play) {
     BgSpot08Iceblock* this = (BgSpot08Iceblock*)thisx;
 
     if (Rand_ZeroOne() < 0.05f) {
-        this->unk190 = Rand_S16Offset(0x12C, 0x64);
-        this->unk192 = Rand_S16Offset(0x320, 0x190);
+        this->yOscillationPulse1 = Rand_S16Offset(0x12C, 0x64);
+        this->yOscillationPulse2 = Rand_S16Offset(0x320, 0x190);
     }
-    this->unk18C += this->unk190;
-    this->unk18E += this->unk192;
-    if (this->unk164 != NULL) {
-        this->unk164(this, play);
+    this->yOscillationPhase1 += this->yOscillationPulse1;
+    this->yOscillationPhase2 += this->yOscillationPulse2;
+    if (this->actionFunc != NULL) {
+        this->actionFunc(this, play);
     }
 }
 
 void BgSpot08Iceblock_Draw(Actor* thisx, PlayState* play) {
-    Gfx* sp1C;
+    Gfx* dl;
     BgSpot08Iceblock* this = (BgSpot08Iceblock*)thisx;
 
     switch (PARAMS_GET_NOSHIFT(this->dyna.actor.params, 9, 1)) {
         case 0x0:
-            sp1C = gZorasFountainIcebergDL;
+            dl = gZorasFountainIcebergDL;
             break;
 
         case 0x200:
-            sp1C = gZorasFountainIceRampDL;
+            dl = gZorasFountainIceRampDL;
             break;
     }
-    Gfx_DrawDListOpa(play, sp1C);
+    Gfx_DrawDListOpa(play, dl);
 }
--- src/overlays/actors/ovl_Bg_Spot08_Iceblock/z_bg_spot08_iceblock.h
+++ src/overlays/actors/ovl_Bg_Spot08_Iceblock/z_bg_spot08_iceblock.h
@@ -8,16 +8,16 @@ struct BgSpot08Iceblock;
 
 typedef struct BgSpot08Iceblock {
     /* 0x000 */ DynaPolyActor dyna;
-    /* 0x164 */ void (*unk164)(struct BgSpot08Iceblock*, struct PlayState*);
-    /* 0x168 */ Vec3f unk168;
-    /* 0x174 */ Vec3f unk174;
-    /* 0x180 */ Vec3f unk180;
-    /* 0x18C */ s16 unk18C;
-    /* 0x18E */ s16 unk18E;
-    /* 0x190 */ s16 unk190;
-    /* 0x192 */ s16 unk192;
-    /* 0x194 */ f32 unk194;
-    /* 0x198 */ f32 unk198;
+    /* 0x164 */ void (*actionFunc)(struct BgSpot08Iceblock*, struct PlayState*);
+    /* 0x168 */ Vec3f unk_168;
+    /* 0x174 */ Vec3f unk_174;
+    /* 0x180 */ Vec3f swayAxis;
+    /* 0x18C */ s16 yOscillationPhase1;
+    /* 0x18E */ s16 yOscillationPhase2;
+    /* 0x190 */ s16 yOscillationPulse1;
+    /* 0x192 */ s16 yOscillationPulse2;
+    /* 0x194 */ f32 sinkHeight;
+    /* 0x198 */ f32 yOscillation;
 } BgSpot08Iceblock; // size = 0x19C
 
 #endif
