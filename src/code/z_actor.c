18c05f2da9dd649c51ded742901b39ff8c707556
Derek Hensley <hensley.derek58@gmail.com>
2026-08-16 14:11:13 -0700

    BSS

--- src/code/z_actor.c
+++ src/code/z_actor.c
@@ -41,8 +41,8 @@
 #include "assets/objects/gameplay_dangeon_keep/gameplay_dangeon_keep.h"
 #include "assets/objects/object_bdoor/object_bdoor.h"
 
-#pragma increment_block_number "gc-eu:128 gc-eu-mq:128 gc-jp:128 gc-jp-ce:128 gc-jp-mq:128 gc-us:128 gc-us-mq:128" \
-                               "ntsc-1.0:0 ntsc-1.1:0 ntsc-1.2:0 pal-1.0:0 pal-1.1:0"
+#pragma increment_block_number "gc-eu:0 gc-eu-mq:0 gc-jp:0 gc-jp-ce:0 gc-jp-mq:0 gc-us:0 gc-us-mq:0 ntsc-1.0:0" \
+                               "ntsc-1.1:0 ntsc-1.2:0 pal-1.0:0 pal-1.1:0"
 
 CollisionPoly* sCurCeilingPoly;
 s32 sCurCeilingBgId;
@@ -2126,8 +2126,8 @@ s32 func_8002F9EC(PlayState* play, Actor* actor, CollisionPoly* poly, s32 bgId, 
     return false;
 }
 
-#pragma increment_block_number "gc-eu:22 gc-eu-mq:22 gc-jp:22 gc-jp-ce:22 gc-jp-mq:22 gc-us:22 gc-us-mq:22" \
-                               "ntsc-1.0:22 ntsc-1.1:22 ntsc-1.2:22 pal-1.0:22 pal-1.1:22"
+#pragma increment_block_number "gc-eu:23 gc-eu-mq:23 gc-jp:23 gc-jp-ce:23 gc-jp-mq:23 gc-us:23 gc-us-mq:23" \
+                               "ntsc-1.0:23 ntsc-1.1:23 ntsc-1.2:23 pal-1.0:23 pal-1.1:23"
 
 // Local data used for Farore's Wind light (stored in BSS)
 LightInfo D_8015BC00;
